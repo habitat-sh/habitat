7ed1d4d4a20c90fdfb25d991be13f41cfbb46aad
Fletcher Nichol <fnichol@nichol.ca>
2015-12-16 14:55:17 -0700

    Allow the demo scripts to use an arbitrarily named Docker Machine.
    For those users who don't wish to use their "default" Docker Machine for
    this demo, now you have a way out--via setting a `DOCKER_MACHINE_NAME`
    environment variable.
    
    > "Thanks!"
        - Fletcher

--- demo/etcd-load.sh
+++ demo/etcd-load.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-etcd_ipaddress=$(docker-machine ip default)
+etcd_ipaddress=$(docker-machine ip ${DOCKER_MACHINE_NAME:-default})
 foo=$(cat $1); curl -L http://$etcd_ipaddress:4001/v2/keys/bldr/redis/default/config -XPUT -d value="${foo}"
