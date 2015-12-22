3b95332064ae0fed1d85ffcc4a112ab2bc3e845f
Fletcher Nichol <fnichol@nichol.ca>
2015-12-21 19:02:34 -0700

    [plan] Add libmpc plan.

---
+++ plans/libmpc/plan.sh
@@ -0,0 +1,36 @@
+pkg_name=libmpc
+pkg_distname=mpc
+pkg_derivation=chef
+pkg_version=1.0.3
+pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
+pkg_license=('lgpl')
+pkg_source=http://www.multiprecision.org/mpc/download/${pkg_distname}-${pkg_version}.tar.gz
+pkg_shasum=617decc6ea09889fb08ede330917a00b16809b8db88c29c31bfbb49cbf88ecc3
+pkg_build_deps=(chef/binutils)
+pkg_deps=(chef/glibc chef/gmp chef/mpfr)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+pkg_gpg_key=3853DA6B
+pkg_dirname=${pkg_distname}-${pkg_version}
+
+do_prepare() {
+  find . -iname "ltmain.sh" | while read file; do
+    build_line "Fixing libtool script $file"
+    sed -i -e 's^eval sys_lib_.*search_path=.*^^' "$file"
+  done
+
+  # TODO: We need a more clever way to calculate/determine the path to ld-*.so
+  LDFLAGS="$LDFLAGS -Wl,-rpath=${LD_RUN_PATH},--enable-new-dtags"
+  LDFLAGS="$LDFLAGS -Wl,--dynamic-linker=$(pkg_path_for glibc)/lib/ld-2.22.so"
+  export LDFLAGS
+  build_line "Updating LDFLAGS=$LDFLAGS"
+}
+
+do_build() {
+  ./configure --prefix=$pkg_prefix
+  make
+
+  if [ -n "${DO_CHECK}" ]; then
+    make check
+  fi
+}
