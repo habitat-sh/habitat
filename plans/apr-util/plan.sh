21736172774d00db8c79a6499b7d0148e2a68283
Julian C. Dunn <jdunn@chef.io>
2016-01-01 22:03:16 -0500

    apr-util doesn't really install anything into sbin; I lied

--- plans/apr-util/plan.sh
+++ plans/apr-util/plan.sh
@@ -6,9 +6,10 @@ pkg_source=http://www.us.apache.org/dist/apr/${pkg_name}-${pkg_version}.tar.bz2
 pkg_shasum=a6cf327189ca0df2fb9d5633d7326c460fe2b61684745fd7963e79a6dd0dc82e
 pkg_gpg_key=3853DA6B
 pkg_deps=(chef/glibc chef/apr)
-pkg_binary_path=(bin sbin)
+pkg_binary_path=(bin)
 
 do_build() {
 	./configure --prefix=${pkg_prefix} --with-apr=$(pkg_path_for chef/apr)
 	make
+        make install
 }
