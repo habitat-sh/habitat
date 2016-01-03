91c607bf70df5cf85bdb98724a4f0102ac4cbb03
Fletcher Nichol <fnichol@nichol.ca>
2016-01-03 15:12:50 -0700

    [plan] Add gcc plan.

---
+++ plans/gcc/plan.sh
@@ -0,0 +1,257 @@
+pkg_name=gcc
+pkg_derivation=chef
+pkg_version=5.2.0
+pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
+pkg_license=('gpl')
+pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}/${pkg_name}-${pkg_version}.tar.bz2
+pkg_shasum=5f835b04b5f7dd4f4d2dc96190ec1621b8d89f2dc6f638f9f8bc1b1014ba8cad
+pkg_deps=(chef/glibc chef/zlib chef/gmp chef/mpfr chef/libmpc chef/binutils)
+pkg_build_deps=(chef/linux-headers chef/m4)
+pkg_binary_path=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+pkg_gpg_key=3853DA6B
+
+do_prepare() {
+  glibc="$(pkg_path_for glibc)"
+  linux_headers="$(pkg_path_for linux-headers)"
+  binutils="$(pkg_path_for binutils)"
+
+  # TODO: For the wrapper scripts to function correctly, we need the full
+  # path to bash. Until a bash plan is created, we're going to wing this...
+  bash=$(command -v bash)
+
+  headers="$(abspath ..)/${pkg_name}-system-header-dir/include"
+  rm -rf $headers
+  mkdir -p $headers
+  pushd $headers > /dev/null
+    ln -sv $(ls -d ${glibc}/include/*) .
+    ln -sv $(ls -d ${linux_headers}/include/* | grep -v 'scsi$') .
+  popd > /dev/null
+
+  find . -iname "ltmain.sh" | while read file; do
+    build_line "Fixing libtool script $file"
+    sed -i -e 's^eval sys_lib_.*search_path=.*^^' "$file"
+  done
+
+  # TODO: We need a more clever way to calculate/determine the path to ld-*.so
+  dynamic_linker="${glibc}/lib/ld-linux-x86-64.so.2"
+
+  LDFLAGS="$LDFLAGS -Wl,-rpath=${LD_RUN_PATH},--enable-new-dtags"
+  LDFLAGS="$LDFLAGS -Wl,--dynamic-linker=$dynamic_linker"
+  build_line "Updating LDFLAGS=$LDFLAGS"
+
+  # Remove glibc and linux-headers include directories from `$CFLAGS` as their
+  # contents will be included in the temporary system headers directory which
+  # combines both sets of headers
+  orig_cflags="$CFLAGS"
+  CFLAGS=
+  for include in $orig_cflags; do
+    if ! echo "$include" | egrep -q "(${glibc}|${linux_headers})" > /dev/null; then
+      CFLAGS="$CFLAGS $include"
+    fi
+  done
+  export CFLAGS
+  build_line "Updating CFLAGS=$CFLAGS"
+
+  # Set `CXXFLAGS` for the c++ code
+  export CXXFLAGS="$CXXFLAGS $CFLAGS"
+  build_line "Setting CXXFLAGS=$CXXFLAGS"
+
+  # Ensure gcc can find the headers for zlib
+  export CPATH="$(pkg_path_for zlib)/include"
+  build_line "Setting CPATH=$CPATH"
+
+  # Ensure gcc can find the shared libs for zlib
+  export LIBRARY_PATH="$(pkg_path_for zlib)/lib"
+  build_line "Setting LIBRARY_PATH=$LIBRARY_PATH"
+
+  # Tell gcc not to look under the default `/lib/` and `/usr/lib/` directories
+  # for libraries
+  #
+  # Thanks to: https://github.com/NixOS/nixpkgs/blob/release-15.09/pkgs/development/compilers/gcc/no-sys-dirs.patch
+  patch -p1 < $PLAN_CONTEXT/no-sys-dirs.patch
+
+  # Patch the configure script so it finds glibc headers
+  #
+  # Thanks to: https://github.com/NixOS/nixpkgs/blob/release-15.09/pkgs/development/compilers/gcc/builder.sh
+  sed -i \
+    -e "s,glibc_header_dir=/usr/include,glibc_header_dir=${headers}," \
+    gcc/configure
+
+  # Use the correct path to the dynamic linker instead of the default
+  # `lib/ld*.so`
+  #
+  # Thanks to: https://github.com/NixOS/nixpkgs/blob/release-15.09/pkgs/development/compilers/gcc/5/default.nix
+  build_line "Fixing the GLIBC_DYNAMIC_LINKER and UCLIBC_DYNAMIC_LINKER macros"
+  for header in "gcc/config/"*-gnu.h "gcc/config/"*"/"*.h; do
+    grep -q LIBC_DYNAMIC_LINKER "$header" || continue
+    build_line "  Fixing $header"
+    sed -i "$header" \
+      -e 's|define[[:blank:]]*\([UCG]\+\)LIBC_DYNAMIC_LINKER\([0-9]*\)[[:blank:]]"\([^\"]\+\)"$|define \1LIBC_DYNAMIC_LINKER\2 "'${headers}'\3"|g' \
+      -e 's|/lib64/ld-linux-|/lib/ld-linux-|g'
+  done
+
+  # Installs x86_64 libraries under `lib/` vs the default `lib64/`
+  #
+  # Thanks to: https://projects.archlinux.org/svntogit/packages.git/tree/trunk/PKGBUILD?h=packages/gcc
+  sed -i '/m64=/s/lib64/lib/' gcc/config/i386/t-linux64
+
+  # Build up the build cflags that will be set for multiple environment
+  # variables in the `make` command
+  build_cflags="-O2"
+  build_cflags="$build_cflags -I${headers}"
+  build_cflags="$build_cflags -B${glibc}/lib/"
+  build_cflags="$build_cflags -idirafter"
+  build_cflags="$build_cflags ${headers}"
+  build_cflags="$build_cflags -idirafter"
+  build_cflags="$build_cflags ${pkg_prefix}/lib/gcc/*/*/include-fixed"
+  build_cflags="$build_cflags -Wl,-L${glibc}/lib"
+  build_cflags="$build_cflags -Wl,-rpath"
+  build_cflags="$build_cflags -Wl,${glibc}/lib"
+  build_cflags="$build_cflags -Wl,-L${glibc}/lib"
+  build_cflags="$build_cflags -Wl,-dynamic-linker"
+  build_cflags="$build_cflags -Wl,${dynamic_linker}"
+
+  # Build up the target ldflags that will be used in the `make` command
+  target_ldflags="-Wl,-L${glibc}/lib"
+  target_ldflags="$target_ldflags -Wl,-rpath"
+  target_ldflags="$target_ldflags -Wl,${glibc}/lib"
+  target_ldflags="$target_ldflags -Wl,-L${glibc}/lib"
+  target_ldflags="$target_ldflags -Wl,-dynamic-linker"
+  target_ldflags="$target_ldflags -Wl,${dynamic_linker}"
+  target_ldflags="$target_ldflags -Wl,-L${glibc}/lib"
+  target_ldflags="$target_ldflags -Wl,-rpath"
+  target_ldflags="$target_ldflags -Wl,${glibc}/lib"
+  target_ldflags="$target_ldflags -Wl,-L${glibc}/lib"
+  target_ldflags="$target_ldflags -Wl,-dynamic-linker"
+  target_ldflags="$target_ldflags -Wl,${dynamic_linker}"
+}
+
+do_build() {
+  rm -rf ../${pkg_name}-build
+  mkdir ../${pkg_name}-build
+  pushd ../${pkg_name}-build > /dev/null
+    SED=sed \
+    LD=$(pkg_path_for chef/binutils)/bin/ld \
+    AS=$(pkg_path_for chef/binutils)/bin/as \
+    ../$pkg_dirname/configure \
+      --prefix=$pkg_prefix \
+      --with-gmp=$(pkg_path_for chef/gmp) \
+      --with-mpfr=$(pkg_path_for chef/mpfr) \
+      --with-mpc=$(pkg_path_for chef/libmpc) \
+      --with-native-system-header-dir=$headers \
+      --enable-languages=c,c++ \
+      --enable-lto \
+      --enable-plugin \
+      --enable-shared \
+      --enable-threads=posix \
+      --enable-install-libiberty \
+      --disable-werror \
+      --disable-multilib \
+      --with-system-zlib \
+      --disable-libstdcxx-pch \
+      --disable-bootstrap
+
+    # Don't store the configure flags in the resulting executables.
+    #
+    # Thanks to: https://github.com/NixOS/nixpkgs/blob/release-15.09/pkgs/development/compilers/gcc/builder.sh
+    sed -e '/TOPLEVEL_CONFIGURE_ARGUMENTS=/d' -i Makefile
+
+    # CFLAGS_FOR_TARGET are needed for the libstdc++ configure script to find
+    # the startfiles.
+    # FLAGS_FOR_TARGET are needed for the target libraries to receive the -Bxxx
+    # for the startfiles.
+    #
+    # Thanks to: https://github.com/NixOS/nixpkgs/blob/release-15.09/pkgs/development/compilers/gcc/builder.sh
+    make \
+      -j$(nproc) \
+      NATIVE_SYSTEM_HEADER_DIR=$headers \
+      SYSTEM_HEADER_DIR=$headers \
+      CFLAGS_FOR_BUILD="$build_cflags" \
+      CXXFLAGS_FOR_BUILD="$build_cflags" \
+      CFLAGS_FOR_TARGET="$build_cflags" \
+      CXXFLAGS_FOR_TARGET="$build_cflags" \
+      FLAGS_FOR_TARGET="$build_cflags" \
+      LDFLAGS_FOR_BUILD="$build_cflags" \
+      LDFLAGS_FOR_TARGET="$target_ldflags" \
+      BOOT_CFLAGS="$build_cflags" \
+      BOOT_LDFLAGS="$build_cflags" \
+      LIMITS_H_TEST=true
+
+    if [ -n "${DO_CHECK}" ]; then
+      # One set of tests in the GCC test suite is known to exhaust the stack,
+      # so increase the stack size prior to running the tests
+      ulimit -s 32768
+
+      unset CPATH LIBRARY_PATH
+      export LIBRARY_PATH="$LD_RUN_PATH"
+      # Do not abort on error as some are "expected"
+      make -k check || true
+      unset LIBRARY_PATH
+
+      build_line "Displaying Test Summary"
+      ../$pkg_dirname/contrib/test_summary
+    fi
+  popd > /dev/null
+}
+
+do_install() {
+  pushd ../${pkg_name}-build > /dev/null
+    # Make 'lib64' a symlink to 'lib'
+    mkdir -pv $pkg_path/lib
+    ln -sv lib $pkg_path/lib64
+
+    make install
+
+    # Install Runtime Library Exception
+    install -Dm644 ../$pkg_dirname/COPYING.RUNTIME \
+      $pkg_path/share/licenses/RUNTIME.LIBRARY.EXCEPTION
+
+    # Replace hard links for x86_64-unknown-linux-gnu etc. with symlinks
+    #
+    # Thanks to: https://github.com/NixOS/nixpkgs/blob/release-15.09/pkgs/development/compilers/gcc/builder.sh
+    for bin in $pkg_path/bin/*-gcc*; do
+      if cmp -s $pkg_path/bin/gcc $bin; then
+        ln -sfnv gcc $bin
+      fi
+    done
+
+    # Replace hard links for x86_64-unknown-linux-g++ etc. with symlinks
+    for bin in $pkg_path/bin/c++ $pkg_path/bin/*-c++* $pkg_path/bin/*-g++*; do
+      if cmp -s $pkg_path/bin/g++ $bin; then
+        ln -sfn g++ $bin
+      fi
+    done
+
+    # Many packages use the name cc to call the C compiler
+    ln -sv gcc $pkg_path/bin/cc
+
+    # Wrap key binaries so we can add some arguments and flags to the real
+    # underlying binary. This should make Plan author's lives a bit easier
+    # as they won't have to worry about setting the correct dynamic linker
+    # (from glibc) and finding the correct path to the special linker object
+    # files such as `crt1.o` and gang.
+    #
+    # Thanks to: https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/cc-wrapper/cc-wrapper.sh
+    # Thanks to: https://gcc.gnu.org/onlinedocs/gcc/Directory-Options.html
+    wrap_binary gcc
+    wrap_binary g++
+    wrap_binary cpp
+  popd > /dev/null
+}
+
+wrap_binary() {
+  local bin="$pkg_path/bin/$1"
+  build_line "Adding wrapper $bin to ${bin}.real"
+  mv -v "$bin" "${bin}.real"
+  sed $PLAN_CONTEXT/cc-wrapper.sh \
+    -e "s^@shell@^${bash}^g" \
+    -e "s^@glibc@^${glibc}^g" \
+    -e "s^@binutils@^${binutils}^g" \
+    -e "s^@gcc@^${pkg_path}^g" \
+    -e "s^@dynamic_linker@^${dynamic_linker}^g" \
+    -e "s^@program@^${bin}.real^g" \
+    > "$bin"
+  chmod 755 "$bin"
+}
