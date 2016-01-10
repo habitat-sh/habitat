d81ca3fb3147e6f853742b54a89f65ea143fee3e
Fletcher Nichol <fnichol@nichol.ca>
2016-01-10 12:45:34 -0700

    [plans] Update patchelf to be built with & without bootstrap tools.
    The patchelf tools needs libgcc and libstdc++ to run, but we want to use
    patchelf in preparing the chef/gcc-libs Plan. Dang, right?
    
    This update modifies the behavior of the chef/patchelf Plan to work
    slightly differently if the bootstrap toolchain tools are uses versus in
    the (future) default way. Namely: by default we add `chef/gcc-libs` as a
    dependency, and if it isn't built yet, we fallback to pulling libgcc and
    libstdc++ off the `/usr/lib` path (provided by the `/tools` bootstrap
    tools and minimal symlinks).
    
    Trust me, this will either make sense in future changes, or it won't
    even matter to future yous. Trust....me, right?

--- plans/patchelf/plan.sh
+++ plans/patchelf/plan.sh
@@ -1,15 +1,30 @@
 pkg_name=patchelf
 pkg_derivation=chef
 pkg_version=0.8
-pkg_license=('GPLv3')
-pkg_source=http://releases.nixos.org/patchelf/patchelf-$pkg_version/patchelf-${pkg_version}.tar.gz
+pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
+pkg_license=('gplv3')
+pkg_source=http://releases.nixos.org/$pkg_name/${pkg_name}-$pkg_version/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=14af06a2da688d577d64ff8dac065bb8903bbffbe01d30c62df7af9bf4ce72fe
-pkg_gpg_key=3853DA6B
 pkg_binary_path=(bin)
-# For now we're not including chef/libstdc++ because that plan is using
-# patchelf to fix itself. Once the proper toolchain is built up, this
-# will no longer be an issue. The side-effect is patchelf will link to
-# a system lib, i.e /usr/lib/x86_64-linux-gnu/libstdc++.so.6 on Ubuntu. Since
-# this is a build dependency tool and our environment is not 100% clean, I'm
-# okay living with this for now - FIN
-pkg_deps=(chef/glibc chef/libgcc)
+pkg_build_deps=(chef/binutils chef/gcc)
+pkg_gpg_key=3853DA6B
+
+if [[ -n "$BOOTSTRAP_TOOLS" ]]; then
+  # Waiting on chef/gcc-libs to link libgcc and libstdc++, but because we need
+  # this package to prepare chef/gcc-libs, we'll do the cheap version first
+  # that relies on the bootstrapping tools' version of these shared libraries
+  pkg_deps=(chef/glibc)
+  build_line "Using bootstrap tools from $BOOTSTRAP_TOOLS for libgcc and libstdc++"
+else
+  pkg_deps=(chef/glibc chef/gcc-libs)
+fi
+
+do_prepare() {
+  # If chef/gcc-libs is not yet a dependency (i.e. when building from
+  # bootstrapping tools), add `/usr/lib` to the `$LD_RUN_PATH` for using the
+  # libgcc and libstdc++ from the bootstrapping tools
+  if [[ -n "$BOOTSTRAP_TOOLS" ]]; then
+    LD_RUN_PATH="$LD_RUN_PATH:/usr/lib"
+    build_line "Updating LD_RUN_PATH=$LD_RUN_PATH"
+  fi
+}
