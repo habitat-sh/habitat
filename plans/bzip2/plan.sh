456eabf67c2eabe2e0d4def920f02bff4365a6c4
Fletcher Nichol <fnichol@nichol.ca>
2016-01-11 22:31:41 -0700

    [plan] Update bzip2 plan for toolchain.

--- plans/bzip2/plan.sh
+++ plans/bzip2/plan.sh
@@ -1,25 +1,49 @@
 pkg_name=bzip2
 pkg_derivation=chef
 pkg_version=1.0.6
+pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_license=('bzip2')
-pkg_source=http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz
+pkg_source=http://www.bzip.org/$pkg_version/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=a2848f34fcd5d6cf47def00461fcb528a0484d8edef8208d6d2e2909dc61d9cd
-pkg_gpg_key=3853DA6B
+pkg_build_deps=(chef/binutils chef/gcc)
 pkg_deps=(chef/glibc)
-pkg_lib_dirs=(lib)
-pkg_include_dirs=(include)
 pkg_binary_path=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+pkg_gpg_key=3853DA6B
+
+do_prepare() {
+  # Makes the symbolic links in installation relative vs. absolute
+  sed -i 's@\(ln -s -f \)$(PREFIX)/bin/@\1@' Makefile
+
+  # Ensure that the man pages are installed under share/man
+  sed -i "s@(PREFIX)/man@(PREFIX)/share/man@g" Makefile
+}
 
 do_build() {
-  make -f Makefile-libbz2_so PREFIX="$pkg_prefix" LDFLAGS="$LDFLAGS"
+  make -f Makefile-libbz2_so PREFIX="$pkg_prefix"
   make bzip2 bzip2recover
+
+  if [[ -n "$DO_CHECK" ]]; then
+    make test
+  fi
 }
 
 do_install() {
+  local maj=$(echo $pkg_version | cut -d "." -f 1)
+  local maj_min=$(echo $pkg_version | cut -d "." -f 1-2)
+
   make install PREFIX="$pkg_prefix"
-  cp $BLDR_SRC_CACHE/$pkg_dirname/libbz2.so.1.0.6 $pkg_prefix/lib
-  ln -s $pkg_prefix/lib/libbz2.so.1.0.6 $pkg_prefix/lib/libbz2.so
-  ln -s $pkg_prefix/lib/libbz2.so.1.0.6 $pkg_prefix/lib/libbz2.so.1
-  ln -s $pkg_prefix/lib/libbz2.so.1.0.6 $pkg_prefix/lib/libbz2.so.1.0
+
+  # Replace some hard links with symlinks
+  rm -fv $pkg_path/bin/{bunzip2,bzcat}
+  ln -sv bzip2 $pkg_path/bin/bunzip2
+  ln -sv bzip2 $pkg_path/bin/bzcat
+
+  # Install the shared library and its symlinks
+  cp -v $BLDR_SRC_CACHE/$pkg_dirname/libbz2.so.$pkg_version $pkg_prefix/lib
+  ln -sv libbz2.so.$pkg_version $pkg_prefix/lib/libbz2.so
+  ln -sv libbz2.so.$pkg_version $pkg_prefix/lib/libbz2.so.$maj
+  ln -sv libbz2.so.$pkg_version $pkg_prefix/lib/libbz2.so.$maj_min
 }
 
