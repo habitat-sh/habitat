6810e6f5d8c3a1c0ece3b45c5c677cd57c677e53
jtimberman <joshua@chef.io>
2016-01-12 15:21:07 -0700

    [plans] Add a ruby plan
    This also adds dependencies for ruby:
    
    * libffi
    * libiconv
    * libtool
    * libyaml
    
    We needed to add the patch ruby-2_1_3-no-mkmf to prevent errors in
    make install. We also needed to force the openssl and libyaml
    directories by using the _resolve_dependency function from bldr-build.

---
+++ plans/libffi/plan.sh
@@ -0,0 +1,17 @@
+pkg_name=libffi
+pkg_version=3.2.1
+pkg_derivation=chef
+pkg_license=('mit')
+pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
+pkg_source=ftp://sourceware.org/pub/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz
+pkg_filename=${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=d06ebb8e1d9a22d19e38d63fdb83954253f39bedc5d46232a05645685722ca37
+pkg_gpg_key=3853DA6B
+pkg_deps=(chef/glibc chef/libtool)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+
+do_build() {
+    ./configure --prefix=${pkg_prefix} --disable-multi-os-directory
+    make
+}
---
+++ plans/ruby/plan.sh
@@ -0,0 +1,29 @@
+pkg_name=ruby
+pkg_derivation=chef
+pkg_version=2.3.0
+pkg_license=('ruby')
+pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
+pkg_source=https://cache.ruby-lang.org/pub/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz
+pkg_filename=${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=ba5ba60e5f1aa21b4ef8e9bf35b9ddb57286cb546aac4b5a28c71f459467e507
+pkg_gpg_key=3853DA6B
+pkg_deps=(chef/glibc chef/ncurses chef/zlib chef/libedit chef/openssl
+          chef/libyaml chef/libiconv chef/libffi)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+
+do_build() {
+    CFLAGS="${CFLAGS} -O3 -g -pipe"
+    patch -p1 -i $PLAN_CONTEXT/patches/ruby-2_1_3-no-mkmf.patch
+
+    ./configure --prefix=${pkg_prefix} \
+                --with-out-ext=dbm \
+                --enable-shared \
+                --enable-libedit \
+                --disable-install-doc \
+                --without-gmp \
+                --without-gdbm \
+                --with-openssl-dir=$(_resolve_dependency chef/openssl) \
+                --with-libyaml-dir=$(_resolve_dependency chef/libyaml)
+    make
+}
