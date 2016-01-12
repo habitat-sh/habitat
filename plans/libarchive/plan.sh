391137ac21d2b150bcaea1f4f7e63280d0fd3044
Jamie Winsor <jamie@vialstudios.com>
2016-01-12 00:59:03 -0800

    add libarchive plan
    add additional dependent plans for libarchive

---
+++ plans/libarchive/plan.sh
@@ -0,0 +1,19 @@
+pkg_name=libarchive
+pkg_derivation=chef
+pkg_version=3.1.2
+pkg_license=('BSD')
+pkg_source=https://github.com/${pkg_name}/${pkg_name}/archive/v${pkg_version}.tar.gz
+pkg_filename=${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=9543ed21ac18fecc238dec4a6fae6abf1cc449dbad5940ca0d80202e0a7237a4
+pkg_gpg_key=3853DA6B
+pkg_deps=(chef/bzip2 chef/lzo chef/openssl chef/xz chef/zlib)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+
+do_build() {
+  build/autogen.sh
+  ./configure \
+    --prefix=$pkg_prefix \
+    --without-xml2
+  make
+}
---
+++ plans/lzo/plan.sh
@@ -0,0 +1,19 @@
+pkg_name=lzo
+pkg_derivation=chef
+pkg_version=2.09
+pkg_license=('GPL')
+pkg_source=http://www.oberhumer.com/opensource/${pkg_name}/download/${pkg_name}-${pkg_version}.tar.gz
+pkg_filename=${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=f294a7ced313063c057c504257f437c8335c41bfeed23531ee4e6a2b87bcb34c
+pkg_gpg_key=3853DA6B
+pkg_deps=(chef/glibc)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+
+do_build() {
+  ./configure \
+    --prefix=$pkg_prefix \
+    --enable-shared \
+    --disable-static
+  make
+}
