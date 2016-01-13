9b7a06b354d8a41687df2d53566166ec1ce662bb
Fletcher Nichol <fnichol@nichol.ca>
2016-01-13 15:32:25 -0700

    [plan] Add bison plan.

---
+++ plans/bison/plan.sh
@@ -0,0 +1,11 @@
+pkg_name=bison
+pkg_derivation=chef
+pkg_version=3.0.4
+pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
+pkg_license=('gplv3')
+pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
+pkg_shasum=a72428c7917bdf9fa93cb8181c971b6e22834125848cf1d03ce10b1bb0716fe1
+pkg_deps=(chef/glibc)
+pkg_build_deps=(chef/gcc chef/m4 chef/coreutils)
+pkg_binary_path=(bin)
+pkg_gpg_key=3853DA6B
