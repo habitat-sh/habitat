78b97c533ad2637dd443598feac0e746f820fd8d
Nathan L Smith <smith@chef.io>
2016-01-14 09:48:25 -0600

    Initial commit of web app
    web/README.md has instructions to run it.
    
    Should show a web page with "Home" and "Dashboard" that change the view
    when you click on them.

---
+++ web/app/boot.ts
@@ -0,0 +1,5 @@
+import {AppComponent} from "./app.component";
+import {ROUTER_PROVIDERS} from "angular2/router";
+import {bootstrap}    from "angular2/platform/browser";
+
+bootstrap(AppComponent, [ROUTER_PROVIDERS]);
---
+++ web/app/home.component.ts
@@ -0,0 +1,7 @@
+import {Component} from "angular2/core";
+
+@Component({
+  template: "<h2>home</h2>"
+})
+
+export class HomeComponent { }
