a190f4f1b471e76847c48f612688795bcdebfb5f
Nathan L Smith <smith@chef.io>
2016-01-15 14:41:23 -0600

    Sign in/out flow
    Also starts adding some CSS.

--- web/app/sign-up-form/sign-up-form.component.ts
+++ web/app/sign-up-form/sign-up-form.component.ts
@@ -1,4 +1,4 @@
-import appState from "./app-state";
+import appState from "../app-state";
 import {Component} from "angular2/core";
 import {RouterLink} from "angular2/router";
 
@@ -9,10 +9,10 @@ import {RouterLink} from "angular2/router";
     <div *ngIf="!isSubmitted">
       <h2>Get Started Now</h2>
       <h3>Create your bldr account</h3>
-      <form (ngSubmit)="onSubmit()">
-        <input placeholder="Username" required>
-        <input type="email" placeholder="Email Address" required>
-        <input type="password" placeholder="Password" required>
+      <form (ngSubmit)="onSubmit(username, email, password)">
+        <input placeholder="Username" required #username>
+        <input type="email" placeholder="Email Address" required #email>
+        <input type="password" placeholder="Password" required #password>
         <button>Sign Up</button>
       </form>
     </div>
@@ -21,15 +21,18 @@ import {RouterLink} from "angular2/router";
       <p>Check your email to activate your account.</p>
       <p><small>
         (ok now pretend you clicked the link in that email and it took you to the
-        <a [routerLink]="['Dashboard']">dashboard</a>.)
+        <a [routerLink]="['Sign In']">sign in page</a>.)
       </small></p>
     <div>
   `,
 })
 
 export class SignUpFormComponent {
-  onSubmit() {
+  onSubmit(username, email, password) {
     appState.set("sign-up-form-submitted", true);
+    appState.set("username", username.value);
+    appState.set("email", email.value);
+    appState.set("password", password.value);
   }
 
   get isSubmitted() {
