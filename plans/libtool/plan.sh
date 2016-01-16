02233a690f7dda512d844aee417a307d5a9888f1
Fletcher Nichol <fnichol@nichol.ca>
2016-01-15 18:34:50 -0700

    [plan] Update libtool plan.

--- plans/libtool/plan.sh
+++ plans/libtool/plan.sh
@@ -1,19 +1,33 @@
 pkg_name=libtool
 pkg_derivation=chef
 pkg_version=2.4.6
-pkg_license=('gplv2')
+pkg_license=('gplv2+')
 pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_source=http://ftp.gnu.org/gnu/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz
 pkg_filename=${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=e3bd4d5d3d025a36c21dd6af7ea818a2afcd4dfc1ea5a17b39d7854bcd0c06e3
-pkg_gpg_key=3853DA6B
-pkg_deps=(chef/glibc)
-pkg_lib_dirs=(lib)
+pkg_deps=(chef/glibc chef/coreutils chef/sed chef/grep)
+pkg_build_deps=(chef/gcc chef/bison chef/bash chef/tar chef/gawk chef/m4)
+pkg_binary_path=(bin)
 pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+pkg_gpg_key=3853DA6B
 
-do_prepare () {
+do_prepare() {
+  do_default_prepare
+
   # Drop the dependency on `help2man` by skipping the generation of a man page
   sed \
     -e '/^dist_man1_MANS =/ s,^.*$,dist_man1_MANS = $(libtoolize_1),g' \
     -i Makefile.in
+}
+
+do_build() {
+  # * `lt_cv_sys_dlsearch_path` Makes the default library search path empty,
+  # rather than `"/lib /usr/lib"`
+  ./configure \
+    --prefix=$pkg_prefix \
+    lt_cv_sys_lib_dlsearch_path_spec="" \
+    lt_cv_sys_lib_search_path_spec=""
+  make
 }
