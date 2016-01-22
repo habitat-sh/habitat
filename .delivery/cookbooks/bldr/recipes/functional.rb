efe6292e2b4e71776e651ad242be938dec56a1ac
jtimberman <joshua@chef.io>
2016-01-22 15:41:47 -0700

    [acceptance] docker machine setup in default
    This commit introduces a new recipe, _docker_machine, that can be used
    to set up the node it runs on with all the keys and configuration
    required to connect to the docker-machine for the acceptance
    environment.
    
    We've lifted this from the provision recipe, and made it a normal recipe
    with the chef resources rather than a giant opaque ruby_block.
    
    We also want to assume that the functional recipe should run against the
    docker machine we've configured.

--- .delivery/cookbooks/bldr/recipes/functional.rb
+++ .delivery/cookbooks/bldr/recipes/functional.rb
@@ -16,6 +16,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+machine_dir = ::File.join(
+  Etc.getpwnam('dbuild').dir,
+  '.docker/machine/machines',
+  'bldr-docker-machine'
+)
+
+docker_machine_config = BldrDockerMachine.load_config
+
 execute 'make clean package functional force=true' do
   cwd node['delivery']['workspace']['repo']
+  environment(
+    'DOCKER_TLS_VERIFY' => '1',
+    'DOCKER_CERT_PATH' => machine_dir,
+    'DOCKER_HOST' => "tcp://#{BldrDockerMachine.machine_ip}:2376",
+    'DOCKER_MACHINE_NAME' => 'bldr-docker-machine'
+  )
 end
