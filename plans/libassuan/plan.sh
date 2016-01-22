1b6642dbabeafb6b9b4391b3d47f706c10f806e8
Fletcher Nichol <fnichol@nichol.ca>
2016-01-21 17:00:49 -0700

    [plan] Update libassuan plan with new build dependencies.

--- plans/libassuan/plan.sh
+++ plans/libassuan/plan.sh
@@ -1,20 +1,28 @@
 pkg_name=libassuan
 pkg_derivation=chef
 pkg_version=2.4.2
-pkg_license=('LGPL')
-pkg_source=https://www.gnupg.org/ftp/gcrypt/${pkg_name}/${pkg_name}-${pkg_version}.tar.bz2
+pkg_license=('lgplv2+')
+pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
+pkg_source=ftp://ftp.gnupg.org/gcrypt/${pkg_name}/${pkg_name}-${pkg_version}.tar.bz2
 pkg_shasum=bb06dc81380b74bf1b64d5849be5c0409a336f3b4c45f20ac688e86d1b5bcb20
-pkg_gpg_key=3853DA6B
+pkg_deps=(chef/glibc chef/libgpg-error)
+pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool chef/diffutils chef/findutils chef/xz chef/gettext chef/gzip chef/make chef/patch chef/texinfo chef/util-linux)
 pkg_binary_path=(bin)
-pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
-pkg_deps=(chef/libgpg-error)
+pkg_lib_dirs=(lib)
+pkg_gpg_key=3853DA6B
 
 do_build() {
   ./configure \
     --prefix=$pkg_prefix \
-    --with-libgpg-error-prefix=$(pkg_path_for chef/libgpg-error)
+    --with-libgpg-error-prefix=$(pkg_path_for chef/libgpg-error) \
+    --enable-static \
+    --enable-shared
   make
+}
+
+do_check() {
+  make check
 }
 
 do_strip() {
