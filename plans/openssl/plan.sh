5c7d5ff1f788fb5b451b29d63139567ae68de164
Dave Parfitt <dparfitt@chef.io>
2016-01-28 15:21:50 -0500

    updated to 1.0.2f
    https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2016-0701

--- plans/openssl/plan.sh
+++ plans/openssl/plan.sh
@@ -1,9 +1,9 @@
 pkg_name=openssl
 pkg_derivation=chef
-pkg_version=1.0.2e
+pkg_version=1.0.2f
 pkg_license=('BSD')
-pkg_source=https://www.openssl.org/source/openssl-1.0.2e.tar.gz
-pkg_shasum=e23ccafdb75cfcde782da0151731aa2185195ac745eea3846133f2e05c0e0bff
+pkg_source=https://www.openssl.org/source/openssl-1.0.2f.tar.gz
+pkg_shasum=932b4ee4def2b434f85435d9e3e19ca8ba99ce9a065a61524b429a9d5e9b2e9c
 pkg_gpg_key=3853DA6B
 pkg_deps=(chef/glibc chef/zlib chef/cacerts)
 pkg_binary_path=(bin)
