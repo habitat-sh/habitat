fc1d18dfe1342f157007a61d8f6244dfe5c8a71d
Fletcher Nichol <fnichol@nichol.ca>
2016-01-30 01:01:40 -0700

    [plan] Update gnupg & gnupg-static plan to 1.x series.
    From: https://en.wikipedia.org/wiki/GNU_Privacy_Guard
    
    > As a command-line based system, GnuPG 1.x is not written as an API
    > that may be incorporated into other software. To overcome this, GPGME
    > (abbreviated from GnuPG Made Easy) was created as an API wrapper
    > around GnuPG that parses the output of GnuPG and provides a stable and
    > maintainable API between the components. This currently requires
    > an out-of-process call to the GnuPG executable for many GPGME API
    > calls; as a result, possible security problems in an application do
    > not propagate to the actual crypto code due to the process barrier.
    > Various graphical front-ends based on GPGME have been created.
    
    > Since GPG 2.x, many of the GPG functionalities are available directly
    > as simple C APIs in Libgcrypt.
    
    Also:
    http://superuser.com/questions/655246/are-gnupg-1-and-gnupg-2-compatible-with-each-other

--- plans/gnupg/plan.sh
+++ plans/gnupg/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=gnupg
 pkg_distname=$pkg_name
 pkg_derivation=chef
-pkg_version=2.1.10
+pkg_version=1.4.20
 pkg_license=('gplv3+')
 pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_source=ftp://ftp.gnupg.org/gcrypt/${pkg_distname}/${pkg_distname}-${pkg_version}.tar.bz2
-pkg_shasum=93bd58d81771a4fa488566e5d2e13b1fd7afc86789401eb41731882abfd26cf9
-pkg_deps=(chef/glibc chef/libgpg-error chef/libassuan chef/libgcrypt chef/libksba chef/npth chef/zlib chef/bzip2 chef/readline)
+pkg_shasum=04988b1030fa28ddf961ca8ff6f0f8984e0cddcb1eb02859d5d8fe0fe237edcc
+pkg_deps=(chef/glibc chef/zlib chef/bzip2 chef/readline)
 pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool chef/diffutils chef/findutils chef/xz chef/gettext chef/gzip chef/make chef/patch chef/texinfo chef/util-linux)
 pkg_binary_path=(bin)
 pkg_gpg_key=3853DA6B
@@ -22,13 +22,5 @@ do_check() {
   find tests -type f \
     | xargs sed -e "s,/bin/pwd,$(pkg_path_for coreutils)/bin/pwd,g" -i
 
-  GNUPGHOME=`pwd` ./agent/gpg-agent --daemon make check
-}
-
-do_install() {
-  do_default_install
-
-  # Add symlinks for older tools
-  ln -sv gpg2 $pkg_path/bin/gpg
-  ln -sv gpgv2 $pkg_path/bin/gpgv
+  make check
 }
