4958fb1e504f6a7f3c49fb4ec0376ecf9c8582d4
Fletcher Nichol <fnichol@nichol.ca>
2016-02-03 10:04:31 -0700

    [plan] Add 'backline' plan for good interactive Studio sessions.

---
+++ plans/backline/plan.sh
@@ -0,0 +1,41 @@
+pkg_name=backline
+pkg_derivation=chef
+pkg_version=0.1.0
+pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
+pkg_license=('apachev2')
+pkg_source=nosuchfile.tar.gz
+pkg_build_deps=()
+pkg_gpg_key=3853DA6B
+
+pkg_deps=(
+  chef/build
+  chef/less
+  chef/make
+  chef/mg
+  chef/util-linux
+  chef/vim
+)
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_unpack() {
+  return 0
+}
+
+do_prepare() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
