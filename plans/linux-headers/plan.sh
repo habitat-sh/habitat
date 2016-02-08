c2392ca72fd312493a3d0e8da60703c2d555fbc0
Fletcher Nichol <fnichol@nichol.ca>
2016-02-07 18:32:54 -0700

    [plan] Update linux-headers to build in a bldr Studio.

--- plans/linux-headers/plan.sh
+++ plans/linux-headers/plan.sh
@@ -6,6 +6,8 @@ pkg_license=('gplv2')
 pkg_source=http://ftp.kernel.org/pub/linux/kernel/v4.x/linux-${pkg_version}.tar.xz
 pkg_shasum=4a622cc84b8a3c38d39bc17195b0c064d2b46945dfde0dae18f77b120bc9f3ae
 pkg_dirname=linux-$pkg_version
+pkg_deps=()
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc)
 pkg_include_dirs=(include)
 pkg_gpg_key=3853DA6B
 
@@ -16,3 +18,15 @@ do_build() {
 do_install() {
   find $pkg_path/include \( -name ..install.cmd -o -name .install \) -print0 | xargs -0 rm -v
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=()
+fi
