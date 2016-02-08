f82168ca19aa579045b1a109b4d8c86fb236e97d
Fletcher Nichol <fnichol@nichol.ca>
2016-02-08 23:49:39 +0000

    [plan] Update libmpc to build in a bldr Studio.

--- plans/libmpc/plan.sh
+++ plans/libmpc/plan.sh
@@ -6,8 +6,8 @@ pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_license=('lgpl')
 pkg_source=http://www.multiprecision.org/mpc/download/${pkg_distname}-${pkg_version}.tar.gz
 pkg_shasum=617decc6ea09889fb08ede330917a00b16809b8db88c29c31bfbb49cbf88ecc3
-pkg_build_deps=(chef/binutils)
 pkg_deps=(chef/glibc chef/gmp chef/mpfr)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/binutils)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 pkg_gpg_key=3853DA6B
@@ -23,3 +23,15 @@ do_prepare() {
 do_check() {
   make check
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/binutils)
+fi
