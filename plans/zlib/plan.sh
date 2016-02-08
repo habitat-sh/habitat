3f0e862bb74c52fcf259ae13500a700ae2db74ca
Fletcher Nichol <fnichol@nichol.ca>
2016-02-08 05:44:22 +0000

    [plan] Update zlib to build in a bldr Studio.

--- plans/zlib/plan.sh
+++ plans/zlib/plan.sh
@@ -6,6 +6,7 @@ pkg_license=('zlib')
 pkg_source=http://zlib.net/current/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=36658cb768a54c1d4dec43c3116c27ed893e88b02ecfcb44f2166f9c0b7f2a0d
 pkg_deps=(chef/glibc)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 pkg_gpg_key=3853DA6B
@@ -17,3 +18,15 @@ do_prepare() {
   export LDFLAGS
   build_line "Updating LDFLAGS=$LDFLAGS"
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=()
+fi
