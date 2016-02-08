faedc030ead2b22f9ed9a5ba42bfcaca4b206782
Fletcher Nichol <fnichol@nichol.ca>
2016-02-08 23:36:24 +0000

    [plan] Update gmp to build in a bldr Studio.
    Note that the `--enable-cxx` configure flag was removed as it seems that
    nothing downstream will need the C++ bindings to libgmp. Adding this extra
    shared library was requiring a dependency on `chef/gcc-libs` for libstdc++. It
    was not possible after an hour of trying to build this library with
    `-static-libstdc++` with the desired effect, so it was easier to drop this
    feature support.

--- plans/gmp/plan.sh
+++ plans/gmp/plan.sh
@@ -5,7 +5,7 @@ pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_license=('gplv3')
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
 pkg_shasum=68dadacce515b0f8a54f510edf07c1b636492bcdb8e8d54c56eb216225d16989
-pkg_build_deps=(chef/binutils chef/m4)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/binutils chef/m4)
 pkg_deps=(chef/glibc)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -20,10 +20,22 @@ do_prepare() {
 }
 
 do_build() {
-  ./configure --prefix=$pkg_prefix --enable-cxx
+  ./configure --prefix=$pkg_prefix
   make -j$(nproc)
 }
 
 do_check() {
   make check
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/binutils chef/m4)
+fi
