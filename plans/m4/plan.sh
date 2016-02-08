6c284586cb3ce16e4f91aea616c453a100de78d4
Fletcher Nichol <fnichol@nichol.ca>
2016-02-08 21:28:54 +0000

    [plan] Update m4 to build in a bldr Studio.

--- plans/m4/plan.sh
+++ plans/m4/plan.sh
@@ -5,8 +5,8 @@ pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_license=('gplv3')
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
 pkg_shasum=f0543c3beb51fa6b3337d8025331591e0e18d8ec2886ed391f1aade43477d508
-pkg_build_deps=(chef/binutils)
 pkg_deps=(chef/glibc)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/binutils)
 pkg_binary_path=(bin)
 pkg_gpg_key=3853DA6B
 
@@ -25,3 +25,15 @@ do_check() {
 
   make check
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/binutils)
+fi
