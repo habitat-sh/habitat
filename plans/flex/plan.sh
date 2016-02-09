3a6ea65541e571fd9db85968783e447244ec7000
Fletcher Nichol <fnichol@nichol.ca>
2016-02-09 23:27:40 +0000

    [plan] Update flex to build in a bldr Studio.

--- plans/flex/plan.sh
+++ plans/flex/plan.sh
@@ -6,7 +6,7 @@ pkg_license=('custom')
 pkg_source=http://downloads.sourceforge.net/sourceforge/${pkg_name}/${pkg_name}-${pkg_version}.tar.xz
 pkg_shasum=d39b15a856906997ced252d76e9bfe2425d7503c6ed811669665627b248e4c73
 pkg_deps=(chef/glibc)
-pkg_build_deps=(chef/gcc chef/m4 chef/coreutils chef/bison)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/m4 chef/bison)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -24,3 +24,15 @@ do_install() {
   # `lex`
   ln -sv flex $pkg_path/bin/lex
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/m4 chef/coreutils chef/bison)
+fi
