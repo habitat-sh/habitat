b15426a30dad6ee64e63f63d257b77ff0219d6d3
Fletcher Nichol <fnichol@nichol.ca>
2016-02-09 18:11:33 +0000

    [plan] Update gcc-libs plan to copy libs from chef/gcc package.
    This seems like a worthwhile shortcut as it prevents us from having to
    completely compile gcc twice over. This plan build time goes from about 10
    minutes (on fast hardware) to 2 seconds. Not bad.

--- plans/gcc-libs/plan.sh
+++ plans/gcc-libs/plan.sh
@@ -2,10 +2,11 @@ source ../gcc/plan.sh
 
 pkg_name=gcc-libs
 pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
-pkg_dirname=${pkg_distname}-${pkg_version}
 
+# The shared libraries only depend on chef/glibc
+pkg_deps=(chef/glibc)
 # Add the same version of the full gcc package as a build dep
-pkg_build_deps+=(chef/gcc/$pkg_version chef/patchelf)
+pkg_build_deps=(chef/gcc/$pkg_version chef/patchelf)
 
 # Zero out the bin and include paths, as we're only shipping shared libs
 pkg_binary_path=()
@@ -13,78 +14,64 @@ pkg_include_dirs=()
 
 pkg_gpg_key=3853DA6B
 
-# We will rely on tests from `gcc`, so skip them here
-unset -f do_check
+# The list of GCC libraries to copy
+_gcc_libs=(
+  libasan
+  libatomic
+  libcilkrts
+  libgcc_s
+  libgomp-plugin-host_nonshm
+  libgomp
+  libitm
+  liblsan
+  libquadmath
+  libstdc++
+  libtsan
+  libubsan
+  libvtv
+)
 
 do_install() {
-  triplet=x86_64-unknown-linux-gnu
+  mkdir -pv $pkg_path/lib
+  for lib in "${_gcc_libs[@]}"; do
+    cp -av $(pkg_path_for gcc)/lib/${lib}.* $pkg_path/lib/
+  done
+  rm -fv $pkg_path/lib/*.spec $pkg_path/lib/*.py
 
-  pushd ../${pkg_name}-build > /dev/null
-    make -C $triplet/libgcc install-shared
-    rm -rf ${pkg_path}/lib/gcc
+  mkdir -pv $pkg_path/share/licenses
+  cp -av $(pkg_path_for gcc)/share/licenses/RUNTIME.LIBRARY.EXCEPTION \
+    $pkg_path/share/licenses/
 
-    for lib in libatomic \
-               libcilkrts \
-               libgomp \
-               libitm \
-               libquadmath \
-               libsanitizer/{a,l,ub}san \
-               libstdc++-v3/src \
-               libvtv \
-               libsanitizer/tsan; do
-      make -C $triplet/$lib install-toolexeclibLTLIBRARIES
-    done
-
-    make -C $triplet/libstdc++-v3/po install
-
-    for lib in libgomp \
-               libitm \
-               libquadmath; do
-      make -C $triplet/$lib install-info
-    done
-
-    # Install Runtime Library Exception
-    install -Dm644 ../$pkg_dirname/COPYING.RUNTIME \
-      $pkg_path/share/licenses/RUNTIME.LIBRARY.EXCEPTION
-
-    # As we're building a full gcc and we're only interested in the shared
-    # libraries, we're going to trim the build dependencies to glibc and
-    # add the others to the build dependency list. Note that this is at
-    # least mildy evil and probably not something a normal Plan author
-    # would want to do.
-    #
-    # To be clear: DO NOT USE THIS PATTERN
-    orig_pkg_deps_resolved=(${pkg_deps_resolved[@]})
-    pkg_deps_resolved=()
-    for dep in "${orig_pkg_deps_resolved[@]}"; do
-      if echo "$dep" | egrep -q "/chef/glibc/" > /dev/null; then
-        pkg_deps_resolved+=($dep)
-      else
-        pkg_build_deps_resolved+=($dep)
-      fi
-    done
-    orig_pkg_tdeps_resolved=(${pkg_tdeps_resolved[@]})
-    pkg_tdeps_resolved=()
-    for dep in "${orig_pkg_tdeps_resolved[@]}"; do
-      if echo "$dep" | egrep -q "/chef/glibc/" > /dev/null; then
-        pkg_tdeps_resolved+=($dep)
-      else
-        pkg_build_tdeps_resolved+=($dep)
-      fi
-    done
-    pkg_build_tdeps_resolved=(
-      $(printf '%s\n' "${pkg_build_tdeps_resolved[@]}" | sort | uniq)
-    )
-
-    build_line "Updated dependency list is: (${pkg_deps_resolved[@]})"
-
-    # Due to the build/run package tricks above, the resulting `RUNPATH` has
-    # more path entries than are actually being used (for mpfr, libmpc, etc),
-    # so we'll use `patchelf` trim these unused path entries for each shared
-    # library
-    find $pkg_path/lib \
-      -type f \
-      -name '*.so.*' \
-      -exec patchelf --shrink-rpath {} \;
-  popd > /dev/null
+  # Due to the copy-from-package trick above, the resulting `RUNPATH` entries
+  # have more path entries than are actually being used (for mpfr, libmpc,
+  # etc), so we'll use `patchelf` trim these unused path entries for each
+  # shared library.
+  find $pkg_path/lib -type f -name '*.so.*' \
+    -exec patchelf --set-rpath $(pkg_path_for glibc)/lib:$pkg_path/lib {} \;
+  find $pkg_path/lib -type f -name '*.so.*' -exec patchelf --shrink-rpath {} \;
 }
+
+# Turn the remaining default phases into no-ops
+
+do_download() {
+  return 0
+}
+
+do_verify() {
+  return 0
+}
+
+do_unpack() {
+  return 0
+}
+
+do_prepare() {
+  return 0
+}
+
+do_build() {
+  return 0
+}
+
+# We will rely on tests from `gcc`, so skip them here
+unset -f do_check
