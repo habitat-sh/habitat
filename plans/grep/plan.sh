873a1541e796975e9ee21044f6ad6874c38b3ec8
Fletcher Nichol <fnichol@nichol.ca>
2016-02-09 23:53:09 +0000

    [plan] Update grep to build in a bldr Studio.

--- plans/grep/plan.sh
+++ plans/grep/plan.sh
@@ -6,10 +6,22 @@ pkg_license=('gplv3+')
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
 pkg_shasum=ca91d22f017bfcb503d4bc3b44295491c89a33a3df0c3d8b8614f2d3831836eb
 pkg_deps=(chef/glibc chef/pcre)
-pkg_build_deps=(chef/gcc chef/coreutils)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/perl)
 pkg_binary_path=(bin)
 pkg_gpg_key=3853DA6B
 
 do_check() {
   make check
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils)
+fi
