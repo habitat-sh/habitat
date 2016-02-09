823914c24f415e0de6b17782d9c4c6abf09b90d6
Fletcher Nichol <fnichol@nichol.ca>
2016-02-09 22:21:21 +0000

    [plan] Update pkg-config to build in a bldr Studio.

--- plans/pkg-config/plan.sh
+++ plans/pkg-config/plan.sh
@@ -5,8 +5,8 @@ pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_license=('gplv2+')
 pkg_source=http://pkgconfig.freedesktop.org/releases/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=c8507705d2a10c67f385d66ca2aae31e81770cc0734b4191eb8c489e864a006b
-pkg_build_deps=(chef/binutils chef/gcc)
 pkg_deps=(chef/glibc)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc)
 pkg_binary_path=(bin)
 pkg_gpg_key=3853DA6B
 
@@ -21,3 +21,15 @@ do_build() {
 do_check() {
   make check
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc)
+fi
