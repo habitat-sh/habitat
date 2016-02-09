b0148c24b24b059e4a4bbc7a5deece009294f774
Fletcher Nichol <fnichol@nichol.ca>
2016-02-09 23:06:46 +0000

    [plan] Update procps-ng to build in a bldr Studio.

--- plans/procps-ng/plan.sh
+++ plans/procps-ng/plan.sh
@@ -6,7 +6,7 @@ pkg_license=('gpl' 'lgpl')
 pkg_source=http://downloads.sourceforge.net/project/${pkg_name}/Production/${pkg_name}-${pkg_version}.tar.xz
 pkg_shasum=e9493169a2d2adc0bc045538707310c8e877b385e4e296143b62607d2bb044ed
 pkg_deps=(chef/glibc chef/ncurses)
-pkg_build_deps=(chef/binutils chef/gcc)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -24,3 +24,15 @@ do_build() {
 do_check() {
   make check
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc)
+fi
