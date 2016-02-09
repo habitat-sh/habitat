80a3b1d5ececab43ef19e74cef1d055d959ec63e
Fletcher Nichol <fnichol@nichol.ca>
2016-02-09 23:33:22 +0000

    [plan] Update pcre to build in a bldr Studio.

--- plans/pcre/plan.sh
+++ plans/pcre/plan.sh
@@ -6,7 +6,7 @@ pkg_license=('bsd')
 pkg_source=http://ftp.csx.cam.ac.uk/pub/software/programming/${pkg_name}/${pkg_name}-${pkg_version}.tar.bz2
 pkg_shasum=b9e02d36e23024d6c02a2e5b25204b3a4fa6ade43e0a5f869f254f49535079df
 pkg_deps=(chef/glibc chef/gcc-libs)
-pkg_build_deps=(chef/gcc chef/coreutils chef/patchelf)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -32,3 +32,15 @@ do_install() {
   # Install license file
   install -Dm644 LICENCE $pkg_path/share/licenses/LICENSE
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils)
+fi
