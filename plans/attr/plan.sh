8982465a735de79e65c775e7f563a7cc9d98c604
Fletcher Nichol <fnichol@nichol.ca>
2016-02-09 22:34:24 +0000

    [plan] Update attr to build in a bldr Studio.

--- plans/attr/plan.sh
+++ plans/attr/plan.sh
@@ -5,8 +5,8 @@ pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_license=('gplv2+')
 pkg_source=http://download.savannah.gnu.org/releases/$pkg_name/$pkg_name-${pkg_version}.src.tar.gz
 pkg_shasum=25772f653ac5b2e3ceeb89df50e4688891e21f723c460636548971652af0a859
-pkg_build_deps=(chef/binutils chef/gcc)
 pkg_deps=(chef/glibc)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/gettext)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -16,3 +16,15 @@ do_install() {
   make install install-dev install-lib
   chmod -v 755 $pkg_path/lib/libattr.so
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc)
+fi
