0cd31d0ed41e9b72eb2b57b6cbbf87e0b5d03360
Fletcher Nichol <fnichol@nichol.ca>
2016-02-09 23:24:51 +0000

    [plan] Update bison to build in a bldr Studio.

--- plans/bison/plan.sh
+++ plans/bison/plan.sh
@@ -6,6 +6,18 @@ pkg_license=('gplv3')
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
 pkg_shasum=a72428c7917bdf9fa93cb8181c971b6e22834125848cf1d03ce10b1bb0716fe1
 pkg_deps=(chef/glibc)
-pkg_build_deps=(chef/gcc chef/m4 chef/coreutils)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/m4 chef/perl)
 pkg_binary_path=(bin)
 pkg_gpg_key=3853DA6B
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/m4 chef/coreutils)
+fi
