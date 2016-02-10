6c5867916c78a66e27cd338cb19de9e1712a94a6
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 05:38:22 +0000

    [plan] Update util-linux to build in a bldr Studio.

--- plans/util-linux/plan.sh
+++ plans/util-linux/plan.sh
@@ -6,7 +6,7 @@ pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_source=http://ftp.kernel.org/pub/linux/utils/${pkg_name}/v${pkg_version%.?}/${pkg_name}-${pkg_version}.tar.xz
 pkg_shasum=0a818fcdede99aec43ffe6ca5b5388bff80d162f2f7bd4541dca94fecb87a290
 pkg_deps=(chef/glibc chef/zlib chef/ncurses)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool chef/diffutils chef/findutils chef/xz chef/gettext chef/gzip chef/make chef/patch)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/sed)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -35,3 +35,15 @@ do_build() {
 do_install() {
   make install usrsbin_execdir=$pkg_path/bin
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/diffutils chef/make chef/patch)
+fi
