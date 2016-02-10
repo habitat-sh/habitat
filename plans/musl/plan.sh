1fce47ca5941489b879e56058508614978c08bbc
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 17:22:03 +0000

    [plan] Update musl to build in a bldr Studio.

--- plans/musl/plan.sh
+++ plans/musl/plan.sh
@@ -6,7 +6,7 @@ pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_source=http://www.musl-libc.org/releases/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=720b83c7e276b4b679c0bffe9509340d5f81fd601508e607e708177df0d31c0e
 pkg_deps=()
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool chef/diffutils chef/findutils chef/xz chef/gettext chef/gzip chef/make chef/patch chef/texinfo chef/util-linux)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/sed)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -25,3 +25,15 @@ do_install() {
   # Install license
   install -Dm0644 COPYRIGHT $pkg_path/share/licenses/COPYRIGHT
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/diffutils chef/make chef/patch)
+fi
