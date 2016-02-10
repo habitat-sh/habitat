80a8af5c50a28d088e5e6cfd9d7e1197814e9a55
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 01:24:35 +0000

    [plan] Update tar to build in a bldr Studio.

--- plans/tar/plan.sh
+++ plans/tar/plan.sh
@@ -6,7 +6,7 @@ pkg_license=('gplv3+')
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=6a6b65bac00a127a508533c604d5bf1a3d40f82707d56f20cefd38a05e8237de
 pkg_deps=(chef/glibc chef/acl chef/attr)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/sed)
 pkg_binary_path=(bin)
 pkg_gpg_key=3853DA6B
 
@@ -20,3 +20,15 @@ do_build() {
 do_check() {
   make check
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/sed)
+fi
