8a735cb83fef7ae1844a2c6077ff91868afe6c24
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 02:44:46 +0000

    [plan] Update inetutils to build in a bldr Studio.

--- plans/inetutils/plan.sh
+++ plans/inetutils/plan.sh
@@ -6,7 +6,7 @@ pkg_license=('gplv3+')
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
 pkg_shasum=849d96f136effdef69548a940e3e0ec0624fc0c81265296987986a0dd36ded37
 pkg_deps=(chef/glibc chef/libcap chef/ncurses)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/sed chef/grep)
 pkg_binary_path=(bin)
 pkg_gpg_key=3853DA6B
 
@@ -40,3 +40,15 @@ do_install() {
   # `libexec/` directory is not used
   rm -rfv $pkg_path/libexec
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/grep)
+fi
