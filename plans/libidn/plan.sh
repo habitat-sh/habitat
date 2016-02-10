08f72899341ee09311f0e3fd7b1180f1f6e56d68
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 16:49:18 +0000

    [plan] Update libidn to build in a bldr Studio.

--- plans/libidn/plan.sh
+++ plans/libidn/plan.sh
@@ -6,7 +6,7 @@ pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=ba5d5afee2beff703a34ee094668da5c6ea5afa38784cebba8924105e185c4f5
 pkg_deps=(chef/glibc)
-pkg_build_deps=(chef/gcc chef/sed chef/bison chef/flex chef/grep chef/bash chef/libtool chef/diffutils chef/findutils chef/xz chef/gettext chef/gzip chef/make chef/patch chef/texinfo chef/util-linux)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -15,3 +15,15 @@ pkg_gpg_key=3853DA6B
 do_check() {
   make check
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/diffutils chef/make chef/patch)
+fi
