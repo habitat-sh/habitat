867912aa1c0612800bc2a8cd80a2512432b0d1b8
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 05:54:03 +0000

    [plan] Update expect to build in a bldr Studio.

--- plans/expect/plan.sh
+++ plans/expect/plan.sh
@@ -7,7 +7,7 @@ pkg_source=http://downloads.sourceforge.net/project/$pkg_name/Expect/${pkg_versi
 pkg_shasum=b28dca90428a3b30e650525cdc16255d76bb6ccd65d448be53e620d95d5cc040
 pkg_dirname=${pkg_name}${pkg_version}
 pkg_deps=(chef/glibc chef/tcl chef/coreutils)
-pkg_build_deps=(chef/gcc chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool chef/diffutils chef/findutils chef/xz chef/gettext chef/gzip chef/make chef/patch chef/texinfo chef/util-linux)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -30,14 +30,28 @@ do_build() {
 }
 
 do_check() {
-  make test
+  # The test suite looks for `libgcc_s`, so we'll add it to the
+  # `LD_LIBRARY_PATH`.
+  make test LD_LIBRARY_PATH="$(pkg_path_for gcc)/lib"
 }
 
 do_install() {
-  do_default_install
+  make install LD_LIBRARY_PATH="$(pkg_path_for gcc)/lib"
 
   # Add an absolute path to `tclsh` in each script binary
   find $pkg_prefix/bin \
     -type f \
     -exec sed -e "s,exec tclsh,exec $(pkg_path_for tcl)/bin/tclsh,g" -i {} \;
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/diffutils chef/make chef/patch)
+fi
