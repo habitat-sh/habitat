854bf2e46863e896807003d6aff4c3d2326ccf79
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 02:25:34 +0000

    [plan] Update gdbm to build in a bldr Studio.

--- plans/gdbm/plan.sh
+++ plans/gdbm/plan.sh
@@ -6,7 +6,7 @@ pkg_license=('gplv3+')
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=8d912f44f05d0b15a4a5d96a76f852e905d051bb88022fcdfd98b43be093e3c3
 pkg_deps=(chef/glibc)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -32,3 +32,15 @@ do_install() {
   ln -sf ../ndbm.h ${pkg_path}/include/gdbm/ndbm.h
   ln -sf ../dbm.h  ${pkg_path}/include/gdbm/dbm.h
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils)
+fi
