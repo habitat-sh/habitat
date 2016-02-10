5ab53fda25d398feddb785da27914a9b459a89dd
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 04:11:53 +0000

    [plan] Update gettext to build in a bldr Studio.

--- plans/gettext/plan.sh
+++ plans/gettext/plan.sh
@@ -6,7 +6,7 @@ pkg_license=('gplv2+' 'lgpl2+')
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=ed4b4c19bd3a3034eb6769500a3592ff616759ef43cf30586dbb7a17c9dd695d
 pkg_deps=(chef/glibc chef/gcc-libs chef/acl chef/xz)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool chef/diffutils chef/findutils)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/sed chef/findutils)
 pkg_binary_path=(bin)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
@@ -21,3 +21,15 @@ do_build() {
 do_check() {
   make check
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/diffutils chef/findutils)
+fi
