be82b04d851a5e25dd7e389bcfdac1cd4c880cf9
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 02:21:54 +0000

    [plan] Update libtool to build in a bldr Studio.

--- plans/libtool/plan.sh
+++ plans/libtool/plan.sh
@@ -7,7 +7,7 @@ pkg_source=http://ftp.gnu.org/gnu/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz
 pkg_filename=${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=e3bd4d5d3d025a36c21dd6af7ea818a2afcd4dfc1ea5a17b39d7854bcd0c06e3
 pkg_deps=(chef/glibc chef/coreutils chef/sed chef/grep)
-pkg_build_deps=(chef/gcc chef/bison chef/bash chef/tar chef/gawk chef/m4)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/m4)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -31,3 +31,15 @@ do_build() {
     lt_cv_sys_lib_search_path_spec=""
   make
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/m4)
+fi
