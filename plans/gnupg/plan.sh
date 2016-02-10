72309d5574c67bdf5e9e7843684cf4bd9e406fb1
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 17:02:04 +0000

    [plan] Update gnupg to build in a bldr Studio.

--- plans/gnupg/plan.sh
+++ plans/gnupg/plan.sh
@@ -7,7 +7,7 @@ pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_source=ftp://ftp.gnupg.org/gcrypt/${pkg_distname}/${pkg_distname}-${pkg_version}.tar.bz2
 pkg_shasum=04988b1030fa28ddf961ca8ff6f0f8984e0cddcb1eb02859d5d8fe0fe237edcc
 pkg_deps=(chef/glibc chef/zlib chef/bzip2 chef/readline)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool chef/diffutils chef/findutils chef/xz chef/gettext chef/gzip chef/make chef/patch chef/texinfo chef/util-linux)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/sed chef/findutils)
 pkg_binary_path=(bin)
 pkg_gpg_key=3853DA6B
 
@@ -24,3 +24,15 @@ do_check() {
 
   make check
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/diffutils chef/findutils chef/make chef/patch)
+fi
