91e15ca64c14f78eea9ec03b290933fe0c8a5b19
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 02:41:35 +0000

    [plan] Update db to build in a bldr Studio.

--- plans/db/plan.sh
+++ plans/db/plan.sh
@@ -6,7 +6,7 @@ pkg_license=('custom')
 pkg_source=http://download.oracle.com/berkeley-db/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=e0a992d740709892e81f9d93f06daf305cf73fb81b545afe72478043172c3628
 pkg_deps=(chef/glibc chef/gcc-libs)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -32,3 +32,15 @@ do_install() {
   # Install license file
   install -Dm644 LICENSE "$pkg_path/share/licenses/LICENSE"
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils)
+fi
