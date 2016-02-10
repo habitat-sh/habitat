ddeb09dcc314824fed64ef2027af4ddd8a329dbb
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 03:57:16 +0000

    [plan] Update autoconf to build in a bldr Studio.

--- plans/autoconf/plan.sh
+++ plans/autoconf/plan.sh
@@ -6,6 +6,18 @@ pkg_license=('gplv2+')
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
 pkg_shasum=64ebcec9f8ac5b2487125a86a7760d2591ac9e1d3dbd59489633f9de62a57684
 pkg_deps=(chef/m4 chef/perl)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/sed chef/gawk)
 pkg_binary_path=(bin)
 pkg_gpg_key=3853DA6B
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/gawk chef/diffutils)
+fi
