8855c0310303d409f511916f9c70802a2d38fbb8
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 16:46:46 +0000

    [plan] Update check to build in a bldr Studio.

--- plans/check/plan.sh
+++ plans/check/plan.sh
@@ -6,7 +6,7 @@ pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_source=http://downloads.sourceforge.net/sourceforge/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=f5f50766aa6f8fe5a2df752666ca01a950add45079aa06416b83765b1cf71052
 pkg_deps=(chef/glibc chef/gawk)
-pkg_build_deps=(chef/gcc chef/sed chef/bison chef/flex chef/grep chef/bash chef/libtool chef/diffutils chef/findutils chef/xz chef/gettext chef/gzip chef/make chef/patch chef/texinfo chef/util-linux)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/sed)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -18,3 +18,15 @@ do_install() {
   # Clean up extra files
   rm -rfv $pkg_path/share/info/dir $pkg_path/share/doc/check/*ChangeLog*
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/diffutils chef/make chef/patch)
+fi
