2ecb246c7258edb0667a71399ad6a881b1e6c169
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 00:58:32 +0000

    [plan] Update bc to build in a bldr Studio.

--- plans/bc/plan.sh
+++ plans/bc/plan.sh
@@ -6,7 +6,7 @@ pkg_license=('gplv3+')
 pkg_source=http://alpha.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.bz2
 pkg_shasum=7ee4abbcfac03d8a6e1a8a3440558a3d239d6b858585063e745c760957725ecc
 pkg_deps=(chef/glibc chef/readline)
-pkg_build_deps=(chef/gcc chef/coreutils chef/bison chef/flex)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/texinfo)
 pkg_binary_path=(bin)
 pkg_gpg_key=3853DA6B
 
@@ -29,3 +29,15 @@ do_build() {
 do_check() {
   echo "quit" | ./bc/bc -l Test/checklib.b
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils)
+fi
