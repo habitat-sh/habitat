67ea48b9760c78cfb74c64cbce8d466310ecff87
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 17:32:50 +0000

    [plan] Update vim to build in a bldr Studio.

--- plans/vim/plan.sh
+++ plans/vim/plan.sh
@@ -6,7 +6,7 @@ pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_source=http://github.com/$pkg_name/$pkg_name/archive/v${pkg_version}.tar.gz
 pkg_shasum=e52f7653a36b690441b47a273b1db72f0eb1e5f6729af25110a84088ca73e872
 pkg_deps=(chef/glibc chef/acl chef/ncurses)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool chef/diffutils chef/findutils chef/xz chef/gettext chef/gzip chef/make chef/patch chef/texinfo chef/util-linux chef/autoconf chef/pkg-config chef/wget)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/sed chef/autoconf)
 pkg_binary_path=(bin)
 pkg_gpg_key=3853DA6B
 
@@ -41,3 +41,15 @@ do_install() {
   # Install license file
   install -Dm644 runtime/doc/uganda.txt $pkg_path/share/licenses/license.txt
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/diffutils chef/make chef/patch chef/autoconf)
+fi
