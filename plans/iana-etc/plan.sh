07b47b58e4c55e32be93c5fb60c6617afa45eb4f
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 02:47:19 +0000

    [plan] Update iana-etc to build in a bldr Studio.

--- plans/iana-etc/plan.sh
+++ plans/iana-etc/plan.sh
@@ -6,7 +6,7 @@ pkg_license=('gplv3+')
 pkg_source=http://sethwklein.net/${pkg_name}-${pkg_version}.tar.bz2
 pkg_shasum=b9a6874fb20012836efef75452ef2acae624022d680feeb1994d73facba3f20d
 pkg_deps=()
-pkg_build_deps=(chef/coreutils chef/sed chef/grep chef/bash chef/gawk)
+pkg_build_deps=(chef/coreutils chef/make chef/gawk)
 pkg_gpg_key=3853DA6B
 
 do_build() {
@@ -16,3 +16,15 @@ do_build() {
 do_install() {
   make install PREFIX=$pkg_prefix
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/coreutils chef/gawk)
+fi
