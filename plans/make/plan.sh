45cad09a9f8924f571ae885f4ce72d24142c9502
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 05:14:26 +0000

    [plan] Update make to build in a bldr Studio.
    Now that we have built our modern Binutils, seems like our deterministic mode
    configuration is at odds with the test suite for Make. Ah well, glorious `ar`
    inline wrapper script provided, courtesy of the Debian bug tracker. Enjoy!

--- plans/make/plan.sh
+++ plans/make/plan.sh
@@ -6,7 +6,7 @@ pkg_license=('gplv3+')
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.bz2
 pkg_shasum=0bc7613389650ee6a24554b52572a272f7356164fd2c4132b0bcf13123e4fca5
 pkg_deps=(chef/glibc)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool chef/diffutils chef/findutils chef/xz chef/gettext chef/gzip chef/perl)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/bash chef/gettext chef/gzip chef/perl chef/binutils)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_gpg_key=3853DA6B
@@ -20,5 +20,28 @@ do_prepare() {
 }
 
 do_check() {
-  make check
+  # Force `ar` to not run in deterministic mode, as the testsuite relies on
+  # UID, GID, timestamp and file mode values to be correctly stored.
+  #
+  # Thanks to: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=782750
+  mkdir -pv wrappers
+  cat <<EOF > wrappers/ar
+#!$(pkg_path_for bash)/bin/sh
+exec $(pkg_path_for binutils)/bin/ar U\$@
+EOF
+  chmod -v 0744 wrappers/ar
+
+  env PATH="$(pwd)/wrappers:$PATH" make check
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/binutils chef/gcc chef/coreutils chef/sed chef/bash chef/perl chef/diffutils chef/gettext chef/gzip)
+fi
