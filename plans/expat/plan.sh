4aefcebd8fcac1de56173e125fb5c281b2c911b9
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 02:28:20 +0000

    [plan] Update expat to build in a bldr Studio.

--- plans/expat/plan.sh
+++ plans/expat/plan.sh
@@ -6,7 +6,7 @@ pkg_license=('mit')
 pkg_source=http://downloads.sourceforge.net/sourceforge/expat/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=823705472f816df21c8f6aa026dd162b280806838bb55b3432b0fb1fcca7eb86
 pkg_deps=(chef/glibc chef/gcc-libs)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc)
 pkg_binary_path=(bin)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
@@ -32,3 +32,15 @@ do_install() {
   # Install license file
   install -Dm644 COPYING "$pkgdir/share/licenses/COPYING"
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils)
+fi
