b3389625c783788077a70a7e6c4e7da1dbbdd79c
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 17:35:49 +0000

    [plan] Update clens to build in a bldr Studio.

--- plans/clens/plan.sh
+++ plans/clens/plan.sh
@@ -6,7 +6,7 @@ pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_source=https://opensource.conformal.com/snapshots/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=064ac9954d38633e2cff6b696fd049dedc3e90b79acffbee1a87754bcf604267
 pkg_deps=(chef/glibc)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool chef/diffutils chef/findutils chef/xz chef/gettext chef/gzip chef/make chef/patch chef/texinfo chef/util-linux chef/wget chef/libbsd)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/libbsd)
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 pkg_gpg_key=3853DA6B
@@ -19,3 +19,15 @@ do_build() {
 do_install() {
   make LOCALBASE=$pkg_prefix install
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/diffutils chef/make chef/patch chef/libbsd)
+fi
