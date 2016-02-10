daed3ca5814aa3c9d438a6fbfe722bcf492a66fb
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 17:37:27 +0000

    [plan] Update mg to build in a bldr Studio.

--- plans/mg/plan.sh
+++ plans/mg/plan.sh
@@ -6,7 +6,7 @@ pkg_maintainer="The Bldr Maintainers <bldr@chef.io>"
 pkg_source=http://homepage.boetes.org/software/$pkg_name/${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=26450b2564bec0b0afc465fd24a1917dc31508c5500c3a36823b9c763a2b8636
 pkg_deps=(chef/glibc chef/ncurses chef/libbsd)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/grep chef/bash chef/gawk chef/libtool chef/diffutils chef/findutils chef/xz chef/gettext chef/gzip chef/make chef/patch chef/texinfo chef/util-linux chef/pkg-config chef/clens)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/sed chef/pkg-config chef/clens)
 pkg_binary_path=(bin)
 pkg_gpg_key=3853DA6B
 
@@ -28,13 +28,21 @@ do_build() {
     STRIP=strip
 }
 
-do_check() {
-  make check
-}
-
 do_install() {
   do_default_install
 
   # Install license file from README
   install -Dm644 README "$pkg_path/share/licenses/README"
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/pkg-config chef/coreutils chef/sed chef/diffutils chef/make chef/patch chef/clens)
+fi
