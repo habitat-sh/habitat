437e4c1878c23def9c7a420b13235901033070b8
Fletcher Nichol <fnichol@nichol.ca>
2016-02-10 04:17:25 +0000

    [plan] Update gzip to build in a bldr Studio.

--- plans/gzip/plan.sh
+++ plans/gzip/plan.sh
@@ -6,7 +6,7 @@ pkg_license=('gplv3+')
 pkg_source=http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz
 pkg_shasum=37dfed1a485d53212c43b3fa2a7c7952f09bf5cd86e37121c222341ee1b27847
 pkg_deps=(chef/glibc chef/less chef/grep)
-pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/bison chef/flex chef/bash chef/gawk chef/libtool chef/diffutils chef/findutils chef/xz)
+pkg_build_deps=(chef/coreutils chef/diffutils chef/patch chef/make chef/gcc chef/sed chef/xz)
 pkg_binary_path=(bin)
 pkg_gpg_key=3853DA6B
 
@@ -33,3 +33,15 @@ do_build() {
 do_check() {
   make check
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/diffutils chef/xz)
+fi
