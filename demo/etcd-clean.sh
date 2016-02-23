657157dcf5a1ef21c2f00191ffcc70436fd7ef26
Julian C. Dunn <jdunn@chef.io>
2016-02-22 22:21:53 -0500

    Remove convenience scripts having to do with old etcd implementation

--- demo/etcd-clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-etcd_ipaddress=$(docker-machine ip ${DOCKER_MACHINE_NAME:-default})
-curl -X DELETE http://$etcd_ipaddress:4001/v2/keys/bldr?recursive=true
--- demo/etcd-load.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-etcd_ipaddress=$(docker-machine ip ${DOCKER_MACHINE_NAME:-default})
-foo=$(cat $1); curl -L http://$etcd_ipaddress:4001/v2/keys/bldr/redis/default/config -XPUT -d value="${foo}"
