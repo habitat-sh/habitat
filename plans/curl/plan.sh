c7ecca292a0751bdcc26d5b9fed3fa4de8ba276e
jtimberman <joshua@chef.io>
2016-02-29 20:47:46 -0700

    node and phantomjs, together at last

---
+++ plans/curl/plan.sh
@@ -0,0 +1,33 @@
+pkg_name=curl
+pkg_version=7.47.1
+pkg_origin=chef
+pkg_license=('curl')
+pkg_source=https://curl.haxx.se/download/${pkg_name}-${pkg_version}.tar.gz
+pkg_filename=${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=4e9d85028e754048887505a73638bf9b254c39582a191f43c95fe7de8e4d8005
+pkg_gpg_key=3853DA6B
+pkg_deps=(chef/glibc chef/openssl chef/zlib)
+pkg_build_deps=(chef/gcc chef/make chef/coreutils chef/perl)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+pkg_binary_path=(bin)
+
+do_build() {
+    fix_interpreter scripts/zsh.pl chef/perl bin/perl
+    ./configure --prefix=${pkg_prefix} \
+                --with-ssl=$(pkg_path_for chef/openssl) \
+                --with-zlib=$(pkg_path_for chef/zlib) \
+                --disable-manual \
+                --disable-ldap \
+                --disable-ldaps \
+                --disable-rtsp \
+                --enable-proxy \
+                --enable-optimize \
+                --disable-dependency-tracking \
+                --enable-ipv6 \
+                --without-libidn \
+                --without-gnutls \
+                --without-librtmp
+    make
+    make install
+}
--- plans/node/plan.sh
+++ plans/node/plan.sh
@@ -9,16 +9,20 @@ pkg_source=https://nodejs.org/dist/v${pkg_version}/${pkg_name}-v${pkg_version}.t
 pkg_dirname=node-v${pkg_version}
 pkg_shasum=ea5e357db8817052b17496d607c719d809ed1383e8fcf7c8ffc5214e705aefdd
 pkg_gpg_key=3853DA6B
-pkg_deps=(chef/glibc chef/gcc-libs)
-pkg_build_deps=(chef/python2 chef/gcc chef/make chef/coreutils)
+pkg_deps=(chef/glibc chef/gcc-libs chef/coreutils/8.24/20160223204924)
+pkg_build_deps=(chef/python2 chef/gcc chef/make)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 pkg_binary_path=(bin)
 pkg_interpreters=(bin/node)
 
 do_build() {
-    fix_interpreter configure chef/coreutils bin/env
+  # Node ships a lot of scripts that hardcode `/usr/bin/env`, so we
+  # need to fix that everywhere.
+  for target in `find . -type f -exec grep -l '^\#\!/usr/bin/env' {} \;`; do
+    fix_interpreter ${target} chef/coreutils bin/env
+  done
 
-    # use --without-snapshot, because https://github.com/nodejs/node/issues/4212
-    ./configure --prefix=${pkg_prefix} --without-snapshot && make && make install
+  # use --without-snapshot, because https://github.com/nodejs/node/issues/4212
+  ./configure --prefix=${pkg_prefix} --without-snapshot && make && make install
 }
