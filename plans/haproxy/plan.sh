29f8144da1faad88e9d8f6895619110cb5c2fe4a
Julian C. Dunn <jdunn@chef.io>
2016-03-04 12:07:50 -0500

    Added required build_deps for haproxy

--- plans/haproxy/plan.sh
+++ plans/haproxy/plan.sh
@@ -7,6 +7,7 @@ pkg_source=http://www.haproxy.org/download/1.5/src/${pkg_name}-${pkg_version}.ta
 pkg_shasum=6648dd7d6b958d83dd7101eab5792178212a66c884bec0ebcd8abc39df83bb78
 pkg_gpg_key=3853DA6B
 pkg_binary_path=(bin)
+pkg_build_deps=(chef/make chef/gcc)
 pkg_deps=(chef/glibc chef/pcre chef/openssl chef/zlib)
 pkg_service_run="bin/haproxy -f /opt/bldr/srvc/haproxy/config/haproxy.conf"
 pkg_docker_build="auto"
