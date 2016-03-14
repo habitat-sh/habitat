bc71342530fb47c391c2c7a673810f9811c61f85
Julian C. Dunn <jdunn@chef.io>
2016-03-13 23:25:15 -0400

    Update to new depot API by ensuring there's a SHA256 of the package posted.

--- plans/support/cheap-upload.sh
+++ plans/support/cheap-upload.sh
@@ -5,6 +5,7 @@ if [ -n "${DEBUG:-}" ]; then set -x; fi
 pkg_file="$1"
 pkg_ident="$(gpg --decrypt $pkg_file 2>/dev/null \
   | tar xOf - --wildcards --no-anchored 'IDENT')"
+pkg_sha="$(openssl dgst -sha256 $pkg_file | awk '{ print $2 }')"
 
 set -x
-${WGET:-wget} --post-file=$pkg_file -O- $BLDR_REPO/pkgs/$pkg_ident
+${WGET:-wget} --post-file=$pkg_file -O- $BLDR_REPO/pkgs/$pkg_ident?checksum=$pkg_sha
