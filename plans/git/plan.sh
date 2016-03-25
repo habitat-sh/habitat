1277058b25a80c34cbf7bb3cc505ddfa06253311
jtimberman <joshua@chef.io>
2016-03-25 13:18:51 -0600

    [plans] add a plan for git

---
+++ plans/git/plan.sh
@@ -0,0 +1,23 @@
+pkg_name=git
+pkg_version=2.7.4
+pkg_origin=chef
+pkg_license=('gplv2')
+pkg_source=https://www.kernel.org/pub/software/scm/git/${pkg_name}-${pkg_version}.tar.gz
+pkg_filename=${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=7104c4f5d948a75b499a954524cb281fe30c6649d8abe20982936f75ec1f275b
+pkg_gpg_key=3853DA6B
+pkg_deps=(chef/glibc chef/zlib chef/perl chef/curl chef/gettext chef/expat chef/cacerts)
+pkg_build_deps=(chef/make chef/gcc)
+pkg_lib_dirs=(lib)
+pkg_include_dirs=(include)
+pkg_binary_path=(bin)
+
+do_prepare() {
+  _perl_path="$(pkg_path_for chef/perl)/bin/perl"
+  sed -e "s#/usr/bin/perl#${_perl_path}#g" -i Makefile
+}
+
+do_build() {
+  ./configure --prefix=${pkg_prefix}
+  make
+}
