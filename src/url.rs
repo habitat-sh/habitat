0a466fe7d67a2801521c4f3def3380ce5275f74e
Fletcher Nichol <fnichol@nichol.ca>
2016-04-02 23:27:14 -0600

    Initial implementation of `hap pkg install`.

--- src/url.rs
+++ src/url.rs
@@ -4,21 +4,5 @@
 // this file ("Licensee") apply to Licensee's use of the Software until such time that the Software
 // is made available under an open source license such as the Apache 2.0 License.
 
-extern crate crypto;
-extern crate gpgme;
-#[macro_use]
-extern crate lazy_static;
-extern crate libarchive;
-#[macro_use]
-extern crate log;
-extern crate regex;
-extern crate rustc_serialize;
-
-pub use self::error::{Error, Result};
-
-pub mod error;
-pub mod fs;
-pub mod gpg;
-pub mod package;
-pub mod url;
-pub mod util;
+/// Default Depot URL
+pub const DEFAULT_DEPOT_URL: &'static str = "http://52.37.151.35:9632";
