a33fb2fcaa92704d83e19a4df53030ec1eba4a01
Fletcher Nichol <fnichol@nichol.ca>
2016-04-12 12:34:02 -0600

    [plan] Add multi-target support to `chef/rust`.
    Namely, this adds support for the x86_64-unknown-linux-musl rustc
    target. If more targets are required in the future, it's all just
    arrays, yay!

--- plans/rust/plan.sh
+++ plans/rust/plan.sh
@@ -2,7 +2,8 @@ pkg_name=rust
 pkg_origin=chef
 pkg_version=1.7.0
 pkg_license=('Apache-2.0' 'MIT')
-pkg_source=https://static.rust-lang.org/dist/${pkg_name}-${pkg_version}-x86_64-unknown-linux-gnu.tar.gz
+_url_base=http://static.rust-lang.org/dist
+pkg_source=$_url_base/${pkg_name}-${pkg_version}-x86_64-unknown-linux-gnu.tar.gz
 pkg_dirname=${pkg_name}-${pkg_version}-x86_64-unknown-linux-gnu
 pkg_shasum=d36634bd8df3d7565487b70af03dfda1c43c635cd6f2993f47cd61fda00d890a
 pkg_bin_dirs=(bin)
@@ -10,27 +11,77 @@ pkg_lib_dirs=(lib)
 pkg_deps=(chef/glibc chef/gcc-libs chef/zlib chef/gcc chef/cacerts)
 pkg_build_deps=(chef/patchelf chef/findutils chef/coreutils)
 
+_target_sources=(
+  $_url_base/${pkg_name}-std-${pkg_version}-x86_64-unknown-linux-musl.tar.gz
+)
+
+_target_shasums=(
+  326b90727bec7b91515184683c9ae6dac76cf621b7d50dbe236fd0c62260f82b
+)
+
+do_download() {
+  do_default_download
+
+  # Download all target sources, providing the corresponding shasums so we can
+  # skip re-downloading if already present and verified
+  for i in $(seq 0 $((${#_target_sources[@]} - 1))); do
+    p="${_target_sources[$i]}"
+    download_file $p $(basename $p) ${_target_shasums[$i]}
+  done; unset i p
+}
+
+do_verify() {
+  do_default_verify
+
+  # Verify all target sources against their shasums
+  for i in $(seq 0 $((${#_target_sources[@]} - 1))); do
+    verify_file $(basename ${_target_sources[$i]}) ${_target_shasums[$i]}
+  done; unset i
+}
+
+do_unpack() {
+  do_default_unpack
+
+  pushd $HAB_CACHE_SRC_PATH/$pkg_dirname > /dev/null
+    # Unpack all targets inside the main source directory
+    for i in $(seq 0 $((${#_target_sources[@]} - 1))); do
+      tar xf $HAB_CACHE_SRC_PATH/$(basename ${_target_sources[$i]})
+    done; unset i
+  popd > /dev/null
+}
+
 do_build() {
   return 0
 }
 
 do_install() {
   ./install.sh --prefix=$pkg_prefix --disable-ldconfig
-  patchelf --interpreter "$(pkg_path_for chef/glibc)/lib/ld-linux-x86-64.so.2" \
-           --set-rpath "$LD_RUN_PATH" \
-           "$pkg_prefix/bin/rustc"
-  patchelf --interpreter "$(pkg_path_for chef/glibc)/lib/ld-linux-x86-64.so.2" \
-           --set-rpath "$LD_RUN_PATH" \
-           "$pkg_prefix/bin/cargo"
-  patchelf --interpreter "$(pkg_path_for chef/glibc)/lib/ld-linux-x86-64.so.2" \
-           --set-rpath "$LD_RUN_PATH" \
-           "$pkg_prefix/bin/rustdoc"
 
+  # Update the dynamic linker & set `RUNPATH` for all ELF binaries under `bin/`
+  for b in rustc cargo rustdoc; do
+    patchelf \
+      --interpreter "$(pkg_path_for chef/glibc)/lib/ld-linux-x86-64.so.2" \
+      --set-rpath "$LD_RUN_PATH" \
+      "$pkg_prefix/bin/$b"
+  done; unset b
 
   # Going to want to write a cargo wrapper
   #    SSL_CERT_FILE=$(pkg_path_for chef/cacerts)/ssl/cert.pem \
 
-  find $pkg_prefix/lib -name *.so | xargs -I '%' patchelf --set-rpath "$LD_RUN_PATH" %
+    # Set `RUNPATH` for all shared libraries under `lib/`
+  find $pkg_prefix/lib -name *.so \
+    | xargs -I '%' patchelf \
+      --set-rpath "$LD_RUN_PATH" \
+      %
+
+  # Install all targets
+  for i in $(seq 0 $((${#_target_sources[@]} - 1))); do
+    local dir="$(basename ${_target_sources[$i]/%.tar.gz/})"
+    pushd $HAB_CACHE_SRC_PATH/$pkg_dirname/$dir > /dev/null
+      build_line "Installing $dir target for Rust"
+      ./install.sh --prefix=$($pkg_prefix/bin/rustc --print sysroot)
+    popd > /dev/null
+  done; unset i
 }
 
 do_strip() {
