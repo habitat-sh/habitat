3ae61213d5a1cec38f94093bd75f356ed0383c55
Fletcher Nichol <fnichol@nichol.ca>
2016-04-15 00:51:48 +0000

    [plans] Add stage1 `pkg_build_deps` to new base packages.

--- plans/libarchive-musl/plan.sh
+++ plans/libarchive-musl/plan.sh
@@ -11,3 +11,15 @@ do_prepare() {
   dynamic_linker="$(pkg_path_for musl)/lib/ld-musl-x86_64.so.1"
   LDFLAGS="$LDFLAGS -Wl,--dynamic-linker=$dynamic_linker"
 }
+
+
+# ----------------------------------------------------------------------------
+# **NOTICE:** What follows are implementation details required for building a
+# first-pass, "stage1" toolchain and environment. It is only used when running
+# in a "stage1" Studio and can be safely ignored by almost everyone. Having
+# said that, it performs a vital bootstrapping process and cannot be removed or
+# significantly altered. Thank you!
+# ----------------------------------------------------------------------------
+if [[ "$STUDIO_TYPE" = "stage1" ]]; then
+  pkg_build_deps=(chef/gcc chef/coreutils chef/sed chef/grep chef/diffutils chef/make)
+fi
