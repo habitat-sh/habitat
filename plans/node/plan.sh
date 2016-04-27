d9ecbc7f7af0a9cde22b121dda58d9c4e2a62786
Fletcher Nichol <fnichol@nichol.ca>
2016-04-27 10:10:14 -0600

    [plan] Update node Plan to remove `fix_interpreter` & update styles.
    This change updates/fixes/remove the following:
    
    * Remove `fix_interpreter()` helper call in favor of 2 strategies. 1) at
      build time, symlink in `/usr/bin/env` so that the hardcoded
      dependencies in the codebase just work. 2) at install time update all
      shebangs under `$pkg_prefix` which use a `#!/usr/bin/env node` shebang
      trick to directly use the full path to the node binary.
    * Remove the runtime dependency on a pinned version of `core/coreutils`
      due to changes above.
    * Add maintainer metadata
    * Re-order the metadata to be more consistent with other Plans.
    * Remove the call to `make install` in the `do_build()` phase which
      should be in the `do_install()` phase. However, since this is default
      behavior, we can remove it and prevent this Plan from
      double-installing (which it was doing previously).
    
    Pull request: #449
    Approved by: adamhjk

--- plans/node/plan.sh
+++ plans/node/plan.sh
@@ -8,20 +8,41 @@ pkg_source=https://nodejs.org/dist/v${pkg_version}/${pkg_name}-v${pkg_version}.t
 # pkg_version is node-4.2.6 (without the v). This tweak makes build happy
 pkg_dirname=node-v${pkg_version}
 pkg_shasum=ea5e357db8817052b17496d607c719d809ed1383e8fcf7c8ffc5214e705aefdd
-pkg_deps=(core/glibc core/gcc-libs core/coreutils/8.24/20160223204924)
-pkg_build_deps=(core/python2 core/gcc core/make)
-pkg_lib_dirs=(lib)
-pkg_include_dirs=(include)
+pkg_deps=(core/glibc core/gcc-libs)
+pkg_build_deps=(core/coreutils core/python2 core/gcc core/make)
 pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
 pkg_interpreters=(bin/node)
+pkg_lib_dirs=(lib)
+
+do_prepare() {
+  # The `/usr/bin/env` path is hardcoded, so we'll add a symlink if needed.
+  if [[ ! -r /usr/bin/env ]]; then
+    ln -sv $(pkg_path_for coreutils)/bin/env /usr/bin/env
+    _clean_env=true
+  fi
+}
 
 do_build() {
-  # Node ships a lot of scripts that hardcode `/usr/bin/env`, so we
-  # need to fix that everywhere.
-  for target in `find . -type f -exec grep -l '^\#\!/usr/bin/env' {} \;`; do
-    fix_interpreter ${target} core/coreutils bin/env
-  done
-
   # use --without-snapshot, because https://github.com/nodejs/node/issues/4212
-  ./configure --prefix=${pkg_prefix} --without-snapshot && make && make install
+  ./configure --prefix=${pkg_prefix} --without-snapshot
+  make
+}
+
+do_install() {
+  do_default_install
+
+  # Node produces a lot of scripts that hardcode `/usr/bin/env`, so we
+  # need to fix that everywhere to point directly at the node binary. There is
+  # no other good reason to add the env indirection trick on these shebangs.
+  grep -nrlI '^\#\!/usr/bin/env' $pkg_prefix | while read target; do
+    sed -e "s#\#\!/usr/bin/env node#\#\!${pkg_prefix}/bin/node#" -i $target
+  done
+}
+
+do_end() {
+  # Clean up the `env` link, if we set it up.
+  if [[ -n "$_clean_env" ]]; then
+    rm -fv /usr/bin/env
+  fi
 }
