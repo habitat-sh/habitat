4b0d8177ca7ea933f82cd3dd6d094c5558a9ea75
Fletcher Nichol <fnichol@nichol.ca>
2016-04-27 09:19:04 -0600

    [plan] Update curl Plan to remove `fix_interpreter` & update styles.
    This change updates/fixes/removes the following:
    
    * Remove the `fix_interpreter()` helper call on the zsh completion
      generating program. This is a build-time only concern and so the
      source code should be "patched" in the `do_prepare()` phase.
    * Add maintainer metadata
    * Fix intendation styling to 2 spaces (vs. 4 spaces)
    * Re-ordering of the metadata to be more consistent with other Plans
    * Remove the call to `make install` in build which should be in the
      `do_install()` phase. However, since this is default behavior, we can
      remove it and prevent this Plan from double-installing (which it was
      doing previously)
    
    Pull request: #449
    Approved by: adamhjk

--- plans/curl/plan.sh
+++ plans/curl/plan.sh
@@ -1,32 +1,35 @@
 pkg_name=curl
-pkg_version=7.47.1
 pkg_origin=core
+pkg_version=7.47.1
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('curl')
 pkg_source=https://curl.haxx.se/download/${pkg_name}-${pkg_version}.tar.gz
-pkg_filename=${pkg_name}-${pkg_version}.tar.gz
 pkg_shasum=4e9d85028e754048887505a73638bf9b254c39582a191f43c95fe7de8e4d8005
 pkg_deps=(core/glibc core/openssl core/zlib)
 pkg_build_deps=(core/gcc core/make core/coreutils core/perl)
-pkg_lib_dirs=(lib)
-pkg_include_dirs=(include)
 pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+
+do_prepare() {
+  # Patch the zsh-generating program to use our perl at build time
+  sed -i "s,/usr/bin/perl,$(pkg_path_for perl)/bin/perl,g" scripts/zsh.pl
+}
 
 do_build() {
-    fix_interpreter scripts/zsh.pl core/perl bin/perl
-    ./configure --prefix=${pkg_prefix} \
-                --with-ssl=$(pkg_path_for openssl) \
-                --with-zlib=$(pkg_path_for zlib) \
-                --disable-manual \
-                --disable-ldap \
-                --disable-ldaps \
-                --disable-rtsp \
-                --enable-proxy \
-                --enable-optimize \
-                --disable-dependency-tracking \
-                --enable-ipv6 \
-                --without-libidn \
-                --without-gnutls \
-                --without-librtmp
-    make
-    make install
+  ./configure --prefix=${pkg_prefix} \
+              --with-ssl=$(pkg_path_for openssl) \
+              --with-zlib=$(pkg_path_for zlib) \
+              --disable-manual \
+              --disable-ldap \
+              --disable-ldaps \
+              --disable-rtsp \
+              --enable-proxy \
+              --enable-optimize \
+              --disable-dependency-tracking \
+              --enable-ipv6 \
+              --without-libidn \
+              --without-gnutls \
+              --without-librtmp
+  make
 }
