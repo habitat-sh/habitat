2467442c910f95f75fbadf192122aac35e2a7db8
Jamie Winsor <jamie@vialstudios.com>
2016-04-27 15:56:12 -0700

    add ParseInto trait for parsing config fields into rust types
    Pull request: #454
    Approved by: adamhjk

--- src/config.rs
+++ src/config.rs
@@ -7,16 +7,19 @@
 use std;
 use std::fs::File;
 use std::io::Read;
+use std::net;
 use std::path::Path;
+use std::result;
+use std::str::FromStr;
 
 use toml;
 
-use error::Error;
+use error::{Error, Result};
 
 pub trait ConfigFile: Sized {
     type Error: std::error::Error + From<Error>;
 
-    fn from_file<T: AsRef<Path>>(filepath: T) -> Result<Self, Self::Error> {
+    fn from_file<T: AsRef<Path>>(filepath: T) -> result::Result<Self, Self::Error> {
         let mut file = match File::open(filepath.as_ref()) {
             Ok(f) => f,
             Err(e) => return Err(Self::Error::from(Error::ConfigFileIO(e))),
@@ -36,7 +39,66 @@ pub trait ConfigFile: Sized {
         }
     }
 
-    fn from_toml(toml: toml::Table) -> Result<Self, Self::Error>;
+    fn from_toml(toml: toml::Table) -> result::Result<Self, Self::Error>;
+}
+
+pub trait ParseInto<T> {
+    fn parse_into(&self, field: &'static str, out: &mut T) -> Result<bool>;
+}
+
+impl ParseInto<net::SocketAddrV4> for toml::Table {
+    fn parse_into(&self, field: &'static str, out: &mut net::SocketAddrV4) -> Result<bool> {
+        if let Some(val) = self.get(field) {
+            if let Some(v) = val.as_str() {
+                match net::SocketAddrV4::from_str(v) {
+                    Ok(addr) => {
+                        *out = addr;
+                        Ok(true)
+                    }
+                    Err(_) => Err(Error::ConfigInvalidSocketAddrV4(field, val.clone())),
+                }
+            } else {
+                Err(Error::ConfigInvalidSocketAddrV4(field, val.clone()))
+            }
+        } else {
+            Ok(false)
+        }
+    }
+}
+
+impl ParseInto<net::Ipv4Addr> for toml::Table {
+    fn parse_into(&self, field: &'static str, out: &mut net::Ipv4Addr) -> Result<bool> {
+        if let Some(val) = self.get(field) {
+            if let Some(v) = val.as_str() {
+                match net::Ipv4Addr::from_str(v) {
+                    Ok(addr) => {
+                        *out = addr;
+                        Ok(true)
+                    }
+                    Err(_) => Err(Error::ConfigInvalidIpv4Addr(field, val.clone())),
+                }
+            } else {
+                Err(Error::ConfigInvalidIpv4Addr(field, val.clone()))
+            }
+        } else {
+            Ok(false)
+        }
+    }
+}
+
+impl ParseInto<String> for toml::Table {
+    fn parse_into(&self, field: &'static str, out: &mut String) -> Result<bool> {
+        if let Some(val) = self.get(field) {
+            if let Some(v) = val.as_str() {
+                *out = v.to_string();
+                Ok(true)
+            } else {
+                Err(Error::ConfigInvalidString(field, val.clone()))
+            }
+        } else {
+            Ok(false)
+        }
+    }
 }
 
 fn format_errors(parser: &toml::Parser) -> String {
