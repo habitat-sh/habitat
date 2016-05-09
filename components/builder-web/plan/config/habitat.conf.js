af5580674bddc8f60ab286bda5ec04aa439ad269
Nathan L Smith <smith@chef.io>
2016-05-09 14:12:33 -0500

    Add publish script
    Run support/publish.sh if when we've merged to master. Builds and
    uploads the core/habitat-builder-web package.
    
    Also makes sure habitat.conf.js exists in the plan config files.
    
    Signed-off-by: Nathan L Smith <smith@chef.io>
    
    Pull request: #496
    Approved by: reset

---
+++ components/builder-web/plan/config/habitat.conf.js
@@ -0,0 +1,11 @@
+habitatConfig({
+    builder_url: "{{cfg.builder_url}}",
+    community_url: "{{cfg.community_url}}",
+    depot_url: "{{cfg.depot_url}}",
+    docs_url: "{{cfg.docs_url}}",
+    environment: "{{cfg.docs_url}}",
+    github_client_id: "{{cfg.github_client_id}}",
+    github_token_auth_url: "{{cfg.github_token_auth_url}}",
+    source_code_url: "{{cfg.source_code_url}}",
+    tutorials_url: "{{cfg.tutorials_url}}",
+});
