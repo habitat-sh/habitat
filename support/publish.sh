0786c900b2fa4af77c670472978fe67837527526
Nathan L Smith <smith@chef.io>
2016-05-11 15:22:40 -0500

    Fix publish command
    support/publish.sh was checking to see if the branch was "master", but
    it turns out it is "auto" once we're merged.
    
    Also clean up the script a little.
    
    Signed-off-by: Nathan L Smith <smith@chef.io>
    
    Pull request: #508
    Approved by: smith

--- support/publish.sh
+++ support/publish.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-set -x
+studio_path=/hab/studios/travis-build
+studio="hab-studio -r $studio_path"
 
-studio="hab-studio -r /hab/studios/travis-build"
-
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "auto" ]; then
+  set -x
   components/studio/install.sh
   $studio new
-  cp core-20160423193745.sig.key /hab/studios/travis-build/hab/cache/keys
+  cp core-20160423193745.sig.key $studio_path/hab/cache/keys
   $studio build components/builder-web/plan
   $studio run "hab artifact upload /hab/cache/artifacts/core-habitat-builder-web-*"
 else echo "Not on master; skipping publish"; fi
