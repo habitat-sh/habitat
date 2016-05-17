60d04a593ed72499200f208176a1fb179de9dbde
Fletcher Nichol <fnichol@nichol.ca>
2016-05-16 22:14:05 -0600

    [plan] Upgrade to libarchive 3.2.0.
    This happily resolves some tar extraction issues noted with our
    `core/hab-static` binary. Yay new releases!
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- plans/libarchive/plan.sh
+++ plans/libarchive/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=libarchive
 pkg_distname=$pkg_name
 pkg_origin=core
-pkg_version=3.1.2
+pkg_version=3.2.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('BSD')
 pkg_source=http://www.libarchive.org/downloads/${pkg_distname}-${pkg_version}.tar.gz
-pkg_shasum=eb87eacd8fe49e8d90c8fdc189813023ccc319c5e752b01fb6ad0cc7b2c53d5e
+pkg_shasum=7bce45fd71ff01dc20d19edd78322d4965583d81b8bed8e26cacb65d6f5baa87
 pkg_dirname=${pkg_distname}-${pkg_version}
 pkg_deps=(core/glibc core/openssl core/zlib core/bzip2 core/xz)
 pkg_build_deps=(core/gcc core/coreutils core/make)
