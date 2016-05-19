faa49851f9176f0e04eca2f4a66bb4644451d9d3
Fletcher Nichol <fnichol@nichol.ca>
2016-05-18 22:35:07 -0600

    [plans] Upgrade to Docker 1.11.1.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- plans/docker/plan.sh
+++ plans/docker/plan.sh
@@ -1,26 +1,21 @@
 pkg_name=docker
 pkg_origin=core
-pkg_version=1.10.1
+pkg_version=1.11.1
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2')
-pkg_source=https://get.docker.com/builds/Linux/x86_64/docker-1.10.1.tgz
-pkg_shasum=2287bc5cbcd1cdad77f1c0c70c2b5b15f1d9c010900c3ffab059fb46fe81d141
+pkg_source=https://get.docker.com/builds/Linux/x86_64/${pkg_name}-${pkg_version}.tgz
+pkg_shasum=893e3c6e89c0cd2c5f1e51ea41bc2dd97f5e791fcfa3cee28445df277836339d
+pkg_dirname=docker
 pkg_deps=()
 pkg_build_deps=()
 pkg_bin_dirs=(bin)
-
-do_unpack() {
-  pushd $HAB_CACHE_SRC_PATH
-  mkdir -p $pkg_dirname
-  tar xf $pkg_filename -C $HAB_CACHE_SRC_PATH/$pkg_dirname
-}
 
 do_build() {
   return 0
 }
 
 do_install() {
-  mkdir -p $pkg_prefix/bin
-  cp $HAB_CACHE_SRC_PATH/$pkg_dirname/usr/local/bin/docker $pkg_prefix/bin
-  chmod a+x $pkg_prefix/bin/docker
+  for bin in docker $(ls -1 docker-*); do
+    install -v -D $bin $pkg_prefix/bin/$bin
+  done
 }
