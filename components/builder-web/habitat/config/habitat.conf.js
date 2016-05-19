3d2ccfb46fde063ba8771a54e62ecacd826a6ca5
Nathan L Smith <smith@chef.io>
2016-05-19 16:32:56 -0500

    Fixes for builder-web deploy
    * Make it so nginx redirects to the http hostname, not localhost
    * Fix typo in habitat.conf.js template
    
    Signed-off-by: Nathan L Smith <smith@chef.io>
    
    Pull request: #543
    Approved by: fnichol

--- components/builder-web/habitat/config/habitat.conf.js
+++ components/builder-web/habitat/config/habitat.conf.js
@@ -3,7 +3,7 @@ habitatConfig({
     community_url: "{{cfg.community_url}}",
     depot_url: "{{cfg.depot_url}}",
     docs_url: "{{cfg.docs_url}}",
-    environment: "{{cfg.docs_url}}",
+    environment: "{{cfg.environment}}",
     github_client_id: "{{cfg.github_client_id}}",
     github_token_auth_url: "{{cfg.github_token_auth_url}}",
     source_code_url: "{{cfg.source_code_url}}",
