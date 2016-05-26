be05a44359bf96348f373e0580128a2dd28b36b3
Kevin J. Dickerson <kevin.dickerson@loom.technology>
2016-05-26 09:22:36 -0700

    Add plan for the which command
    Signed-off-by: Kevin J. Dickerson <kevin.dickerson@loom.technology>
    
    Pull request: #563
    Approved by: reset

---
+++ plans/which/plan.sh
@@ -0,0 +1,11 @@
+pkg_origin=core
+pkg_name=which
+pkg_version=2.21
+pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
+pkg_license=('GPL-3.0')
+pkg_source=http://ftp.gnu.org/gnu/which/which-2.21.tar.gz
+pkg_shasum=f4a245b94124b377d8b49646bf421f9155d36aa7614b6ebf83705d3ffc76eaad
+pkg_deps=()
+pkg_build_deps=(core/make core/gcc)
+pkg_bin_dirs=(bin)
+
