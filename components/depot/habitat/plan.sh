162dd892330105a563921a610abe8c4f69d0ea83
Fletcher Nichol <fnichol@nichol.ca>
2016-05-27 18:18:56 -0600

    Release 0.6.0, now with more "bldr".
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- components/depot/habitat/plan.sh
+++ components/depot/habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=hab-depot
 pkg_origin=core
-pkg_version=0.5.0
+pkg_version=0.6.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('apachev2')
 pkg_source=nosuchfile.tar.gz
