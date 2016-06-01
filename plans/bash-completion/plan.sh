31298af87efb8b34d938b39c3ae97338bb4ecb25
Dave Parfitt <dparfitt@chef.io>
2016-06-01 12:11:57 -0400

    add bash completion for hab command
    Signed-off-by: Dave Parfitt <dparfitt@chef.io>
    
    Pull request: #698
    Approved by: reset

---
+++ plans/bash-completion/plan.sh
@@ -0,0 +1,11 @@
+pkg_name=bash-completion
+pkg_origin=core
+pkg_version=2.3
+pkg_license=('GPLv2')
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_source=https://github.com/scop/bash-completion/releases/download/${pkg_version}/bash-completion-${pkg_version}.tar.xz
+pkg_shasum=b2e081af317f3da4fff3a332bfdbebeb5514ebc6c2d2a9cf781180acab15e8e9
+pkg_bin_dirs=(bin)
+pkg_build_deps=(core/make core/gcc core/autoconf core/automake)
+pkg_deps=(core/glibc)
+
