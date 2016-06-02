046678249d2411188b4c62f36c297e64b3b47c6a
Jamie Winsor <jamie@vialstudios.com>
2016-06-02 11:34:00 -0700

    Update license header in builder components
    Signed-off-by: Jamie Winsor <jamie@vialstudios.com>

--- components/builder-dbcache/src/data_store.rs
+++ components/builder-dbcache/src/data_store.rs
@@ -1,8 +1,9 @@
-// Copyright:: Copyright (c) 2015-2016 Chef Software, Inc.
+// Copyright:: Copyright (c) 2015-2016 The Habitat Maintainers
 //
-// The terms of the Evaluation Agreement (Bldr) between Chef Software Inc. and the party accessing
-// this file ("Licensee") apply to Licensee's use of the Software until such time that the Software
-// is made available under an open source license such as the Apache 2.0 License.
+// The terms of the Evaluation Agreement (Habitat) between Chef Software Inc.
+// and the party accessing this file ("Licensee") apply to Licensee's use of
+// the Software until such time that the Software is made available under an
+// open source license such as the Apache 2.0 License.
 
 //! Traits and Types for persisting protocol messages to a database.
 //!
--- components/builder-protocol/src/routesrv.rs
+++ components/builder-protocol/src/routesrv.rs
@@ -1,7 +1,8 @@
-// Copyright:: Copyright (c) 2015-2016 Chef Software, Inc.
+// Copyright:: Copyright (c) 2015-2016 The Habitat Maintainers
 //
-// The terms of the Evaluation Agreement (Bldr) between Chef Software Inc. and the party accessing
-// this file ("Licensee") apply to Licensee's use of the Software until such time that the Software
-// is made available under an open source license such as the Apache 2.0 License.
+// The terms of the Evaluation Agreement (Habitat) between Chef Software Inc.
+// and the party accessing this file ("Licensee") apply to Licensee's use of
+// the Software until such time that the Software is made available under an
+// open source license such as the Apache 2.0 License.
 
 pub use message::routesrv::*;
