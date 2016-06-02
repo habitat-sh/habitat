e8503629b6d262f6dd6ba6adf72c582df6204630
Fletcher Nichol <fnichol@nichol.ca>
2016-06-01 21:39:33 -0600

    [sup] Add a Plan for a statically built Supervisor ;)
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- components/sup/plan.sh
+++ components/sup/plan.sh
@@ -1,4 +1,5 @@
 pkg_name=hab-sup
+pkg_distname=$pkg_name
 pkg_origin=core
 pkg_version=0.6.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
@@ -13,7 +14,9 @@ pkg_bin_dirs=(bin)
 
 program=$pkg_name
 
-do_prepare() {
+_common_prepare() {
+  do_default_prepare
+
   # Used by the `build.rs` program to set the version of the binaries
   export PLAN_VERSION="${pkg_version}/${pkg_release}"
   build_line "Setting PLAN_VERSION=$PLAN_VERSION"
@@ -21,6 +24,16 @@ do_prepare() {
   # Used by Cargo to fetch registries/crates/etc.
   export SSL_CERT_FILE=$(pkg_path_for cacerts)/ssl/cert.pem
   build_line "Setting SSL_CERT_FILE=$SSL_CERT_FILE"
+
+  # Used to find libgcc_s.so.1 when compiling `build.rs` in dependencies. Since
+  # this used only at build time, we will use the version found in the gcc
+  # package proper--it won't find its way into the final binaries.
+  export LD_LIBRARY_PATH=$(pkg_path_for gcc)/lib
+  build_line "Setting LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+}
+
+do_prepare() {
+  _common_prepare
 
   export rustc_target="x86_64-unknown-linux-gnu"
   build_line "Setting rustc_target=$rustc_target"
