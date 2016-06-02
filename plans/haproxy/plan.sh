929daf8605a4eedd8af97083ad85f8e834dd6035
Kevin J. Dickerson <kevin.dickerson@loom.technology>
2016-06-02 08:24:04 -0700

    Rewrite haproxy plan
    Pull request: #626
    Approved by: reset

--- plans/haproxy/plan.sh
+++ plans/haproxy/plan.sh
@@ -1,30 +1,36 @@
-pkg_name=haproxy
 pkg_origin=core
-pkg_version=1.5.12
-pkg_license=('BSD')
-pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source=http://www.haproxy.org/download/1.5/src/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=6648dd7d6b958d83dd7101eab5792178212a66c884bec0ebcd8abc39df83bb78
+pkg_name=haproxy
+pkg_version=1.6.5
+pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
+pkg_license=('GPL-2.0', 'LGPL-2.1')
+pkg_source=http://www.haproxy.org/download/1.6/src/haproxy-1.6.5.tar.gz
+pkg_shasum=c4b3fb938874abbbbd52782087117cc2590263af78fdce86d64e4a11acfe85de
+pkg_service_run='bin/haproxy -f config/haproxy.conf -db'
+pkg_expose=(8080)
+pkg_deps=(core/zlib core/pcre core/openssl)
+pkg_build_deps=(
+  core/coreutils
+  core/gcc
+  core/pcre
+  core/make
+  core/openssl
+  core/zlib
+)
+
 pkg_bin_dirs=(bin)
-pkg_build_deps=(core/make core/gcc)
-pkg_deps=(core/glibc core/pcre core/openssl core/zlib)
-pkg_service_run="bin/haproxy -f $pkg_svc_config_path/haproxy.conf"
-pkg_docker_build="auto"
-pkg_expose=(80 443)
 
 do_build() {
   make USE_PCRE=1 \
-    USE_PCRE_JIT=1 \
-    CPU=x86_64 \
-    TARGET=linux2628 \
-    USE_OPENSSL=1 \
-    USE_ZLIB=1 \
-    ADDINC="$CFLAGS" \
-    ADDLIB="$LDFLAGS"
+       USE_PCRE_JIT=1 \
+       CPU=native \
+       TARGET=linux2628 \
+       USE_OPENSSL=1 \
+       USE_ZLIB=1 \
+       ADDINC="$CFLAGS" \
+       ADDLIB="$LDFLAGS"
 }
 
 do_install() {
   mkdir -p $pkg_prefix/bin
   cp haproxy $pkg_prefix/bin
 }
-
