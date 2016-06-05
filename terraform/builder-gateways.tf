ff397dee5063955495a7c782a4b86882dffcf34a
Jamie Winsor <jamie@vialstudios.com>
2016-06-05 14:08:06 -0700

    Add Builder Terraform Components
    Signed-off-by: Jamie Winsor <jamie@vialstudios.com>

---
+++ terraform/builder-gateways.tf
@@ -0,0 +1,144 @@
+resource "aws_instance" "builder_api" {
+    ami           = "${lookup(var.aws_ami, var.aws_region)}"
+    instance_type = "t2.medium"
+    key_name      = "${var.aws_key_pair}"
+    subnet_id     = "${var.public_subnet_id}"
+    count         = "${var.rest_api_count}"
+
+    vpc_security_group_ids = [
+        "${var.aws_admin_sg}",
+        "${var.hab_sup_sg}",
+        "${aws_security_group.builder_api.id}",
+        "${aws_security_group.router_gateway.id}",
+    ]
+
+    connection {
+        // JW TODO: switch to private ip after VPN is ready
+        host        = "${self.public_ip}"
+        user        = "ubuntu"
+        private_key = "${file("${var.connection_private_key}")}"
+        agent       = "${var.connection_agent}"
+    }
+
+    # JW TODO: Bake AMIs with updated habitat on them instead of bootstrapping
+    provisioner "remote-exec" {
+        script = "${path.module}/scripts/bootstrap.sh"
+    }
+
+    provisioner "file" {
+        source      = "${path.module}/files/hab-director.service"
+        destination = "/home/ubuntu/hab-director.service"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo mv /home/ubuntu/hab-director.service /etc/systemd/system/hab-director.service",
+            "sudo mkdir -p /hab/etc/director",
+            "cat <<BODY > /tmp/director-config.toml",
+            "${template_file.gateway_director.rendered}",
+            "BODY",
+            "sudo mv /tmp/director-config.toml /hab/etc/director/config.toml",
+            "sudo systemctl daemon-reload",
+            "sudo systemctl start hab-director",
+            "sudo systemctl enable hab-director",
+        ]
+    }
+
+    tags {
+        Name          = "builder-api-${count.index}"
+        X-Contact     = "The Habitat Maintainers <humans@habitat.sh>"
+        X-Environment = "${var.env}"
+        X-Application = "builder"
+    }
+}
+
+resource "template_file" "gateway_director" {
+    template = "${file("${path.module}/templates/gateway-director.toml")}"
+
+    vars {
+        env     = "${var.env}"
+        peer_ip = "${aws_instance.router.0.private_ip}"
+    }
+}
+
+resource "aws_security_group" "builder_api_elb" {
+    name        = "builder-api-elb"
+    description = "Habitat Builder API Load Balancer"
+    vpc_id      = "${var.aws_vpc_id}"
+
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags {
+        X-Contact     = "The Habitat Maintainers <humans@habitat.sh>"
+        X-Environment = "${var.env}"
+        X-Application = "builder"
+    }
+}
+
+resource "aws_security_group" "builder_api" {
+    name        = "builder-api"
+    description = "For traffic to builder-api network services"
+    vpc_id      = "${var.aws_vpc_id}"
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port       = 9636
+        to_port         = 9636
+        protocol        = "tcp"
+        security_groups = [
+            "${aws_security_group.builder_api_elb.id}"
+        ]
+    }
+
+    tags {
+        X-Environment  = "${var.env}"
+        X-Contact      = "The Habitat Maintainers <humans@habitat.sh>"
+        X-Application = "builder"
+    }
+}
+
+resource "aws_elb" "builder_api" {
+    name            = "builder-api"
+    security_groups = ["${aws_security_group.builder_api_elb.id}"]
+    subnets         = ["${var.public_subnet_id}"]
+    instances       = ["${aws_instance.builder_api.*.id}"]
+
+    listener {
+        instance_port      = 9636
+        instance_protocol  = "HTTP"
+        lb_port            = 443
+        lb_protocol        = "HTTPS"
+        ssl_certificate_id = "${var.ssl_certificate_arn}"
+    }
+
+    health_check {
+        healthy_threshold   = 10
+        unhealthy_threshold = 2
+        timeout             = 5
+        target              = "HTTP:9636/v1/status"
+        interval            = 30
+    }
+
+    tags {
+        X-Environment = "${var.env}"
+        X-Application = "builder"
+    }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,70 @@
+variable "env" {
+    description = "Name of logical server environment for network"
+}
+
+variable "dns_zone_id" {
+    description = "DNS Zone for all network"
+}
+
+variable "aws_ami" {
+    description = "Base AMI for all nodes"
+    default = {
+        us-west-2 = "ami-06b94666"
+    }
+}
+
+variable "aws_key_pair" {
+    description = "AWS Key Pair name for instances"
+}
+
 variable "aws_region" {
-  description = "AWS region to launch servers."
-  default     = "us-west-2"
+    description = "AWS Region"
 }
 
-variable "rhel7_ami" {
-  # TODO: Make this a map
-  description = "RHEL 7 AMI in us-west-2"
-  default     = "ami-99bef1a9"
+variable "aws_vpc_id" {
+    description = "VPC resource id to place security groups into"
 }
 
+variable "aws_admin_sg" {
+    description = "Administration security group for all instances"
+}
+
+variable "hab_sup_sg" {
+    description = "Identifier for AWS security group for habitat supervisor connectivity"
+}
+
+variable "ssl_certificate_arn" {
+    description = "Amazon Resource Name (ARN) for the environment's ssl certificate"
+}
+
+variable "public_subnet_id" {
+    description = "Identifier for public AWS subnet"
+}
+
+variable "private_subnet_id" {
+    description = "Identifier for private AWS subnet"
+}
+
+variable "rest_api_count" {
+    description = "Number of Builder REST API servers to start"
+}
+
+variable "router_count" {
+    description = "Number of RouteSrv to start"
+}
+
+variable "service_count" {
+    description = "Number of service servers to start"
+}
+
+variable "jobsrv_worker_count" {
+    description = "Number of JobSrv workers to start"
+}
+
+variable "connection_agent" {
+    description = "Set to false to disable using ssh-agent to authenticate"
+}
+
+variable "connection_private_key" {
+    description = "File path to AWS keypair private key"
+}
