fd49266b2d8d3d2b74a3dfee5b738f5b39412cad
Fletcher Nichol <fnichol@nichol.ca>
2016-06-09 13:04:43 -0600

    [plans] Upgrade to Docker 1.11.2.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>
    
    Pull request: #709
    Approved by: adamhjk

--- plans/docker/plan.sh
+++ plans/docker/plan.sh
@@ -1,10 +1,10 @@
 pkg_name=docker
 pkg_origin=core
-pkg_version=1.11.1
+pkg_version=1.11.2
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2')
 pkg_source=https://get.docker.com/builds/Linux/x86_64/${pkg_name}-${pkg_version}.tgz
-pkg_shasum=893e3c6e89c0cd2c5f1e51ea41bc2dd97f5e791fcfa3cee28445df277836339d
+pkg_shasum=8c2e0c35e3cda11706f54b2d46c2521a6e9026a7b13c7d4b8ae1f3a706fc55e1
 pkg_dirname=docker
 pkg_deps=()
 pkg_build_deps=()
