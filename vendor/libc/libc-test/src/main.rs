ef263d8d3e7164fd257d2f28279e79e5347b9731
Fletcher Nichol <fnichol@nichol.ca>
2016-06-10 22:24:47 -0600

    Bump to new libc release 0.2.12 & unvendor the crate.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- vendor/libc/libc-test/src/main.rs
+++
@@ -1,6 +0,0 @@
-#![allow(bad_style, improper_ctypes)]
-extern crate libc;
-
-use libc::*;
-
-include!(concat!(env!("OUT_DIR"), "/all.rs"));
--- vendor/libc/src/unix/notbsd/linux/musl/b64/powerpc64.rs
+++
@@ -1,1 +0,0 @@
-pub const SYS_perf_event_open: ::c_long = 319;
