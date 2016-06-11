386b58f2a769dcc69a5a6ff19448ff1591742f00
Fletcher Nichol <fnichol@nichol.ca>
2016-06-10 22:24:47 -0600

    Bump to new libc release 0.2.12 & unvendor the crate.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>
    
    Pull request: #735
    Approved by: fnichol

--- vendor/libc/libc-test/src/main-generated.rs
+++
@@ -1,9 +0,0 @@
-// Note that this main file is meant to mirror `main.rs` but is only used on the
-// BSDs where the generated location of `all.rs` is slightly different
-
-#![allow(bad_style, improper_ctypes)]
-extern crate libc;
-
-use libc::*;
-
-include!("../all.rs");
