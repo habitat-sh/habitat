09898a9007ccca695bf4140d1babc1f981f82b1e
Matt Ray <matthewhray@gmail.com>
2016-06-12 15:51:33 -0500

    update from 'apachev2' to 'Apache-2.0'
    Pull request: #772
    Approved by: juliandunn

--- components/sup/plan.sh
+++ components/sup/plan.sh
@@ -3,7 +3,7 @@ pkg_distname=$pkg_name
 pkg_origin=core
 pkg_version=0.6.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_license=('apachev2')
+pkg_license=('Apache-2.0')
 pkg_source=nosuchfile.tar.gz
 pkg_deps=(
   core/busybox-static
