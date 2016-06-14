2aad51fd896781887a28eb90d21515a0c170673e
Dave Parfitt <dparfitt@chef.io>
2016-06-14 00:08:50 -0700

    update copyright header Signed-off-by: Dave Parfitt <dparfitt@chef.io>

--- components/builder-web/app/packages-page/PackagesPageComponent.ts
+++ components/builder-web/app/packages-page/PackagesPageComponent.ts
@@ -1,9 +1,16 @@
-// Copyright:: Copyright (c) 2015-2016 The Habitat Maintainers
+// Copyright (c) 2016 Chef Software Inc. and/or applicable contributors
 //
-// The terms of the Evaluation Agreement (Habitat) between Chef Software Inc.
-// and the party accessing this file ("Licensee") apply to Licensee's use of
-// the Software until such time that the Software is made available under an
-// open source license such as the Apache 2.0 License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import {Control} from "angular2/common";
 import {Component, OnInit} from "angular2/core";
