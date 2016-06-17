d22198f15fb9a68668d6f0817812249e0e4db338
Spencer Owen <owenspencer@gmail.com>
2016-06-17 09:59:14 -0600

    Cleans up unneeded ant variables

--- plans/ant/plan.sh
+++ plans/ant/plan.sh
@@ -3,16 +3,10 @@ pkg_name=ant
 pkg_version=1.9.7
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=('Apache 2')
-pkg_source=http://apache.osuosl.org//ant/binaries/apache-ant-1.9.7-bin.tar.gz
+pkg_source=http://apache.osuosl.org//ant/binaries/apache-ant-${pkg_version}-bin.tar.gz
 pkg_shasum=1d0b808fe82cce9bcc167f8dbb7c7e89c1d7f7534c0d9c64bf615ec7c3e6bfe5
-pkg_distname=${pkg_name}
-pkg_dirname=apache-${pkg_distname}-${pkg_version}
-pkg_deps=(
-  core/server-jre
-)
-pkg_build_deps=()
-pkg_bin_dirs=(bin)
-
+pkg_deps=( core/server-jre )
+pkg_build_deps=( core/server-jre )
 
 do_build() {
   return 0
