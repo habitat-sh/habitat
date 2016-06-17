1fb2baf9a35f4aeee27bea590aec1691b63d68af
Spencer Owen <owenspencer@gmail.com>
2016-06-17 11:06:39 -0600

    Adds pkg_distname

--- plans/ant/plan.sh
+++ plans/ant/plan.sh
@@ -3,6 +3,7 @@ pkg_name=ant
 pkg_version=1.9.7
 pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
 pkg_license=('Apache 2')
+pkg_distname=$pkg_name
 pkg_source=http://apache.osuosl.org//ant/binaries/apache-ant-${pkg_version}-bin.tar.gz
 pkg_shasum=1d0b808fe82cce9bcc167f8dbb7c7e89c1d7f7534c0d9c64bf615ec7c3e6bfe5
 pkg_deps=( core/server-jre )
