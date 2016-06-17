78f7e6ee4e79b8b1f816ea0c1eca7ef0a5e3b67c
Spencer Owen <owenspencer@gmail.com>
2016-06-17 16:33:52 -0600

    Adds fix_interpreter for ant

--- plans/ant/plan.sh
+++ plans/ant/plan.sh
@@ -6,13 +6,15 @@ pkg_license=('Apache 2')
 pkg_distname=$pkg_name
 pkg_source=http://apache.osuosl.org//ant/binaries/apache-ant-${pkg_version}-bin.tar.gz
 pkg_shasum=1d0b808fe82cce9bcc167f8dbb7c7e89c1d7f7534c0d9c64bf615ec7c3e6bfe5
-pkg_deps=( core/jdk8 core/server-jre )
+pkg_deps=( core/coreutils core/jdk8 core/server-jre )
 pkg_build_deps=( core/jdk8 core/server-jre )
-
+pkg_bin_dirs=(bin)
 do_build() {
-  return 0
+  cp -a $PLAN_CONTEXT/apache-ant-${pkg_version}/. $pkg_prefix/
+  export JAVA_HOME=$(hab pkg path core/server-jre)
+  fix_interpreter ${pkg_prefix}/bin/ant core/coreutils bin/env
 }
 
 do_install() {
-  cp -a $PLAN_CONTEXT/apache-ant-${pkg_version}/. $pkg_prefix/
+  export JAVA_HOME=$(hab pkg path core/server-jre)
 }
