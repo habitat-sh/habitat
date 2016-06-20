4a3525b99df044669f15a921e2c35a3bd9f0c4d0
Blake Irvin <blake.irvin@gmail.com>
2016-06-20 23:24:08 +0200

    plan.sh for libev
    Signed-off-by: Blake Irvin <blake.irvin@gmail.com>
    
    Pull request: #935
    Approved by: metadave

---
+++ plans/libev/plan.sh
@@ -0,0 +1,12 @@
+pkg_origin=core
+pkg_name=libev
+pkg_version=4.22
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh"
+pkg_license=('MIT')
+pkg_source=http://dist.schmorp.de/libev/${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=736079e8ac543c74d59af73f9c52737b3bfec9601f020bf25a87a4f4d0f01bd6
+pkg_deps=(core/glibc)
+pkg_build_deps=(core/cacerts core/gcc core/make)
+pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
