58f075e1670d9d22ffbae95201225b7c31656de0
Blake Irvin <blake.irvin@gmail.com>
2016-06-20 20:11:08 +0200

    plan.sh for libevent
    Signed-off-by: Blake Irvin <blake.irvin@gmail.com>

---
+++ plans/libevent/plan.sh
@@ -0,0 +1,13 @@
+pkg_origin=core
+pkg_name=libevent
+pkg_version=2.0.22
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('MIT')
+pkg_source=https://github.com/${pkg_name}/${pkg_name}/releases/download/release-${pkg_version}-stable/${pkg_name}-${pkg_version}-stable.tar.gz
+pkg_shasum=71c2c49f0adadacfdbe6332a372c38cf9c8b7895bb73dabeaa53cdcc1d4e1fa3
+pkg_dirname=${pkg_name}-${pkg_version}-stable
+pkg_deps=(core/glibc)
+pkg_build_deps=(core/cacerts core/gcc core/make)
+pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
