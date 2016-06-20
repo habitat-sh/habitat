8a318314da6b189f05db8a6d3695f7c70bda0995
Samuel Cassiba <s@cassiba.com>
2016-06-20 10:51:34 -0700

    Correct pkg_include_dirs for core/libffi
    Signed-off-by: Samuel Cassiba <s@cassiba.com>
    
    Pull request: #930
    Approved by: smith

--- plans/libffi/plan.sh
+++ plans/libffi/plan.sh
@@ -9,7 +9,7 @@ pkg_shasum=d06ebb8e1d9a22d19e38d63fdb83954253f39bedc5d46232a05645685722ca37
 pkg_deps=(core/glibc core/libtool)
 pkg_build_deps=(core/coreutils core/make core/gcc)
 pkg_lib_dirs=(lib)
-pkg_include_dirs=(include)
+pkg_include_dirs=(lib/${pkg_name}-${pkg_version}/include)
 
 do_build() {
     ./configure --prefix=${pkg_prefix} --disable-multi-os-directory
