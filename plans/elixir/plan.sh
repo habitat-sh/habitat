19db4fdd2b0cbd3d7a6d807c21d3a82ada7c6e7a
Shane Sveller <shane@shanesveller.com>
2016-06-22 10:07:42 -0500

    Update Elixir plan for 1.3.0 release

--- plans/elixir/plan.sh
+++ plans/elixir/plan.sh
@@ -1,10 +1,10 @@
 pkg_origin=core
 pkg_name=elixir
-pkg_version=1.2.5
+pkg_version=1.3.0
 pkg_maintainer="Shane Sveller <shane@shanesveller.com>"
 pkg_license=('apachev2')
-pkg_source=https://github.com/elixir-lang/elixir/archive/v1.2.5.tar.gz
-pkg_shasum=8ed65722aeb55cbfe6022d77d0e36293d463d7b4922198f5c157e8286d896eee
+pkg_source=https://github.com/elixir-lang/elixir/archive/v1.3.0.tar.gz
+pkg_shasum=66cb8448dd60397cad11ba554c2613f732192c9026468cff55e8347a5ae4004a
 pkg_deps=(core/busybox core/cacerts core/coreutils core/openssl core/erlang/18.3)
 pkg_build_deps=(core/busybox core/cacerts core/coreutils core/make core/openssl core/erlang/18.3)
 pkg_bin_dirs=(bin)
