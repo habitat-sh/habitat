aab657e6374949d13dd1ff5fcbdb8c83b69f7d6c
jtimberman <joshua@chef.io>
2016-07-18 17:02:06 -0600

    Publish web site to acceptance
    This commit modifies the deploy_website.sh script to publish the web
    site to acceptance when we're on a pull request.
    
    Signed-off-by: jtimberman <joshua@chef.io>

--- support/ci/deploy_website.sh
+++ support/ci/deploy_website.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 git log HEAD~1..HEAD | grep -q '!!! Temporary Commit !!!'
 is_tmp_commit=$?
 
@@ -9,12 +8,17 @@ set -eu
 cd www
 ./bin/middleman build
 
-# If we are not on a pull request, on the "auto" branch (which homu uses when
-# auto-merging master), and not on a temporary commit, then run the publish
-# script.
+# If we are not on a pull request, and are on the "auto" branch that
+# homu uses when auto-merging to master, and are not on a temporary
+# commit, then publish to acceptance, and then production/live.
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] &&
    [ "${TRAVIS_BRANCH}" = "auto" ] &&
    [[ $is_tmp_commit = 1 ]]; then
+  AWS_BUCKET=habitat-www-acceptance ./bin/middleman s3_sync
   ./bin/middleman s3_sync
-  curl -H "Fastly-Key: ${FASTLY_API_KEY}" -X POST "https://api.fastly.com/service/${FASTLY_SERVICE_KEY}/purge_all"
-else echo "Not on master; skipping website deploy"; fi
+  # If we have published, then we need to purge the CDN cache.
+  curl -H "Fastly-Key: ${FASTLY_API_KEY}" \
+       -X POST "https://api.fastly.com/service/${FASTLY_SERVICE_KEY}/purge_all"
+else
+  echo "Not on master; skipping website deploy"
+fi
