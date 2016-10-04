94443f0b0c0db40e5caf473cba497eef1cc705fc
Jamie Winsor <jamie@vialstudios.com>
2016-10-04 16:07:07 -0700

    Remove vendored sodiumoxide and libsodium-sys from components
    These two vendored components had their necessary improvements merged
    into their master branches and pushed to crates.io, so we can drop our
    vendored code and switch over.
    
    Signed-off-by: Jamie Winsor <jamie@vialstudios.com>

--- components/sodiumoxide/libsodium-sys/src/core.rs
+++
@@ -1,5 +0,0 @@
-// core.h
-
-extern {
-    pub fn sodium_init() -> c_int;
-}
--- components/sodiumoxide/libsodium-sys/src/randombytes.rs
+++
@@ -1,6 +0,0 @@
-// randombytes.h
-
-extern {
-    pub fn randombytes_buf(buf: *mut u8,
-                           size: size_t);
-}
