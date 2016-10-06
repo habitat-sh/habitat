65e60a487d826d99e1295f7b1b8ebed3575b3b5a
Fletcher Nichol <fnichol@nichol.ca>
2016-10-05 19:31:22 -0600

    [ci] Drop requirement for nightly cargo installation.
    Now that the Travis platform is running the Rust 1.12.0 release, we no
    longer need to rely on installing nightly cargo for the workspace
    feature.
    
    Another ~1 min or so saved per run ;)
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- support/ci/install_cargo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -eu
-
-curl -s https://static.rust-lang.org/cargo-dist/cargo-nightly-x86_64-unknown-linux-gnu.tar.gz | tar xzm
-cargo-nightly-x86_64-unknown-linux-gnu/install.sh --prefix=$HOME
