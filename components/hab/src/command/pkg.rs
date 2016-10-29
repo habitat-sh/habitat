88b7ee8d0afe2e1791ecfaf572b23401d2014352
Andrew Hobden <andrew@hoverbear.org>
2016-10-30 00:27:11 +0200

    Remove unwraps from pkg::search::start.
    Falls back to versionless display when they don't exist.
    
    Signed-off-by: Andrew Hobden <andrew@hoverbear.org>

--- components/hab/src/command/pkg.rs
+++ components/hab/src/command/pkg.rs
@@ -375,11 +375,17 @@ pub mod search {
             0 => println!("No packages found that match '{}'", st),
             _ => {
                 for p in &packages {
-                    println!("{}/{}/{}/{}",
-                             p.origin,
-                             p.name,
-                             p.version.clone().unwrap(),
-                             p.release.clone().unwrap());
+                    if let (&Some(ref version), &Some(ref release)) = (&p.version, &p.release) {
+                        println!("{}/{}/{}/{}",
+                                 p.origin,
+                                 p.name,
+                                 version,
+                                 release);
+                    } else {
+                        println!("{}/{}",
+                                 p.origin,
+                                 p.name);
+                    }
                 }
                 if more {
                     println!("Search returned too many items, only showing the first {}",
