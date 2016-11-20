3d39ac9edf48d321136bea0d5fede81a7e226d55
Jamie Winsor <jamie@vialstudios.com>
2016-11-19 19:17:10 -0800

    fix syntax error in macos-pkg.sh
    Signed-off-by: Jamie Winsor <jamie@vialstudios.com>

--- support/ci/macos-pkg.sh
+++ support/ci/macos-pkg.sh
@@ -13,30 +13,30 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   cp /hab/pkgs/$pkg_ident/bin/hab $src_root/out/hab-x86_64-darwin/hab
   zip -9 -r $src_root/out/hab-x86_64-darwin.zip $src_root/out/hab-x86_64-darwin
 
-  cat <<- EOF > $src_root/out/hab-bintray.json
-  {
-    "package": {
-      "name": "hab-x86_64-darwin",
-      "repo": "unstable",
-      "subject": "habitat",
-      "desc": "",
-      "website_url": "https://www.habitat.sh",
-      "issue_tracker_url": "https://github.com/habitat-sh/habitat/issues",
-      "vcs_url": "https://github.com/habitat-sh/habitat",
-      "github_use_tag_release_notes": true,
-      "github_release_notes_file": "RELEASE.md",
-      "licenses": ["Apache-2.0"],
-      "labels": [],
-      "public_download_numbers": false,
-      "public_stats": false
-    },
-    "version": {
-      "name": "${pkg_version}-${pkg_release}"
-    },
-    "files": [
-      {"includePattern": "out/hab-x86_64-darwin.zip"}
-    ],
-    "publish": true
-  }
-  EOF
+cat <<- EOF > $src_root/out/hab-bintray.json
+{
+  "package": {
+    "name": "hab-x86_64-darwin",
+    "repo": "unstable",
+    "subject": "habitat",
+    "desc": "",
+    "website_url": "https://www.habitat.sh",
+    "issue_tracker_url": "https://github.com/habitat-sh/habitat/issues",
+    "vcs_url": "https://github.com/habitat-sh/habitat",
+    "github_use_tag_release_notes": true,
+    "github_release_notes_file": "RELEASE.md",
+    "licenses": ["Apache-2.0"],
+    "labels": [],
+    "public_download_numbers": false,
+    "public_stats": false
+  },
+  "version": {
+    "name": "${pkg_version}-${pkg_release}"
+  },
+  "files": [
+    {"includePattern": "out/hab-x86_64-darwin.zip"}
+  ],
+  "publish": true
+}
+EOF
 fi
