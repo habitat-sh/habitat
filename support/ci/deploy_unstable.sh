afa8e1ddca722bdaa555700b96c4c94246c438d4
Steven Murawski <steven.murawski@gmail.com>
2016-11-22 07:58:55 -0600

    fail faster when on a PR or not on the required branch and add some comments
    Signed-off-by: Steven Murawski <steven.murawski@gmail.com>

--- support/ci/deploy_unstable.sh
+++ support/ci/deploy_unstable.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# fail fast if we aren't on the desired branch or if this is a pull request
+if ([ "${TRAVIS_PULL_REQUEST}" = "true" ] || ["${TRAVIS_BRANCH}" != "smurawski/fix_linux_build" ]); then
+    exit 0
+fi
+
 BOOTSTRAP_DIR=/root/travis_bootstrap
 TEST_BIN_DIR=/root/hab_bins
 TRAVIS_HAB=${BOOTSTRAP_DIR}/hab
@@ -12,9 +17,14 @@ wget -O hab.tar.gz "${HAB_DOWNLOAD_URL}"
 # install it in a custom location
 tar xvzf ./hab.tar.gz --strip 1 -C ${BOOTSTRAP_DIR}
 
+# so key stuff doesn't get funky
 unset SUDO_USER
 
+# move up one level so our hab studio build is in the right place
+# as it expects to be one level up from the source dir.
 cd ..
+
+# create our origin key
 cat << EOF > core.sig.key
 SIG-SEC-1
 core-20160810182414
@@ -25,17 +35,20 @@ EOF
 ${TRAVIS_HAB} origin key import < ./core.sig.key
 rm ./core.sig.key
 
-if ([ "${TRAVIS_PULL_REQUEST}" = "false" ] && ["${TRAVIS_BRANCH}" = "master" ]); then
-    mkdir -p ./release
-    rm -rf ./release/*
-    echo "Building bintray-publish"
-    ${TRAVIS_HAB} studio build habitat/components/bintray-publish > /root/bintray-publish_build.log 2>&1
-    echo "Building hab"
-    ${TRAVIS_HAB} studio build habitat/components/hab > /root/hab_build.log 2>&1
-    echo "Built new unstable version of hab"
-    PUBLISH=$(find ./results -name core-hab-bintray*.hart)
-    RELEASE=$(find ./results -name core-hab-0*.hart)
-    echo "Publishing hab to unstable"
-    ${TRAVIS_HAB} pkg install $PUBLISH
-    ${TRAVIS_HAB} pkg exec core/hab-bintray-publish publish-hab -r unstable $RELEASE
-fi
+# make sure we don't have an older, cached release
+mkdir -p ./release
+rm -rf ./release/*
+
+# until we pulish the newer version of bintray-publish with the cli switches
+# we have to build it here
+echo "Building bintray-publish"
+${TRAVIS_HAB} studio build habitat/components/bintray-publish > /root/bintray-publish_build.log 2>&1
+echo "Building hab"
+${TRAVIS_HAB} studio build habitat/components/hab > /root/hab_build.log 2>&1
+echo "Built new unstable version of hab"
+
+echo "Publishing hab to unstable"
+PUBLISH=$(find ./results -name core-hab-bintray*.hart)
+RELEASE=$(find ./results -name core-hab-0*.hart)
+${TRAVIS_HAB} pkg install $PUBLISH
+${TRAVIS_HAB} pkg exec core/hab-bintray-publish publish-hab -r unstable $RELEASE
