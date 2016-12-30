1eea4e511328625dec71cfff4289efc24f1d24b3
Josh Black <raskchanky@gmail.com>
2016-12-30 15:09:47 -0800

    Warn users if Docker's file sharing isn't enabled.
    Signed-off-by: Josh Black <raskchanky@gmail.com>

--- components/hab/src/error.rs
+++ components/hab/src/error.rs
@@ -35,8 +35,9 @@ pub enum Error {
     CryptoCLI(String),
     DepotClient(depot_client::Error),
     DockerDaemonDown,
-    DockerNetworkDown(String),
+    DockerFileSharingNotEnabled,
     DockerImageNotFound(String),
+    DockerNetworkDown(String),
     ExecCommandNotFound(String),
     FFINulError(ffi::NulError),
     FileNotFound(String),
@@ -69,17 +70,22 @@ impl fmt::Display for Error {
             Error::DockerDaemonDown => {
                 format!("Can not connect to Docker. Is the Docker daemon running?")
             }
-            Error::DockerNetworkDown(ref e) => {
-                format!("The Docker image {} is unreachable due to a network error.\nThe \
-                         image must be reachable to ensure the versions of hab inside and \
-                         outside the studio match.\nYou can specify your own Docker image using \
-                         the HAB_DOCKER_STUDIO_IMAGE environment variable.",
-                        e)
+            Error::DockerFileSharingNotEnabled => {
+                format!("File Sharing must be enabled in order to enter a studio.\nPlease enable \
+                         it in the Docker preferences and share (at a minimum) your home \
+                         directory.")
             }
             Error::DockerImageNotFound(ref e) => {
                 format!("The Docker image {} was not found in the docker registry.\nYou can \
                          specify your own Docker image using the HAB_DOCKER_STUDIO_IMAGE \
                          environment variable.",
+                        e)
+            }
+            Error::DockerNetworkDown(ref e) => {
+                format!("The Docker image {} is unreachable due to a network error.\nThe \
+                         image must be reachable to ensure the versions of hab inside and \
+                         outside the studio match.\nYou can specify your own Docker image using \
+                         the HAB_DOCKER_STUDIO_IMAGE environment variable.",
                         e)
             }
             Error::ExecCommandNotFound(ref c) => {
@@ -122,8 +128,9 @@ impl error::Error for Error {
             Error::CryptoCLI(_) => "A cryptographic error has occurred",
             Error::DepotClient(ref err) => err.description(),
             Error::DockerDaemonDown => "The Docker daemon could not be found.",
-            Error::DockerNetworkDown(_) => "The Docker registry is unreachable.",
+            Error::DockerFileSharingNotEnabled => "Docker file sharing is not enabled.",
             Error::DockerImageNotFound(_) => "The Docker image was not found.",
+            Error::DockerNetworkDown(_) => "The Docker registry is unreachable.",
             Error::ExecCommandNotFound(_) => "Exec command was not found on filesystem or in PATH",
             Error::FFINulError(ref err) => err.description(),
             Error::FileNotFound(_) => "File not found",
