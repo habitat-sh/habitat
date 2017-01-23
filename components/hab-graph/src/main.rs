a8a62c055ed893ae63d1f0c16ac7d3b4e7b5122d
Salim Alam <salam@chef.io>
2017-01-23 13:44:29 -0800

    Add package parsing
    Signed-off-by: Salim Alam <salam@chef.io>

--- components/hab-graph/src/main.rs
+++ components/hab-graph/src/main.rs
@@ -2,10 +2,23 @@
 #[macro_use]
 extern crate log;
 extern crate petgraph;
+extern crate walkdir;
+
 pub mod graph;
 
 use petgraph::Graph;
 use petgraph::dot::{Dot, Config};
+use walkdir::WalkDir;
+
+extern crate habitat_core as hab_core;
+
+use hab_core::package::{FromArchive, PackageArchive};
+
+extern crate habitat_builder_protocol as protocol;
+
+use protocol::depotsrv;
+
+use std::path::PathBuf;
 
 fn main() {
     println!("Hello, graph!\n");
@@ -13,27 +26,40 @@ fn main() {
     let mut deps = Graph::<usize, usize>::new();
     let a = deps.add_node(10);
     let b = deps.add_node(11);
-    let c = deps.add_node(12);
-    let d = deps.add_node(13);
-    let e = deps.add_node(14);
-    let f = deps.add_node(15);
-    let g = deps.add_node(16);
-    let h = deps.add_node(17);
+    deps.extend_with_edges(&[(a, b), (b, a)]);
 
-    deps.extend_with_edges(&[(a, c), (b, c), (c, f), (c, e), (d, e), (e, f), (g, h)]);
+    let path = PathBuf::from("/Users/salam/Workspace/habitat/components/hab-graph/pkgs");
+    build_graph_from_fs(path);
+}
 
-    println!("Input:");
-    println!("{:?}", Dot::with_config(&deps, &[Config::EdgeNoLabel]));
-
-    println!("\nReverse dependencies:");
-
-    match graph::rdeps(&deps, a) {
-        Ok(ref v) => {
-            for n in v {
-                print!("{} ", n);
-            }
-            println!("");
+fn build_graph_from_fs(packages_path: PathBuf) {
+    let mut directories = vec![];
+    for entry in WalkDir::new(&packages_path).follow_links(false) {
+        let entry = entry.unwrap();
+        if entry.metadata().unwrap().is_dir() {
+            // println!("{}", entry.path().display());
+            directories.push(entry);
+            continue;
         }
-        Err(_) => println!("Error"),
+
+        let mut archive = PackageArchive::new(PathBuf::from(entry.path()));
+
+        match archive.ident() {
+            Ok(ident) => {
+                match depotsrv::Package::from_archive(&mut archive) {
+                    Ok(o) => {
+                        println!("{} :", o.get_ident().get_name());
+                        let deps = o.get_deps();
+                        for dep in deps {
+                            println!("    {}", dep.get_name());
+                        }
+                    }
+                    Err(e) => println!("Error parsing package from archive: {:?}", e),
+                }
+            }
+            Err(e) => {
+                println!("Error reading, archive={:?} error={:?}", &archive, &e);
+            }
+        }
     }
 }
