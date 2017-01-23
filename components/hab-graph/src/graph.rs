0fc957a6fe7a9c526f606c38047e46e2dea26d9c
Salim Alam <salam@chef.io>
2017-01-23 10:52:55 -0800

    Add tests
    Signed-off-by: Salim Alam <salam@chef.io>

--- components/hab-graph/src/graph.rs
+++ components/hab-graph/src/graph.rs
@@ -1,11 +1,11 @@
 use std::collections::{HashMap, HashSet};
 
-use petgraph::{Graph, Undirected};
+use petgraph::Graph;
 use petgraph::graph::NodeIndex;
-use petgraph::algo::{toposort, is_cyclic_directed, connected_components, kosaraju_scc};
-use petgraph::dot::{Dot, Config};
+use petgraph::algo::{toposort, is_cyclic_directed};
 use petgraph::visit::{Bfs, Walker};
 
+#[derive(Debug, PartialEq)]
 pub enum GraphErr {
     GraphCyclic,
 }
@@ -47,105 +47,58 @@ pub fn rdeps(g: &Graph<GType, GType>, n: NodeIndex) -> Result<Vec<GType>, GraphE
     Ok(v)
 }
 
-fn iter0() {
-    println!("Hello, graph!\n");
-
-    let mut deps = Graph::<&str, &str>::new();
-    let a = deps.add_node("A");
-    let b = deps.add_node("B");
-    let c = deps.add_node("C");
-    let d = deps.add_node("D");
-    let e = deps.add_node("E");
-    let f = deps.add_node("F");
-    let g = deps.add_node("G");
-    let h = deps.add_node("H");
-
-    deps.extend_with_edges(&[(a, c), (b, c), (c, f), (c, e), (d, e), (e, f), (g, h)]);
-
-    println!("Input:");
-    println!("{:?}", Dot::with_config(&deps, &[Config::EdgeNoLabel]));
-
-    println!("Is cyclic: {}\n", is_cyclic_directed(&deps));
-
-    let mut node_map: HashMap<usize, usize> = HashMap::new(); // Node -> Component #
-
-    let undirected = deps.clone().into_edge_type::<Undirected>();
-    println!("Connected components: {}", connected_components(&deps));
-    let cc = kosaraju_scc(&undirected);
-    for i in 0..cc.len() {
-        print!("Component {}: ", i);
-        let v = &cc[i];
-        for n in v {
-            print!("{} ", n.index());
-            node_map.insert(n.index(), i);
-        }
-        println!("");
-    }
-    println!("");
-
-    println!("BFS on element 0:");
-    let bfs0 = Bfs::new(&deps, a);
-    for k in bfs0.clone().iter(&deps) {
-        print!("{:?} ", k.index());
-    }
-    println!("\n");
-
-    println!("BFS on element 1:");
-    let bfs1 = Bfs::new(&deps, b);
-    for k in bfs1.clone().iter(&deps) {
-        print!("{:?} ", k.index());
-    }
-    println!("\n");
-
-    let t = match toposort(&deps, None) {
-        Ok(ref v) => {
-            println!("Topo sort:");
-            for n in v {
-                print!("{} ", n.index());
-            }
-            println!("");
-            v.clone()
-        }
-        Err(c) => {
-            panic!("Error: cycle found at {:?}", c.node_id());
-        }
-    };
-
-    println!("\nBuilding node, component -> topo maps:");
-    let mut comp_map: HashMap<usize, usize> = HashMap::new(); // Component # -> Topo sort start
-    let mut topo_map: HashMap<usize, usize> = HashMap::new(); // Node -> Topo index
-
-    let mut matched: i32 = -1;
-    for i in 0..t.len() {
-        println!("Node {} -> Topo index {}", t[i].index(), i);
-        topo_map.insert(t[i].index(), i);
-
-        let comp_num = *node_map.get(&t[i].index()).unwrap();
-        if comp_num as i32 != matched {
-            println!("Component {} -> Topo index {}", comp_num, i);
-            comp_map.insert(comp_num as usize, i);
-            matched = comp_num as i32;
-        }
-    }
-
-    println!("\nFinding the ordering for node 1:");
-    let start: usize = 1;
-    let mut curr: usize = *topo_map.get(&start).unwrap();
-
-    let mut bfs_set: HashSet<NodeIndex> = bfs1.iter(&deps).collect();
-
-    while !bfs_set.is_empty() {
-        if bfs_set.contains(&t[curr]) {
-            print!("{} ", t[curr].index());
-            bfs_set.remove(&t[curr]);
-        }
-        curr = curr + 1;
-    }
-    println!("");
-}
-
 #[cfg(test)]
 mod tests {
+    use petgraph::Graph;
+    use graph::*;
+
     #[test]
-    fn it_works() {}
+    fn fails_with_cyclic_graph() {
+        let mut deps = Graph::<usize, usize>::new();
+        let a = deps.add_node(10);
+        let b = deps.add_node(11);
+        let c = deps.add_node(12);
+
+        deps.extend_with_edges(&[(a, b), (b, c), (c, a)]);
+
+        match rdeps(&deps, a) {
+            Ok(_) => panic!("Cyclic graph should fail!"),
+            Err(e) => assert_eq!(e, GraphErr::GraphCyclic),
+        }
+    }
+
+    #[test]
+    fn basic_graph_works() {
+        let mut deps = Graph::<usize, usize>::new();
+        let a = deps.add_node(10);
+        let b = deps.add_node(11);
+        let c = deps.add_node(12);
+        let d = deps.add_node(13);
+        let e = deps.add_node(14);
+        let f = deps.add_node(15);
+        let g = deps.add_node(16);
+        let h = deps.add_node(17);
+
+        deps.extend_with_edges(&[(a, c), (b, c), (c, f), (c, e), (d, e), (e, f), (g, h)]);
+
+        match rdeps(&deps, a) {
+            Ok(v) => {
+                static EXPECTED: &'static [usize] = &[0, 2, 4, 5];
+                assert_eq!(v.as_slice(), EXPECTED);
+            }
+            Err(e) => {
+                panic!("Failed with error: {:?}", e);
+            }
+        }
+
+        match rdeps(&deps, b) {
+            Ok(v) => {
+                static EXPECTED: &'static [usize] = &[1, 2, 4, 5];
+                assert_eq!(v.as_slice(), EXPECTED);
+            }
+            Err(e) => {
+                panic!("Failed with error: {:?}", e);
+            }
+        }
+    }
 }
---
+++ components/hab-graph/src/iter0.rs
@@ -0,0 +1,104 @@
+use std::collections::{HashMap, HashSet};
+
+use petgraph::Graph;
+use petgraph::graph::NodeIndex;
+use petgraph::algo::{toposort, is_cyclic_directed, connected_components, kosaraju_scc};
+use petgraph::dot::{Dot, Config};
+use petgraph::visit::{Bfs, Walker};
+
+fn iter0() {
+    println!("Hello, graph!\n");
+
+    let mut deps = Graph::<&str, &str>::new();
+    let a = deps.add_node("A");
+    let b = deps.add_node("B");
+    let c = deps.add_node("C");
+    let d = deps.add_node("D");
+    let e = deps.add_node("E");
+    let f = deps.add_node("F");
+    let g = deps.add_node("G");
+    let h = deps.add_node("H");
+
+    deps.extend_with_edges(&[(a, c), (b, c), (c, f), (c, e), (d, e), (e, f), (g, h)]);
+
+    println!("Input:");
+    println!("{:?}", Dot::with_config(&deps, &[Config::EdgeNoLabel]));
+
+    println!("Is cyclic: {}\n", is_cyclic_directed(&deps));
+
+    let mut node_map: HashMap<usize, usize> = HashMap::new(); // Node -> Component #
+
+    let undirected = deps.clone().into_edge_type::<Undirected>();
+    println!("Connected components: {}", connected_components(&deps));
+    let cc = kosaraju_scc(&undirected);
+    for i in 0..cc.len() {
+        print!("Component {}: ", i);
+        let v = &cc[i];
+        for n in v {
+            print!("{} ", n.index());
+            node_map.insert(n.index(), i);
+        }
+        println!("");
+    }
+    println!("");
+
+    println!("BFS on element 0:");
+    let bfs0 = Bfs::new(&deps, a);
+    for k in bfs0.clone().iter(&deps) {
+        print!("{:?} ", k.index());
+    }
+    println!("\n");
+
+    println!("BFS on element 1:");
+    let bfs1 = Bfs::new(&deps, b);
+    for k in bfs1.clone().iter(&deps) {
+        print!("{:?} ", k.index());
+    }
+    println!("\n");
+
+    let t = match toposort(&deps, None) {
+        Ok(ref v) => {
+            println!("Topo sort:");
+            for n in v {
+                print!("{} ", n.index());
+            }
+            println!("");
+            v.clone()
+        }
+        Err(c) => {
+            panic!("Error: cycle found at {:?}", c.node_id());
+        }
+    };
+
+    println!("\nBuilding node, component -> topo maps:");
+    let mut comp_map: HashMap<usize, usize> = HashMap::new(); // Component # -> Topo sort start
+    let mut topo_map: HashMap<usize, usize> = HashMap::new(); // Node -> Topo index
+
+    let mut matched: i32 = -1;
+    for i in 0..t.len() {
+        println!("Node {} -> Topo index {}", t[i].index(), i);
+        topo_map.insert(t[i].index(), i);
+
+        let comp_num = *node_map.get(&t[i].index()).unwrap();
+        if comp_num as i32 != matched {
+            println!("Component {} -> Topo index {}", comp_num, i);
+            comp_map.insert(comp_num as usize, i);
+            matched = comp_num as i32;
+        }
+    }
+
+    println!("\nFinding the ordering for node 1:");
+    let start: usize = 1;
+    let mut curr: usize = *topo_map.get(&start).unwrap();
+
+    let mut bfs_set: HashSet<NodeIndex> = bfs1.iter(&deps).collect();
+
+    while !bfs_set.is_empty() {
+        if bfs_set.contains(&t[curr]) {
+            print!("{} ", t[curr].index());
+            bfs_set.remove(&t[curr]);
+        }
+        curr = curr + 1;
+    }
+    println!("");
+}
