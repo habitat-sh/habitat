c437be7c074a51fae084c61c1ae8480ad9504414
Matt Wrock <matt@mattwrock.com>
2017-03-08 14:42:11 -0800

    deploy windows cli to stable bintray on version tag push
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- support/ci/appveyor.ps1
+++ support/ci/appveyor.ps1
@@ -115,14 +115,9 @@ if (Test-SourceChanged -or (test-path env:HAB_FORCE_TEST)) {
                     mkdir $zipDir -Force
                     Copy-Item "/hab/pkgs/core/hab/*/*/bin/*" $zipDir
 
-                    # if($env:APPVEYOR_REPO_TAG_NAME -eq "$(Get-Content VERSION)" -and (!$env:APPVEYOR_REPO_TAG_NAME.EndsWith("dev"))) {
-                    #     Compress-Archive -Path $zipDir -DestinationPath "results/prod/$zip"
-                    # }
-                    Write-Host "tag: $($env:APPVEYOR_REPO_TAG_NAME)"
-                    if($env:APPVEYOR_REPO_TAG_NAME -eq "LOBSTER" -and (!$env:APPVEYOR_REPO_TAG_NAME.EndsWith("dev"))) {
-                        write-host "compressing to results/prod/LOBSTER-$zip"
+                    if($env:APPVEYOR_REPO_TAG_NAME -eq "$(Get-Content VERSION)" -and (!$env:APPVEYOR_REPO_TAG_NAME.EndsWith("dev"))) {
                         mkdir "results/prod" -Force
-                        Compress-Archive -Path $zipDir -DestinationPath "results/prod/LOBSTER-$zip"
+                        Compress-Archive -Path $zipDir -DestinationPath "results/prod/$zip"
                     }
                     Compress-Archive -Path $zipDir -DestinationPath "results/$zip"
                 }
