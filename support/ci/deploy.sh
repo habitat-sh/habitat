2120321dadfd69d73453e60d5e54b39f2de00eda
Matt Wrock <matt@mattwrock.com>
2017-03-10 18:58:23 -0800

    include tag check in deploy.sh
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- support/ci/deploy.sh
+++ support/ci/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # fail fast if we aren't on the desired branch or if this is a pull request
-if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]] || [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+if  [[ "${TRAVIS_BRANCH}" != "$(cat VERSION)" && ("${TRAVIS_PULL_REQUEST}" != "false" || "${TRAVIS_BRANCH}" != "master") ]]; then
     echo "We only publish on successful builds of master."
     exit 0
 fi
