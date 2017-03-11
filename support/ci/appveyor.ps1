c8fc9bbf9b3e9ae4de3d082c8f5f5db70839b685
Matt Wrock <matt@mattwrock.com>
2017-03-10 17:58:54 -0800

    remove debug line
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- support/ci/appveyor.ps1
+++ support/ci/appveyor.ps1
@@ -42,7 +42,6 @@ Write-Host "Configuring build environment"
 
 write-host "TAG: $env:APPVEYOR_REPO_TAG_NAME"
 Write-Host "VERSION: $(Get-Content VERSION)"
-Write-Host ($env:APPVEYOR_REPO_TAG_NAME -eq "$(Get-Content VERSION)")
 if (($env:APPVEYOR_REPO_TAG_NAME -eq "$(Get-Content VERSION)") -or (Test-SourceChanged) -or (test-path env:HAB_FORCE_TEST)) {
     foreach ($BuildAction in ($env:hab_build_action -split ';')) {
         if ($BuildAction -like 'build') {
