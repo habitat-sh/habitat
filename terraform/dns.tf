8ec2794cc813810bf0d24b33aa15df0c26839386
Christian Nunciato <chris@nunciato.org>
2017-05-05 16:49:01 -0700

    Add CNAMES for builder_api
    Adds support for build.habitat.sh, builder.habitat.sh and depot.habitat.sh.
    
    Signed-off-by: Christian Nunciato <chris@nunciato.org>

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -6,6 +6,30 @@ resource "aws_route53_record" "app" {
   records = ["${aws_elb.builder_api.dns_name}"]
 }
 
+resource "aws_route53_record" "build" {
+  zone_id = "${var.dns_zone_id}"
+  name    = "build-${var.env}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_elb.builder_api.dns_name}"]
+}
+
+resource "aws_route53_record" "builder" {
+  zone_id = "${var.dns_zone_id}"
+  name    = "builder-${var.env}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_elb.builder_api.dns_name}"]
+}
+
+resource "aws_route53_record" "depot" {
+  zone_id = "${var.dns_zone_id}"
+  name    = "depot-${var.env}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_elb.builder_api.dns_name}"]
+}
+
 resource "aws_route53_record" "willem" {
   zone_id = "${var.dns_zone_id}"
   name    = "willem-${var.env}"
