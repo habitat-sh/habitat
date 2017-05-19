425b1848020d4f10dbd5c63a0ef25fb9b615f59a
Christian Nunciato <cnunciato@chef.io>
2017-05-19 09:07:07 -0700

    Inter-site navigation improvements, Part II
    This adds some logic to the Habitat web site to detect the visitor’s signed-in status and conditionally display their avatar and a profile menu.
    
    Also adds configurability of the Habitat Depot URL,and updates some styles to match current design specs.
    
    Signed-off-by: Christian Nunciato <cnunciato@chef.io>

--- www/source/javascripts/nav.js
+++ www/source/javascripts/nav.js
@@ -7,6 +7,83 @@ const navPageLinks = ['about', 'docs', 'tutorials', 'community', 'blog'];
 const stickyBreakpoint = 280;
 const stickyVisibleBreakpoint = 300;
 
+(function nav($, cookies) {
+  var profile;
+
+  $(document).ready(function() {
+    var signedOutElements = $(".signed-out");
+    var signedInElements = $(".signed-in");
+    var logo = $(".main-nav--logo a");
+    var avatar = signedInElements.find(".avatar");
+    var dropdown = signedInElements.find(".dropdown");
+    var username = dropdown.find(".username");
+    var signOutLink = dropdown.find(".sign-out");
+
+    if (token()) {
+      $.get("https://api.github.com/user?access_token=" + token())
+        .then(function(p) {
+          signIn(p);
+        }, function(err) {
+          console.error(err);
+          signOut();
+        });
+    }
+    else {
+      signOut();
+    }
+
+    avatar.click(function(e) {
+      e.stopPropagation();
+      dropdown.toggle();
+    });
+
+    signOutLink.click(function() {
+      signOut();
+    });
+
+    logo.click(function(e) {
+      if (signedIn()) {
+        e.preventDefault();
+        location.href = logo.data("builder-url");
+      }
+    });
+
+    $(document).click(function() {
+      dropdown.hide();
+    });
+
+    function token() {
+      return cookies.get("gitHubAuthToken");
+    }
+
+    function signedIn() {
+      return !!token();
+    }
+
+    function signIn(profile) {
+      avatar.find("img").attr("src", profile.avatar_url);
+      username.text(profile.login);
+      signedInElements.css("display", "inline-block");
+    }
+
+    function signOut() {
+      cookies.remove("gitHubAuthState", { domain: cookieDomain() });
+      cookies.remove("gitHubAuthToken", { domain: cookieDomain() });
+      cookies.remove("featureFlags", { domain: cookieDomain() });
+      signedOutElements.css("display", "inline-block");
+      signedInElements.hide();
+    }
+
+    function cookieDomain() {
+        let delim = ".";
+        return location.hostname
+            .split(delim)
+            .splice(-2)
+            .join(delim);
+    }
+  });
+})($, Cookies);
+
 var toggleStickyNav = function() {
   if ($mainNav.is(":not(.has-sidebar)")) {
     if ($(window).width() > navBreakpoint) {
