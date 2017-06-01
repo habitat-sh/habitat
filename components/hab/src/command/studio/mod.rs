4e07b98112a12dd1384cf9c516050f1a74c682a9
Nathan L Smith <smith@chef.io>
2017-06-01 16:46:11 -0500

    Add HAB_UPDATE_STRATEGY_FREQUENCY_MS to studio propagated vars
    This is needed to tell the supervisor how often to check for updates,
    which, in a studio, you often want to do right away after you've built a
    package.
    
    This is not available as a command line option to `hab sup run`, so it
    cannot be set via the `HAB_STUDIO_SUP` options. Therefore, we make it so
    it can be set outside the studio.
    
    Signed-off-by: Nathan L Smith <smith@chef.io>

--- components/hab/src/command/studio/mod.rs
+++ components/hab/src/command/studio/mod.rs
@@ -305,6 +305,7 @@ mod inner {
         let env_vars = vec!["HAB_DEPOT_URL",
                             "HAB_ORIGIN",
                             "HAB_STUDIO_SUP",
+                            "HAB_UPDATE_STRATEGY_FREQUENCY_MS",
                             "http_proxy",
                             "https_proxy"];
         for var in env_vars {
