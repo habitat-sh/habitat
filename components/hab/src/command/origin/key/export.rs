44a42b0c2d8593faf25f6316563391448a4b1252
Vesa Kaihlavirta <vk@discord.tech>
2017-06-24 13:44:14 +0300

    Allow needless_pass_by_value to prevent false neg
    Signed-off-by: Vesa Kaihlavirta <vk@discord.tech>

--- components/hab/src/command/origin/key/export.rs
+++ components/hab/src/command/origin/key/export.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#![cfg_attr(feature="clippy", allow(needless_pass_by_value))]
 
 use std::io;
 use std::fs::File;
