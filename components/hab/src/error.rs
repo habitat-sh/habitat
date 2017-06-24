cbdbafacaa2cd403155ebaeb1be55d5840c849cf
Vesa Kaihlavirta <vk@discord.tech>
2017-06-24 13:59:32 +0300

    Allow large_enum_variant lint
    Signed-off-by: Vesa Kaihlavirta <vk@discord.tech>

--- components/hab/src/error.rs
+++ components/hab/src/error.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#![cfg_attr(feature="clippy", allow(large_enum_variant))]
 
 use std::error;
 use std::ffi;
