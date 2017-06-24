cf37c0c048e3a6f7f576d338cb9bfae1e7a8d6bc
Vesa Kaihlavirta <vk@discord.tech>
2017-06-24 10:59:49 +0300

    Allow or_fun_call lint
    Signed-off-by: Vesa Kaihlavirta <vk@discord.tech>

--- components/hab/src/command/cli/setup.rs
+++ components/hab/src/command/cli/setup.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#![cfg_attr(feature="clippy", allow(or_fun_call))]
 
 use std::path::Path;
 
