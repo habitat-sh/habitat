a3e8c5249d46f882a6f9772910c2cf4b47a36c18
Vesa Kaihlavirta <vk@discord.tech>
2017-06-24 14:22:11 +0300

    Allow useless_format lint
    Signed-off-by: Vesa Kaihlavirta <vk@discord.tech>

--- components/hab/src/error.rs
+++ components/hab/src/error.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #![cfg_attr(feature="clippy", allow(large_enum_variant))]
+#![cfg_attr(feature="clippy", allow(useless_format))]
 
 use std::error;
 use std::ffi;
