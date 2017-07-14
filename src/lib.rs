5685b702855fbcecff8612649e68645dc3bc4c07
George Marshall <george@georgemarshall.name>
2017-07-14 11:49:10 -0700

    Change try! to ?
    Signed-off-by: George Marshall <george@georgemarshall.name>

--- src/lib.rs
+++ src/lib.rs
@@ -49,32 +49,32 @@ mod ssl {
     const CACERT_PEM: &'static str = include_str!(concat!(env!("OUT_DIR"), "/cacert.pem"));
 
     pub fn set_ca(ctx: &mut SslContextBuilder, fs_root_path: Option<&Path>) -> Result<()> {
-        let cacerts_ident = try!(PackageIdent::from_str(CACERTS_PKG_IDENT));
+        let cacerts_ident = PackageIdent::from_str(CACERTS_PKG_IDENT)?;
 
         if let Ok(_) = env::var("SSL_CERT_FILE") {
-            try!(ctx.set_default_verify_paths());
+            ctx.set_default_verify_paths()?;
         } else if let Ok(_) = env::var("SSL_CERT_DIR") {
-            try!(ctx.set_default_verify_paths());
+            ctx.set_default_verify_paths()?;
         } else if let Ok(pkg_install) = PackageInstall::load(&cacerts_ident, fs_root_path) {
             let pkg_certs = pkg_install.installed_path().join("ssl/cert.pem");
             debug!(
                 "Setting CA file for SSL context to: {}",
                 pkg_certs.display()
             );
-            try!(ctx.set_ca_file(pkg_certs));
+            ctx.set_ca_file(pkg_certs)?;
         } else {
             let cached_certs = cache_ssl_path(fs_root_path).join("cert.pem");
             if !cached_certs.exists() {
-                try!(fs::create_dir_all(cache_ssl_path(fs_root_path)));
+                fs::create_dir_all(cache_ssl_path(fs_root_path))?;
                 debug!("Creating cached cacert.pem at: {}", cached_certs.display());
-                let mut file = try!(File::create(&cached_certs));
-                try!(file.write_all(CACERT_PEM.as_bytes()));
+                let mut file = File::create(&cached_certs)?;
+                file.write_all(CACERT_PEM.as_bytes())?;
             }
             debug!(
                 "Setting CA file for SSL context to: {}",
                 cached_certs.display()
             );
-            try!(ctx.set_ca_file(cached_certs));
+            ctx.set_ca_file(cached_certs)?;
         }
         Ok(())
     }
@@ -89,7 +89,7 @@ mod ssl {
     use error::Result;
 
     pub fn set_ca(ctx: &mut SslContextBuilder, _fs_root_path: Option<&Path>) -> Result<()> {
-        try!(ctx.set_default_verify_paths());
+        ctx.set_default_verify_paths()?;
         Ok(())
     }
 }
