a60a7a8477d0a4ed017ce6ec2a88d375dab4548c
Garrett Amini <garrett@chef.io>
2017-09-01 11:03:00 -0400

    building form progress breadcrumb component
    Signed-off-by: Garrett Amini <garrett@chef.io>

---
+++ components/builder-web/app/shared/form-progress/form-progress.component.ts
@@ -0,0 +1,29 @@
+// Copyright (c) 2016-2017 Chef Software Inc. and/or applicable contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: "hab-package-form-progress",
+    template: `
+    <ol class="hab-package-form-progress small">
+      <li class="hab-package-form-progress-step" *ngFor="let step of steps">
+        <a [ngClass]="{current: step.current}" [routerLink]="step.target">{{step.name}}</a>
+      </li>
+    </ol>`
+})
+
+export class FormProgressComponent {
+    @Input() steps;
+}
