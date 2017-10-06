d72add8b4ff1c2f0c03d2ddbe0b834b041be3f07
Fletcher Nichol <fnichol@nichol.ca>
2017-10-06 16:40:34 -0600

    [pkg-cfize] Compute a `PATH` at build time so program finds its deps.
    This change builds a small "shim" script into the `core/hab-pkg-cfize`
    package which contains the full runtime `PATH` entry before `exec`'ing
    to the real program. Now that the program has commands like `docker`,
    `hab-pkg-dockerize`, etc. on its `PATH` the program can be directly
    called without any `hab pkg exec` calls first.
    
    As a result, this allows for calling:
    
    ```sh
    hab pkg export cf
    ```
    
    in an environment with any (or no) `PATH`.
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- components/pkg-cfize/plan.sh
+++ components/pkg-cfize/plan.sh
@@ -5,8 +5,8 @@ pkg_license=('Apache-2.0')
 pkg_description="Habitat Cloud Foundry image exporter"
 pkg_upstream_url="https://github.com/habitat-sh/habitat"
 pkg_deps=(
-  core/coreutils core/findutils core/grep core/sed core/bash
-  core/hab-pkg-dockerize
+  core/coreutils core/findutils core/grep core/sed core/gawk core/bash
+  core/hab-pkg-dockerize core/docker
 )
 pkg_bin_dirs=(bin)
 
@@ -14,6 +14,21 @@ _bins=($pkg_name)
 
 # TODO fn: use `pkg_version()` form
 pkg_version=$(cat "$PLAN_CONTEXT/../../VERSION")
+
+do_prepare() {
+  do_default_prepare
+
+  # This builds up a reasonable runtime `PATH` for the program which includes
+  # all direct and indirect runtime dependencies' `PATH` entries.
+  local p
+  p=()
+  for tdep in "${pkg_tdeps_resolved[@]}"; do
+    if [[ -f "$tdep/PATH" ]]; then
+      p+=($(cat "$tdep/PATH"))
+    fi
+  done
+  _runtime_path="$(join_by : "${p[@]}")"
+}
 
 do_build() {
   local bin
@@ -34,5 +49,25 @@ do_install() {
   local bin
   for bin in "${_bins[@]}"; do
     install -v -D "$CACHE_PATH/$bin" "$pkg_prefix/bin/$bin"
+    # Wrap the program with a script which sets the runtime `PATH` for the
+    # program so that it can find all of the programs it calls out to, such as
+    # `docker`, `hab-pkg-dockerize`, etc.
+    _wrap_binary "$pkg_prefix/bin/$bin"
   done
+}
+
+_wrap_binary() {
+  local bin="$1"
+  build_line "Adding wrapper $bin to ${bin}.real"
+  mv -v "$bin" "${bin}.real"
+  cat <<EOF > "$bin"
+#!$(pkg_path_for bash)/bin/bash
+set -e
+if test -n "\$DEBUG"; then set -x; fi
+
+export PATH="$_runtime_path:\${PATH:-}"
+
+exec ${bin}.real \$@
+EOF
+  chmod -v 755 "$bin"
 }
