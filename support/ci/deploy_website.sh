54e1ee32b08cababc393f4caf9c061b17f22d79b
Christian Nunciato <cnunciato@chef.io>
2017-10-07 12:57:53 -0700

    Environment variables on one line
    Signed-off-by: Christian Nunciato <cnunciato@chef.io>

--- support/ci/deploy_website.sh
+++ support/ci/deploy_website.sh
@@ -10,7 +10,6 @@ else
   if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" =~ ^acceptance_deploy ]]; then
     echo "We are on a PR or against the master branch. Deploying to Acceptance."
     cd www
-    BUILDER_WEB_URL="https://bldr.acceptance.habitat.sh"
-    GITHUB_APP_URL="https://github.com/apps/habitat-builder-acceptance" make acceptance
+    BUILDER_WEB_URL="https://bldr.acceptance.habitat.sh" GITHUB_APP_URL="https://github.com/apps/habitat-builder-acceptance" make acceptance
   fi
 fi
