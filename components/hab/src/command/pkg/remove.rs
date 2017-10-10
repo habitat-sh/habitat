ea0a9631671397cd6c0bc39134073ecb1a0fe845
Scott Macfarlane <macfarlane.scott@gmail.com>
2017-10-10 14:40:15 -0700

    First pass at pkg remove command
    Signed-off-by: Scott Macfarlane <macfarlane.scott@gmail.com>

--- components/hab/src/command/pkg/remove.rs
+++ components/hab/src/command/pkg/remove.rs
@@ -14,14 +14,66 @@
 
 use std::collections::HashSet;
 use std::path::Path;
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use std::str::FromStr;
+use std::cmp::Ordering;
 
 use walkdir::WalkDir;
 
-use error::{Error, Result};
+use error::Result;
 use hcore::fs::PKG_PATH;
-use hcore::package::PackageIdent;
+use hcore::package::{PackageIdent, PackageInstall};
 use common::ui::{Status, UI};
 
-pub fn start(ui: &mut UI, pkg_name: &PackageIdent, fs_root_path: &Path ) -> Result<()> {
-  Ok(())
-} 
+pub fn start(ui: &mut UI, pkg_name: &PackageIdent, fs_root_path: &Path) -> Result<()> {
+    let package_install = PackageInstall::load(pkg_name, Some(fs_root_path))?;
+    let package_ident = package_install.ident();
+    let pkg_root = fs_root_path.join(PKG_PATH);
+
+    let mut dependent_packages = HashSet::new();
+
+    ui.begin(format!(
+        "Checking for installed packages that depend on {}",
+        package_install
+    ))?;
+
+    for entry in WalkDir::new(pkg_root).into_iter().filter_map(|e| e.ok()) {
+        if let Some(e) = entry.path().file_name().and_then(|e| e.to_str()) {
+            if e == "IDENT" {
+                let mut f = File::open(entry.path().to_str().unwrap())?;
+                let mut ident_str = String::new();
+
+                f.read_to_string(&mut ident_str)?;
+
+                let pkg_ident = PackageIdent::from_str(&ident_str.trim())?;
+                let pkg_install = PackageInstall::load(&pkg_ident, Some(fs_root_path))?;
+
+                for tdep in pkg_install.tdeps().unwrap() {
+                    if package_ident.cmp(&tdep) == Ordering::Equal {
+                        dependent_packages.insert(pkg_install.ident().clone());
+                    }
+                }
+            }
+        }
+    }
+
+    if dependent_packages.is_empty() {
+        ui.status(
+            Status::Deleting,
+            format!("{:?}", package_install.installed_path()),
+        )?;
+        fs::remove_dir_all(package_install.installed_path()).expect("Unable to remove package");
+    } else {
+        println!(
+            "Unable to delete {}.  The following packages depend on it:",
+            package_ident
+        );
+        for pkg in dependent_packages {
+            println!("  {}", pkg);
+        }
+    }
+
+    Ok(())
+}
