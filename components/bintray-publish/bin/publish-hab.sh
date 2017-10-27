b8e4b10f4ddbe298f519bbb9979117c6a4826fd6
echohack <echohack@users.noreply.github.com>
2017-10-27 08:38:26 -0700

    update_copyright_date
    Signed-off-by: echohack <echohack@users.noreply.github.com>

--- components/bintray-publish/bin/publish-hab.sh
+++ components/bintray-publish/bin/publish-hab.sh
@@ -11,7 +11,7 @@
 # # License and Copyright
 #
 # ```
-# Copyright:: Copyright (c) 2015-2016 The Habitat Maintainers
+# Copyright:: Copyright (c) 2015-2017 Chef Software Inc. and/or applicable contributors
 # License: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
