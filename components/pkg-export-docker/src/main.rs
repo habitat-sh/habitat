habitat-sh/habitat

commit 110936abd800e2e27c62200144ae56b3af0d9a90

Add ability to export to Amazon ECR for hab-pkg-export-docker
* Note: This PR doesn't wire this into the worker

Signed-off-by: Travis Elliott Davis <edavis@chef.io>

components/pkg-export-docker/src/main.rs
@@ -15,12 +15,17 @@
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
 
+extern crate base64;
 #[macro_use]
 extern crate clap;
 extern crate env_logger;
 extern crate habitat_core as hcore;
 extern crate habitat_common as common;
 extern crate habitat_pkg_export_docker as export_docker;
+extern crate rusoto_core;
+extern crate rusoto_credential as aws_creds;
+extern crate rusoto_ecr;
+extern crate chrono;
 #[macro_use]
 extern crate log;
 extern crate url;
@@ -39,7 +44,11 @@ use hcore::package::PackageIdent;
 use hcore::url as hurl;
 use url::Url;
 
-use export_docker::{BuildSpec, Credentials, Result, Naming};
+use aws_creds::StaticProvider;
+use rusoto_core::Region;
+use rusoto_core::request::*;
+use rusoto_ecr::{Ecr, EcrClient, GetAuthorizationTokenRequest};
+use export_docker::{BuildSpec, Credentials, Error, Result, Naming};
 
 const DEFAULT_HAB_IDENT: &'static str = "core/hab";
 const DEFAULT_LAUNCHER_IDENT: &'static str = "core/hab-launcher";
@@ -59,6 +68,8 @@ fn start(ui: &mut UI) -> Result<()> {
     debug!("clap cli args: {:?}", m);
     let default_channel = channel::default();
     let default_url = hurl::default_bldr_url();
+    let registry_type = m.value_of("REGISTRY_TYPE").unwrap_or("docker");
+    let registry_url = m.value_of("REGISTRY_URL");
 
     let spec = BuildSpec {
         hab: m.value_of("HAB_PKG").unwrap_or(DEFAULT_HAB_IDENT),
@@ -90,6 +101,8 @@ fn start(ui: &mut UI) -> Result<()> {
             true
         },
         custom_tag: m.value_of("TAG_CUSTOM"),
+        registry_url: registry_url,
+        registry_type: registry_type,
     };
 
     let docker_image = export_docker::export(ui, spec, &naming)?;
@@ -98,11 +111,51 @@ fn start(ui: &mut UI) -> Result<()> {
         env::current_dir()?.join("results"),
     )?;
     if m.is_present("PUSH_IMAGE") {
-        let credentials = Credentials {
-            username: m.value_of("REGISTRY_USERNAME").unwrap(),
-            password: m.value_of("REGISTRY_PASSWORD").unwrap(),
-        };
-        docker_image.push(ui, &credentials)?;
+        match registry_type {
+            "amazon" => {
+                // The username and password should be valid IAM credentials
+                let provider = StaticProvider::new_minimal(
+                    m.value_of("REGISTRY_USERNAME").unwrap().to_string(),
+                    m.value_of("REGISTRY_PASSWORD").unwrap().to_string(),
+                );
+                // TODO TED: Make the region configurable
+                let client =
+                    EcrClient::new(default_tls_client().unwrap(), provider, Region::UsWest2);
+                let auth_token_req = GetAuthorizationTokenRequest { registry_ids: None };
+                let token = match client.get_authorization_token(&auth_token_req) {
+                    Ok(resp) => {
+                        match resp.authorization_data {
+                            Some(auth_data) => auth_data[0].clone().authorization_token.unwrap(),
+                            None => return Err(Error::NoECRTokensReturned),
+                        }
+                    }
+                    Err(e) => return Err(Error::TokenFetchFailed(e)),
+                };
+
+                let creds: Vec<String> = match base64::decode(&token) {
+                    Ok(decoded_token) => {
+                        match String::from_utf8(decoded_token) {
+                            Ok(dts) => dts.split(':').map(String::from).collect(),
+                            Err(err) => return Err(Error::InvalidToken(err)),
+                        }
+                    }
+                    Err(err) => return Err(Error::Base64DecodeError(err)),
+                };
+
+                let credentials = Credentials {
+                    username: &creds[0],
+                    password: &creds[1],
+                };
+                docker_image.push(ui, &credentials, registry_url)?;
+            }
+            _ => {
+                let credentials = Credentials {
+                    username: m.value_of("REGISTRY_USERNAME").unwrap(),
+                    password: m.value_of("REGISTRY_PASSWORD").unwrap(),
+                };
+                docker_image.push(ui, &credentials, registry_url)?;
+            }
+        }
     }
     if m.is_present("RM_IMAGE") {
         docker_image.rm(ui)?;
@@ -181,7 +234,10 @@ fn cli<'a, 'b>() -> App<'a, 'b> {
             "Remote registry username, required for pushing image to remote registry")
         (@arg REGISTRY_PASSWORD: --("password") -P +takes_value requires[REGISTRY_USERNAME]
             "Remote registry password, required for pushing image to remote registry")
-
+        (@arg REGISTRY_TYPE: --("registry-type") -P +takes_value
+            "Remote registry type, Ex: Amazon, Docker, Google (default: docker)")
+        (@arg REGISTRY_URL: --("registry-url") -P +takes_value
+            "Remote registry url")
         // Cleanup
         (@arg RM_IMAGE: --("rm-image")
             "Remove local image from engine after build and/or push (default: no)")

components/pkg-export-docker/Cargo.toml
@@ -14,8 +14,10 @@ path = "src/main.rs"
 doc = false
 
 [dependencies]
+base64 = "*"
 clap = { version = "*", features = ["suggestions", "color", "unstable"] }
 clippy = { version = "*", optional = true }
+chrono = "*"
 env_logger = "*"
 hab = { path = "../hab" }
 habitat_core = { path = "../core" }
@@ -23,6 +25,9 @@ habitat_common = { path = "../common" }
 handlebars = { version = "*", features = ["partial4"], default-features = false }
 lazy_static = "*"
 log = "*"
+rusoto_core = "*"
+rusoto_credential = "*"
+rusoto_ecr = "*"
 serde = { version = "*", features = ["rc"] }
 serde_json = "*"
 tempdir = "*"

components/pkg-export-docker/src/docker.rs
@@ -152,13 +152,18 @@ impl<'a> DockerImage {
     /// * If a registry login is not successful
     /// * If a pushing one or more of the image tags fails
     /// * If a registry logout is not successful
-    pub fn push(&self, ui: &mut UI, credentials: &Credentials) -> Result<()> {
+    pub fn push(
+        &self,
+        ui: &mut UI,
+        credentials: &Credentials,
+        registry_url: Option<&str>,
+    ) -> Result<()> {
         ui.begin(format!(
             "Pushing Docker image '{}' with all tags to remote registry",
             self.name()
         ))?;
-        self.logout(ui)?;
-        self.login(ui, credentials)?;
+        self.logout(ui, registry_url)?;
+        self.login(ui, credentials, registry_url)?;
         if self.tags.is_empty() {
             self.push_image(ui, None)?;
         } else {
@@ -166,7 +171,7 @@ impl<'a> DockerImage {
                 self.push_image(ui, Some(tag))?;
             }
         }
-        self.logout(ui)?;
+        self.logout(ui, registry_url)?;
         ui.end(format!(
             "Docker image '{}' published with tags: {}",
             self.name(),
@@ -247,7 +252,12 @@ impl<'a> DockerImage {
         Ok(())
     }
 
-    fn login(&self, ui: &mut UI, credentials: &Credentials) -> Result<()> {
+    fn login(
+        &self,
+        ui: &mut UI,
+        credentials: &Credentials,
+        registry_url: Option<&str>,
+    ) -> Result<()> {
         ui.status(
             Status::Custom('☛', "Logging into".to_string()),
             "remote registry",
@@ -258,6 +268,11 @@ impl<'a> DockerImage {
             .arg(credentials.username)
             .arg("--password")
             .arg(credentials.password);
+
+        if let Some(arg) = registry_url {
+            cmd.arg(arg);
+        }
+
         debug!(
             "Running: {}",
             format!("{:?}", &cmd)
@@ -272,13 +287,17 @@ impl<'a> DockerImage {
         Ok(())
     }
 
-    fn logout(&self, ui: &mut UI) -> Result<()> {
+    fn logout(&self, ui: &mut UI, registry_url: Option<&str>) -> Result<()> {
         ui.status(
             Status::Custom('☒', "Logging out".to_string()),
             "of remote registry",
         )?;
         let mut cmd = docker_cmd();
         cmd.arg("logout");
+
+        if let Some(arg) = registry_url {
+            cmd.arg(arg);
+        }
         debug!("Running: {:?}", &cmd);
         let exit_status = cmd.spawn()?.wait()?;
         if !exit_status.success() {
@@ -461,6 +480,11 @@ impl DockerBuildRoot {
         let image_name = match naming.custom_image_name {
             Some(ref custom) => Handlebars::new().template_render(custom, &json)?,
             None => format!("{}/{}", ident.origin, ident.name),
+        }.to_lowercase();
+
+        let image_name = match naming.registry_url {
+            Some(ref url) => format!("{}/{}", url, image_name),
+            None => image_name,
         }.to_lowercase();
 
         let mut image = DockerImage::new(self.0.workdir(), image_name);

components/pkg-export-docker/src/error.rs
@@ -12,10 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use base64::DecodeError;
 use std::process::ExitStatus;
 use std::fmt;
 use std::io;
 use std::result;
+use std::string::FromUtf8Error;
+use rusoto_ecr::GetAuthorizationTokenError;
 
 use common;
 use hab;
@@ -26,13 +29,17 @@ pub type Result<T> = result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
+    Base64DecodeError(DecodeError),
     BuildFailed(ExitStatus),
     DockerImageIdNotFound(String),
+    InvalidToken(FromUtf8Error),
     Hab(hab::error::Error),
     HabitatCommon(common::Error),
     HabitatCore(hcore::Error),
     LoginFailed(ExitStatus),
     LogoutFailed(ExitStatus),
+    NoECRTokensReturned,
+    TokenFetchFailed(GetAuthorizationTokenError),
     PrimaryServicePackageNotFound(Vec<String>),
     PushImageFailed(ExitStatus),
     RemoveImageFailed(ExitStatus),
@@ -43,6 +50,7 @@ pub enum Error {
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let msg = match *self {
+            Error::Base64DecodeError(ref err) => format!("{}", err),
             Error::BuildFailed(status) => format!("Docker build failed with exit code: {}", status),
             Error::DockerImageIdNotFound(ref image_tag) => {
                 format!(
@@ -54,6 +62,9 @@ impl fmt::Display for Error {
             Error::HabitatCommon(ref err) => format!("{}", err),
             Error::HabitatCore(ref err) => format!("{}", err),
             Error::LoginFailed(status) => format!("Docker login failed with exit code: {}", status),
+            Error::TokenFetchFailed(ref err) => format!("{}", err),
+            Error::NoECRTokensReturned => format!("No ECR Tokens returned"),
+            Error::InvalidToken(ref err) => format!("{}", err),
             Error::LogoutFailed(status) => {
                 format!("Docker logout failed with exit code: {}", status)
             }

components/pkg-export-docker/src/lib.rs
@@ -19,6 +19,7 @@ extern crate hab;
 extern crate habitat_core as hcore;
 extern crate habitat_common as common;
 extern crate handlebars;
+extern crate rusoto_ecr;
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
@@ -26,6 +27,7 @@ extern crate log;
 #[macro_use]
 extern crate serde_json;
 extern crate tempdir;
+extern crate base64;
 
 mod build;
 mod docker;
@@ -38,7 +40,7 @@ use common::ui::UI;
 
 pub use build::BuildSpec;
 pub use docker::{DockerImage, DockerBuildRoot};
-pub use error::Result;
+pub use error::{Error, Result};
 
 /// The version of this library and program when built.
 pub const VERSION: &'static str = include_str!(concat!(env!("OUT_DIR"), "/VERSION"));
@@ -65,6 +67,11 @@ pub struct Naming<'a> {
     pub version_release_tag: bool,
     /// An optional custom tag value for the image.
     pub custom_tag: Option<&'a str>,
+    /// A URL to a custom Docker registry to publish to. This will be used as part of every tag
+    /// before pushing.
+    pub registry_url: Option<&'a str>,
+    /// The type of registry we're publishing to. Ex: Amazon, Docker, Google, Azure.
+    pub registry_type: &'a str,
 }
 
 /// A credentials username and password pair.

commit aebbe3c830b3f10d95fdb882333c64681b6cb71a

pkg-export-docker: Don't duplicate option aliases
This patch fixes the regression from
110936abd800e2e27c62200144ae56b3af0d9a90 where '-P' option aliases were
added multiple times.

Signed-off-by: Zeeshan Ali <zeenix@gmail.com>

components/pkg-export-docker/src/main.rs
@@ -234,9 +234,9 @@ fn cli<'a, 'b>() -> App<'a, 'b> {
             "Remote registry username, required for pushing image to remote registry")
         (@arg REGISTRY_PASSWORD: --("password") -P +takes_value requires[REGISTRY_USERNAME]
             "Remote registry password, required for pushing image to remote registry")
-        (@arg REGISTRY_TYPE: --("registry-type") -P +takes_value
+        (@arg REGISTRY_TYPE: --("registry-type") -R +takes_value
             "Remote registry type, Ex: Amazon, Docker, Google (default: docker)")
-        (@arg REGISTRY_URL: --("registry-url") -P +takes_value
+        (@arg REGISTRY_URL: --("registry-url") -G +takes_value
             "Remote registry url")
         // Cleanup
         (@arg RM_IMAGE: --("rm-image")