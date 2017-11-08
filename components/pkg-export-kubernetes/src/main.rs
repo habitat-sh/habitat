c5c5e02ab1ffb6d2dc58582253783c0560a03ac5
Zeeshan Ali <zeeshan@kinvolk.io>
2017-11-08 17:07:45 +0100

    pkg-export-kubernetes: Generate docker image
    Make use of docker exporter crate to also generate a docker image as part
    of the process.
    
    Signed-off-by: Zeeshan Ali <zeeshan@kinvolk.io>

--- components/pkg-export-kubernetes/src/main.rs
+++ components/pkg-export-kubernetes/src/main.rs
@@ -12,30 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[macro_use]
 extern crate clap;
 extern crate env_logger;
 extern crate habitat_core as hcore;
 extern crate habitat_common as common;
+extern crate habitat_pkg_export_docker as export_docker;
 extern crate handlebars;
 #[macro_use]
 extern crate serde_json;
 #[macro_use]
 extern crate log;
 
-use clap::App;
+use clap::{App, Arg};
 use handlebars::Handlebars;
-use std::fmt;
+use std::env;
 use std::result;
 use std::str::FromStr;
 use std::io::prelude::*;
 use std::io;
 use std::fs::File;
 
+use hcore::channel;
 use hcore::PROGRAM_NAME;
+use hcore::url as hurl;
 use hcore::env as henv;
 use hcore::package::PackageIdent;
 use common::ui::{Coloring, UI, NOCOLORING_ENVVAR, NONINTERACTIVE_ENVVAR};
+
+use export_docker::{Cli, BuildSpec, Error, Naming};
 
 // Synced with the version of the Habitat operator.
 pub const VERSION: &'static str = "0.1.0";
@@ -45,14 +49,14 @@ const MANIFESTFILE: &'static str = include_str!("../defaults/KubernetesManifest.
 
 fn main() {
     env_logger::init().unwrap();
-    let mut ui = ui();
+    let mut ui = get_ui();
     if let Err(e) = start(&mut ui) {
         let _ = ui.fatal(e);
         std::process::exit(1)
     }
 }
 
-fn ui() -> UI {
+fn get_ui() -> UI {
     let isatty = if henv::var(NONINTERACTIVE_ENVVAR)
         .map(|val| val == "true")
         .unwrap_or(false)
@@ -76,7 +80,25 @@ fn start(ui: &mut UI) -> result::Result<(), Error> {
     let m = cli().get_matches();
     debug!("clap cli args: {:?}", m);
 
+    if !m.is_present("NO_DOCKER_IMAGE") {
+        gen_docker_img(ui, &m)?;
+    }
     gen_k8s_manifest(ui, &m)
+}
+
+fn gen_docker_img(ui: &mut UI, matches: &clap::ArgMatches) -> result::Result<(), Error> {
+    let default_channel = channel::default();
+    let default_url = hurl::default_bldr_url();
+    let spec = BuildSpec::new_from_cli_matches(&matches, &default_channel, &default_url);
+    let naming = Naming::new_from_cli_matches(&matches);
+
+    let docker_image = export_docker::export(ui, spec, &naming)?;
+    docker_image.create_report(
+        ui,
+        env::current_dir()?.join("results"),
+    )?;
+
+    Ok(())
 }
 
 fn gen_k8s_manifest(_ui: &mut UI, matches: &clap::ArgMatches) -> result::Result<(), Error> {
@@ -85,10 +107,10 @@ fn gen_k8s_manifest(_ui: &mut UI, matches: &clap::ArgMatches) -> result::Result<
     let group = matches.value_of("GROUP");
     let config_secret_name = matches.value_of("CONFIG_SECRET_NAME");
     let ring_secret_name = matches.value_of("RING_SECRET_NAME");
-    // clap_app!() ensures that we do have the mandatory args so unwrap() is fine here
-    let pkg_ident_str = matches.value_of("PKG_IDENT").unwrap();
+    // clap ensures that we do have the mandatory args so unwrap() is fine here
+    let pkg_ident_str = matches.value_of("PKG_IDENT_OR_ARTIFACT").unwrap();
     let pkg_ident = PackageIdent::from_str(pkg_ident_str)?;
-    let image = matches.value_of("IMAGE").unwrap_or(pkg_ident_str);
+    let image = matches.value_of("IMAGE_NAME").unwrap_or(pkg_ident_str);
 
     let json = json!({
         "metadata_name": pkg_ident.name,
@@ -117,77 +139,93 @@ fn gen_k8s_manifest(_ui: &mut UI, matches: &clap::ArgMatches) -> result::Result<
 
 fn cli<'a, 'b>() -> App<'a, 'b> {
     let name: &str = &*PROGRAM_NAME;
-    clap_app!((name) =>
-        (about: "Creates a Kubernetes manifest for a Habitat package. Habitat \
-                 operator must be deployed within the Kubernetes cluster to \
-                 intercept the created objects.")
-        (version: VERSION)
-        (author: "\nAuthors: The Habitat Maintainers <humans@habitat.sh>\n\n")
-        (@arg IMAGE: --("image") +takes_value
-            "Image of the Habitat service exported as a Docker image")
-        (@arg OUTPUT: -o --("output") +takes_value
-            "Name of manifest file to create. Pass '-' for stdout (default: -)")
-        (@arg COUNT: --("count") +takes_value {valid_natural_number}
-            "Count is the number of desired instances")
-        (@arg TOPOLOGY: -t --("service-topology") +takes_value
-            "A topology describes the intended relationship between peers \
-             within a service group. Specify either standalone or leader \
-             topology (default: standalone)")
-        (@arg GROUP: -g --("service-group") +takes_value
-            "group is a logical grouping of services with the same package and \
-             topology type connected together in a ring (default: default)")
-        (@arg CONFIG_SECRET_NAME: -n --("config-secret-name") +takes_value
-            "name of the Kubernetes Secret containing the config file - \
-             user.toml - that the user has previously created. Habitat will \
-             use it for initial configuration of the service")
-        (@arg RING_SECRET_NAME: -r --("ring-secret-name") +takes_value
-             "name of the Kubernetes Secret that contains the ring key, which \
-              encrypts the communication between Habitat supervisors")
-        (@arg PKG_IDENT: +required
-            "Habitat package identifier (ex: acme/redis)")
-    )
+    let about = "Creates a docker image and Kubernetes manifest for a Habitat package. Habitat \
+                 operator must be deployed within the Kubernetes cluster to intercept the created \
+                 objects.";
+
+    let app = Cli::new(name, about)
+        .add_base_packages_args()
+        .add_builder_args()
+        .add_tagging_args()
+        .app;
+
+    app.arg(
+        Arg::with_name("OUTPUT")
+            .value_name("OUTPUT")
+            .long("output")
+            .short("o")
+            .help(
+                "Name of manifest file to create. Pass '-' for stdout (default: -)",
+            ),
+    ).arg(
+            Arg::with_name("COUNT")
+                .value_name("COUNT")
+                .long("count")
+                .validator(valid_natural_number)
+                .help("Count is the number of desired instances"),
+        )
+        .arg(
+            Arg::with_name("TOPOLOGY")
+                .value_name("TOPOLOGY")
+                .long("topology")
+                .short("t")
+                .possible_values(&["standalone", "leader"])
+                .help(
+                    "A topology describes the intended relationship between peers \
+                    within a service group. Specify either standalone or leader \
+                    topology (default: standalone)",
+                ),
+        )
+        .arg(
+            Arg::with_name("GROUP")
+                .value_name("GROUP")
+                .long("service-group")
+                .short("g")
+                .help(
+                    "group is a logical grouping of services with the same package and \
+                    topology type connected together in a ring (default: default)",
+                ),
+        )
+        .arg(
+            Arg::with_name("CONFIG_SECRET_NAME")
+                .value_name("CONFIG_SECRET_NAME")
+                .long("config-secret-name")
+                .short("n")
+                .help(
+                    "name of the Kubernetes Secret containing the config file - \
+                    user.toml - that the user has previously created. Habitat will \
+                    use it for initial configuration of the service",
+                ),
+        )
+        .arg(
+            Arg::with_name("RING_SECRET_NAME")
+                .value_name("RING_SECRET_NAME")
+                .long("ring-secret-name")
+                .short("r")
+                .help(
+                    "name of the Kubernetes Secret that contains the ring key, which \
+                    encrypts the communication between Habitat supervisors",
+                ),
+        )
+        .arg(
+            Arg::with_name("NO_DOCKER_IMAGE")
+                .long("no-docker-image")
+                .short("d")
+                .help(
+                    "Disable creation of docker image and only create Kubernetes manifest",
+                ),
+        )
+        .arg(
+            Arg::with_name("PKG_IDENT_OR_ARTIFACT")
+                .value_name("PKG_IDENT_OR_ARTIFACT")
+                .required(true)
+                .help("Habitat package identifier (ex: acme/redis)"),
+        )
 }
 
 fn valid_natural_number(val: String) -> result::Result<(), String> {
     match val.parse::<u32>() {
         Ok(_) => Ok(()),
         Err(_) => Err(format!("{} is not a natural number", val)),
-    }
-}
-
-// TODO: We should remove this Error & all impl below after we start using docker crate
-#[derive(Debug)]
-pub enum Error {
-    HabitatCore(hcore::Error),
-    TemplateRenderError(handlebars::TemplateRenderError),
-    IO(io::Error),
-}
-
-impl From<hcore::Error> for Error {
-    fn from(err: hcore::Error) -> Self {
-        Error::HabitatCore(err)
-    }
-}
-
-impl From<handlebars::TemplateRenderError> for Error {
-    fn from(err: handlebars::TemplateRenderError) -> Self {
-        Error::TemplateRenderError(err)
-    }
-}
-
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Self {
-        Error::IO(err)
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let msg = match *self {
-            Error::HabitatCore(ref err) => format!("{}", err),
-            Error::TemplateRenderError(ref err) => format!("{}", err),
-            Error::IO(ref err) => format!("{}", err),
-        };
-        write!(f, "{}", msg)
     }
 }
