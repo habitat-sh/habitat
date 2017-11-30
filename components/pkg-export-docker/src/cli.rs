068066a89480a78b433d812c3ed3af43dfbd0875
Zeeshan Ali <zeeshan@kinvolk.io>
2017-11-30 13:15:55 +0100

    pkg-export-docker: More concrete examples in help message
    "core/redis" is not a good example for a package to be specified as a
    launcher or supervisor.
    
    Signed-off-by: Zeeshan Ali <zeeshan@kinvolk.io>

--- components/pkg-export-docker/src/cli.rs
+++ components/pkg-export-docker/src/cli.rs
@@ -69,9 +69,10 @@ impl<'a, 'b> Cli<'a, 'b> {
                     .value_name("HAB_LAUNCHER_PKG")
                     .validator(valid_ident_or_hart)
                     .help(
-                        "Launcher package identifier (ex: acme/redis) or filepath to a Habitat \
-                        artifact (ex: /home/acme-redis-3.0.7-21120102031201-x86_64-linux.hart) to \
-                        install (default: core/hab-launcher)",
+                        "Launcher package identifier (ex: core/hab-launcher) or filepath to a \
+                        Habitat artifact (ex: \
+                        /home/core-hab-launcher-6083-20171101045646-x86_64-linux.hart) to install \
+                        (default: core/hab-launcher)",
                     ),
             )
             .arg(
@@ -80,9 +81,10 @@ impl<'a, 'b> Cli<'a, 'b> {
                     .value_name("HAB_SUP_PKG")
                     .validator(valid_ident_or_hart)
                     .help(
-                        "Supervisor package identifier (ex: acme/redis) or filepath to a Habitat \
-                        artifact (ex: /home/acme-redis-3.0.7-21120102031201-x86_64-linux.hart) to \
-                        install (default: core/hab-sup)",
+                        "Supervisor package identifier (ex: core/hab-sup) or filepath to a Habitat \
+                        artifact (ex: \
+                        /home/ore-hab-sup-0.39.1-20171118011657-x86_64-linux.hart) to install \
+                        (default: core/hab-sup)",
                     ),
             );
 
