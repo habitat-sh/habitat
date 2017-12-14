habitat-sh/habitat

commit a0c2e5f42393b4d38767e45b238debda8b0d5e4e

Allow for running containers as a non-root user
In some situations, it can be useful to generate a customized Docker
container with control over the user ID of a service's main user, as
well as whether or not the Supervisor itself should run as root, or as
the service user.

This is motivated by a need to run in certain OpenShift environments.
Note, however, that this does not (yet?) make it possible to run in
_any_ OpenShift environment. In particular, this allows running in
environments where the user ID of the container user is defined (and
not random, as is the default for OpenShift Online, for
instance). Further work is needed to unlock *that* use case.

This commit introduces a number of changes in container generation to
make this possible:

1.) By specifying a value for the `--user-id` option, you can control
the ID for the service user. Previously, this was a hardcoded value of
`42`, which remains the default.

2.) By specifying the flag `--non-root`, the Supervisor will be run as
the service user. This may be used with or without `--user-id`, as
necessary.

3.) In order to support non-root user cases, the `/hab` directory tree
is no longer unconditionally owned by `root`. Instead, if `--non-root`
is specified, the service user owns the directory. The
`/hab/svc/SERVICE/{data,config}` volumes are also owned by this user,
as appropriate.

Be aware that because of this change of permissions, non-root
containers will be larger, due to the additional layer the `chown`
command creates.

4.) To support running as the service user, we no longer
unconditionally create a `hab` user and group. Instead, we consult the
service's `SVC_USER` and `SVC_GROUP` metadata. This allows services to
be run as their appropriate owner.

As an example, if you would like to generate a `core/redis` container
image with the Supervisor running as a non-root service user with a
UID of `12345`, you would run:

    hab pkg export docker core/redis --user-id=12345 --non-root

Fixes #4137

components/pkg-export-docker/src/cli.rs
@@ -144,6 +144,24 @@ impl<'a, 'b> Cli<'a, 'b> {
         Cli { app: app }
     }
 
+    pub fn add_image_customization_args(self) -> Self {
+        let app = self.app
+            .arg(
+                Arg::with_name("USER_ID")
+                    .long("user-id")
+                    .short("i")
+                    .value_name("USER_ID")
+                    .validator(valid_user_id)
+                    .help("Specify the numeric ID of the service user and group (default: 42)")
+            )
+            .arg(
+                Arg::with_name("NON_ROOT")
+                    .long("non-root")
+                    .help("Run the container as a non-root user (default: false)")
+            );
+        Cli { app: app }
+    }
+
     pub fn add_tagging_args(self) -> Self {
         let app = self.app
             .arg(
@@ -262,5 +280,16 @@ fn valid_url(val: String) -> result::Result<(), String> {
     match Url::parse(&val) {
         Ok(_) => Ok(()),
         Err(_) => Err(format!("URL: '{}' is not valid", &val)),
+    }
+}
+
+fn valid_user_id(val: String) -> result::Result<(), String> {
+    match val.parse::<u32>() {
+        Ok(_) => {
+            // TODO (CM): need to filter out problematic
+            // values (e.g., 0, existing user/group IDs)
+            Ok(())
+        }
+        Err(_) => Err(format!("User ID: '{}' is not valid", &val)),
     }
 }

components/pkg-export-docker/src/docker.rs
@@ -390,7 +390,7 @@ impl DockerBuildRoot {
 
     fn add_users_and_groups(&self, ui: &mut UI) -> Result<()> {
         let ctx = self.0.ctx();
-        let (users, groups) = ctx.svc_users_and_groups();
+        let (users, groups) = ctx.svc_users_and_groups()?;
         {
             let file = "etc/passwd";
             let mut f = OpenOptions::new().append(true).open(
@@ -451,13 +451,13 @@ impl DockerBuildRoot {
                 .to_string_lossy()
                 .as_ref(),
             "path": ctx.env_path(),
-            "hab_path": util::pkg_path_for(
-                &PackageIdent::from_str("core/hab")?,
-                ctx.rootfs()
-                        )?.join("bin/hab").to_string_lossy().replace("\\", "/"),
-            "volumes": ctx.svc_volumes().join(" "),
+            "hab_path": util::pkg_path_for(&PackageIdent::from_str("core/hab")?, ctx.rootfs())?.join("bin/hab").to_string_lossy().replace("\\", "/"),
+            "volumes": ctx.svc_volumes(),
             "exposes": ctx.svc_exposes().join(" "),
             "primary_svc_ident": ctx.primary_svc_ident().to_string(),
+            "primary_user_id": ctx.primary_user_id(),
+            // Yup, the group is the same as the user for now.
+            "primary_group_id": ctx.primary_user_id(),
         });
         util::write_file(
             self.0.workdir().join("Dockerfile"),

components/pkg-export-docker/defaults/Dockerfile.hbs
@@ -1,7 +1,32 @@
 FROM scratch
 ENV PATH {{path}}
 ADD {{rootfs}} /
-VOLUME {{volumes}}
+
+# if primary_user_id != 0, chown!
+#
+# We can do this because a primary_user_id of 0 (i.e., root) is
+# "falsy".  There's no reason to do this if the primary user is root,
+# because root already owns all of /hab (because that's how ADD always
+# works)
+#
+# (Also, sorry for `-R` instead of `--recursive`; Busybox doesn't
+# recognize the latter :'(
+{{#if primary_user_id }}
+RUN chown -R {{primary_user_id}}:{{primary_user_id}} /hab
+
+# This environment variable disables certain checks around installing
+# packages (important for upgrade scenarios!). It is a short-term fix;
+# long-term, we should just check to see if the user can write to the
+# various directories under /hab that it needs.
+ENV HAB_NON_ROOT=1
+{{/if}}
+
+USER {{primary_user_id}}:{{primary_group_id}}
+
+{{#each volumes}}
+RUN mkdir -p {{this}}
+VOLUME {{this}}
+{{/each}}
 EXPOSE 9631 {{exposes}}
 ENTRYPOINT ["/init.sh"]
 CMD ["start", "{{primary_svc_ident}}"]

components/pkg-export-docker/defaults/Dockerfile_win.hbs
@@ -1,5 +1,7 @@
 FROM microsoft/windowsservercore
 ADD {{rootfs}} /
-VOLUME {{volumes}}
+{{#each volumes}}
+VOLUME {{this}}
+{{/each}}
 EXPOSE 9631 {{exposes}}
 ENTRYPOINT ["{{hab_path}}", "sup", "start", "{{primary_svc_ident}}"]

components/pkg-export-docker/src/build.rs
@@ -39,6 +39,7 @@ use util;
 const DEFAULT_HAB_IDENT: &'static str = "core/hab";
 const DEFAULT_LAUNCHER_IDENT: &'static str = "core/hab-launcher";
 const DEFAULT_SUP_IDENT: &'static str = "core/hab-sup";
+const DEFAULT_USER_ID: u32 = 42;
 
 /// The specification for creating a temporary file system build root, based on Habitat packages.
 ///
@@ -64,6 +65,10 @@ pub struct BuildSpec<'a> {
     /// A list of either Habitat Package Identifiers or local paths to Habitat Artifact files which
     /// will be installed.
     pub idents_or_archives: Vec<&'a str>,
+    /// Numeric user ID of the user / group
+    pub user_id: u32,
+    /// Run the container as a non-root user?
+    pub non_root: bool,
 }
 
 impl<'a> BuildSpec<'a> {
@@ -84,6 +89,14 @@ impl<'a> BuildSpec<'a> {
             base_pkgs_url: m.value_of("BASE_PKGS_BLDR_URL").unwrap_or(&default_url),
             base_pkgs_channel: m.value_of("BASE_PKGS_CHANNEL").unwrap_or(&default_channel),
             idents_or_archives: m.values_of("PKG_IDENT_OR_ARTIFACT").unwrap().collect(),
+            user_id: match m.value_of("USER_ID") {
+                Some(i) => {
+                    // unwrap OK because validation function ensures it
+                    i.parse::<u32>().unwrap()
+                }
+                None => DEFAULT_USER_ID,
+            },
+            non_root: m.is_present("NON_ROOT"),
         }
     }
 
@@ -369,6 +382,11 @@ pub struct BuildRootContext {
     channel: String,
     /// The path to the root of the file system.
     rootfs: PathBuf,
+    /// The user ID of the primary service user
+    user_id: u32,
+    /// Whether or not the container should be tailored to run Habitat
+    /// as a non-root user
+    non_root: bool,
 }
 
 impl BuildRootContext {
@@ -416,6 +434,8 @@ impl BuildRootContext {
             env_path: bin_path.to_string_lossy().into_owned(),
             channel: spec.channel.into(),
             rootfs: rootfs,
+            user_id: spec.user_id,
+            non_root: spec.non_root,
         };
         context.validate()?;
 
@@ -440,13 +460,17 @@ impl BuildRootContext {
         )
     }
 
+    fn primary_svc(&self) -> Result<PackageInstall> {
+        PackageInstall::load(self.primary_svc_ident(), Some(&self.rootfs)).map_err(From::from)
+    }
+
     /// Returns the fully qualified Package Identifier for the first service package.
     ///
     /// # Errors
     ///
     /// * If the primary service package could not be loaded from disk
     pub fn installed_primary_svc_ident(&self) -> Result<PackageIdent> {
-        let pkg_install = PackageInstall::load(self.primary_svc_ident(), Some(&self.rootfs))?;
+        let pkg_install = self.primary_svc()?;
         Ok(pkg_install.ident().clone())
     }
 
@@ -482,21 +506,33 @@ impl BuildRootContext {
 
     /// Returns a tuple of users to be added to the image's passwd database and groups to be added
     /// to the image's group database.
-    pub fn svc_users_and_groups(&self) -> (Vec<String>, Vec<String>) {
+    pub fn svc_users_and_groups(&self) -> Result<(Vec<String>, Vec<String>)> {
         let mut users = Vec::new();
         let mut groups = Vec::new();
-        let id = 42;
+        let id = self.user_id;
 
-        users.push(format!(
-            "hab:x:{uid}:{gid}:hab User:/:/bin/false\n",
-            uid = id,
-            gid = id
-        ));
-        groups.push(format!("hab:x:{gid}:hab\n", gid = id));
-
+        let pkg = self.primary_svc()?;
+        let user_name = pkg.svc_user().unwrap_or(Some(String::from("hab"))).unwrap();
+        let group_name = pkg.svc_group()
+            .unwrap_or(Some(String::from("hab")))
+            .unwrap();
+        if user_name != "root" {
+            users.push(format!(
+                "{name}:x:{uid}:{gid}:{name} User:/:/bin/false\n",
+                name = user_name,
+                uid = id,
+                gid = id
+            ));
+            groups.push(format!(
+                "{name}:x:{gid}:{user_name}\n",
+                name = group_name,
+                gid = id,
+                user_name = user_name
+            ));
+        }
         // TODO fn: add remaining missing users and groups from service packages
 
-        (users, groups)
+        Ok((users, groups))
     }
 
     /// Returns the `bin` path which is used for all program symlinking.
@@ -517,6 +553,10 @@ impl BuildRootContext {
     /// Returns the root file system which is used to export an image.
     pub fn rootfs(&self) -> &Path {
         self.rootfs.as_ref()
+    }
+
+    pub fn primary_user_id(&self) -> u32 {
+        if self.non_root { self.user_id } else { 0 }
     }
 
     fn validate(&self) -> Result<()> {
@@ -598,6 +638,8 @@ mod test {
             base_pkgs_url: "base_pkgs_url",
             base_pkgs_channel: "base_pkgs_channel",
             idents_or_archives: Vec::new(),
+            user_id: 42,
+            non_root: false,
         }
     }
 
@@ -617,6 +659,10 @@ mod test {
         let prefix = hcore::fs::pkg_install_path(&ident, Some(rootfs));
         util::write_file(prefix.join("IDENT"), &ident.to_string()).unwrap();
         util::write_file(prefix.join("TARGET"), &PackageTarget::default().to_string()).unwrap();
+
+        util::write_file(prefix.join("SVC_USER"), "my_user").unwrap();
+        util::write_file(prefix.join("SVC_GROUP"), "my_group").unwrap();
+
         if let Some(bins) = bins {
             util::write_file(
                 prefix.join("PATH"),
@@ -867,11 +913,11 @@ mod test {
                 HashSet::from_iter(ctx.svc_volumes().iter().cloned())
             );
 
-            let (users, groups) = ctx.svc_users_and_groups();
+            let (users, groups) = ctx.svc_users_and_groups().unwrap();
             assert_eq!(1, users.len());
-            assert!(users[0].starts_with("hab:"));
+            assert!(users[0].starts_with("my_user:"));
             assert_eq!(1, groups.len());
-            assert!(groups[0].starts_with("hab:"));
+            assert!(groups[0].starts_with("my_group:"));
             // TODO fn: check ctx.svc_exposes()
         }
     }

components/pkg-export-docker/src/main.rs
@@ -81,6 +81,7 @@ fn cli<'a, 'b>() -> App<'a, 'b> {
     let app = Cli::new(name, about)
         .add_base_packages_args()
         .add_builder_args()
+        .add_image_customization_args()
         .add_tagging_args()
         .add_publishing_args()
         .app;

commit 69a6e8af052802b3b6751745ef14aec0c22af070

pkg-export-docker: Fix rustfmt breakage
Commits 967dfef7 and a0c2e5f4 broke rustfmt check. This patch fixes it.

Signed-off-by: Zeeshan Ali <zeeshan@kinvolk.io>

components/pkg-export-docker/src/cli.rs
@@ -152,13 +152,13 @@ impl<'a, 'b> Cli<'a, 'b> {
                     .short("i")
                     .value_name("USER_ID")
                     .validator(valid_user_id)
-                    .help("Specify the numeric ID of the service user and group (default: 42)")
+                    .help(
+                        "Specify the numeric ID of the service user and group (default: 42)",
+                    ),
             )
-            .arg(
-                Arg::with_name("NON_ROOT")
-                    .long("non-root")
-                    .help("Run the container as a non-root user (default: false)")
-            );
+            .arg(Arg::with_name("NON_ROOT").long("non-root").help(
+                "Run the container as a non-root user (default: false)",
+            ));
         Cli { app: app }
     }
 
@@ -247,8 +247,8 @@ impl<'a, 'b> Cli<'a, 'b> {
                  .value_name("REGISTRY_TYPE")
                  .help("Remote registry type, Ex: Amazon, Docker (default: docker)"))
             .arg(Arg::with_name("REGISTRY_URL")
-                 // This is not strictly a requirement but will keep someone from making a mistake when
-                 // inputing an ECR URL
+                 // This is not strictly a requirement but will keep someone from making a mistake
+                 // when inputing an ECR URL
                  .requires("REGISTRY_TYPE")
                  .long("registry-url")
                  .short("G")

components/pkg-export-docker/src/docker.rs
@@ -451,7 +451,11 @@ impl DockerBuildRoot {
                 .to_string_lossy()
                 .as_ref(),
             "path": ctx.env_path(),
-            "hab_path": util::pkg_path_for(&PackageIdent::from_str("core/hab")?, ctx.rootfs())?.join("bin/hab").to_string_lossy().replace("\\", "/"),
+            "hab_path": util::pkg_path_for(
+                &PackageIdent::from_str("core/hab")?,
+                ctx.rootfs())?.join("bin/hab")
+                .to_string_lossy()
+                .replace("\\", "/"),
             "volumes": ctx.svc_volumes(),
             "exposes": ctx.svc_exposes().join(" "),
             "primary_svc_ident": ctx.primary_svc_ident().to_string(),