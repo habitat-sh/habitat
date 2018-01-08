751fa62a095f6c5fdd1783685c71b56a9bec343f
Scott Macfarlane <macfarlane.scott@gmail.com>
2018-01-08 15:12:03 -0800

    Fix bldr job promote --interactive to inject newlines in pkg ident strings before passing to an editor
    Signed-off-by: Scott Macfarlane <macfarlane.scott@gmail.com>

--- components/hab/src/command/bldr/job/promote.rs
+++ components/hab/src/command/bldr/job/promote.rs
@@ -17,7 +17,7 @@ use hcore::package::PackageIdent;
 use std::str::FromStr;
 
 use api_client;
-use depot_client;
+use depot_client::{self, SchedulerResponse};
 use common::ui::{Status, UI};
 
 use {PRODUCT, VERSION};
@@ -38,18 +38,10 @@ fn in_origin(ident: &str, origin: Option<&str>) -> bool {
 
 pub fn get_ident_list(
     ui: &mut UI,
-    bldr_url: &str,
-    group_id: u64,
+    group_status: &SchedulerResponse,
     origin: Option<&str>,
     interactive: bool,
 ) -> Result<Vec<String>> {
-    let depot_client = depot_client::Client::new(bldr_url, PRODUCT, VERSION, None)
-        .map_err(Error::DepotClient)?;
-
-    let group_status = depot_client.get_schedule(group_id as i64).map_err(|e| {
-        Error::ScheduleStatus(e)
-    })?;
-
     let mut idents: Vec<String> = group_status
         .projects
         .iter()
@@ -69,6 +61,8 @@ pub fn get_ident_list(
 
     idents.insert(0, prelude);
 
+    idents = idents.iter().map(|s| format!("{}\n", s)).collect();
+
     Ok(
         ui.edit(&idents)?
             .split("\n")
@@ -76,6 +70,17 @@ pub fn get_ident_list(
             .map(|s: &str| s.to_string())
             .collect(),
     )
+}
+
+fn get_group_status(bldr_url: &str, group_id: u64) -> Result<SchedulerResponse> {
+    let depot_client = depot_client::Client::new(bldr_url, PRODUCT, VERSION, None)
+        .map_err(Error::DepotClient)?;
+
+    let group_status = depot_client.get_schedule(group_id as i64).map_err(|e| {
+        Error::ScheduleStatus(e)
+    })?;
+
+    Ok(group_status)
 }
 
 pub fn start(
@@ -118,7 +123,8 @@ pub fn start(
         }
     };
 
-    let idents = get_ident_list(ui, bldr_url, gid, origin, interactive)?;
+    let group_status = get_group_status(bldr_url, gid)?;
+    let idents = get_ident_list(ui, &group_status, origin, interactive)?;
 
     if idents.len() == 0 {
         ui.warn("No matching packages found")?;
@@ -175,4 +181,121 @@ pub fn start(
     };
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use std::env;
+    use std::io::{self, Cursor, Write};
+    use std::sync::{Arc, RwLock};
+
+    use common::ui::{Coloring, UI};
+    use depot_client::{Project, SchedulerResponse};
+    use super::get_ident_list;
+
+    fn sample_project_list() -> Vec<Project> {
+        let project1 = Project {
+            name: "Project1".to_string(),
+            ident: "core/project1/1.0.0/20180101000000".to_string(),
+            state: "Success".to_string(),
+            job_id: "12345678".to_string(),
+        };
+        let project2 = Project {
+            name: "Project2".to_string(),
+            ident: "core/project2/1.0.0/20180101000000".to_string(),
+            state: "Success".to_string(),
+            job_id: "12345678".to_string(),
+        };
+
+        vec![project1, project2]
+    }
+
+    fn ui() -> (UI, OutputBuffer, OutputBuffer) {
+        let stdout_buf = OutputBuffer::new();
+        let stderr_buf = OutputBuffer::new();
+
+        let ui = UI::with_streams(
+            Box::new(io::empty()),
+            || Box::new(stdout_buf.clone()),
+            || Box::new(stderr_buf.clone()),
+            Coloring::Never,
+            false,
+        );
+
+        (ui, stdout_buf, stderr_buf)
+    }
+
+    #[derive(Clone)]
+    pub struct OutputBuffer {
+        pub cursor: Arc<RwLock<Cursor<Vec<u8>>>>,
+    }
+
+    impl OutputBuffer {
+        fn new() -> Self {
+            OutputBuffer { cursor: Arc::new(RwLock::new(Cursor::new(Vec::new()))) }
+        }
+    }
+
+    impl Write for OutputBuffer {
+        fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+            self.cursor
+                .write()
+                .expect("Cursor lock is poisoned")
+                .write(buf)
+        }
+
+        fn flush(&mut self) -> io::Result<()> {
+            self.cursor
+                .write()
+                .expect("Cursor lock is poisoned")
+                .flush()
+        }
+    }
+
+    #[test]
+    fn test_get_ident_list() {
+        let (mut ui, _stdout, _stderr) = ui();
+        let group_status = SchedulerResponse {
+            id: "12345678".to_string(),
+            state: "Finished".to_string(),
+            projects: sample_project_list(),
+            created_at: "Properly formated timestamp".to_string(),
+            project_name: "Test Project".to_string(),
+        };
+
+        let ident_list = get_ident_list(&mut ui, &group_status, Some("core"), false)
+            .expect("Error fetching ident list");
+
+        assert_eq!(
+            ident_list,
+            [
+                "core/project1/1.0.0/20180101000000",
+                "core/project2/1.0.0/20180101000000",
+            ]
+        )
+    }
+
+    #[test]
+    fn test_get_ident_list_interactive() {
+        let (mut ui, _stdout, _stderr) = ui();
+        let group_status = SchedulerResponse {
+            id: "12345678".to_string(),
+            state: "Finished".to_string(),
+            projects: sample_project_list(),
+            created_at: "Properly formated timestamp".to_string(),
+            project_name: "Test Project".to_string(),
+        };
+        env::set_var("EDITOR", "cat");
+
+        let ident_list = get_ident_list(&mut ui, &group_status, Some("core"), true)
+            .expect("Error fetching ident list");
+
+        assert_eq!(
+            ident_list,
+            [
+                "core/project1/1.0.0/20180101000000",
+                "core/project2/1.0.0/20180101000000",
+            ]
+        )
+    }
 }
