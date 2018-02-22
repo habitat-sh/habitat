92ba991bdf590fb69fec04059683ba83ab1a799f
Elliott Davis <elliott@excellent.io>
2018-02-22 10:54:49 -0600

    Add command to download encryption key
    `hab origin key download --encryption <ORIGIN>`
    
    Signed-off-by: Elliott Davis <elliott@excellent.io>

--- components/hab/src/command/origin/key/download.rs
+++ components/hab/src/command/origin/key/download.rs
@@ -30,6 +30,7 @@ pub fn start(
     origin: &str,
     revision: Option<&str>,
     secret: bool,
+    encryption: bool,
     token: Option<&str>,
     cache: &Path,
 ) -> Result<()> {
@@ -37,6 +38,8 @@ pub fn start(
 
     if secret {
         handle_secret(ui, &depot_client, origin, token, cache)
+    } else if encryption {
+        handle_encryption(ui, &depot_client, origin, token, cache)
     } else {
         handle_public(ui, &depot_client, origin, revision, cache)
     }
@@ -117,6 +120,68 @@ fn handle_secret(
         "Download of {} public origin keys completed.",
         &origin
     ))?;
+    Ok(())
+}
+
+fn handle_encryption(
+    ui: &mut UI,
+    depot_client: &Client,
+    origin: &str,
+    token: Option<&str>,
+    cache: &Path,
+) -> Result<()> {
+    if token.is_none() {
+        ui.end(format!(
+            "No auth token found. You must pass a token to download secret keys."
+        ))?;
+        return Ok(());
+    }
+
+    ui.begin(
+        format!("Downloading public encryption origin key for {}", origin),
+    )?;
+    download_public_encryption_key(ui, &depot_client, origin, token.unwrap(), cache)?; // unwrap is safe because we already checked it above
+    ui.end(format!(
+        "Download of {} public encryption keys completed.",
+        &origin
+    ))?;
+    Ok(())
+}
+
+fn download_public_encryption_key(
+    ui: &mut UI,
+    depot_client: &Client,
+    name: &str,
+    token: &str,
+    cache: &Path,
+) -> Result<()> {
+    let download_fn = || -> Result<()> {
+        ui.status(Status::Downloading, "latest public encryption key")?;
+        let key_path = depot_client.fetch_origin_public_encryption_key(
+            name,
+            token,
+            cache,
+            ui.progress(),
+        )?;
+        ui.status(
+            Status::Cached,
+            key_path.file_name().unwrap().to_str().unwrap(), // lol
+        )?;
+        Ok(())
+    };
+
+    if retry(RETRIES, RETRY_WAIT, download_fn, |res| res.is_ok()).is_err() {
+        return Err(Error::from(depot_client::Error::DownloadFailed(format!(
+            "We tried {} \
+                                                                            times but \
+                                                                            could not \
+                                                                            download the latest \
+                                                                            public encryption key. \
+                                                                            Giving up.",
+            RETRIES,
+        ))));
+    }
+
     Ok(())
 }
 
