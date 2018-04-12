af7af4a08142866a57cd5310bb1d6ddba8f35000
Fletcher Nichol <fnichol@nichol.ca>
2018-04-12 17:55:49 -0600

    [http-client] Address compiler warnings in api-client module.
    Removes the following warnings:
    
    ```
    warning: use of deprecated item 'openssl::ssl::SslConnectorBuilder::builder_mut': SslConnectorBuilder now implements DerefMut<Target=SslContextBuilder>
       --> components/http-client/src/api_client.rs:317:22
        |
    317 |     ssl::set_ca(conn.builder_mut(), fs_root_path)?;
        |                      ^^^^^^^^^^^
        |
        = note: #[warn(deprecated)] on by default
    
    warning: use of deprecated item 'openssl::ssl::SslConnectorBuilder::builder_mut': SslConnectorBuilder now implements DerefMut<Target=SslContextBuilder>
       --> components/http-client/src/api_client.rs:318:10
        |
    318 |     conn.builder_mut().set_options(options);
        |          ^^^^^^^^^^^
    
    warning: use of deprecated item 'openssl::ssl::SslConnectorBuilder::builder_mut': SslConnectorBuilder now implements DerefMut<Target=SslContextBuilder>
       --> components/http-client/src/api_client.rs:319:10
        |
    319 |     conn.builder_mut().set_cipher_list(
        |          ^^^^^^^^^^^
    
    ```
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- src/api_client.rs
+++ src/api_client.rs
@@ -314,9 +314,9 @@ fn ssl_connector(fs_root_path: Option<&Path>) -> Result<SslConnector> {
     options.toggle(SSL_OP_NO_SSLV2);
     options.toggle(SSL_OP_NO_SSLV3);
     options.toggle(SSL_OP_NO_COMPRESSION);
-    ssl::set_ca(conn.builder_mut(), fs_root_path)?;
-    conn.builder_mut().set_options(options);
-    conn.builder_mut().set_cipher_list(
+    ssl::set_ca(&mut conn, fs_root_path)?;
+    conn.set_options(options);
+    conn.set_cipher_list(
         "ALL!EXPORT!EXPORT40!EXPORT56!aNULL!LOW!RC4@STRENGTH",
     )?;
     Ok(conn.build())
