53e84be3a0b9ae00cc4ce1fba60adbb68b1e135d
Jon Bauman <5906042+baumanj@users.noreply.github.com>
2018-06-26 11:50:59 -0400

    Fix shellcheck lints instead of just suppressing them
    Signed-off-by: Jon Bauman <5906042+baumanj@users.noreply.github.com>

--- components/plan-build/bin/composite_build_functions.bash
+++ components/plan-build/bin/composite_build_functions.bash
@@ -1,11 +1,10 @@
 #!/bin/bash
-# shellcheck disable=2086,2124,2154,2162,1097,2068,2180
-# ^ FIXME, see https://github.com/habitat-sh/habitat/pull/4974#issuecomment-395916191
 # These functions are used when building a composite package.
 
 # Ensure that a composite package is internally consistent.
 _validate_composite() {
     _assert_more_than_one_service
+    # shellcheck disable=2154
     _resolve_service_dependencies "${pkg_services[@]}"
     _validate_services
 
@@ -32,7 +31,7 @@ _setup_pkg_export_map(){
 _setup_global_associative_array(){
   local var_name=${1}
   debug "Creating '${var_name}' global associative array"
-  declare -A -g ${var_name}
+  declare -A -g "${var_name}"
 }
 
 _setup_composite_build_global_variables(){
@@ -66,7 +65,7 @@ _resolve_service_dependencies() {
     for service in "${services[@]}"; do
       build_line "Installing ${service} locally"
       _install_dependency "${service}"
-      if resolved="$(_resolve_dependency $service)"; then
+      if resolved="$(_resolve_dependency "$service")"; then
         build_line "Resolved service '$service' to $resolved"
         resolved_services[$service]=$resolved
       else
@@ -117,8 +116,8 @@ _resolve_all_exports() {
 
   for rs in "${!resolved_services[@]}"; do
     resolved=${resolved_services[$rs]}
-    exports=("$(_exports_for_pkg ${resolved})")
-    pkg_export_map[$resolved]="${exports[@]}"
+    exports=("$(_exports_for_pkg "${resolved}")")
+    pkg_export_map[$resolved]="${exports[*]}"
   done
 }
 
@@ -127,6 +126,7 @@ _resolve_all_exports() {
 # * The binds are actually defined for the given package
 # * The package that satisfies the bind actually exports what the bind requires
 _validate_bind_mappings() {
+  # shellcheck disable=2154
   for pkg in "${!pkg_bind_map[@]}"; do
     debug "Resolving binds for ${pkg}"
 
@@ -144,17 +144,17 @@ _validate_bind_mappings() {
 
     if [[ -f "${resolved}/BINDS" ]]; then
       while read -r line; do
-        IFS== read bind_name exports <<< "${line}"
-        all_binds_for_pkg[$bind_name]="${exports[@]}"
+        IFS="=" read -r bind_name exports <<< "${line}"
+        all_binds_for_pkg[$bind_name]="${exports[*]}"
       done < <(_read_metadata_file_for "${resolved}" BINDS)
     fi
     if [[ -f "${resolved}/BINDS_OPTIONAL" ]]; then
       while read -r line; do
-        IFS== read bind_name exports <<< "${line}"
+        IFS="=" read -r bind_name exports <<< "${line}"
         if [[ -n "${all_binds_for_pkg[$bind_name]}" ]]; then
           exit_with "The bind ${bind_name} has already been declared in pkg_binds for package ${pkg}, it cannot also be declared in pkg_binds_optional"
         fi
-        all_binds_for_pkg[$bind_name]="${exports[@]}"
+        all_binds_for_pkg[$bind_name]="${exports[*]}"
       done < <(_read_metadata_file_for "${resolved}" BINDS_OPTIONAL)
     fi
 
@@ -162,10 +162,11 @@ _validate_bind_mappings() {
     bind_mappings=("${pkg_bind_map[$pkg]}")
 
     # This is space-delimited, so no quotes
+    # shellcheck disable=2068,2180
     for mapping in ${bind_mappings[@]}; do
       # Each mapping is of the form `bind_name:package`, like so:
       #     router:core/builder-router
-      IFS=: read bind_name satisfying_package <<< "${mapping}"
+      IFS=: read -r bind_name satisfying_package <<< "${mapping}"
 
       # Assert that the named bind exists
       debug "Verifying that ${resolved} has a bind named '${bind_name}'"
@@ -181,7 +182,7 @@ _validate_bind_mappings() {
       # Assert that the mapped service satisfies all the exports
       # of this bind
       for required_exported_value in ${all_binds_for_pkg[$bind_name][@]}; do
-        if ! _array_contains "$required_exported_value" ${satisfying_package_exports[@]}; then
+        if ! _array_contains "$required_exported_value" "${satisfying_package_exports[@]}"; then
           exit_with "${satisfying_package} does not export '${required_exported_value}', which is required by the '${bind_name}' bind of ${resolved}"
         fi
       done
@@ -216,16 +217,17 @@ _render_metadata_SERVICES() {
   deps="$(printf '%s\n' "${pkg_services[@]}" | sort)"
   if [[ -n "$deps" ]]; then
     debug "Rendering SERVICES metadata file"
-    echo "$deps" > $pkg_prefix/SERVICES
+    # shellcheck disable=2154
+    echo "$deps" > "$pkg_prefix"/SERVICES
   fi
 }
 
 # Render the services AS RESOLVED at build time; when you install the
 # composite, these are the releases that will be downloaded.
 _render_metadata_RESOLVED_SERVICES() {
-    _render_dependency_metadata_file $pkg_prefix RESOLVED_SERVICES resolved_services
+    _render_dependency_metadata_file "$pkg_prefix" RESOLVED_SERVICES resolved_services
 }
 
 _render_metadata_BIND_MAP() {
-  _render_associative_array_file ${pkg_prefix} BIND_MAP pkg_bind_map
+  _render_associative_array_file "${pkg_prefix}" BIND_MAP pkg_bind_map
 }
