40ef19966bb84f8b35b1dc20d83e3d3f8530e159
Elliott Davis <elliott@excellent.io>
2018-08-15 09:30:00 -0500

    move build logic for rootless studio into buildkite
    Signed-off-by: Elliott Davis <elliott@excellent.io>

--- components/rootless_studio/build-docker-image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source ./docker-base.sh
-
-docker build -t habitat:hab-base .
-
-docker build --build-arg BLDR_CHANNEL="${HAB_BLDR_CHANNEL}" --no-cache -t "${IMAGE_NAME}:${VERSION}" "./${STUDIO_TYPE}"
