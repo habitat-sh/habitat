84582d09cb5db538c64cf23a99c41c0cf05ca509
Scott Macfarlane <smacfarlane@chef.io>
2018-10-19 13:54:44 -0700

    Move set_hab_binary to shared.sh, so that it can be reused in bintray_upload
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- .buildkite/scripts/build_component.sh
+++ .buildkite/scripts/build_component.sh
@@ -4,68 +4,6 @@ set -euo pipefail
 
 source .buildkite/scripts/shared.sh
 
-# Until we have built both a new core/hab _and_ a new core/hab-studio
-# package, we should continue to use the `hab` binary provided on our
-# Buildkite builders (managed by Release Engineering) (these should be
-# the latest stable release, btw).
-#
-# Once we have bootstrapped ourselves enough, however, we should
-# switch subsequent builds to use the new hab, which in turn uses the
-# new studio.
-set_hab_binary() {
-    echo "--- :thinking_face: Determining which 'hab' binary to use"
-    # This ensure the hab cli we use going forward has the correct
-    # ActiveTarget. Otherwise, if we were to attempt to install an
-    # `x86_64-linux-kernel2` package with the `hab` on our path, it
-    # would result in an error and fail the build.
-    if [[ "$BUILD_PKG_TARGET" == "x86_64-linux-kernel2" ]]; do
-        install_hab_kernel2_binary
-        hab_binary="$(which hab-x86_64-linux-kernel2)"
-    else 
-        hab_binary="$(which hab)"
-    fi 
-
-    if buildkite-agent meta-data exists hab-version &&
-            buildkite-agent meta-data exists studio-version; then
-        echo "Buildkite metadata found; installing new versions of 'core/hab' and 'core/hab-studio'"
-        # By definition, these will be fully-qualified identifiers,
-        # and thus do not require a `--channel` option. However, they
-        # should be coming from the release channel, and should be the
-        # same packages built previously in this same release pipeline.
-        hab_ident=$(buildkite-agent meta-data get hab-version)
-
-        # Note that we are explicitly not binlinking here; this is to
-        # prevent accidentally polluting the builder for any future
-        # runs that may take place on it.
-        sudo hab pkg install "${hab_ident}"
-        sudo hab pkg install "$(buildkite-agent meta-data get studio-version)"
-        hab_binary="/hab/pkgs/${hab_ident}/bin/hab"
-        declare -g new_studio=1
-    else
-        echo "Buildkite metadata NOT found; using previously-installed hab binary: $hab_binary"
-    fi
-    declare -g hab_binary
-    echo "--- :habicat: Using $(${hab_binary} --version)"
-}
-
-# This installation step is a temporary shim until we have done at
-# least one release. Once we have a release, we can update ci-studio-common
-# to fetch this binary from bintray using the install.sh script and the install
-# step is no longer needed. Until then, we need to fetch it from our 
-# bootstrap pipeline. 
-install_hab_kernel2_binary() {
-    local hab_src_url tempdir
-    hab_src_url="http://habitat-boostrap-artifacts.s3.amazonaws.com/x86_64-linux-kernel2/stage2/habitat-stage2-x86_64-linux-kernel2-latest"
-    tempdir=$(mktemp -d hab-kernel2-XXXX)
-
-    pushd $tmpdir >/dev/null
-    curl "$hab_src_url" -o hab-x86_64-linux-kernel2
-    sudo mv hab-x86_64-linux-kernel2 /bin/hab-x86_64-linux-kernel2
-    sudo chmod +x /bin/hab-x86_64-linux-kernel2
-    popd 
-    rm -rf "$tmpdir"
-}
-
 ########################################################################
 
 # `component` should be the subdirectory name in `components` where a
