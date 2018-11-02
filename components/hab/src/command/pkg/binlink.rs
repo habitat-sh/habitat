bc5de6d581f2e608f90c385fad42bf0e0eb8c33f
Scott Macfarlane <smacfarlane@chef.io>
2018-11-02 07:50:58 -0700

    Correct cfg to properly reflect test targets
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- components/hab/src/command/pkg/binlink.rs
+++ components/hab/src/command/pkg/binlink.rs
@@ -253,7 +253,7 @@ impl Binlink {
 }
 
 #[cfg(test)]
-#[cfg(any(linux, windows))]
+#[cfg(any(target_os = "linux", target_os = "windows"))]
 mod test {
     use std::collections::HashMap;
     use std::env;
