ac5ebe270a11a1e567b5174fc22941e9b2e9d35e
Scott Macfarlane <smacfarlane@chef.io>
2018-11-14 13:17:23 -0800

    Fix dockerfile and build issues
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- .buildkite/scripts/dockerhub_upload.sh
+++ .buildkite/scripts/dockerhub_upload.sh
@@ -15,7 +15,7 @@ image_name="habitat/default-studio-${target}"
 image_name_with_tag="${image_name}:${version}"
 
 if is_fake_release; then
-  image_name="habitat/fakey-mc-fake-face-studio"
+  image_name_with_tag="habitat/fakey-mc-fake-face-studio"
 fi
 
 docker login \
@@ -26,7 +26,7 @@ trap 'rm -f $HOME/.docker/config.json' INT TERM EXIT
 
 pushd ./components/rootless_studio >/dev/null
 docker build --build-arg PACKAGE_TARGET="${target}" -t "habitat-${target}:hab-base" .
-docker build --build-arg BLDR_CHANNEL="${channel}" --no-cache --tag "${image_name_with_tag}" ./default
+docker build --build-arg BLDR_CHANNEL="${channel}" --build-arg PACKAGE_TARGET="${target}" --no-cache --tag "${image_name_with_tag}" ./default
 docker push "${image_name_with_tag}"
 popd >/dev/null
 
