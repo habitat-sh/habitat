027e8f063fa486a6c196f8588c86677dbb183da0
Scott Hain <shain@chef.io>
2019-02-14 11:31:20 -0800

    Update correct lints
    Signed-off-by: Scott Hain <shain@chef.io>

--- test/run_clippy.sh
+++ test/run_clippy.sh
@@ -34,77 +34,76 @@ export PATH=/opt/rust/bin:$PATH
 sudo chown -R buildkite-agent /home/buildkite-agent
 
  # Lints we need to work through and decide as a team whether to allow or fix
-unexamined_lints=("clippy::cyclomatic_complexity" \
-                  "clippy::large_enum_variant" \
-                  "clippy::len_without_is_empty" \
-                  "clippy::module_inception" \
-                  "clippy::needless_pass_by_value" \
-                  "clippy::needless_return" \
-                  "clippy::new_ret_no_self" \
-                  "clippy::new_without_default" \
-                  "clippy::new_without_default_derive" \
-                  "clippy::question_mark" \
-                  "clippy::redundant_field_names" \
-                  "clippy::too_many_arguments" \
-                  "clippy::trivially_copy_pass_by_ref" \
-                  "clippy::wrong_self_convention" \
-                  "renamed_and_removed_lints")
+unexamined_lints=("")
 
  # Lints we disagree with and choose to keep in our code with no warning
-allowed_lints=("")
+allowed_lints=("clippy::module_inception" \
+                "clippy::new_ret_no_self" \
+                "clippy::new_without_default" \
+                "clippy::new_without_default_derive")
 
  # Known failing lints we want to receive warnings for, but not fail the build
-lints_to_fix=("")
+lints_to_fix=("clippy::cyclomatic_complexity" \
+               "clippy::large_enum_variant" \
+               "clippy::len_without_is_empty" \
+               "clippy::needless_pass_by_value" \
+               "clippy::needless_return" \
+               "clippy::question_mark" \
+               "clippy::redundant_field_names" \
+               "clippy::too_many_arguments" \
+               "clippy::trivially_copy_pass_by_ref" \
+               "clippy::wrong_self_convention")
 
  # Lints we don't expect to have in our code at all and want to avoid adding
 # even at the cost of failing the build
 denied_lints=("clippy::assign_op_pattern" \
-              "clippy::blacklisted_name" \
-              "clippy::block_in_if_condition_stmt" \
-              "clippy::bool_comparison" \
-              "clippy::cast_lossless" \
-              "clippy::clone_on_copy" \
-              "clippy::cmp_owned" \
-              "clippy::collapsible_if" \
-              "clippy::const_static_lifetime" \
-              "clippy::correctness" \
-              "clippy::deref_addrof" \
-              "clippy::expect_fun_call" \
-              "clippy::for_kv_map" \
-              "clippy::get_unwrap" \
-              "clippy::identity_conversion" \
-              "clippy::if_let_some_result" \
-              "clippy::len_zero" \
-              "clippy::let_and_return" \
-              "clippy::let_unit_value" \
-              "clippy::map_clone" \
-              "clippy::match_bool" \
-              "clippy::match_ref_pats" \
-              "clippy::needless_bool" \
-              "clippy::needless_collect" \
-              "clippy::needless_range_loop" \
-              "clippy::ok_expect" \
-              "clippy::op_ref" \
-              "clippy::option_map_unit_fn" \
-              "clippy::or_fun_call" \
-              "clippy::println_empty_string" \
-              "clippy::ptr_arg" \
-              "clippy::redundant_closure" \
-              "clippy::redundant_pattern_matching" \
-              "clippy::single_char_pattern" \
-              "clippy::single_match" \
-              "clippy::string_lit_as_bytes" \
-              "clippy::toplevel_ref_arg" \
-              "clippy::unit_arg" \
-              "clippy::unnecessary_operation" \
-              "clippy::unreadable_literal" \
-              "clippy::unused_label" \
-              "clippy::unused_unit" \
-              "clippy::useless_asref" \
-              "clippy::useless_format" \
-              "clippy::useless_let_if_seq" \
-              "clippy::useless_vec" \
-              "clippy::write_with_newline")
+               "clippy::blacklisted_name" \
+               "clippy::block_in_if_condition_stmt" \
+               "clippy::bool_comparison" \
+               "clippy::cast_lossless" \
+               "clippy::clone_on_copy" \
+               "clippy::cmp_owned" \
+               "clippy::collapsible_if" \
+               "clippy::const_static_lifetime" \
+               "clippy::correctness" \
+               "clippy::deref_addrof" \
+               "clippy::expect_fun_call" \
+               "clippy::for_kv_map" \
+               "clippy::get_unwrap" \
+               "clippy::identity_conversion" \
+               "clippy::if_let_some_result" \
+               "clippy::len_zero" \
+               "clippy::let_and_return" \
+               "clippy::let_unit_value" \
+               "clippy::map_clone" \
+               "clippy::match_bool" \
+               "clippy::match_ref_pats" \
+               "clippy::needless_bool" \
+               "clippy::needless_collect" \
+               "clippy::needless_range_loop" \
+               "clippy::ok_expect" \
+               "clippy::op_ref" \
+               "clippy::option_map_unit_fn" \
+               "clippy::or_fun_call" \
+               "clippy::println_empty_string" \
+               "clippy::ptr_arg" \
+               "clippy::redundant_closure" \
+               "clippy::redundant_pattern_matching" \
+               "clippy::single_char_pattern" \
+               "clippy::single_match" \
+               "clippy::string_lit_as_bytes" \
+               "clippy::toplevel_ref_arg" \
+               "clippy::unit_arg" \
+               "clippy::unnecessary_operation" \
+               "clippy::unreadable_literal" \
+               "clippy::unused_label" \
+               "clippy::unused_unit" \
+               "clippy::useless_asref" \
+               "clippy::useless_format" \
+               "clippy::useless_let_if_seq" \
+               "clippy::useless_vec" \
+               "clippy::write_with_newline" \
+               "renamed_and_removed_lints")
 
  # stick together unexamined lints
 for unexamined_lint in "${unexamined_lints[@]}"
