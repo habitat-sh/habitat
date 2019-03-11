892a2aa6f3c36fca8e1527edd354374284301420
Jon Bauman <5906042+baumanj@users.noreply.github.com>
2019-03-11 11:59:22 -0700

    Fix platform-specific unused import warning
    gcc crate is only used on windows, so move the use statement inside the
    windows main implementation.
    
    Signed-off-by: Jon Bauman <5906042+baumanj@users.noreply.github.com>

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
-use gcc;
-
 #[cfg(windows)]
-fn main() { gcc::compile_library("libsid.a", &["./src/obtain_sid.c"]); }
+fn main() {
+    use gcc;
+    gcc::compile_library("libsid.a", &["./src/obtain_sid.c"]);
+}
 
 #[cfg(not(windows))]
 fn main() {}
