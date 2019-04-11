cc9cdba40442741f1e85ba42764526c204d1a033
Christopher Maier <cmaier@chef.io>
2019-04-11 10:04:21 -0400

    Move AutomateAuthToken to the common crate
    This will allow us to refer to it from both `hab` (for the CLI) and
    `hab-sup`, without introducing an unwanted dependency on `hab-sup` for
    `hab`
    
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- components/sup/src/manager/mod.rs
+++ components/sup/src/manager/mod.rs
@@ -44,7 +44,6 @@ use crate::{census::{CensusRing,
                     EventCore,
                     EventStreamConfig},
             http_gateway,
-            AutomateAuthToken,
             VERSION};
 use cpu_time::ProcessTime;
 use futures::{future,
@@ -57,7 +56,8 @@ use habitat_butterfly::{member::Member,
                                  Suitability},
                         trace::Trace};
 use habitat_common::{outputln,
-                     types::ListenCtlAddr,
+                     types::{AutomateAuthToken,
+                             ListenCtlAddr},
                      FeatureFlag};
 use habitat_core::{crypto::SymKey,
                    env::{self,
