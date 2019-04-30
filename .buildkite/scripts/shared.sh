ac3e7d60c7cb0232ca0f1a18942627e9aeaf664d
Scott Hain <shain@chef.io>
2019-04-30 10:39:12 -0700

    Remove curl based promote workaround
    Signed-off-by: Scott Hain <shain@chef.io>

--- .buildkite/scripts/shared.sh
+++ .buildkite/scripts/shared.sh
@@ -237,34 +237,6 @@ set_version() {
     buildkite-agent meta-data set "version" "${version}"
 }
 
-# curl-based promotion function to work around https://github.com/habitat-sh/builder/issues/940
-# Remove after 0.79.0
-promote() {
-    package_ident="${1}"
-    target="${2}"
-    to_channel="${3}"
-
-    # Extract the individual bits of the fully-qualified identifier
-    IFS="/" read -r origin package version release <<< "${package_ident}"
-
-    # Create the channel, if necessary.
-    #
-    # Don't use --fail here, because trying to create a channel that
-    # already exists returns a 409, and we don't want to fail in that case.
-    echo "--- :partyparrot: Manually creating '${to_channel}' channel (if it doesn't exist already)"
-    curl --request POST \
-         --header "Authorization: Bearer $HAB_AUTH_TOKEN" \
-         --verbose \
-         "https://bldr.habitat.sh/v1/depot/channels/${origin}/${to_channel}"
-
-    echo "--- :partyparrot: Manually promoting '${package_ident}' (${target}) to the '${to_channel}' channel"
-    curl --request PUT \
-         --header "Authorization: Bearer $HAB_AUTH_TOKEN" \
-         --fail \
-         --verbose \
-         "https://bldr.habitat.sh/v1/depot/channels/${origin}/${to_channel}/pkgs/${package}/${version}/${release}/promote?&target=${target}"
-}
-
 # Until we can reliably deal with packages that have the same
 # identifier, but different target, we'll track the information in
 # Buildkite metadata.
