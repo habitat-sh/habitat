d157081d2f2aed5207e440a92d622242e9531920
mwrock <matt@mattwrock.com>
2019-04-30 07:20:14 -0700

    set machine scoped HAB_LICENSE variable for windows docker studio supervisor
    Signed-off-by: mwrock <matt@mattwrock.com>

--- .buildkite/scripts/build_docker_image.ps1
+++ .buildkite/scripts/build_docker_image.ps1
@@ -57,6 +57,7 @@ SHELL ["powershell", "-Command", "`$ErrorActionPreference = 'Stop'; `$ProgressPr
 # view them by tailing the log to the console. Because we are in a Docker studio, it is safe to
 # assume that the terminal supports ANSI sequence codes.
 RUN `$env:HAB_LICENSE='accept-no-persist'; ``
+    SETX HAB_LICENSE accept-no-persist /m; ``
     &/hab/pkgs/$ident/bin/hab/hab.exe pkg exec core/windows-service install; ``
     (Get-Content /hab/svc/windows-service/HabService.dll.config).replace('--no-color', '') | Set-Content /hab/svc/windows-service/HabService.dll.config; ``
     (Get-Content /hab/svc/windows-service/log4net.xml).replace('%date - ', '') | Set-Content /hab/svc/windows-service/log4net.xml
