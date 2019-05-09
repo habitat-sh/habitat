85598d3c5c64c777f88aaff121415548a5d21dee
Salim Afiune <afiune@chef.io>
2019-05-08 22:23:13 -0500

    Removes impl of getdomainname C function
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- components/core/src/os/net/unix.rs
+++ components/core/src/os/net/unix.rs
@@ -30,20 +30,6 @@ pub fn hostname() -> io::Result<String> {
     }
 }
 
-pub fn domainname() -> io::Result<String> {
-    let len = 255;
-    let mut buf = Vec::<u8>::with_capacity(len);
-    let ptr = buf.as_mut_slice().as_mut_ptr();
-    match unsafe { getdomainname(ptr as *mut libc::c_char, len as libc::size_t) } {
-        0 => {
-            let c_str = unsafe { CStr::from_ptr(ptr as *const libc::c_char) };
-            Ok(c_str.to_string_lossy().into_owned())
-        }
-        code => Err(io::Error::from_raw_os_error(code)),
-    }
-}
-
 extern "C" {
     pub fn gethostname(name: *mut libc::c_char, size: libc::size_t) -> libc::c_int;
-    pub fn getdomainname(name: *mut libc::c_char, len: libc::size_t) -> libc::c_int;
 }
