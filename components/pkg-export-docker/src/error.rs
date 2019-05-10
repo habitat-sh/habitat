4e5c5b8f5dfdf69c49a01d380f91f941d4573c21
Josh Black <raskchanky@gmail.com>
2019-05-10 10:48:46 -0700

    Fix a few errors caused by the cargo update
    Signed-off-by: Josh Black <raskchanky@gmail.com>

--- components/pkg-export-docker/src/error.rs
+++ components/pkg-export-docker/src/error.rs
@@ -1,24 +1,10 @@
-// Copyright (c) 2016-2017 Chef Software Inc. and/or applicable contributors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 use base64::DecodeError;
+use failure;
+use rusoto_core::RusotoError;
 use rusoto_ecr::GetAuthorizationTokenError;
 use std::{process::ExitStatus,
           result,
           string::FromUtf8Error};
-
-use failure;
 
 pub type Result<T> = result::Result<T, failure::Error>;
 
@@ -45,7 +31,7 @@ pub enum Error {
     #[fail(display = "No ECR Tokens returned")]
     NoECRTokensReturned,
     #[fail(display = "{}", _0)]
-    TokenFetchFailed(GetAuthorizationTokenError),
+    TokenFetchFailed(RusotoError<GetAuthorizationTokenError>),
     #[fail(display = "A primary service package could not be determined from: {:?}. At least \
                       one package with a run hook must be provided.",
            _0)]
--- components/sup/src/http_gateway.rs
+++ components/sup/src/http_gateway.rs
@@ -1,17 +1,3 @@
-// Copyright (c) 2016-2017 Chef Software Inc. and/or applicable contributors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 use crate::{error::{Result,
                     SupError},
             manager::{self,
@@ -265,7 +251,7 @@ impl Middleware<AppState> for Metrics {
 
 // End middleware
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug)]
 pub enum ServerStartup {
     NotStarted,
     Started,
@@ -333,7 +319,10 @@ impl Server {
             };
 
             cvar.notify_one();
-            sys.run();
+
+            if let Err(e) = sys.run() {
+                error!("The actix system experienced an error: {}", e);
+            }
         });
     }
 }
