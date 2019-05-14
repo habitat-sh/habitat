c8b03d26f973c183db0fa4b34cbec63c4c56f9ea
Christopher Maier <cmaier@chef.io>
2019-05-14 14:08:43 -0400

    [PR Feedback] Disable some problematic rustfmt effects
    Looks like it's having trouble properly formatting deeply-nested /
    chained futures.
    
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- components/sup/src/event/ratsio.rs
+++ components/sup/src/event/ratsio.rs
@@ -23,10 +23,9 @@ pub(super) fn init_stream(conn_info: EventConnectionInfo) -> Result<EventStream>
     let (event_tx, event_rx) = futures_mpsc::unbounded();
     let (sync_tx, sync_rx) = std_mpsc::sync_channel(0); // rendezvous channel
 
-    // TODO (CM): We could theoretically create this future and spawn
-    // it in the Supervisor's Tokio runtime, but there's currently a
-    // bug: https://github.com/YellowInnovation/nitox/issues/24
-
+    // Disabling rustfmt on this... I think we might be running into
+    // https://github.com/rust-lang/rustfmt/issues/1762
+    #[rustfmt::skip]
     thread::Builder::new().name("events".to_string())
                           .spawn(move || {
                               let EventConnectionInfo { name,
@@ -36,8 +35,7 @@ pub(super) fn init_stream(conn_info: EventConnectionInfo) -> Result<EventStream>
                                                         auth_token, } = conn_info;
                               let nats_options =
                                   NatsClientOptions::builder().cluster_uris(cluster_uri)
-                                  // TODO (CM): implement Into<String>?
-                                  .auth_token(auth_token.to_string())
+                                                              .auth_token(auth_token.to_string())
                                                               .verbose(verbose)
                                                               .build()
                                                               .unwrap();
@@ -57,17 +55,17 @@ pub(super) fn init_stream(conn_info: EventConnectionInfo) -> Result<EventStream>
                                              .expect("Couldn't synchronize event thread!");
 
                                       event_rx.for_each(move |event: Vec<u8>| {
-                                                  let stan_msg =
-                                                      StanMessage::new(HABITAT_SUBJECT.into(),
-                                                                       event);
-                                                  let publish_event = client
-                            .send(stan_msg)
-                            .map_err(|e| {
-                                error!("Error publishing event: {}", e)
-                            });
-                                                  executor::spawn(publish_event);
-                                                  Ok(())
-                                              })
+                                          let stan_msg =
+                                              StanMessage::new(HABITAT_SUBJECT.into(),
+                                                               event);
+                                          let publish_event = client
+                                              .send(stan_msg)
+                                              .map_err(|e| {
+                                                  error!("Error publishing event: {}", e)
+                                              });
+                                          executor::spawn(publish_event);
+                                          Ok(())
+                                      })
                                   });
 
                               ThreadRuntime::new().expect("Couldn't create event stream runtime!")
@@ -75,7 +73,7 @@ pub(super) fn init_stream(conn_info: EventConnectionInfo) -> Result<EventStream>
                                                   .run()
                                                   .expect("something seriously wrong has occurred");
                           })
-                          .map_err(Error::SpawnEventThreadError)?; // TODO (CM): ratsio error variant
+                          .map_err(Error::SpawnEventThreadError)?;
 
     sync_rx.recv_timeout(EventThreadStartupWait::configured_value().into())
            .map_err(Error::ConnectEventServerError)?;
