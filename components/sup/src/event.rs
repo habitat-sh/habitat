a793008a1a50ab20eab9c07205171fb05cf3f1b9
Salim Afiune <afiune@chef.io>
2019-05-16 19:30:28 +0200

    Implement Site field in EventMetadata message
    The user will be able to set this parameter through an environment
    variable or an option in the habitat CLI, just as you will be able to do
    with application and environment.
    
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- components/sup/src/event.rs
+++ components/sup/src/event.rs
@@ -88,6 +88,7 @@ pub struct EventStreamConfig {
     meta:        EventStreamMetadata,
     token:       AutomateAuthToken,
     url:         String,
+    site:        Option<String>,
 }
 
 impl<'a> From<&'a ArgMatches<'a>> for EventStreamConfig {
@@ -102,7 +103,8 @@ impl<'a> From<&'a ArgMatches<'a>> for EventStreamConfig {
                             token:       AutomateAuthToken::from(m),
                             url:         m.value_of("EVENT_STREAM_URL")
                                           .map(str::to_string)
-                                          .expect("Required option for EventStream feature"), }
+                                          .expect("Required option for EventStream feature"),
+                            site:        m.value_of("EVENT_STREAM_SITE").map(str::to_string), }
     }
 }
 
@@ -143,6 +145,7 @@ pub struct EventCore {
     fqdn: String,
     application: String,
     environment: String,
+    site: Option<String>,
     meta: EventStreamMetadata,
 }
 
@@ -151,6 +154,7 @@ impl EventCore {
         EventCore { supervisor_id: sys.member_id.clone(),
                     ip_address: sys.gossip_listen(),
                     fqdn,
+                    site: config.site.clone(),
                     environment: config.environment.clone(),
                     application: config.application.clone(),
                     meta: config.meta.clone() }
--- components/sup/src/event/types.rs
+++ components/sup/src/event/types.rs
@@ -66,6 +66,7 @@ impl EventCore {
                         fqdn:          self.fqdn.clone(),
                         application:   self.application.clone(),
                         environment:   self.environment.clone(),
+                        site:          self.site.clone().unwrap_or_default(),
                         occurred_at:   None,
                         meta:          self.meta.clone().into(), }
     }
