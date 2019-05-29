bcf319c54a2f6b2af2501a94d47e1349e21a2be9
Jon Bauman <5906042+baumanj@users.noreply.github.com>
2019-05-29 10:59:41 -0700

    Update `hab pkg install` `--binlink` arg docs
    This clarifies connection between `--binlink` and `--binlink-dir`
    
    Signed-off-by: Jon Bauman <5906042+baumanj@users.noreply.github.com>

--- components/hab/src/cli.rs
+++ components/hab/src/cli.rs
@@ -882,7 +882,7 @@ fn sub_pkg_install(feature_flags: FeatureFlag) -> App<'static, 'static> {
             "One or more Habitat package identifiers (ex: acme/redis) and/or filepaths \
             to a Habitat Artifact (ex: /home/acme-redis-3.0.7-21120102031201-x86_64-linux.hart)")
         (@arg BINLINK: -b --binlink
-            "Binlink all binaries from installed package(s)")
+            "Binlink all binaries from installed package(s) into BINLINK_DIR")
         (@arg BINLINK_DIR: --("binlink-dir") +takes_value {non_empty} env(BINLINK_DIR_ENVVAR)
             default_value(DEFAULT_BINLINK_DIR) "Binlink all binaries from installed package(s) into BINLINK_DIR")
         (@arg FORCE: -f --force "Overwrite existing binlinks")
