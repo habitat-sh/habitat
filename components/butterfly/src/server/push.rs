f89a97571e5abcd13c0d43af9320a3d66d704e06
Jon Bauman <5906042+baumanj@users.noreply.github.com>
2019-06-07 18:30:01 -0700

    Update clippy::cyclomatic_complexity -> cognitive_complexity
    The name of this lint changed with the 1.35.0 rust release
    
    Signed-off-by: Jon Bauman <5906042+baumanj@users.noreply.github.com>

--- components/butterfly/src/server/push.rs
+++ components/butterfly/src/server/push.rs
@@ -143,10 +143,10 @@ impl PushWorker {
     /// connection and socket open for 1 second longer - so it is possible, but unlikely, that this
     /// method can lose messages.
     // If we ever need to modify this function, it would be an excellent opportunity to
-    // simplify the redundant aspects and remove this allow(clippy::cyclomatic_complexity),
+    // simplify the redundant aspects and remove this allow(clippy::cognitive_complexity),
     // but changing it in the absence of other necessity seems like too much risk for the
     // expected reward.
-    #[allow(clippy::cyclomatic_complexity)]
+    #[allow(clippy::cognitive_complexity)]
     fn send_rumors(&self, member: &Member, rumors: &[RumorKey]) {
         let socket = (**ZMQ_CONTEXT).as_mut()
                                     .socket(zmq::PUSH)
--- components/butterfly/tests/rumor/election.rs
+++ components/butterfly/tests/rumor/election.rs
@@ -86,7 +86,7 @@ fn five_members_elect_a_new_leader_when_the_old_one_dies() {
 }
 
 #[test]
-#[allow(clippy::cyclomatic_complexity)]
+#[allow(clippy::cognitive_complexity)]
 fn five_members_elect_a_new_leader_when_they_are_quorum_partitioned() {
     let mut net = btest::SwimNet::new_with_suitability(vec![1, 0, 0, 0, 0]);
     net[0].member
