7c94195642f478c68036d90ea9718c91b4e9357e
Christopher Maier <cmaier@chef.io>
2019-08-02 15:55:35 -0400

    testing
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- test/end-to-end/test_launcher_restarts_supervisor.sh
+++ test/end-to-end/test_launcher_restarts_supervisor.sh
@@ -8,28 +8,36 @@
 
 set -eou pipefail
 
+
+find_if_exists() {
+  command -v "${1}" || { log "Required utility '${1}' cannot be found!  Aborting."; exit 1; }
+}
+
+find_if_exists pgrep
+
 wait_for_sup_to_start() {
-	until pgrep hab-sup &>/dev/null; do
-		echo -n .
-		sleep 1
-	done
-	echo
+    until pgrep hab-sup &>/dev/null; do
+        echo -n .
+        sleep 1
+    done
+    echo
 }
 
 if pgrep hab-launch &>/dev/null; then
-	echo "Error: launcher process is already running"
-	exit 1
+    echo "Error: launcher process is already running"
+    exit 1
 fi
 
 TESTING_FS_ROOT=$(mktemp -d)
 export TESTING_FS_ROOT
-sup_log="$TESTING_FS_ROOT/hab/sup/default/sup.log"
+#sup_log="$TESTING_FS_ROOT/hab/sup/default/sup.log"
+sup_log="sup.log"
 exit_file=$(mktemp)
 exit_code=${1:-1}
 
 mkdir -p "$(dirname "$sup_log")"
 echo -n "Starting launcher with root $TESTING_FS_ROOT (logging to $sup_log)..."
-env HAB_FEAT_TEST_EXIT="$exit_file" hab sup run &> "$sup_log" &
+env FS_ROOT=$TESTING_FS_ROOT HAB_FEAT_TEST_EXIT="$exit_file" hab sup run &> "$sup_log" &
 trap 'hab sup term' INT TERM EXIT
 
 wait_for_sup_to_start
@@ -42,14 +50,14 @@ echo "$exit_code" >> "$exit_file"
 
 echo -n "Waiting for old supervisor process to exit..."
 while ps -p "$supervisor_pid" &>/dev/null; do
-	echo -n .
-	sleep 1
+    echo -n .
+    sleep 1
 done
 echo
 
 if ! pgrep hab-launch &>/dev/null; then
-	echo "Failure! Launcher process exited"
-	exit 2
+    echo "Failure! Launcher process exited"
+    exit 2
 fi
 
 echo "Waiting for new supervisor process to start..."
@@ -58,11 +66,11 @@ read -r new_launcher_pid < <(pgrep hab-launch)
 read -r new_supervisor_pid < <(pgrep hab-sup)
 
 if [[ $supervisor_pid == "$new_supervisor_pid" ]]; then
-	echo "Failure! Supervisor process did not change"
-	exit 3
+    echo "Failure! Supervisor process did not change"
+    exit 3
 elif [[ $launcher_pid != "$new_launcher_pid" ]]; then
-	echo "Failure! Launcher process changed unexpectedly"
-	exit 4
+    echo "Failure! Launcher process changed unexpectedly"
+    exit 4
 else
-	echo "Success! Launcher restarted supervisor process"
+    echo "Success! Launcher restarted supervisor process"
 fi
