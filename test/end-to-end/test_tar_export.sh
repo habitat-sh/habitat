cacabb2805f3a3de6e5d118b42026f7e8b2e75f7
Christopher Maier <cmaier@chef.io>
2019-08-02 15:14:38 -0400

    Add ability to query tarball contents to tar exporter test
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- test/end-to-end/test_tar_export.sh
+++ test/end-to-end/test_tar_export.sh
@@ -12,6 +12,37 @@ set -euo pipefail
 #     test_tar_export.sh core/gzip stable
 #
 
+# Extract the identifier of a particular Habitat package in a tarball.
+#
+# Provide just the package name; the origin is always assumed to be
+# "core"
+#
+#     echo $(extract_ident my.tar.gz hab)
+#     # -> core/hab/0.83.0/20190712231625
+extract_ident() {
+    local tar_archive=${1}
+    local package_name=${2}
+
+    # Extract the path to the IDENT file for a given "core/XXX" package that
+    # should be present in the tarball.
+    local ident_file
+    ident_file=$(tar \
+                     --list \
+                     --file=${tar_archive} | \
+                 grep --basic-regexp \
+                      "hab/pkgs/core/${package_name}/.*/.*/IDENT")
+
+    if [[ -n "${ident_file}" ]]; then
+        # Extract the *contents* of that IDENT file
+        tar --extract \
+            --to-stdout \
+            --file=${tar_archive} \
+            "${ident_file}"
+    else
+        return 1
+    fi
+}
+
 pkg_ident="${1}"
 channel="${2}"
 
@@ -30,4 +61,11 @@ else
     exit 1
 fi
 
-echo "--- Woo!"
+# Query contents for core Habitat packages
+# TODO: validate these against expected values
+hab_ident=$(extract_ident "${tarball}" hab)
+echo "core/hab in tarball = ${hab_ident}"
+launcher_ident=$(extract_ident "${tarball}" hab-launcher)
+echo "core/hab-launcher in tarball = ${launcher_ident}"
+sup_ident=$(extract_ident "${tarball}" hab-sup)
+echo "core/hab-sup in tarball = ${sup_ident}"
