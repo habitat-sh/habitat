a70970e63bae19137db47a4e3bd62cd9a7b8d5dc
Christopher Maier <cmaier@chef.io>
2019-08-02 11:28:18 -0400

    Tweak socket file test
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- test/end-to-end/test_socket_file_cleanup.sh
+++ test/end-to-end/test_socket_file_cleanup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Regression test to address https://github.com/habitat-sh/habitat/issues/4673
+# Fixed in https://github.com/habitat-sh/habitat/pull/5365
+
 set -exou pipefail
 
 find_socket_files() {
-	find /tmp -maxdepth 1 -name "rust-ipc-socket.*"
+    find /tmp -maxdepth 1 -name "rust-ipc-socket.*"
 }
 
 socket_files_before=$(mktemp)
@@ -16,14 +19,14 @@ hab sup run &> "$sup_log" &
 retries=0
 max_retries=60
 until hab sup status &> /dev/null; do
-	echo -n .
-	if [[ $((retries++)) -gt $max_retries ]]; then
-		echo "--- timed out; dumping log:"
-		cat "$sup_log"
-		exit 2
-	else
-		sleep 1
-	fi
+    echo -n .
+    if [[ $((retries++)) -gt $max_retries ]]; then
+        echo "--- timed out; dumping log:"
+        cat "$sup_log"
+        exit 2
+    else
+        sleep 1
+    fi
 done
 echo
 
@@ -31,14 +34,15 @@ hab sup term
 echo "--- Waiting for launcher to exit..."
 wait
 
+echo "--- Checking for socket files left behind..."
 socket_files_after=$(mktemp)
 find_socket_files > "$socket_files_after"
 
-echo "--- Checking for socket files left behind..."
 if grep -vf "$socket_files_before" "$socket_files_after"; then
-	echo "--- Failure! Dumping supervisor log:"
-	cat "$sup_log"
-	exit 1
+    echo "--- Failure! Dumping supervisor log:"
+    cat "$sup_log"
+    exit 1
 else
-	echo "--- Success! No socket file left behind"
+    echo "--- Success! No socket file left behind; Supervisor logs follow:"
+    cat "$sup_log"
 fi
