f274401927de94e998a333aa45d3eb1ed385e8a7
David McNeil <mcneil.david2@gmail.com>
2019-10-03 08:04:17 -0400

    Change example IP address
    Signed-off-by: David McNeil <mcneil.david2@gmail.com>

--- components/hab/src/cli.rs
+++ components/hab/src/cli.rs
@@ -1124,7 +1124,7 @@ pub fn sub_sup_run(feature_flags: FeatureFlag) -> App<'static, 'static> {
                             (@arg SYS_IP_ADDRESS: --("sys-ip-address") +takes_value {valid_ipv4_address}
                              "The IPv4 address to use as the `sys.ip` template variable. If this \
                              argument is not set, the supervisor tries to dynamically determine \
-                             an IP address. If that fails, the supervisor defaults to using
+                             an IP address. If that fails, the supervisor defaults to using \
                              `127.0.0.1`.")
     );
 
@@ -1380,7 +1380,11 @@ fn file_exists_or_stdin(val: String) -> result::Result<(), String> {
 fn valid_ipv4_address(val: String) -> result::Result<(), String> {
     match Ipv4Addr::from_str(&val) {
         Ok(_) => Ok(()),
-        Err(_) => Err(format!("'{}' is not a valid IPv4 address, eg: '0.0.0.0'", val)),
+        Err(_) => {
+            Err(format!("'{}' is not a valid IPv4 address, eg: \
+                         '192.168.1.105'",
+                        val))
+        }
     }
 }
 
