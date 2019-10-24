cadb05f7ee72701d27f57abc605e67d0e5eff385
mwrock <matt@mattwrock.com>
2019-10-24 13:46:16 -0700

    do not fail test if unable to start service with bogus args
    Signed-off-by: mwrock <matt@mattwrock.com>

--- test/end-to-end/test_windows_service_stops_on_launcher_termination.ps1
+++ test/end-to-end/test_windows_service_stops_on_launcher_termination.ps1
@@ -15,7 +15,10 @@ Describe "Bogus Launcher args" {
     Stop-Service Habitat
     (Get-Content c:/hab/svc/windows-service/HabService.dll.config).replace('--no-color', '--poop') |
       Set-Content c:/hab/svc/windows-service/HabService.dll.config
-    Start-Service Habitat
+    # Depending on random timing issues, starting the service will either
+    # fail with an exception or succeed and immediately stop itself
+    # either way, we just want to move on and validate that it is not running
+    Start-Service Habitat -ErrorAction SilentlyContinue
     Start-Sleep -Seconds 2
 
     It "Stops service" {
