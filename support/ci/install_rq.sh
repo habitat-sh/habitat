fa5b4afbb1ffc24150032c4189bcb20d1340e019
Christopher Maier <cmaier@chef.io>
2019-11-07 11:39:29 -0500

    Remove a bunch of old Travis CI support code
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- support/ci/install_rq.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eu
-
-echo "Installing rq"
-curl -o /usr/local/bin/rq https://s3-eu-west-1.amazonaws.com/record-query/record-query/x86_64-unknown-linux-musl/rq
-chmod +x /usr/local/bin/rq
