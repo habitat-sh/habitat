782e212093b440596fec59a564c38997238c8d5b
Christopher Maier <cmaier@chef.io>
2019-12-12 17:45:05 -0500

    Use habitat-testing/test-probe package
    Stop using my private version :)
    
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- test/end-to-end/multi-supervisor/testcases/new-config-is-gossiped/run_test.ps1
+++ test/end-to-end/multi-supervisor/testcases/new-config-is-gossiped/run_test.ps1
@@ -1,7 +1,7 @@
 Describe "gossiping new config" {
     Load-SupervisorService "core/redis" -Remote "alpha.habitat.dev"
-    Load-SupervisorService "christophermaier/test-probe" -Bind "thing_with_a_port:redis.default" -Remote "beta.habitat.dev"
-    
+    Load-SupervisorService "habitat-testing/test-probe" -Bind "thing_with_a_port:redis.default" -Remote "beta.habitat.dev"
+
     It "probe service should bind to redis 6379" {
         $current_port = (Invoke-WebRequest "http://beta.habitat.dev:8000/context" | ConvertFrom-Json).bind.thing_with_a_port.first.cfg.port
         $current_port | Should -Be 6379
