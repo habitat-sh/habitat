359ea431eac7cf3bb1d7c072665dfd83209f695c
David McNeil <mcneil.david2@gmail.com>
2020-02-04 13:30:08 -0500

    Remove trailing periods in help
    Signed-off-by: David McNeil <mcneil.david2@gmail.com>

--- components/hab/src/cli.rs
+++ components/hab/src/cli.rs
@@ -60,50 +60,50 @@ use url::Url;
         )]
 #[allow(dead_code)]
 struct SubSupRun {
-    /// The listen address for the Gossip System Gateway[.]
+    /// The listen address for the Gossip System Gateway
     #[structopt(name = "LISTEN_GOSSIP",
                 long = "listen-gossip",
                 env = GossipListenAddr::ENVVAR,
                 default_value = GossipListenAddr::default_as_str())]
-    listen_gossip: SocketAddr,
-    /// Start the supervisor in local mode[.]
+    listen_gossip:         SocketAddr,
+    /// Start the supervisor in local mode
     #[structopt(name = "LOCAL_GOSSIP_MODE",
                 long = "local-gossip-mode",
                 conflicts_with_all = &["LISTEN_GOSSIP", "PEER", "PEER_WATCH_FILE"])]
-    local_gossip_mode: bool,
-    /// The listen address for the HTTP Gateway[.]
+    local_gossip_mode:     bool,
+    /// The listen address for the HTTP Gateway
     #[structopt(name = "LISTEN_HTTP",
                 long = "listen-http",
                 env = HttpListenAddr::ENVVAR,
                 default_value = HttpListenAddr::default_as_str())]
-    listen_http: SocketAddr,
+    listen_http:           SocketAddr,
     /// Disable the HTTP Gateway completely
     #[structopt(name = "HTTP_DISABLE", long = "http-disable", short = "D")]
-    http_disable: bool,
+    http_disable:          bool,
     /// The listen address for the Control Gateway. If not specified, the value will be taken from
-    /// the HAB_LISTEN_CTL environment variable if defined[.]
+    /// the HAB_LISTEN_CTL environment variable if defined
     #[structopt(name = "LISTEN_CTL",
                 long = "listen-ctl",
                 env = ListenCtlAddr::ENVVAR,
                 default_value = ListenCtlAddr::default_as_str())]
-    listen_ctl: SocketAddr,
-    /// The organization that the Supervisor and its subsequent services are part of[.]
+    listen_ctl:            SocketAddr,
+    /// The organization that the Supervisor and its subsequent services are part of
     #[structopt(name = "ORGANIZATION", long = "org")]
-    organization: Option<String>,
+    organization:          Option<String>,
     /// The listen address of one or more initial peers (IP[:PORT])
     #[structopt(name = "PEER", long = "peer")]
     // TODO (DM): This could probably be a different type for better validation (Vec<SockAddr>?)
-    peer: Vec<String>,
+    peer:                  Vec<String>,
     /// If this Supervisor is a permanent peer
     #[structopt(name = "PERMANENT_PEER", long = "permanent-peer", short = "I")]
-    permanent_peer: bool,
+    permanent_peer:        bool,
     /// Watch this file for connecting to the ring
     #[structopt(name = "PEER_WATCH_FILE",
                 long = "peer-watch-file",
                 conflicts_with = "PEER")]
-    peer_watch_file: PathBuf,
+    peer_watch_file:       PathBuf,
     /// Path to search for encryption keys. Default value is hab/cache/keys if root and
-    /// .hab/cache/keys under the home directory otherwise[.]
+    /// .hab/cache/keys under the home directory otherwise
     // TODO (DM): I dont think the default value comment is correct. It looks like it always is set
     // to hab/cache/keys.
     #[structopt(name = "CACHE_KEY_PATH",
@@ -111,7 +111,7 @@ struct SubSupRun {
                 env = CACHE_KEY_PATH_ENV_VAR,
                 default_value = CACHE_KEY_PATH,
                 hide_default_value = true)]
-    cache_key_path: PathBuf,
+    cache_key_path:        PathBuf,
     /// The name of the ring used by the Supervisor when running with wire encryption. (ex: hab sup
     /// run --ring myring)
     #[structopt(name = "RING",
@@ -119,7 +119,7 @@ struct SubSupRun {
                 short = "r",
                 env = RING_ENVVAR,
                 conflicts_with = "RING_KEY")]
-    ring: String,
+    ring:                  String,
     /// The contents of the ring key when running with wire encryption. (Note: This option is
     /// explicitly undocumented and for testing purposes only. Do not use it in a production
     /// system. Use the corresponding environment variable instead.) (ex: hab sup run --ring-key
@@ -129,12 +129,12 @@ struct SubSupRun {
                 env = RING_KEY_ENVVAR,
                 hidden = true,
                 conflicts_with = "RING")]
-    ring_key: Option<String>,
+    ring_key:              Option<String>,
     /// Receive Supervisor updates from the specified release channel
     #[structopt(name = "CHANNEL", long = "channel", default_value = "stable")]
-    channel: String,
+    channel:               String,
     /// Specify an alternate Builder endpoint. If not specified, the value will be taken from the
-    /// HAB_BLDR_URL environment variable if defined[.] (default: https://bldr.habitat.sh)
+    /// HAB_BLDR_URL environment variable if defined (default: https://bldr.habitat.sh)
     #[structopt(name = "BLDR_URL",
                 long = "url",
                 short = "u",
@@ -142,29 +142,29 @@ struct SubSupRun {
                 // env = BLDR_URL_ENVVAR,
                 // default_value = DEFAULT_BLDR_URL
             )]
-    bldr_url: Url,
+    bldr_url:              Url,
     /// Use package config from this path, rather than the package itself
     #[structopt(name = "CONFIG_DIR", long = "config-from")]
-    config_dir: Option<PathBuf>,
+    config_dir:            Option<PathBuf>,
     /// Enable automatic updates for the Supervisor itself
     #[structopt(name = "AUTO_UPDATE", long = "auto-update", short = "A")]
-    auto_update: bool,
+    auto_update:           bool,
     /// Used for enabling TLS for the HTTP gateway. Read private key from KEY_FILE. This should be
-    /// a RSA private key or PKCS8-encoded private key, in PEM format[.]
+    /// a RSA private key or PKCS8-encoded private key, in PEM format
     #[structopt(name = "KEY_FILE", long = "key", requires = "CERT_FILE")]
-    key_file: Option<PathBuf>,
+    key_file:              Option<PathBuf>,
     /// Used for enabling TLS for the HTTP gateway. Read server certificates from CERT_FILE. This
     /// should contain PEM-format certificates in the right order (the first certificate should
-    /// certify KEY_FILE, the last should be a root CA)[.]
+    /// certify KEY_FILE, the last should be a root CA)
     #[structopt(name = "CERT_FILE", long = "certs", requires = "KEY_FILE")]
-    cert_file: Option<PathBuf>,
+    cert_file:             Option<PathBuf>,
     /// Used for enabling client-authentication with TLS for the HTTP gateway. Read CA certificate
     /// from CA_CERT_FILE. This should contain PEM-format certificate that can be used to validate
-    /// client requests[.]
+    /// client requests
     #[structopt(name = "CA_CERT_FILE",
                 long = "ca-certs",
                 requires_all = &["CERT_FILE", "KEY_FILE"])]
-    ca_cert_file: Option<PathBuf>,
+    ca_cert_file:          Option<PathBuf>,
     /// Load the given Habitat package as part of the Supervisor startup specified by a package
     /// identifier (ex: core/redis) or filepath to a Habitat Artifact (ex:
     /// /home/core-redis-3.0.7-21120102031201-x86_64-linux.hart)
@@ -174,44 +174,44 @@ struct SubSupRun {
     // TODO (DM): This flag can eventually be removed.
     // See https://github.com/habitat-sh/habitat/issues/7339
     #[structopt(name = "APPLICATION", long = "application", hidden = true)]
-    application: Vec<String>,
+    application:           Vec<String>,
     // TODO (DM): This flag can eventually be removed.
     // See https://github.com/habitat-sh/habitat/issues/7339
     #[structopt(name = "ENVIRONMENT", long = "environment", hidden = true)]
-    environment: Vec<String>,
+    environment:           Vec<String>,
     /// The service group; shared config and topology [default: default]
     // TODO (DM): This should set a default value
     #[structopt(name = "GROUP", long = "group")]
-    group: String,
+    group:                 String,
     /// Service topology; [default: none]
     // TODO (DM): I dont think saying the default is none makes sense here
     #[structopt(name = "TOPOLOGY",
                 long = "topology",
                 short = "t",
                 possible_values = &["standalone", "leader"])]
-    topology: Option<habitat_sup_protocol::types::Topology>,
+    topology:              Option<habitat_sup_protocol::types::Topology>,
     /// The update strategy; [default: none] [values: none, at-once, rolling]
     // TODO (DM): this should set a default_value and use possible_values = &["none", "at-once",
     // "rolling"]
     #[structopt(name = "STRATEGY", long = "strategy", short = "s")]
-    strategy: Option<habitat_sup_protocol::types::UpdateStrategy>,
+    strategy:              Option<habitat_sup_protocol::types::UpdateStrategy>,
     /// One or more service groups to bind to a configuration
     #[structopt(name = "BIND", long = "bind")]
-    bind: Vec<String>,
+    bind:                  Vec<String>,
     /// Governs how the presence or absence of binds affects service startup. `strict` blocks
     /// startup until all binds are present. [default: strict] [values: relaxed, strict]
     // TODO (DM): This should set default_value and use possible_values
     #[structopt(name = "BINDING_MODE", long = "binding-mode")]
-    binding_mode: Option<habitat_sup_protocol::types::BindingMode>,
+    binding_mode:          Option<habitat_sup_protocol::types::BindingMode>,
     /// Verbose output; shows file and line/column numbers
     #[structopt(name = "VERBOSE", short = "v")]
-    verbose: bool,
+    verbose:               bool,
     /// Turn ANSI color off
     #[structopt(name = "NO_COLOR", long = "no-color")]
-    no_color: bool,
+    no_color:              bool,
     /// Use structured JSON logging for the Supervisor. Implies NO_COLOR
     #[structopt(name = "JSON", long = "json-logging")]
-    json_logging: bool,
+    json_logging:          bool,
     /// The interval (seconds) on which to run health checks [default: 30]
     // TODO (DM): Should use default_value = "30"
     #[structopt(name = "HEALTH_CHECK_INTERVAL",
@@ -220,9 +220,9 @@ struct SubSupRun {
     health_check_interval: HealthCheckInterval,
     /// The IPv4 address to use as the `sys.ip` template variable. If this argument is not set, the
     /// supervisor tries to dynamically determine an IP address. If that fails, the supervisor
-    /// defaults to using `127.0.0.1`[.]
+    /// defaults to using `127.0.0.1`
     #[structopt(name = "SYS_IP_ADDRESS", long = "sys-ip-address")]
-    sys_ip_address: Option<Ipv4Addr>,
+    sys_ip_address:        Option<Ipv4Addr>,
 }
 
 pub fn get(feature_flags: FeatureFlag) -> App<'static, 'static> {
@@ -1329,18 +1329,18 @@ pub fn sub_sup_run(feature_flags: FeatureFlag) -> App<'static, 'static> {
             // see: https://github.com/kbknapp/clap-rs/blob/2724ec5399c500b12a1a24d356f4090f4816f5e2/src/app/mod.rs#L373-L394
             (usage: "hab sup run [FLAGS] [OPTIONS] [--] [PKG_IDENT_OR_ARTIFACT]")
             (@arg LISTEN_GOSSIP: --("listen-gossip") env(GossipListenAddr::ENVVAR) default_value(GossipListenAddr::default_as_str()) {valid_socket_addr}
-             "The listen address for the Gossip System Gateway[.]")
+             "The listen address for the Gossip System Gateway")
             (@arg LOCAL_GOSSIP_MODE: --("local-gossip-mode") conflicts_with("LISTEN_GOSSIP") conflicts_with("PEER") conflicts_with("PEER_WATCH_FILE")
-             "Start the supervisor in local mode[.]")
+             "Start the supervisor in local mode")
             (@arg LISTEN_HTTP: --("listen-http") env(HttpListenAddr::ENVVAR) default_value(HttpListenAddr::default_as_str()) {valid_socket_addr}
-             "The listen address for the HTTP Gateway[.]")
+             "The listen address for the HTTP Gateway")
             (@arg HTTP_DISABLE: --("http-disable") -D
              "Disable the HTTP Gateway completely")
             (@arg LISTEN_CTL: --("listen-ctl") env(ListenCtlAddr::ENVVAR) default_value(ListenCtlAddr::default_as_str()) {valid_socket_addr}
              "The listen address for the Control Gateway. If not specified, the value will \
-              be taken from the HAB_LISTEN_CTL environment variable if defined[.]")
+              be taken from the HAB_LISTEN_CTL environment variable if defined")
             (@arg ORGANIZATION: --org +takes_value
-             "The organization that the Supervisor and its subsequent services are part of[.]")
+             "The organization that the Supervisor and its subsequent services are part of")
             (@arg PEER: --peer +takes_value +multiple
              "The listen address of one or more initial peers (IP[:PORT])")
             (@arg PERMANENT_PEER: --("permanent-peer") -I "If this Supervisor is a permanent peer")
@@ -1349,7 +1349,7 @@ pub fn sub_sup_run(feature_flags: FeatureFlag) -> App<'static, 'static> {
             )
             (arg: arg_cache_key_path("Path to search for encryption keys. \
                                       Default value is hab/cache/keys if root and .hab/cache/keys under the home \
-                                      directory otherwise[.]"))
+                                      directory otherwise"))
             (@arg RING: --ring -r env(RING_ENVVAR) conflicts_with("RING_KEY") {non_empty}
              "The name of the ring used by the Supervisor when running with wire encryption. \
               (ex: hab sup run --ring myring)")
@@ -1364,7 +1364,7 @@ pub fn sub_sup_run(feature_flags: FeatureFlag) -> App<'static, 'static> {
              "Receive Supervisor updates from the specified release channel")
             (@arg BLDR_URL: -u --url +takes_value {valid_url}
              "Specify an alternate Builder endpoint. If not specified, the value will \
-              be taken from the HAB_BLDR_URL environment variable if defined[.] (default: \
+              be taken from the HAB_BLDR_URL environment variable if defined (default: \
               https://bldr.habitat.sh)")
 
             (@arg CONFIG_DIR: --("config-from") +takes_value {dir_exists}
@@ -1373,14 +1373,14 @@ pub fn sub_sup_run(feature_flags: FeatureFlag) -> App<'static, 'static> {
                                                      itself")
             (@arg KEY_FILE: --key +takes_value {file_exists} requires[CERT_FILE]
              "Used for enabling TLS for the HTTP gateway. Read private key from KEY_FILE. \
-              This should be a RSA private key or PKCS8-encoded private key, in PEM format[.]")
+              This should be a RSA private key or PKCS8-encoded private key, in PEM format")
             (@arg CERT_FILE: --certs +takes_value {file_exists} requires[KEY_FILE]
              "Used for enabling TLS for the HTTP gateway. Read server certificates from CERT_FILE. \
               This should contain PEM-format certificates in the right order (the first certificate \
-              should certify KEY_FILE, the last should be a root CA)[.]")
+              should certify KEY_FILE, the last should be a root CA)")
             (@arg CA_CERT_FILE: --("ca-certs") +takes_value {file_exists} requires[CERT_FILE] requires[KEY_FILE]
              "Used for enabling client-authentication with TLS for the HTTP gateway. Read CA certificate from CA_CERT_FILE. \
-              This should contain PEM-format certificate that can be used to validate client requests[.]")
+              This should contain PEM-format certificate that can be used to validate client requests")
             // === Optional arguments to additionally load an initial service for the Supervisor
             (@arg PKG_IDENT_OR_ARTIFACT: +takes_value "Load the given Habitat package as part of \
                                                        the Supervisor startup specified by a package identifier \
@@ -1411,7 +1411,7 @@ pub fn sub_sup_run(feature_flags: FeatureFlag) -> App<'static, 'static> {
              "The IPv4 address to use as the `sys.ip` template variable. If this \
              argument is not set, the supervisor tries to dynamically determine \
              an IP address. If that fails, the supervisor defaults to using \
-             `127.0.0.1`[.]")
+             `127.0.0.1`")
         )
     };
     let sub = add_event_stream_options(sub);
