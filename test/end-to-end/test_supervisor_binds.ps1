44649d26afbea68558c547344c5b5e8d843ce3a9
mwrock <matt@mattwrock.com>
2020-02-11 12:57:48 -0800

    add e2e tests for hab sup term
    Signed-off-by: mwrock <matt@mattwrock.com>

--- test/end-to-end/test_supervisor_binds.ps1
+++ test/end-to-end/test_supervisor_binds.ps1
@@ -1,12 +1,3 @@
-Function Invoke-BuildAndInstall($PackageName) {
-    hab pkg build test/fixtures/$PackageName
-    if($IsLinux) {
-        Get-Content "results/last_build.env" | ForEach-Object { Add-Content "results/last_build.ps1" -Value "`$$($_.Replace("=", '="'))`"" }
-    }
-    . ./results/last_build.ps1
-    hab pkg install ./results/$pkg_artifact
-}
-
 Describe "Supervisor binds" {
     BeforeAll {
         hab origin key generate $env:HAB_ORIGIN
