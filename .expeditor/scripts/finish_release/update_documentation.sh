a0501c079407f7ea9031e55cd1ecac89727bdb71
Christopher Maier <cmaier@chef.io>
2020-02-19 13:01:33 -0500

    [CI] Automate update of Habitat documentation
    Signed-off-by: Christopher Maier <cmaier@chef.io>

---
+++ .expeditor/scripts/finish_release/update_documentation.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# shellcheck source=.expeditor/scripts/shared.sh
+source .expeditor/scripts/shared.sh
+
+install_hub
+
+branch="expeditor/documentation-update-$(date +"%Y%m%d%H%M%S")"
+git checkout -b "$branch"
+
+echo "--- :hammer_and_pick: Building new automated Habitat documentation"
+(
+    cd www
+    make cli_docs
+    make template_reference
+)
+
+echo "--- :git: Publishing updated documentation"
+git add --update
+git commit \
+    --signoff \
+    --message "Automated update of Habitat Documentation"
+push_current_branch
+
+echo "--- :github: Creating PR"
+hub pull-request \
+    --force \
+    --no-edit \
+    --message "Automated update of Habitat Documentation"
