546027e50d01658a16dfe22ed125de3587c74c53
Christopher Maier <cmaier@chef.io>
2020-02-19 15:43:28 -0500

    [CI] Move CLI documentation generation to pipeline
    It makes more sense to have the code live with the pipeline now.
    
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- .expeditor/scripts/finish_release/update_documentation.sh
+++ .expeditor/scripts/finish_release/update_documentation.sh
@@ -5,37 +5,34 @@ set -euo pipefail
 # shellcheck source=.expeditor/scripts/shared.sh
 source .expeditor/scripts/shared.sh
 
+echo "--- :hammer_and_pick: Installing prerequisites"
 install_hub
+
 # We want latest stable, since that's what the documentation script assumes
 curlbash_hab "x86_64-linux"
+
+hab pkg install core/hab-studio
+hab pkg install core/hab-sup
+hab pkg install core/hab-launcher
+hab pkg install core/node --binlink
 
 branch="expeditor/documentation-update-$(date +"%Y%m%d%H%M%S")"
 git checkout -b "$branch"
 
-echo "--- :hammer_and_pick: Building new automated Habitat documentation"
-(
-    cd www
+echo "--- :hammer_and_pick: Generating CLI docs"
+# TODO: can't currently use `hab pkg exec core/node node ...` for
+# this because that blows away $PATH for the command, making it
+# impossible to find `hab` :(
+node .expeditor/scripts/finish_release/generate-cli-docs > www/source/docs/habitat-cli.html.md.erb
 
-    hab pkg install core/hab-studio
-    hab pkg install core/hab-sup
-    hab pkg install core/hab-launcher
+echo "--- :hammer_and_pick: Generating template reference docs"
+tempdir="$(mktemp --directory --tmpdir="$(pwd)" -t "docs-XXXX")"
 
-    hab pkg install core/node --binlink
+cp components/sup/doc/* "${tempdir}"
 
-    # Generate CLI docs
-    # TODO: can't currently use `hab pkg exec core/node node ...` for
-    # this because that blows away $PATH for the command, making it
-    # impossible to find `hab` :(
-    node scripts/generate-cli-docs > source/docs/habitat-cli.html.md.erb
-
-    # Generate template reference docs
-    mkdir tmp
-    cp ../components/sup/doc/* tmp/
-
-    npm install json-schema-ref-parser@6.1.0
-    node scripts/generate-template-reference.js \
-        tmp/render_context_schema.json > ./source/partials/docs/_reference-template-data.html.md.erb
-)
+npm install json-schema-ref-parser@6.1.0
+node .expeditor/scripts/finish_release/generate-template-reference.js \
+     "${tempdir}"/render_context_schema.json > www/source/partials/docs/_reference-template-data.html.md.erb
 
 echo "--- :git: Publishing updated documentation"
 git add --update
