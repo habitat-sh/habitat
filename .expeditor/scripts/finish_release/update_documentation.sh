856d82cf507a4a54ef46f5f224aea06168e3add2
Christopher Maier <cmaier@chef.io>
2020-02-19 15:04:08 -0500

    manually update the docs
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- .expeditor/scripts/finish_release/update_documentation.sh
+++ .expeditor/scripts/finish_release/update_documentation.sh
@@ -14,8 +14,23 @@ git checkout -b "$branch"
 echo "--- :hammer_and_pick: Building new automated Habitat documentation"
 (
     cd www
-    make cli_docs
-    make template_reference
+
+    hab pkg install core/hab-studio
+    hab pkg install core/hab-sup
+    hab pkg install core/hab-launcher
+
+    hab pkg install core/node
+
+    # Generate CLI docs
+    hab pkg exec core/node node scripts/generate-cli-docs > source/docs/habitat-cli.html.md.erb
+
+    # Generate template reference docs
+    mkdir tmp
+    cp ../components/sup/doc/* tmp/
+
+    hab pkg exec core/node npm install json-schema-ref-parser@6.1.0
+    hab pkg exec core/node node scripts/generate-template-reference.js \
+        tmp/render_context_schema.json > ./source/partials/docs/_reference-template-data.html.md.erb
 )
 
 echo "--- :git: Publishing updated documentation"
