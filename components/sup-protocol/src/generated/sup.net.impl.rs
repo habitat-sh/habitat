e92d5e38bcf5c3d659204d9e047c93f370ac9443
Christopher Maier <cmaier@chef.io>
2020-02-26 17:33:43 -0500

    sup-protocol: Inline code from generated/sup.net.impl.rs
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- components/sup-protocol/src/generated/sup.net.impl.rs
+++
@@ -1,8 +0,0 @@
-use crate::message;
-
-impl message::MessageStatic for NetOk {
-    const MESSAGE_ID: &'static str = "NetOk";
-}
-impl message::MessageStatic for NetErr {
-    const MESSAGE_ID: &'static str = "NetErr";
-}
