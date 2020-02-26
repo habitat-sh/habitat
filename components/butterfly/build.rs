284a18af2c765e4b4f20c7dcad2e8c6e828e6f4a
Christopher Maier <cmaier@chef.io>
2020-02-26 17:59:53 -0500

    butterfly: Stop commiting generated protobuf impls
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- components/butterfly/build.rs
+++ components/butterfly/build.rs
@@ -1,5 +1,11 @@
 use prost_build;
 
+/// Automatically generate Rust code from our protobuf definitions at
+/// compile time.
+///
+/// Generated code is deposited in `OUT_DIR` and automatically
+/// `include!`-ed in our Rust modules, per standard Prost practice.
+
 fn main() {
     let mut config = prost_build::Config::new();
     config.type_attribute(".butterfly.newscast.Rumor.payload",
@@ -10,25 +16,4 @@ fn main() {
                             "protocols/swim.proto"],
                           &["protocols/"])
           .expect("Couldn't compile protobufs!");
-    for file in generated_files() {
-        fs::copy(&file,
-                 // NB: src/generated is presumed to exist; if you delete
-                 // it, this'll fail.
-                 format!("src/generated/{}",
-                         file.file_name().unwrap().to_string_lossy())).expect("Could not copy \
-                                                                               generated code to \
-                                                                               src/generated");
-    }
-}
-
-fn generated_files() -> Vec<PathBuf> {
-    let mut files = vec![];
-    for entry in fs::read_dir(env::var("OUT_DIR").unwrap()).unwrap() {
-        let file = entry.unwrap();
-        if file.file_name().to_str().unwrap().ends_with(".rs") && file.metadata().unwrap().is_file()
-        {
-            files.push(file.path());
-        }
-    }
-    files
 }
