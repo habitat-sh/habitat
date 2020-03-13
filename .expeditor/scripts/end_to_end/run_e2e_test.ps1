1609a782b272a95effd5c84ff6b949f184c9db72
mwrock <matt@mattwrock.com>
2020-03-12 18:45:36 -0700

    sdafsf
    Signed-off-by: mwrock <matt@mattwrock.com>

--- .expeditor/scripts/end_to_end/run_e2e_test.ps1
+++ .expeditor/scripts/end_to_end/run_e2e_test.ps1
@@ -4,9 +4,13 @@ param (
     [string]$BuilderUrl = $env:HAB_BLDR_URL
 )
 
+. .expeditor/scripts/shared.ps1
 $count = 0
 while($count -lt 30) {
     $count +=1
     Write-Host "LAST EXIT: $LASTEXITCODE"
     Start-Sleep -Seconds 10
 }
+
+. .expeditor/scripts/end_to_end/setup_environment.ps1 $Channel $BuilderUrl
+Invoke-NativeCommand pwsh .expeditor/scripts/end_to_end/run_e2e_test_core.ps1 $TestName $BuilderUrl
