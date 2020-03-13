dd7063d72cc2596d76cb536c7f928ae10d360fdb
mwrock <matt@mattwrock.com>
2020-03-13 12:45:00 -0700

    allow plan context to be rooted in target folder
    Signed-off-by: mwrock <matt@mattwrock.com>

---
+++ test/fixtures/plan-in-habitat-target/habitat/x86_64-linux/plan.sh
@@ -0,0 +1,7 @@
+pkg_name="habitat_target_plan"
+pkg_origin="habitat-testing"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_version="0.0.0"
+
+do_build() { :; }
+do_install() { :; }
---
+++ test/fixtures/plan-in-root-and-habitat/plan.sh
@@ -0,0 +1,7 @@
+pkg_name="root_habitat_plan"
+pkg_origin="habitat-testing"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_version="0.0.0"
+
+do_build() { :; }
+do_install() { :; }
