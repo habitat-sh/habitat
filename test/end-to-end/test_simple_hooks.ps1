218741a2844614e336fdb0e78e7851ba51a2b9a3
mwrock <matt@mattwrock.com>
2020-03-13 18:26:00 -0700

    dfgdfgd
    Signed-off-by: mwrock <matt@mattwrock.com>

--- test/end-to-end/test_simple_hooks.ps1
+++ test/end-to-end/test_simple_hooks.ps1
@@ -14,29 +14,29 @@ Describe "Simple hooks output" {
         $path | Should -FileContentMatchExactly "install hook $svc"
     }
 
-    # It "Has correct 'init' hook stdout" {
-    #     $path = Join-Path $pkgLogsPath "init.stdout.log"
-    #     Wait-PathHasContentUpdatedAfter $path $TestStartTime 10
-    #     $path | Should -FileContentMatchExactly "init hook $svc"
-    # }
+    It "Has correct 'init' hook stdout" {
+        $path = Join-Path $pkgLogsPath "init.stdout.log"
+        Wait-PathHasContentUpdatedAfter $path $TestStartTime 10
+        $path | Should -FileContentMatchExactly "init hook $svc"
+    }
 
-    # It "Has correct 'run' hook stdout" {
-    #     $out = Get-Content $sup_log
-    #     $out | Should -Contain "$svc.default(O): run hook $svc"
-    # }
+    It "Has correct 'run' hook stdout" {
+        $out = Get-Content $sup_log
+        $out | Should -Contain "$svc.default(O): run hook $svc"
+    }
 
-    # It "Has correct 'post-run' hook stdout" {
-    #     $path = Join-Path $pkgLogsPath "post-run.stdout.log"
-    #     Wait-PathHasContentUpdatedAfter $path $TestStartTime 10
-    #     $path | Should -FileContentMatchExactly "post-run hook $svc"
-    # }
+    It "Has correct 'post-run' hook stdout" {
+        $path = Join-Path $pkgLogsPath "post-run.stdout.log"
+        Wait-PathHasContentUpdatedAfter $path $TestStartTime 10
+        $path | Should -FileContentMatchExactly "post-run hook $svc"
+    }
 
-    # $beforeUnloadTime = Get-Date
-    # Unload-SupervisorService -PackageName $pkg -Timeout 20
+    $beforeUnloadTime = Get-Date
+    Unload-SupervisorService -PackageName $pkg -Timeout 20
 
-    # It "Has correct 'post-stop' hook stdout" {
-    #     $path = Join-Path $pkgLogsPath "post-stop.stdout.log"
-    #     Wait-PathHasContentUpdatedAfter $path $beforeUnloadTime 10
-    #     $path | Should -FileContentMatchExactly "post-stop hook $svc"
-    # }
+    It "Has correct 'post-stop' hook stdout" {
+        $path = Join-Path $pkgLogsPath "post-stop.stdout.log"
+        Wait-PathHasContentUpdatedAfter $path $beforeUnloadTime 10
+        $path | Should -FileContentMatchExactly "post-stop hook $svc"
+    }
 }
