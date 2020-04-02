6633340a7ec4b7a5edbc4720eef6ba7020e00eeb
David McNeil <mcneil.david2@gmail.com>
2020-04-01 20:53:16 -0400

    Add --keep-atest flag to hab pkg uninstall
    Signed-off-by: David McNeil <mcneil.david2@gmail.com>

--- components/hab/src/command/pkg/uninstall.rs
+++ components/hab/src/command/pkg/uninstall.rs
@@ -2,22 +2,54 @@ use super::{ExecutionStrategy,
             Scope};
 use crate::{command::pkg::uninstall_impl,
             error::Result};
+use clap::ArgMatches;
 use habitat_common::ui::UI;
 use habitat_core::package::PackageIdent;
 use std::path::Path;
+
+#[derive(Clone, Copy)]
+pub enum UninstallMode {
+    Single,
+    KeepLatest(usize),
+}
+
+impl<'a> From<&'a ArgMatches<'a>> for UninstallMode {
+    fn from(m: &ArgMatches) -> Self {
+        m.value_of("KEEP_LATEST")
+         .and_then(|s| s.parse().ok())
+         .map(|n| Self::KeepLatest(n))
+         .unwrap_or(Self::Single)
+    }
+}
 
 pub async fn start(ui: &mut UI,
                    ident: &PackageIdent,
                    fs_root_path: &Path,
                    execution_strategy: ExecutionStrategy,
+                   mode: UninstallMode,
                    scope: Scope,
                    excludes: &[PackageIdent])
                    -> Result<()> {
-    uninstall_impl::uninstall(ui,
-                              ident,
-                              fs_root_path,
-                              execution_strategy,
-                              scope,
-                              excludes,
-                              false).await
+    match mode {
+        UninstallMode::Single => {
+            uninstall_impl::uninstall(ui,
+                                      ident,
+                                      fs_root_path,
+                                      execution_strategy,
+                                      scope,
+                                      excludes,
+                                      false).await
+        }
+        UninstallMode::KeepLatest(number_latest_to_keep) => {
+            uninstall_impl::uninstall_all_but_latest(ui,
+                                                     ident,
+                                                     number_latest_to_keep,
+                                                     fs_root_path,
+                                                     execution_strategy,
+                                                     scope,
+                                                     excludes,
+                                                     false).await?;
+            Ok(())
+        }
+    }
 }
