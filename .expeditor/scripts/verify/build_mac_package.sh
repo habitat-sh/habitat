3f2d77e41b2c6067d85c35cd25efe09b33d4940b
Scott Macfarlane <smacfarlane@chef.io>
2020-05-01 14:11:29 -0700

    Refactor shared macos build scripts
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- .expeditor/scripts/verify/build_mac_package.sh
+++ .expeditor/scripts/verify/build_mac_package.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
 set -eou pipefail
+
+source .expeditor/scripts/shared.sh
 
 package_path=${1?package_path argument required}
 
@@ -14,61 +16,13 @@ JOB_TEMP_ROOT=$(mktemp -d /tmp/job-root-XXXXXX)
 export HAB_CACHE_KEY_PATH
 HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys"
 
-echo "--- Installing mac bootstrap package"
-# subscribe to releases: https://github.com/habitat-sh/release-engineering/issues/84
-bootstrap_package_version="$(cat MAC_BOOTSTRAPPER_VERSION)"
-bootstrap_package_name="mac-bootstrapper-${bootstrap_package_version}-1"
-curl "https://packages.chef.io/files/stable/mac-bootstrapper/${bootstrap_package_version}/mac_os_x/10.13/${bootstrap_package_name}.dmg" -O
-sudo hdiutil attach "${bootstrap_package_name}.dmg"
-sudo installer -verbose -pkg "/Volumes/Habitat macOS Bootstrapper/${bootstrap_package_name}.pkg" -target /
-sudo hdiutil detach "/Volumes/Habitat macOS Bootstrapper"
-brew install wget
-export PATH=/opt/mac-bootstrapper/embedded/bin:/usr/local/bin:$PATH
-
-declare -g hab_binary
-
-########################################################################
-# BEGIN CERTIFICATE SHENANIGANS
-
-echo "--- Prepare cert file"
-# So, originally, we got this cert file from Homebrew's openssl
-# package. We currently need this because it gets baked into the
-# binaries we ship as part of how our API client library works. We may
-# be able to remove this in the near future, but for the time being,
-# we can just grab a cert file from the Linux core/cacerts Habitat
-# package.
-cacerts_scratch_dir="cacerts_scratch"
-mkdir "${cacerts_scratch_dir}"
-${hab_binary} pkg download core/cacerts \
-    --target=x86_64-linux \
-    --download-directory="${cacerts_scratch_dir}"
-cacerts_hart=$(find "${cacerts_scratch_dir}"/artifacts -type f -name 'core-cacerts-*-x86_64-linux.hart')
-
-# GNU tail, tar, from the mac-bootstrapper
-tail --lines=+6 "${cacerts_hart}" | \
-    tar --extract \
-        --verbose \
-        --xz \
-        --strip-components=8 \
-        --wildcards "hab/pkgs/core/cacerts/*/*/ssl/certs"
-
-cert_file="cacert.pem"
-if [ ! -f "${cert_file}" ]; then
-    echo "Couldn't extract ${cert_file} file from core/cacerts package!"
-    exit 1
-fi
-export SSL_CERT_FILE
-SSL_CERT_FILE="$(pwd)/${cert_file}"
-
-# END CERTIFICATE SHENANIGANS
-########################################################################
+macos_install_bootstrap_package
+macos_use_cert_file_from_linux_cacerts_package
 
 echo "--- :key: Generating fake origin key"
 hab origin key generate
-# We invoke hab-plan-build.sh directly via sudo, so we don't get the key management that studio provides.
-# Copy keys from the user account Habitat cache to the system Habitat cache so that they are present for root.
-sudo mkdir -p /hab/cache/keys
-sudo cp -r ~/.hab/cache/keys/* /hab/cache/keys/
+
+macos_sync_cache_signing_keys
 
 # set the rust toolchain
 install_rustup
@@ -76,8 +30,4 @@ rust_toolchain="$(cat rust-toolchain)"
 echo "--- :rust: Using Rust toolchain ${rust_toolchain}"
 rustc --version # just 'cause I'm paranoid and I want to double check
 
-echo "--- :habicat: Building components/hab"
-
-sudo -E bash \
-        components/plan-build/bin/hab-plan-build.sh \
-        "$package_path"
+macos_build "$package_path"
