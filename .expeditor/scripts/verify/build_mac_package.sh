6a26fb50a24f15a45be8eb59b682804ef25d0fbc
Scott Macfarlane <smacfarlane@chef.io>
2020-05-01 14:54:18 -0700

    Don't use throwaway cache dir for keys
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- .expeditor/scripts/verify/build_mac_package.sh
+++ .expeditor/scripts/verify/build_mac_package.sh
@@ -11,12 +11,6 @@ package_path=${1?package_path argument required}
 export HAB_ORIGIN
 HAB_ORIGIN=throwaway
 
-# let's make a selfcontained tempdir for this job
-export JOB_TEMP_ROOT
-JOB_TEMP_ROOT=$(mktemp -d /tmp/job-root-XXXXXX)
-export HAB_CACHE_KEY_PATH
-HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys"
-
 curlbash_hab "$BUILD_PKG_TARGET"
 
 macos_install_bootstrap_package
