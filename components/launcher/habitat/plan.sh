e2a3dab8a1954a8b2fd60b41bb5a8394297d29b7
Christopher Maier <cmaier@chef.io>
2020-05-21 17:04:21 -0400

    Revert "Merge pull request #7688 from habitat-sh/cm/static-launcher"
    This reverts commit dc7234169366a41298b8f7c9812dfe0c1680351b, reversing
    changes made to d5497169422a8c2dac275c1d3f79fab8b1a52996.
    
    Additional testing revealed some (possibly minor) behavioral
    changes. Until they can be verified as harmless, we're reverting out
    of an abundance of caution.
    
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- components/launcher/habitat/plan.sh
+++ components/launcher/habitat/plan.sh
@@ -3,20 +3,15 @@ pkg_name=hab-launcher
 pkg_origin=core
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
-pkg_build_deps=(
-    core/musl
-    core/zlib-musl
-    core/xz-musl
-    core/bzip2-musl
-    core/libarchive-musl
-    core/openssl-musl
-
-    core/coreutils
-    core/rust/"$(cat "$SRC_PATH/../../rust-toolchain")"
-    core/gcc
-    core/git
-    core/protobuf
-)
+pkg_deps=(core/glibc
+          core/gcc-libs
+          core/libarchive
+          core/openssl)
+pkg_build_deps=(core/coreutils
+                core/rust/"$(cat "$SRC_PATH/../../rust-toolchain")"
+                core/gcc
+                core/git
+                core/protobuf)
 pkg_bin_dirs=(bin)
 bin="hab-launch"
 
@@ -36,35 +31,20 @@ do_prepare() {
   # Can be either `--release` or `--debug` to determine cargo build strategy
   build_line "Building artifacts with \`${cargo_build_mode#--}' mode"
 
-  export rustc_target="x86_64-unknown-linux-musl"
+  export rustc_target="x86_64-unknown-linux-gnu"
   build_line "Setting rustc_target=$rustc_target"
 
   # Used by Cargo to use a pristine, isolated directory for all compilation
   export CARGO_TARGET_DIR="$HAB_CACHE_SRC_PATH/$pkg_dirname"
   build_line "Setting CARGO_TARGET_DIR=$CARGO_TARGET_DIR"
-
+  
   export PLAN_VERSION="${pkg_version}/${pkg_release}"
   build_line "Setting PLAN_VERSION=$PLAN_VERSION"
 
-  la_ldflags="-L$(pkg_path_for zlib-musl)/lib -lz"
-  la_ldflags="$la_ldflags -L$(pkg_path_for xz-musl)/lib -llzma"
-  la_ldflags="$la_ldflags -L$(pkg_path_for bzip2-musl)/lib -lbz2"
-  la_ldflags="$la_ldflags -L$(pkg_path_for openssl-musl)/lib -lssl -lcrypto"
-
-  export LIBARCHIVE_LIB_DIR=$(pkg_path_for libarchive-musl)/lib
-  export LIBARCHIVE_INCLUDE_DIR=$(pkg_path_for libarchive-musl)/include
-  export LIBARCHIVE_LDFLAGS="$la_ldflags"
-  export LIBARCHIVE_STATIC=true
-  export OPENSSL_LIB_DIR=$(pkg_path_for openssl-musl)/lib
-  export OPENSSL_INCLUDE_DIR=$(pkg_path_for openssl-musl)/include
-  export OPENSSL_STATIC=true
-
-  # Used to find libgcc_s.so.1 when compiling `build.rs` in
-  # dependencies (`log`, in particular). Since this used only at build
-  # time, we will use the version found in the gcc package proper--it
-  # won't find its way into the final binaries.
-  export LD_LIBRARY_PATH=$(pkg_path_for gcc)/lib
-  build_line "Setting LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+  export LIBARCHIVE_LIB_DIR=$(pkg_path_for libarchive)/lib
+  export LIBARCHIVE_INCLUDE_DIR=$(pkg_path_for libarchive)/include
+  export OPENSSL_LIB_DIR=$(pkg_path_for openssl)/lib
+  export OPENSSL_INCLUDE_DIR=$(pkg_path_for openssl)/include
 
   # Prost (our Rust protobuf library) embeds a `protoc` binary, but
   # it's dynamically linked, which means it won't work in a
