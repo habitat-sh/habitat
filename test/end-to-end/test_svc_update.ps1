409276d70dfab2a0a287e2db2627800fe46dcf65
Christopher Maier <cmaier@chef.io>
2020-07-06 15:27:02 -0400

    Rephrase test in terms of `hab svc update`
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- test/end-to-end/test_svc_update.ps1
+++ test/end-to-end/test_svc_update.ps1
@@ -38,46 +38,46 @@ if ($IsWindows) {
     $release="habitat-testing/nginx/1.18.0/20200626184200"
 }
 
-Describe "modification of Supervisor-specific service configuration" {
-    Start-Supervisor -Timeout 45
+Describe "hab svc update" {
+    Context "an updating service" {
+        Start-Supervisor -Timeout 45
 
-    hab pkg promote $release $testChannelOne
-    hab pkg promote $release $testChannelTwo
+        hab pkg promote $release $testChannelOne
+        hab pkg promote $release $testChannelTwo
 
-    Load-SupervisorService $pkg -Strategy "at-once" -Channel $testChannelOne
-    Wait-Release -Ident $release
+        Load-SupervisorService $pkg -Strategy "at-once" -Channel $testChannelOne
+        Wait-Release -Ident $release
 
-    $proc = Get-Process nginx
+        $proc = Get-Process nginx
 
-    It "starts updating from the first channel" {
-        '/hab/sup/default/specs/nginx.spec' | Should -FileContentMatchExactly "channel = `"$testChannelOne`""
-    }
-
-    Context "modify update channel" {
-        # We must include the strategy as well, because `hab svc load`
-        # is not a patchy operation.
-        hab svc load $pkg --force --strategy "at-once" --channel $testChannelTwo
-
-        # Give *plenty* of time to pick up the new spec (as well as
-        # time for a service to restart, if things are broken and
-        # that's a thing that could happen).
-        Start-Sleep -Seconds 10
-
-        It "has the new channel in the spec file" {
-            '/hab/sup/default/specs/nginx.spec' | Should -FileContentMatchExactly "channel = `"$testChannelTwo`""
-            '/hab/sup/default/specs/nginx.spec' | Should -FileContentMatchExactly "strategy = `"at-once`""
+        It "starts updating from the first channel" {
+            '/hab/sup/default/specs/nginx.spec' | Should -FileContentMatchExactly "channel = `"$testChannelOne`""
         }
 
-        It "does not restart the service process" {
-            $currentProc = Get-Process nginx
-            $proc.Id | Should -Be $currentProc.Id
-        }
-    }
+        Context "modify update channel" {
+            hab svc update $pkg --channel $testChannelTwo
 
-    AfterAll {
-        hab bldr channel destroy $testChannelOne --origin habitat-testing
-        hab bldr channel destroy $testChannelTwo --origin habitat-testing
-        Unload-SupervisorService -PackageName $pkg -Timeout 20
-        Stop-Supervisor
+            # Give *plenty* of time to pick up the new spec (as well as
+            # time for a service to restart, if things are broken and
+            # that's a thing that could happen).
+            Start-Sleep -Seconds 10
+
+            It "has the new channel in the spec file" {
+                '/hab/sup/default/specs/nginx.spec' | Should -FileContentMatchExactly "channel = `"$testChannelTwo`""
+                '/hab/sup/default/specs/nginx.spec' | Should -FileContentMatchExactly "update_strategy = `"at-once`""
+            }
+
+            It "does not restart the service process" {
+                $currentProc = Get-Process nginx
+                $proc.Id | Should -Be $currentProc.Id
+            }
+        }
+
+        AfterAll {
+            hab bldr channel destroy $testChannelOne --origin habitat-testing
+            hab bldr channel destroy $testChannelTwo --origin habitat-testing
+            Unload-SupervisorService -PackageName $pkg -Timeout 20
+            Stop-Supervisor
+        }
     }
 }
