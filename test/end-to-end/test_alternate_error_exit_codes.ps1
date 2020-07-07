69f3c293755e274bb9cb8489999a768af8e9c833
David McNeil <mcneil.david2@gmail.com>
2020-07-07 10:51:40 -0500

    Add alternate error exit code tests
    Signed-off-by: David McNeil <mcneil.david2@gmail.com>

---
+++ test/end-to-end/test_alternate_error_exit_codes.ps1
@@ -0,0 +1,39 @@
+Describe "install and uninstall hook error codes are propogated" {
+    BeforeAll {
+        $PkgName = "custom-hook-exit-code"
+        $PkgOrigin = "habitat-testing"
+        $PkgIdent = "$PkgOrigin/$PkgName"
+        $Env:HAB_ORIGIN = $PkgOrigin
+        hab origin key generate $PkgOrigin
+        Invoke-Build $PkgName
+        . ./results/last_build.ps1
+    }
+
+    It "`hab pkg install` exits with install hook exit code" {
+        $Env:CUSTOM_HOOK_EXIT_CODE = 42
+        hab pkg install ./results/$pkg_artifact
+        $LastExitCode | Should -Be $Env:CUSTOM_HOOK_EXIT_CODE
+    }
+
+    It "`hab pkg install` succeeds" {
+        $Env:CUSTOM_HOOK_EXIT_CODE = ""
+        hab pkg install ./results/$pkg_artifact
+        $LastExitCode | Should -Be 0
+    }
+
+    It "`hab pkg uninstall` exits with uninstall hook exit code" {
+        $Env:CUSTOM_HOOK_EXIT_CODE = 97
+        hab pkg uninstall $PkgIdent
+        $LastExitCode | Should -Be $Env:CUSTOM_HOOK_EXIT_CODE
+    }
+
+    It "`hab pkg uninstall` succeeds" {
+        $Env:CUSTOM_HOOK_EXIT_CODE = ""
+        hab pkg uninstall $PkgIdent
+        $LastExitCode | Should -Be 0
+    }
+
+    AfterAll {
+        hab pkg uninstall --keep-latest 0 $PkgIdent
+    }
+}
