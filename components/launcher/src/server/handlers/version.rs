e50375c01948be04fa4e55efbdbe6d2555288609
mwrock <matt@mattwrock.com>
2020-08-04 13:13:01 -0700

    fix launcher version parsing
    Signed-off-by: mwrock <matt@mattwrock.com>

--- components/launcher/src/server/handlers/version.rs
+++ components/launcher/src/server/handlers/version.rs
@@ -13,14 +13,18 @@ impl Handler for VersionHandler {
     fn handle(_: Self::Message, _: &mut ServiceTable) -> HandleResult<Self::Reply> {
         // VERSION will be none if this is a cargo built binary as opposed to
         // being built by our hab plan. So in that case we will fallback to u32::MAX.
-        match VERSION.unwrap_or(&u32::MAX.to_string()).parse::<u32>() {
-            Ok(version) => {
-                let reply = protocol::VersionNumber { version };
+        let max = u32::MAX.to_string();
+        let version = VERSION.unwrap_or(&max);
+        match version.parse::<u32>() {
+            Ok(v) => {
+                let reply = protocol::VersionNumber { version: v };
                 Ok(reply)
             }
-            Err(_) => {
+            Err(err) => {
+                let err_msg = format!("Unable to parse version {}: {}", version, err.to_string());
                 let mut reply = protocol::NetErr::default();
                 reply.code = protocol::ErrCode::InvalidVersionNumber;
+                reply.msg = err_msg;
                 Err(reply)
             }
         }
