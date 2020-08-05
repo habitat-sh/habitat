ce86e811039adf0f44f61a97db4904be17998a85
Christopher Maier <cmaier@chef.io>
2020-08-05 14:50:04 -0400

    [REFACTOR] Update `hab origin secret upload` to use new types
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- components/hab/src/command/origin/secret/upload.rs
+++ components/hab/src/command/origin/secret/upload.rs
@@ -2,13 +2,12 @@ use super::super::key::download::download_public_encryption_key;
 use crate::{api_client::Client,
             common::ui::{Status,
                          UIWriter,
-                         UI}};
-
-use crate::{error::{Error,
+                         UI},
+            error::{Error,
                     Result},
-            hcore::crypto::BoxKeyPair,
             PRODUCT,
             VERSION};
+use habitat_core::crypto::keys::KeyCache;
 use std::path::Path;
 
 pub async fn start(ui: &mut UI,
@@ -17,26 +16,27 @@ pub async fn start(ui: &mut UI,
                    origin: &str,
                    key: &str,
                    secret: &str,
-                   cache: &Path)
+                   cache_dir: &Path)
                    -> Result<()> {
     let api_client = Client::new(bldr_url, PRODUCT, VERSION, None).map_err(Error::APIClient)?;
 
-    let encryption_key = match BoxKeyPair::get_latest_pair_for(origin, cache) {
+    let cache = KeyCache::new(cache_dir);
+    let encryption_key = match cache.latest_origin_public_encryption_key(origin) {
         Ok(key) => key,
         Err(_) => {
             debug!("Didn't find public encryption key in cache path");
-            download_public_encryption_key(ui, &api_client, origin, token, cache).await?;
-            BoxKeyPair::get_latest_pair_for(origin, cache)?
+            download_public_encryption_key(ui, &api_client, origin, token, cache_dir).await?;
+            cache.latest_origin_public_encryption_key(origin)?
         }
     };
 
     ui.status(Status::Encrypting, format!("value for key {}.", key))?;
-    let encrypted_secret_string = encryption_key.encrypt(secret.as_bytes(), None)?;
+    let encrypted_box = encryption_key.encrypt(secret.as_bytes());
     ui.status(Status::Encrypted, format!("{}=[REDACTED].", key))?;
 
     ui.status(Status::Uploading, format!("secret for key {}.", key))?;
 
-    api_client.create_origin_secret(origin, token, key, &encrypted_secret_string)
+    api_client.create_origin_secret(origin, token, key, &encrypted_box)
               .await
               .map_err(Error::APIClient)?;
 
