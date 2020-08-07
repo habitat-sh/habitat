3bdb4056d538e77089b4609cbbfb445d60fa5d46
Christopher Maier <cmaier@chef.io>
2020-08-07 11:55:03 -0400

    [REFACTOR] Consolidate all encryption keys in a submodule
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- components/core/src/crypto/keys/encryption.rs
+++ components/core/src/crypto/keys/encryption.rs
@@ -1,5 +1,17 @@
 mod message;
+mod origin_key;
+mod service_key;
+mod user_key;
 
 pub use message::{AnonymousBox,
                   EncryptedSecret,
                   SignedBox};
+pub use origin_key::{generate_origin_encryption_key_pair,
+                     OriginPublicEncryptionKey,
+                     OriginSecretEncryptionKey};
+pub use service_key::{generate_service_encryption_key_pair,
+                      ServicePublicEncryptionKey,
+                      ServiceSecretEncryptionKey};
+pub use user_key::{generate_user_encryption_key_pair,
+                   UserPublicEncryptionKey,
+                   UserSecretEncryptionKey};
