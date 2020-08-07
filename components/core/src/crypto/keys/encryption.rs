0adbf9eaf2497bb131c2f0b80d74470108446c40
Christopher Maier <cmaier@chef.io>
2020-08-07 12:39:29 -0400

    [REFACTOR] Further tightening up of encryption keys
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- components/core/src/crypto/keys/encryption.rs
+++ components/core/src/crypto/keys/encryption.rs
@@ -15,3 +15,28 @@ pub use service_key::{generate_service_encryption_key_pair,
 pub use user_key::{generate_user_encryption_key_pair,
                    UserPublicEncryptionKey,
                    UserSecretEncryptionKey};
+
+/// The suffix on the end of a public encryption key file
+const PUBLIC_KEY_SUFFIX: &str = "pub";
+/// The suffix on the end of a secret encryption key file
+const SECRET_BOX_KEY_SUFFIX: &str = "box.key";
+/// Format version identifier for public encryption keys.
+const PUBLIC_BOX_KEY_VERSION: &str = "BOX-PUB-1";
+/// Format version identifier for secret encryption keys.
+const SECRET_BOX_KEY_VERSION: &str = "BOX-SEC-1";
+
+/// Private module to re-export the various sodiumoxide concepts we
+/// use, to ensure everyone is using them consistently.
+mod primitives {
+    pub use sodiumoxide::crypto::{box_::{curve25519xsalsa20poly1305::{gen_nonce,
+                                                                      Nonce,
+                                                                      PublicKey,
+                                                                      SecretKey},
+                                         gen_keypair,
+                                         open,
+                                         seal},
+                                  sealedbox};
+
+    from_slice_impl_for_sodiumoxide_key!(PublicKey);
+    from_slice_impl_for_sodiumoxide_key!(SecretKey);
+}
--- components/core/src/crypto/keys/encryption/origin_key.rs
+++ components/core/src/crypto/keys/encryption/origin_key.rs
@@ -1,17 +1,17 @@
-use crate::{crypto::{keys::{box_key_pair::WrappedSealedBox,
-                            encryption::{AnonymousBox,
-                                         EncryptedSecret},
-                            Key,
-                            KeyRevision,
-                            NamedRevision},
-                     PUBLIC_BOX_KEY_VERSION,
-                     SECRET_BOX_KEY_VERSION},
+use crate::{crypto::keys::{box_key_pair::WrappedSealedBox,
+                           encryption::{primitives,
+                                        AnonymousBox,
+                                        EncryptedSecret,
+                                        PUBLIC_BOX_KEY_VERSION,
+                                        PUBLIC_KEY_SUFFIX,
+                                        SECRET_BOX_KEY_SUFFIX,
+                                        SECRET_BOX_KEY_VERSION},
+                           Key,
+                           KeyRevision,
+                           NamedRevision},
             error::{Error,
                     Result}};
-use sodiumoxide::crypto::{box_::{self,
-                                 curve25519xsalsa20poly1305::{PublicKey as BoxPublicKey,
-                                                              SecretKey as BoxSecretKey}},
-                          sealedbox};
+
 use std::{path::PathBuf,
           str};
 
@@ -23,7 +23,7 @@ pub fn generate_origin_encryption_key_pair(
     origin_name: &str)
     -> (OriginPublicEncryptionKey, OriginSecretEncryptionKey) {
     let revision = KeyRevision::new();
-    let (pk, sk) = box_::gen_keypair();
+    let (pk, sk) = primitives::gen_keypair();
 
     let public = OriginPublicEncryptionKey::from_raw(origin_name.to_string(), revision.clone(), pk);
     let secret = OriginSecretEncryptionKey::from_raw(origin_name.to_string(), revision.clone(), sk);
@@ -34,18 +34,18 @@ pub fn generate_origin_encryption_key_pair(
 
 pub struct OriginPublicEncryptionKey {
     named_revision: NamedRevision,
-    key:            BoxPublicKey,
+    key:            primitives::PublicKey,
     path:           PathBuf,
 }
 
 impl Key for OriginPublicEncryptionKey {
-    type Crypto = BoxPublicKey;
+    type Crypto = primitives::PublicKey;
 
-    const EXTENSION: &'static str = "pub";
+    const EXTENSION: &'static str = PUBLIC_KEY_SUFFIX;
     const PERMISSIONS: crate::fs::Permissions = crate::fs::DEFAULT_PUBLIC_KEY_PERMISSIONS;
     const VERSION_STRING: &'static str = PUBLIC_BOX_KEY_VERSION;
 
-    fn key(&self) -> &BoxPublicKey { &self.key }
+    fn key(&self) -> &primitives::PublicKey { &self.key }
 
     fn named_revision(&self) -> &NamedRevision { &self.named_revision }
 }
@@ -57,7 +57,10 @@ try_from_path_buf_impl_for_key!(OriginPublicEncryptionKey);
 as_ref_path_impl_for_key!(OriginPublicEncryptionKey);
 
 impl OriginPublicEncryptionKey {
-    pub(crate) fn from_raw(name: String, revision: KeyRevision, key: BoxPublicKey) -> Self {
+    pub(crate) fn from_raw(name: String,
+                           revision: KeyRevision,
+                           key: primitives::PublicKey)
+                           -> Self {
         let named_revision = NamedRevision::new(name, revision);
         let path = named_revision.filename::<Self>();
         Self { named_revision,
@@ -68,7 +71,7 @@ impl OriginPublicEncryptionKey {
     // corresponds to old encrypt_anonymous_box
     /// Encrypt a secret
     pub fn encrypt(&self, data: &[u8]) -> WrappedSealedBox {
-        let ciphertext = sealedbox::seal(data, self.key());
+        let ciphertext = primitives::sealedbox::seal(data, self.key());
         let anon = AnonymousBox::new(self.named_revision().clone(), ciphertext);
         // TODO (CM): Eventually do away with WrappedSealedBox; this
         // is just for compatibility now
@@ -80,18 +83,18 @@ impl OriginPublicEncryptionKey {
 
 pub struct OriginSecretEncryptionKey {
     named_revision: NamedRevision,
-    key:            BoxSecretKey,
+    key:            primitives::SecretKey,
     path:           PathBuf,
 }
 
 impl Key for OriginSecretEncryptionKey {
-    type Crypto = BoxSecretKey;
+    type Crypto = primitives::SecretKey;
 
-    const EXTENSION: &'static str = "box.key";
+    const EXTENSION: &'static str = SECRET_BOX_KEY_SUFFIX;
     const PERMISSIONS: crate::fs::Permissions = crate::fs::DEFAULT_SECRET_KEY_PERMISSIONS;
     const VERSION_STRING: &'static str = SECRET_BOX_KEY_VERSION;
 
-    fn key(&self) -> &BoxSecretKey { &self.key }
+    fn key(&self) -> &primitives::SecretKey { &self.key }
 
     fn named_revision(&self) -> &NamedRevision { &self.named_revision }
 }
@@ -103,7 +106,10 @@ try_from_path_buf_impl_for_key!(OriginSecretEncryptionKey);
 as_ref_path_impl_for_key!(OriginSecretEncryptionKey);
 
 impl OriginSecretEncryptionKey {
-    pub(crate) fn from_raw(name: String, revision: KeyRevision, key: BoxSecretKey) -> Self {
+    pub(crate) fn from_raw(name: String,
+                           revision: KeyRevision,
+                           key: primitives::SecretKey)
+                           -> Self {
         let named_revision = NamedRevision::new(name, revision);
         let path = named_revision.filename::<Self>();
         Self { named_revision,
