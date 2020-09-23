142013c77a03ad4b90996b687cba1bbf7c5737e4
David McNeil <mcneil.david2@gmail.com>
2020-09-23 14:15:08 -0500

    Cleanup rustls readers
    Signed-off-by: David McNeil <mcneil.david2@gmail.com>

--- components/core/src/tls/rustls_wrapper/readers.rs
+++ components/core/src/tls/rustls_wrapper/readers.rs
@@ -5,7 +5,7 @@ use rustls::{internal::pemfile,
              Certificate,
              PrivateKey,
              RootCertStore};
-use std::{fs,
+use std::{fs::File,
           io::{self,
                BufReader},
           path::{Path,
@@ -17,85 +17,51 @@ pub enum Error {
     #[error("failed to read file {0}, err: {1}")]
     FailedToReadFile(PathBuf, io::Error),
 
-    #[error("failed to read PEM certificates")]
-    FailedToReadCerts,
     #[error("failed to read PEM certificates from file {0}")]
-    FailedToReadCertsFromFile(PathBuf),
+    FailedToReadCerts(PathBuf),
 
-    #[error("failed to read PEM, PKCS8 private keys")]
-    FailedToReadPrivateKeys,
     #[error("failed to read PEM, PKCS8 private keys from file {0}")]
-    FailedToReadPrivateKeysFromFile(PathBuf),
-    #[error("no PEM, PKCS8 private key")]
-    NoPrivateKey,
+    FailedToReadPrivateKeys(PathBuf),
     #[error("no PEM, PKCS8 private keys in file {0}")]
-    NoPrivateKeyFromFile(PathBuf),
+    NoPrivateKey(PathBuf),
 
-    #[error("failed to read PEM root certificate store")]
-    FailedToReadRootCertificateStore,
     #[error("failed to read PEM root certificate store {0}")]
-    FailedToReadRootCertificateStoreFromFile(PathBuf),
-    #[error("failed to read {0} certificates from PEM root certificate store")]
-    FailedToReadCertificatesFromRootStore(usize),
+    FailedToReadRootCertificateStore(PathBuf),
     #[error("failed to read {0} certificates from PEM root certificate store file {1}")]
-    FailedToReadCertificatesFromRootStoreFile(usize, PathBuf),
+    FailedToReadCertificatesFromRootCertificateStore(usize, PathBuf),
 }
 
-fn certificates_from_buf(buf: &[u8]) -> Result<Vec<Certificate>, Error> {
-    let mut cursor = BufReader::new(buf);
-    pemfile::certs(&mut cursor).map_err(|_| Error::FailedToReadCerts)
+fn buf_from_file(path: impl AsRef<Path>) -> Result<BufReader<File>, Error> {
+    let file =
+        File::open(path.as_ref()).map_err(|e| Error::FailedToReadFile(path.as_ref().into(), e))?;
+    Ok(BufReader::new(file))
 }
 
 pub fn certificates_from_file(path: impl AsRef<Path>) -> Result<Vec<Certificate>, Error> {
-    let buf =
-        fs::read(path.as_ref()).map_err(|e| Error::FailedToReadFile(path.as_ref().into(), e))?;
-    certificates_from_buf(&buf).map_err(|_| Error::FailedToReadCertsFromFile(path.as_ref().into()))
+    let mut buf = buf_from_file(path.as_ref())?;
+    pemfile::certs(&mut buf).map_err(|_| Error::FailedToReadCerts(path.as_ref().into()))
 }
 
-fn private_keys_from_buf(buf: &[u8]) -> Result<Vec<PrivateKey>, Error> {
-    let mut cursor = BufReader::new(buf);
-    pemfile::pkcs8_private_keys(&mut cursor).map_err(|_| Error::FailedToReadPrivateKeys)
-}
-
-pub fn private_keys_from_file(path: impl AsRef<Path>) -> Result<Vec<PrivateKey>, Error> {
-    let buf =
-        fs::read(path.as_ref()).map_err(|e| Error::FailedToReadFile(path.as_ref().into(), e))?;
-    private_keys_from_buf(&buf).map_err(|_| {
-                                   Error::FailedToReadPrivateKeysFromFile(path.as_ref().into())
-                               })
+fn private_keys_from_file(path: impl AsRef<Path>) -> Result<Vec<PrivateKey>, Error> {
+    let mut buf = buf_from_file(path.as_ref())?;
+    pemfile::pkcs8_private_keys(&mut buf).map_err(|_| {
+                                             Error::FailedToReadPrivateKeys(path.as_ref().into())
+                                         })
 }
 
 pub fn private_key_from_file(path: impl AsRef<Path>) -> Result<PrivateKey, Error> {
-    private_keys_from_file(path.as_ref())?.into_iter()
-                                          .next()
-                                          .ok_or_else(|| {
-                                              Error::NoPrivateKeyFromFile(path.as_ref().into())
-                                          })
-}
-
-fn root_certificate_store_from_buf(buf: &[u8]) -> Result<RootCertStore, Error> {
-    let mut root_certificate_store = RootCertStore::empty();
-    let mut cursor = BufReader::new(buf);
-    let (_, failed) = root_certificate_store.add_pem_file(&mut cursor)
-                                            .map_err(|_| Error::FailedToReadPrivateKeys)?;
-    if failed > 0 {
-        return Err(Error::FailedToReadCertificatesFromRootStore(failed));
-    }
-    Ok(root_certificate_store)
+    private_keys_from_file(path.as_ref())?.pop()
+                                          .ok_or_else(|| Error::NoPrivateKey(path.as_ref().into()))
 }
 
 pub fn root_certificate_store_from_file(path: impl AsRef<Path>) -> Result<RootCertStore, Error> {
-    let buf =
-        fs::read(path.as_ref()).map_err(|e| Error::FailedToReadFile(path.as_ref().into(), e))?;
-    root_certificate_store_from_buf(&buf).map_err(|e| {
-        use Error::{FailedToReadCertificatesFromRootStore as ReadCertificates,
-                    FailedToReadCertificatesFromRootStoreFile as ReadCertificatesFromFile,
-                    FailedToReadRootCertificateStore as RootCertStore,
-                    FailedToReadRootCertificateStoreFromFile as RootCertStoreFromFile};
-        match e {
-            RootCertStore => RootCertStoreFromFile(path.as_ref().into()),
-            ReadCertificates(failed) => ReadCertificatesFromFile(failed, path.as_ref().into()),
-            _ => e,
-        }
-    })
+    let mut buf = buf_from_file(path.as_ref())?;
+    let mut root_certificate_store = RootCertStore::empty();
+    let (_, failed) = root_certificate_store.add_pem_file(&mut buf)
+                                            .map_err(|_| Error::FailedToReadRootCertificateStore(path.as_ref().into()))?;
+    if failed > 0 {
+        Err(Error::FailedToReadCertificatesFromRootCertificateStore(failed, path.as_ref().into()));
+    } else {
+        Ok(root_certificate_store)
+    }
 }
