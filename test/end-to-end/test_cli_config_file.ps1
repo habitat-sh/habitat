83bdd3eca305dc6870f9e8c74642150f3a98d0b2
David McNeil <mcneil.david2@gmail.com>
2020-09-25 11:48:33 -0500

    Add cli.toml e2e test
    Signed-off-by: David McNeil <mcneil.david2@gmail.com>

---
+++ test/end-to-end/test_cli_config_file.ps1
@@ -0,0 +1,31 @@
+$configPath = "/hab/etc/cli.toml"
+$ctlSecretPath = "/hab/sup/default/CTL_SECRET"
+
+Describe "reading from supervisor and service config files" {
+    BeforeAll {
+        Remove-Item -Force -Recurse -ErrorAction Ignore $configPath
+        Start-Supervisor -Timeout 45
+        $ctlSecret = Get-Content -Path $ctlSecretPath
+    }
+
+    It "CLI succeeds with secret from file" {
+        Invoke-NativeCommand hab svc status
+    }
+
+
+    It "CLI fails without secret set" {
+        Remove-Item -Force -Recurse -ErrorAction Ignore $ctlSecretPath
+        {
+            Invoke-NativeCommand hab svc status
+        } | Should -Throw
+    }
+
+    It "CLI succeeds with secret set in config file" {
+        Set-Content -Path $configPath -Value "ctl_secret='$ctlSecret'"
+        Invoke-NativeCommand hab svc status
+    }
+
+    AfterAll {
+        Stop-Supervisor
+    }
+}
