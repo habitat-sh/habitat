c836ffb11adf7b8a704ac63c38b64cc5d137e86a
IanMadd <imaddaus@chef.io>
2020-11-04 13:07:56 -0800

    Update automatic generation of Service Template Data docs
    Signed-off-by: IanMadd <imaddaus@chef.io>

--- .expeditor/scripts/finish_release/generate-template-reference.js
+++ .expeditor/scripts/finish_release/generate-template-reference.js
@@ -3,8 +3,19 @@ const stdout = process.stdout;
 let lines = [];
 
 function writeHeader() {
-  lines.push(`<!-- This is a generated file, do not edit it directly. See https://github.com/habitat-sh/habitat/blob/master/www/scripts/generate-template-reference.js -->`)
-  lines.push(`## Template Data`);
+  lines.push(`+++`);
+  lines.push(`title = "Service Template Data"`);
+  lines.push(`date = ` + new Date().toISOString());
+  lines.push(`draft = false`);
+  lines.push(``);
+  lines.push(`[menu]`);
+  lines.push(`  [menu.habitat]`);
+  lines.push(`    title = "Service Template Data"`);
+  lines.push(`    identifier = "habitat/reference/service_templates Service Template Data"`);
+  lines.push(`    parent = "habitat/reference"`);
+  lines.push(`+++`);
+  lines.push(``);
+  lines.push(`<!-- This is a generated file, do not edit it directly. See https://github.com/habitat-sh/habitat/blob/master/.expeditor/scripts/finish_release/generate-template-reference.js -->`)
   lines.push('');
   lines.push(`The following settings can be used during a Chef Habitat service's lifecycle. This means that you can use these settings in any of the plan hooks, such as \`init\`, or \`run\`, and also in any templatized configuration file for your application or service.`)
   lines.push('');
@@ -13,7 +24,7 @@ function writeHeader() {
 }
 
 function writeDefinitions() {
-  lines.push(`### Reference Objects`);
+  lines.push(`## Reference Objects`);
   lines.push('');
   lines.push(`Some of the template expressions referenced above return objects of a specific shape; for example, the \`svc.me\` and \`svc.first\` expressions return "service member" objects, and the \`pkg\` property of a service member returns a "package identifier" object. These are defined below.`);
   lines.push('');
@@ -22,7 +33,7 @@ function writeDefinitions() {
     .map(key => {
       const p = schema.definitions[key];
 
-      lines.push(`### ${key}`);
+      lines.push(`## ${key}`);
       lines.push('');
       lines.push(p.description);
       lines.push('');
@@ -38,7 +49,7 @@ function writeProperties() {
       const properties = p.properties;
       const additional = p.additionalProperties;
 
-      lines.push(`### ${key}`);
+      lines.push(`## ${key}`);
       lines.push('');
       lines.push(p.description);
       lines.push('');
