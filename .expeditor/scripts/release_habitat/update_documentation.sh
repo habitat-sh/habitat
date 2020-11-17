e40702464ba2fef853cea9c210006cc04c0678e0
Christopher Maier <cmaier@chef.io>
2020-11-17 15:22:11 -0500

    [CI] Move documentation generation to release pipeline
    To harmonize with the new company-wide way of managing our
    documentation updates, we are changing how we generate our
    documentation.
    
    Previously, once a release had been promoted to stable, we generated
    two pieces of dynamically-updatable documentation, created a PR for
    them, and then merged them back into the repository.
    
    Now, we generate these docs during our release pipeline, for every
    potential release candidate. Rather than storing the documentation
    back in the repository, we save them in a tarball (specifically,
    `generated-documentation-${VERSION}.tar.gz`) in our S3 bucket
    with the rest of the artifacts we generate. Later on, this artifact
    will be used to assemble the final documentation that will actually be
    published (that part of the process is owned by release engineering;
    this PR simply makes what changes are needed to enable their work).
    
    Since we are no longer committing generated content to the repository,
    and in order to prevent future confusion, the content of the existing
    documentation files has been replaced with a placeholder comment
    explaining the situation.
    
    Signed-off-by: Christopher Maier <cmaier@chef.io>

---
+++ .expeditor/scripts/release_habitat/update_documentation.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# shellcheck source=.expeditor/scripts/release_habitat/shared.sh
+source .expeditor/scripts/release_habitat/shared.sh
+
+export HAB_BLDR_URL="${PIPELINE_HAB_BLDR_URL}"
+
+channel=$(get_release_channel)
+version=$(get_version_from_repo)
+
+# We want the version of Habitat we just built. This is important
+# because it will be where we get the CLI docs from.
+#
+# At some point we can hopefully get this some other way, but for now,
+# the binary tells us how to use it.
+declare -g hab_binary
+install_release_channel_hab_binary "$BUILD_PKG_TARGET"
+
+# Ensure that we have this version of `hab` on the $PATH.
+#
+# See https://github.com/habitat-sh/habitat/pull/7835
+# and https://github.com/chef/release-engineering/issues/1241 for
+# further background.
+${hab_binary} pkg binlink "core/hab/${version}" --force --dest=/usr/bin
+
+hab pkg install core/hab-studio --channel="${channel}"
+hab pkg install core/hab-sup --channel="${channel}"
+hab pkg install core/hab-launcher --channel="${channel}"
+hab pkg install core/node --binlink
+
+echo "--- :hammer_and_pick: Generating CLI docs"
+
+# Note: we're using this directory name to capture the generated
+# artifacts by Buildkite, too, just for visibility and ease of
+# troubleshooting.
+#
+# If we ever start doing more than generating just 2 documentation
+# files this way, we may want to rethink this.
+docs_dir="generated-documentation"
+mkdir "${docs_dir}"
+
+# TODO: can't currently use `hab pkg exec core/node node ...` for
+# this because that blows away $PATH for the command, making it
+# impossible to find `hab` :(
+node .expeditor/scripts/release_habitat/generate-cli-docs.js > "${docs_dir}/habitat_cli.md"
+
+echo "--- :hammer_and_pick: Generating template reference docs"
+tempdir="$(mktemp --directory --tmpdir="$(pwd)" -t "docs-XXXX")"
+
+cp components/sup/doc/* "${tempdir}"
+
+npm install json-schema-ref-parser@6.1.0
+node .expeditor/scripts/release_habitat/generate-template-reference.js \
+     "${tempdir}"/render_context_schema.json > "${docs_dir}/service_templates.md"
+
+echo "--- :package: Packaging generated documentation into a tarball"
+docs_tarball="${docs_dir}-${version}.tar.gz"
+tar --create \
+    --gzip \
+    --verbose \
+    --file "${docs_tarball}" \
+    "${docs_dir}"
+
+echo "--- Pushing generated documentation to S3"
+import_gpg_keys
+store_in_s3 "${version}" "${docs_tarball}"
