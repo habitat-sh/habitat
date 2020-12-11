c3a1abdaaa4520c36e4892dba6416f5dad3c83ef
Christopher Maier <cmaier@chef.io>
2020-12-11 09:17:00 -0500

    [PR FEEDBACK] Address rustfmt errors
    Needed to sort module declarations alphabetically
    
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- components/sup/src/lib.rs
+++ components/sup/src/lib.rs
@@ -61,8 +61,8 @@ pub mod ctl_gateway;
 pub mod error;
 pub mod event;
 pub mod http_gateway;
-pub mod logger; // must be pub if used in the `hab-sup` binary
 pub mod lock_file;
+pub mod logger; // must be pub if used in the `hab-sup` binary
 pub mod manager;
 mod sys;
 #[cfg(test)]
