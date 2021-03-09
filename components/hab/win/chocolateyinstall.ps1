1f0c7f1dafe38c56984dc51be486ab488e2caa17
Matt Wrock <matt@mattwrock.com>
2021-03-09 14:21:44 -0800

    set tls version for choco download
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- components/hab/win/chocolateyinstall.ps1
+++ components/hab/win/chocolateyinstall.ps1
@@ -3,4 +3,5 @@ $url = "https://packages.chef.io/files/habitat/$version/hab-x86_64-windows.zip"
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $checksum = '$checksum$'
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Install-ChocolateyZipPackage "habitat" $url $unzipLocation -checksum $checksum -checksumType sha256
