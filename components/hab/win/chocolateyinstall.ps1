4ff3e183e3a3dfeaafa7a306b237f2b21c114945
Matt Wrock <matt@mattwrock.com>
2021-03-10 13:52:11 -0800

    revert changes to chocolateyinstall
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- components/hab/win/chocolateyinstall.ps1
+++ components/hab/win/chocolateyinstall.ps1
@@ -2,10 +2,5 @@ $version = '$version$'
 $url = "https://packages.chef.io/files/habitat/$version/hab-x86_64-windows.zip"
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $checksum = '$checksum$'
-$has_tls12 = [Enum]::GetNames([Net.SecurityProtocolType]) -contains 'Tls12'
-Write-Host "has tls? -- $has_tls12"
-if(!$has_tls12) {
-    throw "I have not the required tls"
-}
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 Install-ChocolateyZipPackage "habitat" $url $unzipLocation -checksum $checksum -checksumType sha256
