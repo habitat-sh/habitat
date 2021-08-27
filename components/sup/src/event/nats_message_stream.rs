a1fc2377a0d1cb56d624a56c1ffc7ebc7b795a8f
pozsgaic <chris.pozsgai@progress.com>
2021-08-27 08:24:19 -0400

    DRAFT review for discussion only
    Signed-off-by: pozsgaic <chris.pozsgai@progress.com>

--- components/sup/src/event/nats_message_stream.rs
+++ components/sup/src/event/nats_message_stream.rs
@@ -1,14 +1,61 @@
-use crate::event::{Error,
+use crate::event::{error,
+                   Error,
                    EventStreamConfig,
                    Result};
 use futures::{channel::{mpsc as futures_mpsc,
                         mpsc::UnboundedSender},
               stream::StreamExt};
-use rants::{error::Error as RantsError,
-            native_tls::TlsConnector,
-            Client,
-            Subject};
+use rants::Subject;
+use nats;
+use nats::asynk::{Options, Connection};
 use tokio::time;
+use std::io::Result as IOResult;
+use habitat_core;
+
+pub struct NatsClient<'a> {
+    options: &'a Options,
+    connect_url : String,
+    connection : Option<Connection>,
+}
+
+impl <'a> NatsClient<'a> {
+    pub fn new(mut opts: &'a Options, url: &str) -> NatsClient<'a> {
+        NatsClient {options: opts, connect_url: url.to_string(), connection: None}
+    }
+
+    pub async fn connect(&mut self) -> Result<Connection> {
+        let connection = self.options.connect(&self.connect_url).await;
+        match &connection {
+            Ok(connection) => {
+                println!("nats_connect global function connection OK");
+                connection.publish("habitat.event.healthcheck", "nats.rs client is UP").await;
+                Ok(Connection::clone(&connection))
+            },
+            _ => {
+                println!("nats_connect global function failed to connect and set connection");
+                Err(error::Error::ConnectNatsServer)
+            }
+        }
+    }
+
+    pub fn clone(&self) -> NatsClient {
+        if let Some(connection) = &self.connection {
+                NatsClient{options: &self.options, connect_url: self.connect_url.to_string(), connection: Some(Connection::clone(&connection))}
+        }
+        else {
+                NatsClient{options: &self.options, connect_url: self.connect_url.to_string(), connection: None}
+        }
+    }
+
+    pub async fn publish(&mut self, subject: &str, payload: &[u8]) {
+        if let Some(connection) =  &self.connection {
+            connection.publish(subject, payload);
+        }
+        else {
+            println!("Publish failed");
+        }
+    }
+}
 
 /// The subject and payload of a NATS message.
 #[derive(Debug)]
@@ -16,6 +63,8 @@ pub struct NatsMessage {
     subject: &'static Subject,
     payload: Vec<u8>,
 }
+
+//g_connection: Option<Connection> = None;
 
 impl NatsMessage {
     pub fn new(subject: &'static Subject, payload: Vec<u8>) -> Self {
@@ -34,74 +83,92 @@ pub struct NatsMessageStream(pub(super) UnboundedSender<NatsMessage>);
 
 impl NatsMessageStream {
     pub async fn new(supervisor_id: &str, config: EventStreamConfig) -> Result<NatsMessageStream> {
-        let EventStreamConfig { url,
-                                token,
-                                connect_method,
-                                server_certificate,
-                                .. } = config;
 
-        let mut client = Client::new(vec![url]);
+        println!("NatsMessageStream::new at {}", config.url);
 
-        // Configure the client connect message
-        client.connect_mut()
-              .await
-              .name(format!("hab_client_{}", supervisor_id))
-              .verbose(true)
-              .token(token.to_string());
+        let token_str = format!("{}", config.token);
+        let name = format!("hab_client_{}", supervisor_id);
+        let cert_path = format!("{}", config.server_certificate.unwrap());
+        let config_url = format!("{}", config.url);
 
-        // Configure the tls connector
-        let mut tls_connector = TlsConnector::builder();
-        for certificate in habitat_core::tls::native_tls_wrapper::certificates(None)? {
-            tls_connector.add_root_certificate(certificate);
-        }
-        if let Some(certificate) = server_certificate {
-            tls_connector.add_root_certificate(certificate.into());
-        }
-        let tls_connector = tls_connector.build()?;
-        client.set_tls_config(tls_connector).await;
+        let nats_options: Options = Options::with_token(&token_str) 
+            .add_root_certificate(&cert_path);
+
+        let mut nats_client: NatsClient = NatsClient::new(&nats_options, &config_url);
+
+        let (tx, rx) = futures_mpsc::unbounded::<NatsMessage>();
 
         // Connect to the server. If a timeout was set, we want to ensure we establish a connection
         // before exiting the function. If we do not connect within the timeout we return an error.
         // If we do not have a timeout, we dont care if we can immediately connect. Instead we spawn
         // a future that will resolve when a connection is possible. Once we establish a
         // connection, the client will handle reconnecting if necessary.
-        if let Some(timeout) = connect_method.into() {
-            time::timeout(timeout, client.connect()).await
-                                                    .map_err(|_| Error::ConnectNatsServer)?;
-        } else {
-            let client = Client::clone(&client);
-            tokio::spawn(async move { client.connect().await });
+
+        if let Some(timeout) = config.connect_method.into() {
+            println!("About to connect with timeout");
+            let res = time::timeout(timeout, nats_client.connect())
+                .await
+                .map_err(|_| Error::ConnectNatsServer)?;
+            println!("result from timeout = {:?}", res);
+        }
+        else {
+            println!("About to connect - no timeout specified");
+            let nats_opts: Options = Options::with_token(&token_str) 
+                .add_root_certificate(&cert_path);
+            tokio::spawn(async move { nats_client.connect().await });
         }
 
+/*
+ *      This works on initial connection but gets moved so cannot be used later.
+        let connection : IOResult<Connection> = nats_options.connect(&config_url).await;
+        match &connection {
+            Ok(connection) => {
+                connection.publish("habitat.event.healthcheck", "nats.rs client is UP").await;
+                println!("Connection = {:?}", connection);
+            },
+            Err(connection) => {
+                println!("Nats client failed to connect");
+            }
+        }       
+*/
         let (tx, mut rx) = futures_mpsc::unbounded::<NatsMessage>();
 
         // Spawn a task to handle publishing received messages
         tokio::spawn(async move {
             while let Some(packet) = rx.next().await {
-                if let Err(e) = client.publish(packet.subject, packet.payload()).await {
-                    // We do not retry any messages. If we are not connected when the message is
-                    // processed or there is an error in publishing the message, the message will
-                    // never be sent.
-                    if let RantsError::NotConnected = e {
-                        trace!("Failed to publish message to subject '{}' because the client is \
-                                not connected",
-                               packet.subject);
-                    } else {
-                        error!("Failed to publish message to subject '{}', err: {}",
-                               packet.subject, e);
-                    }
-                }
+                let subj = format!("{}", packet.subject);
+                println!("Have packet to send for subject {}", subj);
+
+                let res = nats_client.publish(&subj, packet.payload()).await;
+                println!("Result from publish: {:?}", res);
             }
         });
 
         Ok(NatsMessageStream(tx))
     }
 
+
     /// Queues a NATS message to be published
     pub fn send(&self, event_packet: NatsMessage) {
-        trace!("Queueing message: {:?}", event_packet);
+        println!("Queueing message: {:?}", event_packet.subject);
         if let Err(e) = self.0.unbounded_send(event_packet) {
             error!("Failed to queue message, err: {}", e);
+        }
+    }
+}
+
+//  Free function to connect to nats
+pub async fn nats_connect(options: Options, config_url: &str) -> Result<Connection> {
+    let conn : IOResult<Connection> = options.connect(&config_url).await;
+    match conn {
+        Ok(conn) => {
+            println!("nats_connect global function connection OK");
+            conn.publish("habitat.event.healthcheck", "nats.rs client is UP").await;
+            Ok(conn)
+        },
+        _ => {
+            println!("nats_connect global function failed to connect and set connection");
+            Err(error::Error::ConnectNatsServer)
         }
     }
 }
