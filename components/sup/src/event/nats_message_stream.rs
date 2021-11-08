b62d3bb0cd224ff406b72fd092d84d799022c27f
Chris Pozsgai <87671689+pozsgaic@users.noreply.github.com>
2021-11-08 09:54:51 -0500

    Revert "Revert "replace rants dependency with nats.io crate""

--- components/sup/src/event/nats_message_stream.rs
+++ components/sup/src/event/nats_message_stream.rs
@@ -4,25 +4,94 @@ use crate::event::{Error,
 use futures::{channel::{mpsc as futures_mpsc,
                         mpsc::UnboundedSender},
               stream::StreamExt};
-use rants::{error::Error as RantsError,
-            native_tls::TlsConnector,
-            Client,
-            Subject};
-use tokio::time;
+use nats::{self,
+           asynk::{Connection,
+                   Options}};
+use std::{path::PathBuf,
+          sync::Arc,
+          thread,
+          time::Duration};
+use tokio::{sync::Mutex,
+            time};
 
 /// The subject and payload of a NATS message.
 #[derive(Debug)]
 pub struct NatsMessage {
-    subject: &'static Subject,
+    subject: &'static str,
     payload: Vec<u8>,
 }
 
 impl NatsMessage {
-    pub fn new(subject: &'static Subject, payload: Vec<u8>) -> Self {
-        NatsMessage { subject, payload }
-    }
+    pub fn new(subject: &'static str, payload: Vec<u8>) -> Self { NatsMessage { subject, payload } }
 
     pub fn payload(&self) -> &[u8] { self.payload.as_slice() }
+}
+
+/// NatsClient is main accessor to connect to NATS Server and
+/// to publish messages to NATS.
+#[derive(Clone)]
+struct NatsClient(Arc<Mutex<Option<Connection>>>);
+
+impl NatsClient {
+    fn new() -> NatsClient { NatsClient(Arc::new(Mutex::new(None))) }
+
+    // Connect to the server. If a timeout was set, we want to ensure we establish a connection
+    // before exiting the function. If we do not connect within the timeout we return an error.
+    // If we do not have a timeout, we don't care if we can immediately connect. Instead we spawn
+    // a future that will resolve when a connection is possible. Once we establish a
+    // connection, this client will handle reconnecting if necessary.
+    async fn connect(self, supervisor_id: String, config: EventStreamConfig) -> Result<()> {
+        match config.connect_method.into() {
+            Some(timeout) => {
+                time::timeout(timeout, self.connect_impl(supervisor_id, &config)).await??;
+            }
+            None => {
+                tokio::spawn(async move { self.connect_impl(supervisor_id, &config).await });
+            }
+        }
+        Ok(())
+    }
+
+    async fn connect_impl(self, supervisor_id: String, config: &EventStreamConfig) -> Result<()> {
+        while self.0.lock().await.is_none() {
+            match Self::options_from_config(&supervisor_id, config)?.connect(&config.url
+                                                                                    .to_string())
+                                                                    .await
+            {
+                Ok(conn) => *self.0.lock().await = Some(conn),
+                Err(e) => {
+                    trace!("Failed to connect to NATS server: {}", e);
+                    thread::sleep(Duration::from_millis(1000));
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn options_from_config(supervisor_id: &str, config: &EventStreamConfig) -> Result<Options> {
+        let name = format!("hab_client_{}", supervisor_id);
+        let ca_certs = habitat_core::tls::native_tls_wrapper::installed_cacerts(None)?;
+        let mut options = Options::with_token(&config.token.to_string())
+            .with_name(&name)
+            .add_root_certificate(ca_certs.expect("No core/cacerts installed"))
+            .max_reconnects(None);
+
+        if let Some(ref cert_path) = config.server_certificate {
+            let cert_path: PathBuf = cert_path.clone().into();
+            options = options.add_root_certificate(cert_path);
+        }
+        Ok(options)
+    }
+
+    async fn publish(&self, subject: &str, msg: impl AsRef<[u8]>) -> Result<()> {
+        if let Some(conn) = &*self.0.lock().await {
+            conn.publish(subject, msg)
+                .await
+                .map_err(Error::PublishFailed)
+        } else {
+            Err(Error::NotConnected)
+        }
+    }
 }
 
 /// A lightweight handle for the NATS message stream. All events are converted into a NatsMessage
@@ -34,44 +103,10 @@ pub struct NatsMessageStream(pub(super) UnboundedSender<NatsMessage>);
 
 impl NatsMessageStream {
     pub async fn new(supervisor_id: &str, config: EventStreamConfig) -> Result<NatsMessageStream> {
-        let EventStreamConfig { url,
-                                token,
-                                connect_method,
-                                server_certificate,
-                                .. } = config;
-
-        let mut client = Client::new(vec![url]);
-
-        // Configure the client connect message
-        client.connect_mut()
-              .await
-              .name(format!("hab_client_{}", supervisor_id))
-              .verbose(true)
-              .token(token.to_string());
-
-        // Configure the tls connector
-        let mut tls_connector = TlsConnector::builder();
-        for certificate in habitat_core::tls::native_tls_wrapper::certificates(None)? {
-            tls_connector.add_root_certificate(certificate);
-        }
-        if let Some(certificate) = server_certificate {
-            tls_connector.add_root_certificate(certificate.into());
-        }
-        let tls_connector = tls_connector.build()?;
-        client.set_tls_config(tls_connector).await;
-
-        // Connect to the server. If a timeout was set, we want to ensure we establish a connection
-        // before exiting the function. If we do not connect within the timeout we return an error.
-        // If we do not have a timeout, we dont care if we can immediately connect. Instead we spawn
-        // a future that will resolve when a connection is possible. Once we establish a
-        // connection, the client will handle reconnecting if necessary.
-        if let Some(timeout) = connect_method.into() {
-            time::timeout(timeout, client.connect()).await
-                                                    .map_err(|_| Error::ConnectNatsServer)?;
-        } else {
-            let client = Client::clone(&client);
-            tokio::spawn(async move { client.connect().await });
-        }
+        let client = NatsClient::new();
+        client.clone()
+              .connect(supervisor_id.to_string(), config)
+              .await?;
 
         let (tx, mut rx) = futures_mpsc::unbounded::<NatsMessage>();
 
@@ -82,7 +117,7 @@ impl NatsMessageStream {
                     // We do not retry any messages. If we are not connected when the message is
                     // processed or there is an error in publishing the message, the message will
                     // never be sent.
-                    if let RantsError::NotConnected = e {
+                    if let Error::NotConnected = e {
                         trace!("Failed to publish message to subject '{}' because the client is \
                                 not connected",
                                packet.subject);
@@ -99,7 +134,7 @@ impl NatsMessageStream {
 
     /// Queues a NATS message to be published
     pub fn send(&self, event_packet: NatsMessage) {
-        trace!("Queueing message: {:?}", event_packet);
+        trace!("Queueing message: {:?}", event_packet.subject);
         if let Err(e) = self.0.unbounded_send(event_packet) {
             error!("Failed to queue message, err: {}", e);
         }
