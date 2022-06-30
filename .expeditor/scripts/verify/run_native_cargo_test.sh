4a3558645e1a81202dcfb8c61a975f4e684b7f6c
Johny Jose <johny.jose@progress.com>
2022-06-30 13:13:09 +0000

    fix test script
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- .expeditor/scripts/verify/run_native_cargo_test.sh
+++ .expeditor/scripts/verify/run_native_cargo_test.sh
@@ -2,6 +2,7 @@
 
 set -eou pipefail
 
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ca-certificates gcc libc6-dev wget openssl make pkg-config libzmq3-dev curl
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y ca-certificates gcc libc6-dev wget openssl make pkg-config libzmq3-dev curl
 cargo test
