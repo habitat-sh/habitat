ec4b3cc80b482bdad56619cd3783f587b57950da
Johny Jose <johny.jose@progress.com>
2022-06-30 13:19:18 +0000

    add cmake to build environment
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- .expeditor/scripts/verify/run_native_cargo_test.sh
+++ .expeditor/scripts/verify/run_native_cargo_test.sh
@@ -4,5 +4,5 @@ set -eou pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y ca-certificates gcc libc6-dev wget openssl make pkg-config libzmq3-dev curl
-cargo test
+apt-get install -y ca-certificates gcc libc6-dev wget openssl make pkg-config libzmq3-dev curl cmake
+cargo test --no-fail-fast
