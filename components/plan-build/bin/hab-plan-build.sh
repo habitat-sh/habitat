acd1f3209875fd21ba3d4750af439872a173a049
Johny Jose <johny.jose@progress.com>
2022-06-30 10:50:11 +0000

    removes sudo requirement for native plan build
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- components/plan-build/bin/hab-plan-build.sh
+++ components/plan-build/bin/hab-plan-build.sh
@@ -2422,6 +2422,7 @@ _do_final_callback_wrapper() {
 
 # # Main Flow
 ########################################################################
+build_line "Using HAB_ROOT_PATH=${HAB_ROOT_PATH}"
 
 # Parse depot flag (-u)
 OPTIND=2
