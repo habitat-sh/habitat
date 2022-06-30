cec8c9012d2ca47813922c141d493a51382008ff
Johny Jose <johny.jose@progress.com>
2022-06-30 13:05:28 +0000

    ci: try docker plugin
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- .expeditor/scripts/verify/run_native_cargo_test.sh
+++ .expeditor/scripts/verify/run_native_cargo_test.sh
@@ -3,6 +3,5 @@
 set -eou pipefail
 
 sudo apt-get update
-sudo apt-get install -y ca-certificates gcc libc6-dev wget openssl make pkg-config libzmq3-dev curl
-curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ca-certificates gcc libc6-dev wget openssl make pkg-config libzmq3-dev curl
 cargo test
