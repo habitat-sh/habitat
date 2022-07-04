835a3f39e35d1bfedebf6f571c52febd836f8345
Johny Jose <johny.jose@progress.com>
2022-07-04 16:11:56 +0000

    fix tests that required sudo privileges
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- .expeditor/scripts/verify/run_native_cargo_test.sh
+++ .expeditor/scripts/verify/run_native_cargo_test.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -eou pipefail
+# The test user as which the tests will be run
 export TEST_USER=ubuntu
-
+# Set filesystem root to a temporary folder that does not require sudo privileges
+export FS_ROOT=/tmp/hab-test
+# This is required to enable non-interactive installation of timezone data
 export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y ca-certificates sudo gcc libc6-dev wget openssl make pkg-config libzmq3-dev curl cmake
 
+# Ensure all build and test dependencies are installed
+apt-get update && apt-get install -y ca-certificates sudo gcc libc6-dev wget openssl make pkg-config libzmq3-dev curl cmake
+
+# Create the test user and fix permissions
 useradd -rm -d /home/$TEST_USER -s /bin/bash -g root -G sudo -u 1001 $TEST_USER
 chown -R ubuntu:root /workdir
 
 # Build binaries to be used in integration test
 sudo -u $TEST_USER -H -E --preserve-env=PATH bash -c "cargo build"
+
+# Run all test cases without stopping for failures
 sudo -u $TEST_USER -H -E --preserve-env=PATH bash -c "cargo test --no-fail-fast"
