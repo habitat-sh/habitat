6d5c7c440c5764fde5fe5a151e14ba6e947240d5
Johny Jose <johny.jose@progress.com>
2022-07-04 16:20:08 +0000

    fix lints
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- components/sup/src/main.rs
+++ components/sup/src/main.rs
@@ -455,7 +455,6 @@ mod test {
         use std::{collections::HashMap,
                   fs::File,
                   io::Write,
-                  path::PathBuf,
                   str::FromStr,
                   time::Duration};
         use sup::manager::ServiceRestartConfig;
