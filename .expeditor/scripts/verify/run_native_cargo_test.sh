ead3ecb2188d403abdc8588a2adbd25b3704767a
Johny Jose <johny.jose@progress.com>
2022-07-04 11:26:54 +0000

    fix tests
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- .expeditor/scripts/verify/run_native_cargo_test.sh
+++ .expeditor/scripts/verify/run_native_cargo_test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
 set -eou pipefail
+export TEST_USER=ubuntu
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y ca-certificates gcc libc6-dev wget openssl make pkg-config libzmq3-dev curl cmake
+apt-get install -y ca-certificates sudo gcc libc6-dev wget openssl make pkg-config libzmq3-dev curl cmake
+
+useradd -rm -d /home/$TEST_USER -s /bin/bash -g root -G sudo -u 1001 $TEST_USER
 
 # Build binaries to be used in integration test
-cargo build
-cargo test --no-fail-fast
+sudo -u $TEST_USER -H cargo build
+sudo -u $TEST_USER -H cargo test --no-fail-fast
