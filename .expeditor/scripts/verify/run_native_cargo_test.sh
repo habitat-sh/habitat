9f148f25a7e51471aafb9911f95a1ef5b71e938b
Johny Jose <johny.jose@progress.com>
2022-07-04 13:21:18 +0000

    fix permissions
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- .expeditor/scripts/verify/run_native_cargo_test.sh
+++ .expeditor/scripts/verify/run_native_cargo_test.sh
@@ -7,6 +7,7 @@ apt-get update
 apt-get install -y ca-certificates sudo gcc libc6-dev wget openssl make pkg-config libzmq3-dev curl cmake
 
 useradd -rm -d /home/$TEST_USER -s /bin/bash -g root -G sudo -u 1001 $TEST_USER
+chown -R ubuntu:root /workdir
 
 # Build binaries to be used in integration test
 sudo -u $TEST_USER -H -E --preserve-env=PATH bash -c "cargo build"
