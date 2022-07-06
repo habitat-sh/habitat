e84fc6c23a7714d39c323889a0bf1c651c7bf2bf
Johny Jose <johny.jose@progress.com>
2022-07-06 14:36:40 +0000

    fix build script
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- .expeditor/scripts/release_habitat/build_arm_hab_binary.sh
+++ .expeditor/scripts/release_habitat/build_arm_hab_binary.sh
@@ -13,9 +13,10 @@ apt-get update && apt-get install -y ca-certificates sudo gcc libc6-dev wget ope
 # Build all binaries
 cargo build --release
 
+cd target/release
+
 strip --strip-debug hab-sup
 strip --strip-debug hab-launch
 strip --strip-debug hab
 tar -zcvf hab-aarch64-linux.tar.gz hab hab-sup hab-launch
 store_in_s3 "$(get_version_from_repo)" "hab-aarch64-linux.tar.gz"
-
