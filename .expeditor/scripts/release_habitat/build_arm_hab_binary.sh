e8a025fd6cb3c80c2bbb343a089fa46ec4d31199
Johny Jose <johny.jose@progress.com>
2022-07-06 14:57:24 +0000

    add gpg key import to upload step
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- .expeditor/scripts/release_habitat/build_arm_hab_binary.sh
+++ .expeditor/scripts/release_habitat/build_arm_hab_binary.sh
@@ -15,8 +15,15 @@ cargo build --release
 
 cd target/release
 
+# Strip all debug symbols from our binaries
 strip --strip-debug hab-sup
 strip --strip-debug hab-launch
 strip --strip-debug hab
+
+# Put all the binaries in an archive
 tar -zcvf hab-aarch64-linux.tar.gz hab hab-sup hab-launch
+
+# Import GPG keys for s3 upload
+import_gpg_keys
+# Upload to S3
 store_in_s3 "$(get_version_from_repo)" "hab-aarch64-linux.tar.gz"
