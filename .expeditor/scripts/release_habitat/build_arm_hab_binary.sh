8b0bf8b0b50e8e54f1299dcc57175d49315b5fac
Johny Jose <johny.jose@progress.com>
2022-07-08 12:17:32 +0000

    add upload to build script
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- .expeditor/scripts/release_habitat/build_arm_hab_binary.sh
+++ .expeditor/scripts/release_habitat/build_arm_hab_binary.sh
@@ -21,4 +21,17 @@ strip --strip-debug hab-launch
 strip --strip-debug hab
 
 # Put all the binaries in an archive
-tar -zcvf hab-aarch64-linux.tar.gz hab hab-sup hab-launch
+tar -zcf hab-aarch64-linux.tar.gz hab hab-sup hab-launch
+
+HAB_VERSION=$(get_version_from_repo)
+
+# Copy archive to temp directory to ensure we have read/write permissions
+WORKDIR=$(mktemp -d -t hab-build-XXXXXX)
+cp hab-aarch64-linux.tar.gz "$WORKDIR"
+
+# Import GPG keys for s3 upload
+cd "$WORKDIR"
+import_gpg_keys
+
+# Upload to S3
+store_in_s3 "$HAB_VERSION" "hab-aarch64-linux.tar.gz"
