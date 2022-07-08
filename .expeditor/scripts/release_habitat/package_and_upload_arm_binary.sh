bc25d3898bafcced866a1ce13a7fcf7c0495c073
Johny Jose <johny.jose@progress.com>
2022-07-08 10:52:54 +0000

    attempt to fix upload
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- .expeditor/scripts/release_habitat/package_and_upload_arm_binary.sh
+++ .expeditor/scripts/release_habitat/package_and_upload_arm_binary.sh
@@ -13,11 +13,15 @@ docker run \
     rust:buster \
     /bin/sh -e -c .expeditor/scripts/release_habitat/build_arm_hab_binary.sh
 
+
+
+# Copy archive to temp directory to ensure we have read/write permissions
+WORKDIR=$(mktemp -d -t hab-build-XXXXXX)
+cp target/release/hab-aarch64-linux.tar.gz "$WORKDIR"
+
 # Import GPG keys for s3 upload
-pushd /tmp 
+cd "$WORKDIR"
 import_gpg_keys 
-popd
 
 # Upload to S3
-pushd target/release 
 store_in_s3 "$(get_version_from_repo)" "hab-aarch64-linux.tar.gz"
