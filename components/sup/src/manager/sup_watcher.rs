15023820cf532b00ee4bfd1940cb6d02e319e13c
Jason Heath <jason.heath@progress.com>
2023-02-13 08:20:54 -0500

    Bump notify from 4.0.17 to 5.0.0
    Complete rewrite of FileWatcher which eliminated the use of the notify crate.
    Removed **/sup/**/manager/debug.rs which was only used in old FileWatcher impl.
    Updated usage of notify crate in the places it is still used.
    
    Signed-off-by: Jason Heath <jason.heath@progress.com>

--- components/sup/src/manager/sup_watcher.rs
+++ components/sup/src/manager/sup_watcher.rs
@@ -1,62 +1,58 @@
 //! Watcher interface implementation for Habitat Supervisor.
-
 use habitat_core::package::target::{PackageTarget,
                                     AARCH64_DARWIN};
 use log::debug;
 use notify::{poll::PollWatcher,
-             DebouncedEvent,
+             Config,
+             EventHandler,
              RecommendedWatcher,
              RecursiveMode,
              Result,
-             Watcher};
+             Watcher,
+             WatcherKind};
 use std::{env,
           path::Path,
-          str::FromStr,
-          sync::mpsc::Sender,
-          time::Duration};
+          str::FromStr};
 
+#[derive(Debug)]
 pub enum SupWatcher {
     Native(RecommendedWatcher),
     Fallback(PollWatcher),
 }
 
 impl Watcher for SupWatcher {
-    fn new_raw(tx: Sender<notify::RawEvent>) -> Result<Self> {
-        let target = PackageTarget::from_str(&env::var("HAB_STUDIO_HOST_ARCH").
-                                             unwrap_or_default()).
-                                             unwrap_or_else(|_| PackageTarget::active_target());
-        if target == AARCH64_DARWIN {
-            Ok(SupWatcher::Fallback(PollWatcher::new_raw(tx).unwrap()))
-        } else {
-            Ok(SupWatcher::Native(RecommendedWatcher::new_raw(tx).unwrap()))
-        }
-    }
-
-    fn new(tx: Sender<DebouncedEvent>, delay: Duration) -> Result<Self> {
+    fn new<F: EventHandler>(event_handler: F, config: Config) -> Result<Self> {
         let target = PackageTarget::from_str(&env::var("HAB_STUDIO_HOST_ARCH").
                                              unwrap_or_default()).
                                              unwrap_or_else(|_| PackageTarget::active_target());
         if target == AARCH64_DARWIN {
             debug!("Using pollwatcher");
-            Ok(SupWatcher::Fallback(PollWatcher::new(tx, delay).unwrap()))
+            Ok(SupWatcher::Fallback(PollWatcher::new(event_handler, config).unwrap()))
         } else {
             debug!("Using native watcher");
-            Ok(SupWatcher::Native(RecommendedWatcher::new(tx, delay).unwrap()))
+            Ok(SupWatcher::Native(RecommendedWatcher::new(event_handler, config).unwrap()))
         }
     }
 
-    fn watch<P: AsRef<Path>>(&mut self, path: P, recursive_mode: RecursiveMode) -> Result<()> {
+    fn watch(&mut self, path: &Path, recursive_mode: RecursiveMode) -> Result<()> {
         match self {
             SupWatcher::Native(watcher) => watcher.watch(path, recursive_mode),
             SupWatcher::Fallback(watcher) => watcher.watch(path, recursive_mode),
         }
     }
 
-    fn unwatch<P: AsRef<Path>>(&mut self, path: P) -> Result<()> {
+    fn unwatch(&mut self, path: &Path) -> Result<()> {
         match self {
             SupWatcher::Native(watcher) => watcher.unwatch(path),
             SupWatcher::Fallback(watcher) => watcher.unwatch(path),
         }
+    }
+
+    fn kind() -> WatcherKind
+        where Self: Sized
+    {
+        unimplemented!("Required by the Watcher trait but we're not a watcher backend.");
+        // https://github.com/notify-rs/notify/pull/441#discussion_r961970946
     }
 }
 
@@ -73,11 +69,12 @@ mod test {
     fn sup_watcher_constructor_test_polling() {
         let (sender, _) = channel();
         let delay = Duration::from_millis(1000);
+        let config = Config::default().with_poll_interval(delay);
 
         let lock = lock_env_var();
         lock.set("aarch64-darwin");
 
-        let _sup_watcher = SupWatcher::new(sender, delay);
+        let _sup_watcher = SupWatcher::new(sender, config);
         let watcher_type = match _sup_watcher {
             Ok(SupWatcher::Native(_sup_watcher)) => "Native",
             Ok(SupWatcher::Fallback(_sup_watcher)) => "Fallback",
@@ -93,11 +90,12 @@ mod test {
     fn sup_watcher_constructor_test_notify() {
         let (sender, _) = channel();
         let delay = Duration::from_millis(1000);
+        let config = Config::default().with_poll_interval(delay);
 
         let lock = lock_env_var();
         lock.unset();
 
-        let _sup_watcher = SupWatcher::new(sender, delay);
+        let _sup_watcher = SupWatcher::new(sender, config);
         let watcher_type = match _sup_watcher {
             Ok(SupWatcher::Native(_sup_watcher)) => "Native",
             Ok(SupWatcher::Fallback(_sup_watcher)) => "Fallback",
