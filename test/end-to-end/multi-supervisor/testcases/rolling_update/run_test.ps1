50b664d9efaa3c04230eb33ddc7d1f0030a24962
Matt Wrock <matt@mattwrock.com>
2023-02-14 11:54:06 -0800

    force basic rolling update/rollback e2e to terminate process
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- test/end-to-end/multi-supervisor/testcases/rolling_update/run_test.ps1
+++ test/end-to-end/multi-supervisor/testcases/rolling_update/run_test.ps1
@@ -1,19 +1,24 @@
 # This is a simple "happy path" test of a rolling update.
-# We will load services on two nodes to achieve quorum and
-# then promote an update and expect the new release to show
-# up after waiting 15 seconds. Note: we set HAB_UPDATE_STRATEGY_FREQUENCY_MS
-# to 3000 in the docker-compose.override.yml.
+# We will load services on five nodes then promote an update and expect the new release to show
+# up after waiting 15 seconds. Then we demote the package and validate that the nodes
+# rolled back. The package will "hang" upon receiving its SIGTERM which will trigger the supervisor
+# to forcefully terminate the service. This tests an edge case where the package incarnation was being
+# reset to 0 and causing nodes to get stuck and not update or roll back.
+# Note: we set HAB_UPDATE_STRATEGY_FREQUENCY_MS to 3000 in the docker-compose.override.yml.
 
 $testChannel = "rolling-$([DateTime]::Now.Ticks)"
 
 Describe "Rolling Update and Rollback" {
-    $initialRelease="habitat-testing/nginx/1.17.4/20191115184838"
-    $updatedRelease="habitat-testing/nginx/1.17.4/20191115185517"
+    $initialRelease="habitat-testing/force-kill/0.1.0/20230214152940"
+    $updatedRelease="habitat-testing/force-kill/0.1.0/20230214154036"
     hab pkg promote $initialRelease $testChannel
-    Load-SupervisorService "habitat-testing/nginx" -Remote "alpha.habitat.dev" -Topology leader -Strategy rolling -UpdateCondition "track-channel" -Channel $testChannel
-    Load-SupervisorService "habitat-testing/nginx" -Remote "beta.habitat.dev" -Topology leader -Strategy rolling -UpdateCondition "track-channel" -Channel $testChannel
+    Load-SupervisorService "habitat-testing/force-kill" -Remote "alpha.habitat.dev" -Topology leader -Strategy rolling -UpdateCondition "track-channel" -Channel $testChannel
+    Load-SupervisorService "habitat-testing/force-kill" -Remote "beta.habitat.dev" -Topology leader -Strategy rolling -UpdateCondition "track-channel" -Channel $testChannel
+    Load-SupervisorService "habitat-testing/force-kill" -Remote "gamma1.habitat.dev" -Topology leader -Strategy rolling -UpdateCondition "track-channel" -Channel $testChannel
+    Load-SupervisorService "habitat-testing/force-kill" -Remote "gamma2.habitat.dev" -Topology leader -Strategy rolling -UpdateCondition "track-channel" -Channel $testChannel
+    Load-SupervisorService "habitat-testing/force-kill" -Remote "gamma3.habitat.dev" -Topology leader -Strategy rolling -UpdateCondition "track-channel" -Channel $testChannel
 
-    @("alpha", "beta") | ForEach-Object {
+    @("alpha", "beta", "gamma1", "gamma2", "gamma3") | ForEach-Object {
         It "loads initial release on $_" {
             Wait-Release -Ident $initialRelease -Remote $_
         }
@@ -22,7 +27,7 @@ Describe "Rolling Update and Rollback" {
     Context "promote update" {
         hab pkg promote $updatedRelease $testChannel
 
-        @("alpha", "beta") | ForEach-Object {
+        @("alpha", "beta", "gamma1", "gamma2", "gamma3") | ForEach-Object {
             It "updates release on $_" {
                 Wait-Release -Ident $updatedRelease -Remote $_
             }
@@ -32,7 +37,7 @@ Describe "Rolling Update and Rollback" {
     Context "demote update" {
         hab pkg demote $updatedRelease $testChannel
 
-        @("alpha", "beta") | ForEach-Object {
+        @("alpha", "beta", "gamma1", "gamma2", "gamma3") | ForEach-Object {
             It "rollback release on $_" {
                 Wait-Release -Ident $initialRelease -Remote $_
             }
