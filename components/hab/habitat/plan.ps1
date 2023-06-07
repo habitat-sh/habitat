a127239da75afebbe1ac2fffaf9617bceabd734e
Matt Wrock <matt@mattwrock.com>
2023-06-07 11:58:42 -0700

    fix quoting
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- components/hab/habitat/plan.ps1
+++ components/hab/habitat/plan.ps1
@@ -8,7 +8,7 @@ $pkg_deps=@(
 $pkg_bin_dirs = @("bin")
 $pkg_build_deps = @(
     "core/visual-cpp-build-tools-2015",
-    "core/rust/$((ConvertFrom-StringData (Get-Content "$PLAN_CONTEXT/../../../rust-toolchain")[1]).channel.Replace("`"", ""))",
+    "core/rust/$((ConvertFrom-StringData (Get-Content "$PLAN_CONTEXT/../../../rust-toolchain")[1]).channel.Replace('"', ''))",
     "core/cacerts",
     "core/protobuf"
 )
