61a58b682bf4f69b533dfd9bb697de92037b3ae3
Johny Jose <johny.jose@progress.com>
2023-07-10 18:36:48 +0530

    add 'build' command to macos studio
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- components/studio/libexec/hab-studio-type-darwin-bootstrap.sh
+++ components/studio/libexec/hab-studio-type-darwin-bootstrap.sh
@@ -8,11 +8,21 @@
 studio_type="bootstrap"
 studio_env_command="/usr/bin/env"
 studio_enter_environment="STUDIO_ENTER=true"
-studio_enter_command="$libexec_path/hab pkg exec core/build-tools-hab-backline bash --login +h"
+studio_enter_command="$libexec_path/hab pkg exec core/build-tools-hab-backline bash --rcfile $HAB_STUDIO_ROOT/.bashrc"
 studio_build_environment=
 studio_build_command="$libexec_path/hab pkg exec core/build-tools-hab-plan-build hab-plan-build --"
 studio_run_environment=
-studio_run_command="$libexec_path/hab pkg exec core/build-tools-hab-backline bash --login"
+studio_run_command="$libexec_path/hab pkg exec core/build-tools-hab-backline bash --rcfile $HAB_STUDIO_ROOT/.bashrc"
 
 run_user="hab"
 run_group="$run_user"
+
+finish_setup() {
+    $cat_cmd <<EOF > "$HAB_STUDIO_ROOT"/.bashrc
+function build() {
+    $libexec_path/hab pkg exec core/build-tools-hab-plan-build hab-plan-build -- "$@"
+}
+export -f build
+EOF
+    return 0
+}
