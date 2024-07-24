f2a8ff73775e3483ccc6d5d859ec8e40e3c11222
Sougata Bhattacharjee <sougatab@progress.com>
2024-07-24 01:24:43 +0000

    Made test fail fixes
    Signed-off-by: Sougata Bhattacharjee <sougatab@progress.com>

--- components/hab/install.ps1
+++ components/hab/install.ps1
@@ -16,7 +16,7 @@ Specifies a version (ex: 0.75.0, 0.75.0/20190219232208)
 
 param (
     [Alias("c")]
-    [string]$Channel="stable",
+    [string]$Channel="LTS-2024",
     [Alias("v")]
     [string]$Version
 )
