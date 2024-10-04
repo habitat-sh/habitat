1ebbaf33e6a907f5a3703eb854578a010ec597be
Abhijit Gadgil <agadgil@progress.com>
2024-10-04 18:24:05 +0530

    Updates to Election Algorithm
    If we receive an election with a `new_term`, we start a new election
    *only if* we were the leader of previous term. And if we were not
    *leader* of the previous term, we join the election rumor only when we
    receive one from the leader.
    
    When the leader dies (2 or more) members will re-start election with
    *new term* and hence the above *new term* logic won't come into play and
    the normal *leader Election* algorithm will come into play and will
    work.
    
    Signed-off-by: Abhijit Gadgil <agadgil@progress.com>

--- components/butterfly/src/server.rs
+++ components/butterfly/src/server.rs
@@ -1133,10 +1133,23 @@ impl Server {
                                    .map(|stored_term| election.term > stored_term)
                                    .unwrap_or(false);
                 if new_term {
-                    debug!("removing old rumor and starting new election");
-                    self.election_store
-                        .remove_rsw(election.key(), election.id());
-                    self.start_election_rsw_mlr_rhw_msr(&election.service_group, election.term);
+                    if Some(self.member_id()) == self.election_store.lock_rsr().get_member_id(election.key()) {
+                        debug!("I am the leader of previous term!");
+                        debug!("removing old rumor and starting new election");
+                        self.election_store.remove_rsw(election.key(), election.id());
+                        self.start_election_rsw_mlr_rhw_msr(&election.service_group, election.term);
+                    } else {
+                        if Some(election.member_id.as_str()) == self.election_store.lock_rsr().get_member_id(election.key()) {
+                            debug!("Received New Term election from the leader. Starting my own to merge. Term: {}", election.term);
+                            self.election_store.remove_rsw(election.key(), election.id());
+                            trace!("Removed old election.");
+                            self.start_election_rsw_mlr_rhw_msr(&election.service_group, election.term);
+                            trace!("Started new election.");
+                        } else {
+                            warn!("Received a New Term Election, but not from the current leader, ignoring!");
+                            return;
+                        }
+                    }
                 }
                 // If we are the member that this election is voting for, then check to see if the
                 // election is over! If it is, mark this election as final before you process it.
