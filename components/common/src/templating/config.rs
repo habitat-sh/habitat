d428d9d2caf6d673aa1c60b2b7f806e3be28673d
Phani Sajja <psajja@progress.com>
2024-10-17 23:18:15 +0530

    aarch64-linux has standard pkgs
    Signed-off-by: Phani Sajja <psajja@progress.com>

--- components/common/src/templating/config.rs
+++ components/common/src/templating/config.rs
@@ -1151,7 +1151,8 @@ mod test {
                             "config message is {{cfg.message}}");
 
         // Platforms without standard package support require all packages to be native packages
-        #[cfg(not(any(all(target_os = "linux", any(target_arch = "x86_64")),
+        #[cfg(not(any(all(target_os = "linux",
+                          any(target_arch = "x86_64", target_arch = "aarch64")),
                       all(target_os = "windows", target_arch = "x86_64"))))]
         {
             create_with_content(pkg_dir.join(MetaFile::PackageType.to_string()), "native");
