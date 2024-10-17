89cd753a5fe4c4ecb614cbb9c983621a38e2d0af
Phani Sajja <psajja@progress.com>
2024-10-17 23:39:37 +0530

    fix unused import
    Signed-off-by: Phani Sajja <psajja@progress.com>

--- components/sup/src/manager/service/hooks.rs
+++ components/sup/src/manager/service/hooks.rs
@@ -732,8 +732,9 @@ mod tests {
                          types::{GossipListenAddr,
                                  HttpListenAddr,
                                  ListenCtlAddr}};
-    #[cfg(not(any(all(target_os = "linux", any(target_arch = "x86_64")),
-                      all(target_os = "windows", target_arch = "x86_64"))))]
+    #[cfg(not(any(all(target_os = "linux",
+                          any(target_arch = "x86_64", target_arch = "aarch64")),
+                      all(target_os = "windows", target_arch = "x86_64"),)))]
     use habitat_core::package::metadata::MetaFile;
     use habitat_core::{crypto::keys::KeyCache,
                        fs::CACHE_KEY_PATH,
