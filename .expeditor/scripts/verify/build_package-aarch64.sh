a53ce9c8a27ad76cd0f62239ddf3737a360d26f9
Phani Sajja <psajja@progress.com>
2024-10-25 18:15:11 +0530

    fix shellcheck
    Signed-off-by: Phani Sajja <psajja@progress.com>

--- .expeditor/scripts/verify/build_package-aarch64.sh
+++ .expeditor/scripts/verify/build_package-aarch64.sh
@@ -5,7 +5,7 @@ set -eou pipefail
 package_path=${1?package_path argument required}
 
 # Install hab from a temporarily uploaded aarch64 package
-curl https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.sh | sudo bash -s -- -t $BUILD_PKG_TARGET -c $HAB_FALLBACK_CHANNEL -v 1.6.1178
+curl https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.sh | sudo bash -s -- -t "$BUILD_PKG_TARGET" -c "$HAB_FALLBACK_CHANNEL" -v 1.6.1178
 
 # Since we are only verifying we don't have build failures, make everything
 # temp!
@@ -22,5 +22,5 @@ hab origin key generate
 echo "--- :hab: Running hab pkg build for $package_path"
 # Install the temporarily built hab-studio.
 # Once hab is released in the LTS channel, this step may no longer be required.
-hab pkg install core/hab-studio -c $HAB_FALLBACK_CHANNEL
+hab pkg install core/hab-studio -c "$HAB_FALLBACK_CHANNEL"
 hab pkg build "$package_path"
