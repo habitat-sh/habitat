893231484dc3c92ec4029d2da292c4d5aaec0809
Matt Wrock <matt@mattwrock.com>
2024-10-28 09:15:07 -0700

    perms on tmp
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- .expeditor/scripts/end_to_end/run_e2e_test.sh
+++ .expeditor/scripts/end_to_end/run_e2e_test.sh
@@ -8,10 +8,11 @@ test_name=${2:-}
 ls -la /tmp
 
 source .expeditor/scripts/end_to_end/setup_environment.sh "$channel"
+
+ls -la /tmp
+
 if [ -n "$test_name" ]; then
     sudo -E hab pkg exec core/powershell pwsh .expeditor/scripts/end_to_end/run_e2e_test_core.ps1 "$test_name"
 else
     bash
 fi
-
-ls -la /tmp
