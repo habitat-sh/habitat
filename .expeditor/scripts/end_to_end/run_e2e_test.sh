948a11986cb9fe01069d94283807e868ea0fe2ad
Matt Wrock <matt@mattwrock.com>
2024-10-28 08:47:44 -0700

    perms on tmp
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- .expeditor/scripts/end_to_end/run_e2e_test.sh
+++ .expeditor/scripts/end_to_end/run_e2e_test.sh
@@ -5,9 +5,13 @@ set -euo pipefail
 channel=${1:?You must specify a channel value}
 test_name=${2:-}
 
+ls -la /tmp
+
 source .expeditor/scripts/end_to_end/setup_environment.sh "$channel"
 if [ -n "$test_name" ]; then
     sudo -E hab pkg exec core/powershell pwsh .expeditor/scripts/end_to_end/run_e2e_test_core.ps1 "$test_name"
 else
     bash
 fi
+
+ls -la /tmp
