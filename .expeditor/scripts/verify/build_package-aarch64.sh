1385d03b3c074439b01bd6e93314d4968faa5815
Phani Sajja <psajja@progress.com>
2024-10-29 18:30:34 +0530

    use core origin in verity
    Signed-off-by: Phani Sajja <psajja@progress.com>

--- .expeditor/scripts/verify/build_package-aarch64.sh
+++ .expeditor/scripts/verify/build_package-aarch64.sh
@@ -10,15 +10,15 @@ curl https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/in
 # Since we are only verifying we don't have build failures, make everything
 # temp!
 export HAB_ORIGIN
-HAB_ORIGIN=throwaway
+HAB_ORIGIN=core
 # let's make a selfcontained tempdir for this job
 export JOB_TEMP_ROOT
 JOB_TEMP_ROOT=$(mktemp -d /tmp/job-root-XXXXXX)
 export HAB_CACHE_KEY_PATH
 HAB_CACHE_KEY_PATH="$JOB_TEMP_ROOT/keys"
 
-echo "--- :key: Generating fake origin key"
-hab origin key generate
+echo "--- :key: Generating temporary origin key"
+hab origin key generate "$HAB_ORIGIN"
 echo "--- :hab: Running hab pkg build for $package_path"
 # Install the temporarily built hab-studio.
 # Once hab is released in the LTS channel, this step may no longer be required.
