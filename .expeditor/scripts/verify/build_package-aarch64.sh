29ca9564396f6c7ec1a4935b7dfe2aa4dea749c5
Phani Sajja <psajja@progress.com>
2024-11-21 18:59:54 +0530

    fixes for release pipeline
    Signed-off-by: Phani Sajja <psajja@progress.com>

--- .expeditor/scripts/verify/build_package-aarch64.sh
+++ .expeditor/scripts/verify/build_package-aarch64.sh
@@ -5,7 +5,7 @@ set -eou pipefail
 package_path=${1?package_path argument required}
 
 # Install hab from a temporarily uploaded aarch64 package
-curl https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.sh | sudo bash -s -- -t "$BUILD_PKG_TARGET" -c "$HAB_FALLBACK_CHANNEL" -v 1.6.1178
+curl https://raw.githubusercontent.com/habitat-sh/habitat/main/components/hab/install.sh | sudo bash -s -- -t "$BUILD_PKG_TARGET"
 
 # Since we are only verifying we don't have build failures, make everything
 # temp!
