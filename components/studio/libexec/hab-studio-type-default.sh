fc37b74eecea6f29a33cbc062e7d444331737714
Phani Sajja <psajja@progress.com>
2024-12-09 22:13:54 +0530

    binlink sleep cmd
    Signed-off-by: Phani Sajja <psajja@progress.com>

--- components/studio/libexec/hab-studio-type-default.sh
+++ components/studio/libexec/hab-studio-type-default.sh
@@ -101,6 +101,9 @@ finish_setup() {
   _hab pkg binlink core/bash bash
   _hab pkg binlink core/bash sh
 
+  # binlink sleep command
+  _hab pkg binlink "$coreutils_path/bin/sleep" sleep
+
   # Create a wrapper to `build` so that any calls to it have a super-stripped
   # `$PATH` and not whatever augmented version is currently in use. This should
   # mean that running `build` from inside a `studio enter` and running `studio
