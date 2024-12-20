07a59d6d8bf1e12fc0808cde2cc14df2c5c3e77c
Jason Heath <jh@jasonheath.com>
2024-12-20 14:03:15 -0500

    Reverts PR #9429: fix broken stdlib linking
    PR: https://github.com/habitat-sh/habitat/pull/9429
    COMMAND: git revert a9eb9663b --mainline 1 --strategy-option=ours
    
    Revert "Merge pull request #9429 from habitat-sh/broke"
    
    This reverts commit a9eb9663b5047fa1ed7d481013c0bd8c773a87d2, reversing
    changes made to d64dc5d7e97c4150cf6d4b93a706b57c64fdde82.

--- components/launcher/habitat/plan.sh
+++ components/launcher/habitat/plan.sh
@@ -4,7 +4,7 @@ pkg_origin=core
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_deps=(core/glibc
-          core/gcc-base)
+          core/gcc-libs)
 pkg_build_deps=(core/coreutils
                 core/rust/"$(tail -n 1 "$SRC_PATH/../../rust-toolchain"  | cut -d'"' -f 2)"
                 core/gcc
