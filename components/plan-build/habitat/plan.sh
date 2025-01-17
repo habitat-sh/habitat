bdc7a8e8e1769faee8873a3775eb7380055f615f
Phani Sajja <psajja@progress.com>
2025-01-17 11:37:13 +0530

    fix shellcheck
    Signed-off-by: Phani Sajja <psajja@progress.com>

--- components/plan-build/habitat/plan.sh
+++ components/plan-build/habitat/plan.sh
@@ -37,7 +37,7 @@ do_build() {
   # shellcheck disable=SC2154
   kernel="${pkg_target#*-}"
   kernel="${kernel%%-*}"  # Remove 'kernel2' part if exists
-  cp -v "$SRC_PATH"/bin/${program}-${kernel}.sh "$CACHE_PATH/$program"
+  cp -v "$SRC_PATH"/bin/${program}-"${kernel}".sh "$CACHE_PATH/$program"
 
   # Use the bash from our dependency list as the shebang. Also, embed the
   # release version of the program.
