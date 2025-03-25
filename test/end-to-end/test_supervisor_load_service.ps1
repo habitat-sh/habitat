729dde932f5f6e5b397e571c99219fcdea6686bb
Jason Heath <jh@jasonheath.com>
2025-03-25 16:24:26 -0400

    Tests umask /hab/svc managment
    - Replaces test/end-to-end/hab-svc-load.exp with test_supervisor_load_service.ps1 on Linux
    - Tests perm mgmt on /hab/svc directories
    
    Signed-off-by: Jason Heath <jh@jasonheath.com>

--- test/end-to-end/test_supervisor_load_service.ps1
+++ test/end-to-end/test_supervisor_load_service.ps1
@@ -3,23 +3,26 @@ hab pkg install core/nginx --channel stable
 $job = Start-Job { hab sup run }
 Wait-Supervisor -Timeout 120
 
-Describe "hab svc load" {
+Describe 'hab svc load' {
     $loadOut = hab svc load core/nginx
     Wait-SupervisorService nginx -Timeout 20
 
-    It "Successfully loads service" {
-        $loadOut | Should -Be "The core/nginx service was successfully loaded"
+    It 'Successfully loads service' {
+        ($loadOut | Out-String) | Should -Match 'The core/nginx service was successfully loaded'
     }
 
-    It "Reports service on HTTP Gateway as UP" {
-        ((Invoke-WebRequest "http://localhost:9631/services/nginx/default" -UseBasicParsing).content | ConvertFrom-Json).process.state | Should -Be "up"
+    It 'Reports service on HTTP Gateway as UP' {
+        ((Invoke-WebRequest 'http://localhost:9631/services/nginx/default' -UseBasicParsing).content | ConvertFrom-Json).process.state | Should -Be 'up'
     }
 
-    It "Actually runs nginx" {
+    It 'Actually runs nginx' {
         # This will error with a 403 because nginx is not running any sites
-        try { Invoke-WebRequest "http://localhost" }
-        catch [Microsoft.PowerShell.Commands.HttpResponseException] { $headers = $_.Exception.Response.Headers }
-        [string]$headers.Server | Should -BeLike "nginx/*"
+        try {
+            Invoke-WebRequest 'http://localhost'
+        } catch [Microsoft.PowerShell.Commands.HttpResponseException] {
+            $headers = $_.Exception.Response.Headers
+        }
+        [string]$headers.Server | Should -BeLike 'nginx/*'
     }
 
     AfterAll {
@@ -29,32 +32,54 @@ Describe "hab svc load" {
         Remove-Job -Job $job
     }
 }
+
 if ($IsLinux) {
+
+    # To test perms on /hab/svc and# /hab/svc/$SERVICE/{hooks,logs} we blow
+    # away that hierarchy at this point to start clean
+    (bash -c 'rm -rf /hab/svc')
+
     $job = Start-Job { bash -c 'umask 077; hab sup run' }
     Wait-Supervisor -Timeout 120
 
-    Describe "hab svc load on hardened system" {
+    Describe 'hab svc load on Linux with umask 077' {
+
         $loadOut = hab svc load core/nginx
-        Wait-SupervisorService nginx -Timeout 20
+        Wait-SupervisorService nginx -Timeout 2
 
-        It "Successfully loads service" {
-            $loadOut | Should -Be "The core/nginx service was successfully loaded"
+        It 'Successfully loads service' {
+            ($loadOut | Out-String) |
+                Should -Match 'The core/nginx service was successfully loaded'
         }
 
-        It "Reports service on HTTP Gateway as UP" {
-        ((Invoke-WebRequest "http://localhost:9631/services/nginx/default" -UseBasicParsing).content | ConvertFrom-Json).process.state | Should -Be "up"
+        It 'Reports service on HTTP Gateway as UP' {
+            ((Invoke-WebRequest 'http://localhost:9631/services/nginx/default' -UseBasicParsing).content |
+                ConvertFrom-Json).process.state |
+                    Should -Be 'up'
         }
 
-        It "Actually runs nginx" {
+        It 'Actually runs nginx' {
             # This will error with a 403 because nginx is not running any sites
-            try { Invoke-WebRequest "http://localhost" }
-            catch [Microsoft.PowerShell.Commands.HttpResponseException] { $headers = $_.Exception.Response.Headers }
-            [string]$headers.Server | Should -BeLike "nginx/*"
+            try {
+                Invoke-WebRequest 'http://localhost'
+            } catch [Microsoft.PowerShell.Commands.HttpResponseException] {
+                $headers = $_.Exception.Response.Headers
+            }
+            [string]$headers.Server | Should -BeLike 'nginx/*'
         }
 
-        It "is running with umask 0077" {
-            $s = grep Umask /proc/$(pgrep hab-sup)/status
-            $s | Should -Match "Umask:\s+0077"
+        It 'nginx is running with umask 0077' {
+            $out = (grep Umask /proc/$(pgrep hab-sup)/status)
+            $out | Should -Match 'Umask:\s+0077'
+        }
+
+        It 'has correct permissions on relevant svc directories' {
+            $out = (bash -c 'ls -l /hab/svc | tail -n 1 | cut -d' ' -f 1')
+            ($out | Out-String) | Should -Match 'drwxr-xr-x'
+            $out = (bash -c 'ls -l /hab/svc/nginx | grep hooks | tail -n 1 | cut -d' ' -f 1')
+            ($out | Out-String) | Should -Match 'drwxr-xr-x'
+            $out = (bash -c 'ls -l /hab/svc/nginx | grep logs | tail -n 1 | cut -d' ' -f 1')
+            ($out | Out-String) | Should -Match 'drwxr-xr-x'
         }
 
         AfterAll {
