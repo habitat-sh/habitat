05b6651d29c9a631cf35f7fa06e76478b89e0ec0
Matt Wrock <matt@mattwrock.com>
2025-08-25 14:03:51 -0700

    fix config tests
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- test/end-to-end/test_config_files.ps1
+++ test/end-to-end/test_config_files.ps1
@@ -1,26 +1,17 @@
-$Env:HAB_FEAT_SERVICE_CONFIG_FILES = "1"
 $configPath = "/hab/sup/default/config"
 $supConfig = "/hab/sup/default/config/sup.toml"
 $supBadConfig = "/hab/sup/default/config/bad_sup.toml"
-$pkgName = "core/nginx"
-$svcName = ($pkgName -split "/")[1]
-$svcConfig = "/hab/sup/default/config/svc.toml"
-$svcDirConfig = "/hab/sup/default/config/svc"
-$mySvcConfig = "$svcDirConfig/my_svc.toml"
 
 Describe "reading from supervisor and service config files" {
     Remove-Item -Force -Recurse -ErrorAction Ignore $supConfig
-    Remove-Item -Force -Recurse -ErrorAction Ignore $svcConfig
-    Remove-Item -Force -Recurse -ErrorAction Ignore $svcDirConfig
     New-Item -ItemType directory -Force -Path $configPath
-    New-Item -ItemType directory -Force -Path $svcDirConfig
 
     It "Supervisor starts with output of 'hab sup run --generate-config'" {
         # Run this twice. The first time the output is polluted with installing the launcher.
         $out = hab sup run --generate-config
         $out = hab sup run --generate-config
         Set-Content -Path $supConfig -Force -Value $out
-        Get-Content -Path $supConfig | Should -Contain "### The listen address for the Gossip Gateway"
+        Get-Content -Path $supConfig | Should -Contain "###  The listen address for the Gossip Gateway."
         $supLog = New-SupervisorLogFile("supervisor_starts_with_output_of_hab_sup_run_--generate-config")
         Start-Supervisor -LogFile $supLog -Timeout 45
     }
@@ -52,48 +43,6 @@ Describe "reading from supervisor and service config files" {
         } | Should -Throw
     }
 
-    Stop-Supervisor
-    $supLog = New-SupervisorLogFile("test_config_files")
-    Start-Supervisor -LogFile $supLog -Timeout 45
-
-    It "service does not start without ident" {
-        $out = hab svc load --generate-config
-        Set-Content -Path $svcConfig -Force -Value $out
-        Get-Content -Path $svcConfig | Should -Contain "# pkg_ident ="
-
-        {
-            Invoke-NativeCommand hab svc load
-        } | Should -Throw
-    }
-
-    It "service does start with ident" {
-        ((Get-Content -Path $svcConfig -Raw) -replace "# pkg_ident =", "pkg_ident = '$pkgName'") | Set-Content -Path $svcConfig
-
-        Invoke-NativeCommand hab svc load
-        Wait-SupervisorService $svcName
-    }
-
-    Unload-SupervisorService -PackageName $pkgName -Timeout 20
-    Remove-Item -Force -Recurse -ErrorAction Ignore $svcConfig
-
-    It "service starts with bulkload" {
-        Set-Content -Path $mySvcConfig -Force -Value "pkg_ident = '$pkgName'"
-        hab svc bulkload
-        Wait-SupervisorService $svcName
-    }
-
-    Unload-SupervisorService -PackageName $pkgName -Timeout 20
-    Stop-Supervisor
-
-    It "service starts on Supervisor startup" {
-        $supLog = New-SupervisorLogFile("service_starts_on_supervisor_startup")
-        Start-Supervisor -LogFile $supLog
-        Wait-SupervisorService $svcName
-    }
-
     Remove-Item -Force -Recurse -ErrorAction Ignore $supConfig
-    Remove-Item -Force -Recurse -ErrorAction Ignore $svcConfig
-    Remove-Item -Force -Recurse -ErrorAction Ignore $svcDirConfig
-    Unload-SupervisorService -PackageName $pkgName -Timeout 20
     Stop-Supervisor
 }
