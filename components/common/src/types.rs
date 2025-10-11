35c4b0e91eb9ebbb1a6684a1c1f58f14aeecdc81
Matt Wrock <matt@mattwrock.com>
2025-10-10 20:52:08 -0700

    get rid of unwrap
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- components/common/src/types.rs
+++ components/common/src/types.rs
@@ -433,7 +433,7 @@ impl ListenCtlAddr {
     pub fn config_or_default_as_str() -> &'static str {
         lazy_static::lazy_static! {
             pub static ref DEFAULT: String = {
-                let config = CliConfig::load().unwrap();
+                let config = CliConfig::load().unwrap_or_default();
                 config.listen_ctl.unwrap_or_default().to_string()
             };
         }
