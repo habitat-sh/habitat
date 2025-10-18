4fdfccb20aa8567fbb08ef0d4ceb0d635d0cba24
Matt Wrock <matt@mattwrock.com>
2025-10-17 17:19:01 -0700

    some debugging
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- test/end-to-end/test_container_exporter.ps1
+++ test/end-to-end/test_container_exporter.ps1
@@ -17,9 +17,9 @@ function New-Image() {
     )
 
     # NOTE: the container exporter is installed in setup_environment.{sh,ps1}
-    $env:RUST_LOG="debug"
-    Write-Host "running: hab pkg export container --base-pkgs-channel=$env:HAB_BLDR_CHANNEL core/nginx --tag-custom=$tag $extra_args"
-    Write-Host (hab pkg export container --base-pkgs-channel=$env:HAB_BLDR_CHANNEL core/nginx --tag-custom=$tag $extra_args | Out-String)
+    $command = "hab pkg export container --base-pkgs-channel=$env:HAB_BLDR_CHANNEL core/nginx --tag-custom=$tag $extra_args"
+    Write-Host "running: $command"
+    Write-Host (Invoke-Expression $command | Out-String)
     write-host "Created image"
     "core/nginx:$tag"
 }
