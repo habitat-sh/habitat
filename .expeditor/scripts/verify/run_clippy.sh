1ec0f09ca785ce574c41d87631de252c90b5d32c
Jason Heath <jason.heath@progress.com>
2025-12-12 09:10:37 -0500

    Updates .expeditor/scripts/verify/run_clippy.sh
    When upgrading to 1.91.1 there were issues running cargo clippy due to
    issues with our manipulation of the linker/dynamic interpreter.  The
    issue was that when clippy executed and compiled build scripts the
    executables were being compiled with the system interpreter instead of
    the interpreter we wanted from the base-2025 channel. The issue
    presented itself as symbols missing from glibc as base-2025 uses glibc
    2.41 while my system glibc happens to be 2.36.
    
    Here's what that turns into.
    
    We don't need to patchelf in this script anymore because we've pushed
    some of that down into our habitatized rust pkgs.  There's also a better
    way to address it for other places.  Nixed all that.
    
    Because we're many years down the road from when this script was created
    we also don't need to rustup in order to get clippy. Nixed all that.
    
    We can also play LD_LIBRARY_PATH games slightly better, maybe?  Starting
    out LD_LIBRARY_PATH is managed to some extent down in our down rust
    habitat packaging now.  However, even that doesn't resolve the core
    issue with the environment in which build scripts are compiled.  To that
    extent our preferred gcc-base library is added here.  In addition the
    RUSTFLAGS are configured in order to set the dynamic interpreter our
    preferred glibc more pervasively than the previous patchelf technique.
    
    Signed-off-by: Jason Heath <jason.heath@progress.com>

--- .expeditor/scripts/verify/run_clippy.sh
+++ .expeditor/scripts/verify/run_clippy.sh
@@ -4,53 +4,35 @@ set -euo pipefail
 
 source .expeditor/scripts/verify/shared.sh
 
-export RUSTFLAGS="-D warnings"
-
+export HAB_BLDR_CHANNEL="${HAB_BLDR_CHANNEL:="base-2025"}"
 toolchain=$(get_toolchain)
-install_rustup
-install_rust_toolchain "$toolchain"
+install_hab_pkg core/glibc core/gcc-base core/rust/"$toolchain" core/zeromq core/protobuf
 
-# Install clippy
-echo "--- :rust: Installing clippy"
-rustup component add --toolchain "$toolchain" clippy
+RUSTFLAGS="-D warnings"
+RUSTFLAGS+=" -C link-arg=-Wl,--dynamic-linker=$(hab pkg path core/glibc)/lib/ld-linux-x86-64.so.2"
+export RUSTFLAGS
 
-# TODO: these should be in a shared script?
-install_hab_pkg core/zeromq core/protobuf core/patchelf
-sudo -E hab pkg install core/rust/"$toolchain"
-
-# Yes, this is terrible but we need the clippy binary to run under our glibc.
-# This became an issue with the latest refresh and can likely be dropped in
-# the future when rust and supporting components are build against a later
-# glibc.
-sudo cp "$HOME"/.rustup/toolchains/"$toolchain"-x86_64-unknown-linux-gnu/bin/cargo-clippy "$(hab pkg path core/rust/"$toolchain")/bin"
-sudo cp "$HOME"/.rustup/toolchains/"$toolchain"-x86_64-unknown-linux-gnu/bin/clippy-driver "$(hab pkg path core/rust/"$toolchain")/bin"
-sudo hab pkg exec core/patchelf patchelf -- --set-interpreter "$(hab pkg path core/glibc)/lib/ld-linux-x86-64.so.2" "$(hab pkg path core/rust/"$toolchain")/bin/clippy-driver"
-sudo hab pkg exec core/patchelf patchelf -- --set-interpreter "$(hab pkg path core/glibc)/lib/ld-linux-x86-64.so.2" "$(hab pkg path core/rust/"$toolchain")/bin/cargo-clippy"
-
-export LIBZMQ_PREFIX
-LIBZMQ_PREFIX=$(hab pkg path core/zeromq)
-# now include zeromq so it exists in the runtime library path when cargo test is run
+LD_LIBRARY_PATH="$(cat "$(hab pkg path core/gcc-base)"/LD_RUN_PATH)"
+LD_LIBRARY_PATH+=":$(cat "$(hab pkg path core/zeromq)"/LD_RUN_PATH)"
 export LD_LIBRARY_PATH
-LD_LIBRARY_PATH="$(hab pkg path core/gcc-base)/lib64:$(hab pkg path core/zeromq)/lib"
-old_path=$PATH
-eval "$(hab pkg env core/rust/"$toolchain")"
-export PATH=$PATH:$old_path
+
+PATH="$(hab pkg path core/rust/"$toolchain")/bin:$PATH"
+export PATH
+
+LIBZMQ_PREFIX=$(hab pkg path core/zeromq)
+export LIBZMQ_PREFIX
 
 export PROTOC_NO_VENDOR=1
-export PROTOC
 PROTOC=$(hab pkg path core/protobuf)/bin/protoc
+export PROTOC
 
 # Lints we need to work through and decide as a team whether to allow or fix
 mapfile -t unexamined_lints < "$1"
-
 # Lints we disagree with and choose to keep in our code with no warning
 mapfile -t allowed_lints < "$2"
-
 # Known failing lints we want to receive warnings for, but not fail the build
 mapfile -t lints_to_fix < "$3"
-
-# Lints we don't expect to have in our code at all and want to avoid adding
-# even at the cost of failing the build
+# Lints we want to avoid adding even at the cost of failing the build
 mapfile -t denied_lints < "$4"
 
 clippy_args=()
@@ -58,8 +40,7 @@ clippy_args=()
 add_lints_to_clippy_args() {
   flag=$1
   shift
-  for lint
-  do
+  for lint; do
     clippy_args+=("$flag" "${lint}")
   done
 }
@@ -73,6 +54,6 @@ set -u
 
 echo "--- Running clippy!"
 cargo version
-cargo-clippy --version
-echo "Clippy rules: cargo clippy --all-targets --tests -- ${clippy_args[*]}"
-cargo-clippy clippy --all-targets --tests -- "${clippy_args[@]}"
+cargo clippy --version
+echo "EXECUTING: cargo clippy --workspace --all-targets --no-deps -- ${clippy_args[*]}"
+cargo clippy --workspace --all-targets --no-deps -- "${clippy_args[@]}"
