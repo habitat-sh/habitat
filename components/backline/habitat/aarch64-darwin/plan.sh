fb3e14febe8758c184c135277b63abd4bb8ed59f
Abhijit Gadgil <agadgil@progress.com>
2026-02-04 17:25:28 +0530

    `plan.sh` for `hab-backline` package
    Signed-off-by: Abhijit Gadgil <agadgil@progress.com>

---
+++ components/backline/habitat/aarch64-darwin/plan.sh
@@ -0,0 +1,35 @@
+# shellcheck disable=2034
+
+pkg_name="hab-backline"
+pkg_origin="chef"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('Apache-2.0')
+
+pkg_build_deps=()
+
+pkg_deps=(
+	chef/hab-plan-build
+	core/diffutils
+	core/less
+	core/make
+	core/mg
+	core/patch
+	core/vim
+)
+
+pkg_version() {
+	cat "$SRC_PATH/../../VERSION"
+}
+
+do_unpack() {
+	do_default_unpack
+	update_pkg_version
+}
+
+do_build() {
+	return 0
+}
+
+do_install() {
+	return 0
+}
