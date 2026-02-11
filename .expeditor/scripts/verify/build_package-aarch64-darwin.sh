c4ba929372e74b13e24e0467874e3e3820683200
Abhijit Gadgil <agadgil@progress.com>
2026-02-11 21:55:23 +0530

    Using `aarch64-darwin-test` channel for testing
    Signed-off-by: Abhijit Gadgil <agadgil@progress.com>

--- .expeditor/scripts/verify/build_package-aarch64-darwin.sh
+++ .expeditor/scripts/verify/build_package-aarch64-darwin.sh
@@ -6,6 +6,10 @@ source .expeditor/scripts/shared.sh
 
 package_path=${1?package_path argument required}
 
+# Not sure whether this is required but exporting it regardless just to be safe
+export HAB_AUTH_TOKEN
+HAB_AUTH_TOKEN=$(hab_auth_token)
+
 setup_hab_root_macos_pipeline
 
 # Since we are using the *bootstrap* packages right now, we will need to 'install' `hab`
@@ -30,7 +34,7 @@ sudo -E ${bootstrap_hab_binary} origin key generate
 # Install hab-studio from the chef origin via the acceptance channel.
 # By default, it installs from the stable channel only,
 # so this may need updating to support other channels.
-${bootstrap_hab_binary} pkg install chef/hab-studio -c aarch64-darwin
+${bootstrap_hab_binary} pkg install chef/hab-studio -c aarch64-darwin-test
 
 export HAB_STUDIO_SECRET_HAB_BLDR_CHANNEL="aarch64-darwin"
 
