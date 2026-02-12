cc913d3dea2a98648f2db43b5f54d611f27e731e
Abhijit Gadgil <agadgil@progress.com>
2026-02-12 13:01:08 +0530

    shellcheck fixes and logic to delete old volume
    Fixed shellcheck issues. Also added logic to delete old volume if any.
    
    Reverted runner to run on MacOS 12
    
    Signed-off-by: Abhijit Gadgil <agadgil@progress.com>

--- .expeditor/scripts/verify/build_package-aarch64-darwin.sh
+++ .expeditor/scripts/verify/build_package-aarch64-darwin.sh
@@ -1,4 +1,4 @@
-#!/opt/homebrew/bin/bash
+#!/bin/bash
 
 set -eou pipefail
 
@@ -7,8 +7,8 @@ source .expeditor/scripts/shared.sh
 package_path=${1?package_path argument required}
 
 # Not sure whether this is required but exporting it regardless just to be safe
-export HAB_AUTH_TOKEN
-HAB_AUTH_TOKEN=$(hab_auth_token)
+# export HAB_AUTH_TOKEN
+# HAB_AUTH_TOKEN=$(hab_auth_token)
 
 setup_hab_root_macos_pipeline
 
@@ -29,7 +29,7 @@ export HAB_ORIGIN
 HAB_ORIGIN=throwaway
 
 echo "--- :key: Generating fake origin key"
-sudo -E ${bootstrap_hab_binary} origin key generate
+sudo -E "${bootstrap_hab_binary}" origin key generate
 
 # Install hab-studio from the chef origin via the acceptance channel.
 # By default, it installs from the stable channel only,
@@ -39,7 +39,7 @@ ${bootstrap_hab_binary} pkg install chef/hab-studio -c aarch64-darwin-test
 export HAB_STUDIO_SECRET_HAB_BLDR_CHANNEL="aarch64-darwin"
 
 echo "--- :hab: Running hab pkg build for $package_path"
-sudo -E ${bootstrap_hab_binary} pkg build "$package_path" || macos_teardown_exit
+sudo -E "${bootstrap_hab_binary}" pkg build "$package_path" || macos_teardown_exit
 
 teardown_hab_root_macos_pipeline
 
