3533231e8a0d044a6ac8122343e19b99e4fd815d
Sean Simmons <ssimmons@progress.com>
2026-03-04 15:34:16 -0500

    testing different endpoint
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- .expeditor/scripts/finish_release/sync_acceptance.sh
+++ .expeditor/scripts/finish_release/sync_acceptance.sh
@@ -8,7 +8,7 @@ trap 'rm -rf $tempdir' INT TERM EXIT
 
 hab pkg download --file .expeditor/scripts/finish_release/sync_acceptance.toml --download-directory "${tempdir}"
 hab pkg bulkupload \
-        --url "https://bldr.acceptance.habitat.sh" \
+        --url "https://bldr-acceptance.habitat.sh" \
         --auth "${PIPELINE_ACCEPTANCE_AUTH_TOKEN}" \
         --channel 'stable' \
         "${tempdir}"
